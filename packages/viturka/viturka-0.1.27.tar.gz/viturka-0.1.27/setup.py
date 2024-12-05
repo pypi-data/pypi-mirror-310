89d77f90072cba3ae89b52d80a42b273b0e0c7d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:40 +0000

    Add package viturka/0.1.27/viturka-0.1.27.tar.gz

---
+++ packages/viturka/viturka-0.1.27.tar.gz/viturka-0.1.27/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='viturka',
+    version='0.1.27',
+    description='A client library for federated learning platform.',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.24.3',# Example of dependencies
+        'pandas',
+        'fastFM',
+        'scikit-learn',
+        'imbalanced-learn'
+    ],
+    python_requires='>=3.6'
+)
+
