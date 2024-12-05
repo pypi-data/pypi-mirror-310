482a9d6222097bc5ad8c62015ed318c300b13d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:05:38 +0000

    Add package antimatter-api/2.0.13/antimatter_api-2.0.13.tar.gz

---
+++ packages/antimatter-api/antimatter_api-2.0.13.tar.gz/antimatter_api-2.0.13/antimatter_api/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from antimatter_api.api.account_management_api import AccountManagementApi
+from antimatter_api.api.authentication_api import AuthenticationApi
+from antimatter_api.api.capsules_api import CapsulesApi
+from antimatter_api.api.contexts_api import ContextsApi
+from antimatter_api.api.encryption_api import EncryptionApi
+from antimatter_api.api.general_api import GeneralApi
+from antimatter_api.api.internal_api import InternalApi
+from antimatter_api.api.keychain_api import KeychainApi
+from antimatter_api.api.policy_api import PolicyApi
+
