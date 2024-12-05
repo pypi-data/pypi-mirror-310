c54026e686598041261878f4ecfec422ad260492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:06:02 +0000

    Add package pygame-simple-events/1.0.0/pygame_simple_events-1.0.0.tar.gz

---
+++ packages/pygame-simple-events/pygame_simple_events-1.0.0.tar.gz/pygame_simple_events-1.0.0/src/simple_events/__init__.py
@@ -0,0 +1,3 @@
+from .event_manager import getEventManager, notifyEventManagers  # noqa: F401, E501
+from .key_manager import getKeyListener, notifyKeyListeners  # noqa: F401, E501
+from .file_parser import JSONParser  # noqa: F401, E501
