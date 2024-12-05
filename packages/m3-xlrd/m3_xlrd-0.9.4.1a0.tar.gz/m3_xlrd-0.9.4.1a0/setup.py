9859f270f54fe6c0b980f089b7b0c12d7cf7b042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:52 +0000

    Add package m3-xlrd/0.9.4.1a0/m3_xlrd-0.9.4.1a0.tar.gz

---
+++ packages/m3-xlrd/m3_xlrd-0.9.4.1a0.tar.gz/m3_xlrd-0.9.4.1a0/setup.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='m3_xlrd',
+    version='0.9.4.1a',
+    url='https://stash.bars-open.ru/projects/M3/repos/m3-xlrd/browse',
+    license='Apache License, Version 2.0',
+    author='BARS Group',
+    author_email='a.morozov@bars.group',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    description='Патч официальной библиотеки xlrd для работы с excel-файлами',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Natural Language :: Russian',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
