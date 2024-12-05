0c1ab7579463305c0df84a8bc68065cff1abb69a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:45 +0000

    Add package DopeSQL/0.1.0/dopesql-0.1.0.tar.gz

---
+++ packages/DopeSQL/dopesql-0.1.0.tar.gz/dopesql-0.1.0/setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+
+setuptools.setup(
+    name="DopeSQL",
+    version="0.1.0",
+    author="shut up and just publish!",
+    author_email="fill@this.later",
+    description="None",
+    long_description=open('README.md', "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://fill.this/latter",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
+
+
