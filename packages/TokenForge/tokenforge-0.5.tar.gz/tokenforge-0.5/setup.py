b5c309bc780369ce1f11e7db3838d02c6620a767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:05 +0000

    Add package TokenForge/0.5/tokenforge-0.5.tar.gz

---
+++ packages/TokenForge/tokenforge-0.5.tar.gz/tokenforge-0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TokenForge',
+    version='0.5',
+    packages=find_packages(),
+    description='A package for JWT token management and authentication',
+    author='Mohammad Ridzwan Syah bin Irwan',
+    author_email='ridzwansyahirwan@gmail.com',
+    url='https://github.com/ridzwansyahirwan/TokenForge.git',
+    install_requires=[
+        'Flask',
+        'python-dotenv',
+        'PyJWT',
+    ],
+)
