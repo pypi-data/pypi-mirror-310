b643a87c8847f53279f88cb5a28a549166106332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:54 +0000

    Add package stofory-sdk/0.3.35/stofory_sdk-0.3.35.tar.gz

---
+++ packages/stofory-sdk/stofory_sdk-0.3.35.tar.gz/stofory_sdk-0.3.35/stofory_sdk/catalog/models/__init__.py
@@ -0,0 +1,64 @@
+from .product import Product
+from .platform import Platform
+from .category import Category
+from .category_name import CategoryName
+from .localization import Localization
+
+from .product_platform_image import ProductPlatformImage
+from .product_platform_parameter import ProductPlatformParameter
+from .product_platform_pricing import ProductPlatformPricing
+from .product_platform_video import ProductPlatformVideo
+from .product_platform import ProductPlatform
+
+from .parameter_name import ParameterName
+from .parameter_comment import ParameterComment
+from .parameter_option import ParameterOption
+
+from .parameter_option_name import ParameterOptionName
+
+from .product_platform_name import ProductPlatformName
+from .product_platform_description import ProductPlatformDescription
+from .product_platform_category import ProductPlatformCategory
+from .product_platform_bonus import ProductPlatformBonus
+from .product_platform_discount import ProductPlatformDiscount
+from .product_platform_guarantee import ProductPlatformGuarantee
+
+from .product_platform_sync_task import ProductPlatformSyncTask
+from .product_platform_external_connection import ProductPlatformExternalConnection
+from .product_platform_versioning import ProductPlatformVersioning
+
+from .sync_queue_mapping import SyncQueueMapping
+
+
+__all__ = (
+    "Product",
+    "Platform",
+    "Category",
+    "CategoryName",
+    "Localization",
+
+    "ProductPlatformImage",
+    "ProductPlatformParameter",
+    "ProductPlatformPricing",
+    "ProductPlatformVideo",
+    "ProductPlatform",
+
+    "ParameterName",
+    "ParameterComment",
+    "ParameterOption",
+
+    "ParameterOptionName",
+
+    "ProductPlatformName",
+    "ProductPlatformDescription",
+    "ProductPlatformCategory",
+    "ProductPlatformBonus",
+    "ProductPlatformDiscount",
+    "ProductPlatformGuarantee",
+
+    "ProductPlatformSyncTask",
+    "ProductPlatformExternalConnection",
+    "ProductPlatformVersioning",
+
+    "SyncQueueMapping",
+)
---
+++ packages/stofory-sdk/stofory_sdk-0.3.35.tar.gz/stofory_sdk-0.3.35/stofory_sdk/catalog/models/enums/__init__.py
@@ -0,0 +1,21 @@
+from .currency import Currency
+from .locale import Locale
+from .modifier_operator import ModifierOperator
+from .parameter_type import ParameterType
+from .pricing_type import PricingType
+from .product_types import ProductTypes
+from .sync_task_type import SyncTaskType
+from .sync_task_status import SyncTaskStatus
+from .sync_task_priority import SyncTaskPriority
+
+__all__ = (
+    "Currency",
+    "Locale",
+    "ModifierOperator",
+    "ParameterType",
+    "PricingType",
+    "ProductTypes",
+    "SyncTaskType",
+    "SyncTaskStatus",
+    "SyncTaskPriority",
+)
