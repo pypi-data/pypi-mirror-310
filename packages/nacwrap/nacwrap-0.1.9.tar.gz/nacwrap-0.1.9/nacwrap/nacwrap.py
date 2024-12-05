8782340df7d4ada859fe5901a41b263002e5c382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:21 +0000

    Add package nacwrap/0.1.9/nacwrap-0.1.9.tar.gz

---
+++ packages/nacwrap/nacwrap-0.1.9.tar.gz/nacwrap-0.1.9/nacwrap/nacwrap.py
@@ -0,0 +1,8 @@
+"""
+This module contains functions to interact with the Nintex Workflow Cloud API.
+"""
+
+from .instances import *
+from .tasks import *
+from .users import *
+from .data_model import *
