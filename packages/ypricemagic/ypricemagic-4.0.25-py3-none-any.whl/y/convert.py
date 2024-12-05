2cb59b58bd155bb93db23934580f4e6c88a12a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:24 +0000

    Add package ypricemagic/4.0.25/ypricemagic-4.0.25-py3-none-any.whl

---
+++ packages/ypricemagic/ypricemagic-4.0.25-py3-none-any.whl/y/convert.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+import brownie.convert
+from brownie.convert.datatypes import HexBytes
+
+from y import ENVIRONMENT_VARIABLES as ENVS
+from y.datatypes import AnyAddressType
+
+
+@lru_cache(maxsize=ENVS.CHECKSUM_CACHE_MAXSIZE)
+def to_address(address: AnyAddressType) -> str:
+    if type(address) == int:
+        return _int_to_address(address)
+    return brownie.convert.to_address(address)
+
+
+def _int_to_address(int_address: int) -> str:
+    hex_value = HexBytes(int_address).hex()[2:]
+    padding = "0" * (40 - len(hex_value))
+    return brownie.convert.to_address(f"0x{padding}{hex_value}")
