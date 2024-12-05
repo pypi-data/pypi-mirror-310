c3c5987c39fadf1c284f32ec3312c22612f99e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:13 +0000

    Add package chancy/0.13.2/chancy-0.13.2-py3-none-any.whl

---
+++ packages/chancy/chancy-0.13.2-py3-none-any.whl/chancy/executors/asyncex.py
@@ -0,0 +1,76 @@
+import asyncio
+
+from chancy.executors.base import Executor
+from chancy.job import QueuedJob, Limit
+
+
+class AsyncExecutor(Executor):
+    """
+    An Executor which uses asyncio to run its jobs in the main event loop.
+
+    This executor is useful for running large numbers of IO-bound jobs, as it
+    can run many jobs concurrently without blocking the main event loop and
+    without the high overhead of new processes or threads. However, it is not
+    suitable for CPU-bound jobs, as it will block the main event loop and
+    prevent other jobs & queues from running.
+
+    To use this executor, simply pass the import path to this class in the
+    ``executor`` field of your queue configuration:
+
+    .. code-block:: python
+
+        async with Chancy(dsn="postgresql://localhost/postgres") as chancy:
+            await chancy.declare(
+                Queue(
+                    name="default",
+                    executor="chancy.executors.asyncex.AsyncExecutor",
+                )
+            )
+    """
+
+    def __init__(self, worker, queue):
+        super().__init__(worker, queue)
+        self.running_jobs: set[asyncio.Task] = set()
+
+    async def push(self, job: QueuedJob):
+        task = asyncio.create_task(self._job_wrapper(job))
+        task.add_done_callback(self._job_cleanup)
+        self.running_jobs.add(task)
+
+    def __len__(self):
+        return len(self.running_jobs)
+
+    def _job_cleanup(self, task: asyncio.Task):
+        self.running_jobs.discard(task)
+        task.exception()
+
+    async def _job_wrapper(self, job: QueuedJob):
+        try:
+            func, kwargs = Executor.get_function_and_kwargs(job)
+            if not asyncio.iscoroutinefunction(func):
+                raise ValueError(
+                    f"Function {job.func!r} is not an async function, which is"
+                    f" required for the AsyncExecutor. Please use the"
+                    f" ThreadedExecutor or ProcessExecutor instead."
+                )
+
+            timeout = next(
+                (
+                    limit.value
+                    for limit in job.limits
+                    if limit.type_ == Limit.Type.TIME
+                ),
+                None,
+            )
+
+            try:
+                # This annoyingly creates quite the excessive traceback,
+                # we should revisit this and clean it up.
+                await asyncio.wait_for(func(**kwargs), timeout=timeout)
+            except (asyncio.TimeoutError, TimeoutError):
+                raise asyncio.TimeoutError(
+                    f"Job {job.id} timed out after {timeout} seconds"
+                )
+            await self.job_completed(job)
+        except Exception as exc:
+            await self.job_completed(job, exc)
---
+++ packages/chancy/chancy-0.13.2-py3-none-any.whl/chancy/executors/base.py
@@ -0,0 +1,146 @@
+import abc
+import inspect
+import typing
+import traceback
+import dataclasses
+from datetime import datetime, timezone
+from typing import get_type_hints, Callable
+
+from chancy.job import QueuedJob
+from chancy.queue import Queue
+
+if typing.TYPE_CHECKING:
+    from chancy.worker import Worker
+
+
+class Executor(abc.ABC):
+    """
+    The base class for all executors.
+
+    Executors are responsible for managing the execution of jobs after they've
+    been retrieved from a queue.
+
+    See :class:`~chancy.executors.process.ProcessExecutor` and
+    :class:`~chancy.executors.asyncex.AsyncExecutor` for examples of built-in
+    executors.
+    """
+
+    def __init__(self, worker: "Worker", queue: "Queue"):
+        self.worker = worker
+        self.queue = queue
+
+    @abc.abstractmethod
+    async def push(self, job: QueuedJob):
+        """
+        Push a job onto the job pool.
+        """
+
+    async def job_completed(self, job: QueuedJob, exc: Exception | None = None):
+        """
+        Called when a job has completed.
+
+        This method should be called by the executor when a job has completed
+        execution. It will update the job's state in the queue and handle
+        retries if necessary.
+
+        :param job: The job that has completed.
+        :param exc: The exception that was raised during execution, if any.
+        """
+        if exc is None:
+            now = datetime.now(tz=timezone.utc)
+            new_instance = dataclasses.replace(
+                job,
+                state=QueuedJob.State.SUCCEEDED,
+                completed_at=now,
+                attempts=job.attempts + 1,
+            )
+        else:
+            is_failure = job.attempts + 1 >= job.max_attempts
+
+            new_state = (
+                QueuedJob.State.FAILED
+                if is_failure
+                else QueuedJob.State.RETRYING
+            )
+
+            new_instance = dataclasses.replace(
+                job,
+                state=new_state,
+                attempts=job.attempts + 1,
+                completed_at=(
+                    datetime.now(tz=timezone.utc) if is_failure else None
+                ),
+                errors=[
+                    *job.errors,
+                    {
+                        "traceback": "".join(
+                            traceback.format_exception(
+                                type(exc), exc, exc.__traceback__
+                            )
+                        ),
+                        "attempt": job.attempts,
+                    },
+                ],
+            )
+
+        # Each plugin has a chance to modify the job instance after it's
+        # completed.
+        for plugin in self.worker.chancy.plugins:
+            try:
+                new_instance = await plugin.on_job_completed(
+                    new_instance, worker=self.worker, exc=exc
+                )
+            except NotImplementedError:
+                continue
+
+        await self.worker.queue_update(new_instance)
+
+    @staticmethod
+    def get_function_and_kwargs(job: QueuedJob) -> tuple[Callable, dict]:
+        """
+        Finds the function which should be executed for the given job and
+        returns its keyword arguments.
+
+        :param job: The job instance to get the function and arguments for.
+        :return: A tuple containing the function and its keyword arguments.
+        """
+        mod_name, func_name = job.func.rsplit(".", 1)
+        mod = __import__(mod_name, fromlist=[func_name])
+        try:
+            func = getattr(mod, func_name)
+        except AttributeError:
+            raise AttributeError(
+                f"Could not find function {func_name} in module {mod_name}."
+            )
+
+        # We take a look at the type signature for the function to see if the
+        # user has specified that the job instance should be passed as a
+        # keyword argument.
+        type_hints = get_type_hints(func)
+        sig = inspect.signature(func)
+        kwargs = job.kwargs or {}
+        for param_name, param in sig.parameters.items():
+            if (
+                param.kind == param.KEYWORD_ONLY
+                and type_hints.get(param_name) is QueuedJob
+            ):
+                kwargs[param_name] = job
+
+        return func, kwargs
+
+    async def stop(self):
+        """
+        Stop the executor.
+        """
+
+    @abc.abstractmethod
+    def __len__(self):
+        """
+        Get the number of pending and running jobs in the pool.
+        """
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.stop()
