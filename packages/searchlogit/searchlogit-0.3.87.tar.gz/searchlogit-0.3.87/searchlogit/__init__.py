44a473ef4156823e629fa1d61766a72a9bed15e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:22 +0000

    Add package searchlogit/0.3.87/searchlogit-0.3.87.tar.gz

---
+++ packages/searchlogit/searchlogit-0.3.87.tar.gz/searchlogit-0.3.87/searchlogit/__init__.py
@@ -0,0 +1,11 @@
+#import _device as dev
+from .latent_class_mixed_model import LatentClassMixedModel
+from .latent_class_model import LatentClassModel
+from .mixed_logit import MixedLogit
+from .multinomial_logit import MultinomialLogit
+from .search import Search
+from .siman import*
+from .threshold import*
+from .misc import*
+from ._device import*
+from ._device import Device as dev
