2f712660189a3202e39ae059dbdcd233fe68eaeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:05:58 +0000

    Add package Metamorphic/0.0.15/MetaMorphic-0.0.15-cp312-cp312-win_amd64.whl

---
+++ packages/Metamorphic/MetaMorphic-0.0.15-cp312-cp312-win_amd64.whl/Metamorphic/__init__.py
@@ -0,0 +1,13 @@
+
+__version__='0.0.15'
+
+import pkg_resources
+import pandas as pd
+
+# pkg_resources로 설치된 패키지 안의 절대 경로 가져오기
+csv_path = pkg_resources.resource_filename('Metamorphic', 'data/표준정규분포표.csv')
+
+# CSV 파일 읽기
+df = pd.read_csv(csv_path)
+print(df)
+
