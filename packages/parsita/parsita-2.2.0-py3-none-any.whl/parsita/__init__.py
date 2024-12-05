4c6303e539f2923aa0a2a4e316a8e3e0f0434f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:05:53 +0000

    Add package parsita/2.2.0/parsita-2.2.0-py3-none-any.whl

---
+++ packages/parsita/parsita-2.2.0-py3-none-any.whl/parsita/__init__.py
@@ -0,0 +1,30 @@
+from .metaclasses import ParserContext, fwd
+from .parsers import (
+    Parser,
+    any1,
+    debug,
+    eof,
+    failure,
+    first,
+    lit,
+    longest,
+    opt,
+    pred,
+    reg,
+    rep,
+    rep1,
+    rep1sep,
+    repsep,
+    success,
+    until,
+)
+from .state import (
+    Failure,
+    ParseError,
+    Reader,
+    RecursionError,
+    Result,
+    SequenceReader,
+    StringReader,
+    Success,
+)
