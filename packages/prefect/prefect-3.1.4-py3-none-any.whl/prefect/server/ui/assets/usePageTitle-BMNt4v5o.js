ba8e9b8f4a62f5263fcf749608e6c1c04eda1729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:05:56 +0000

    Add package prefect/3.1.4/prefect-3.1.4-py3-none-any.whl

---
+++ packages/prefect/prefect-3.1.4-py3-none-any.whl/prefect/server/ui/assets/usePageTitle-BMNt4v5o.js
@@ -0,0 +1,2 @@
+import{e as a,bA as c,k as n}from"./index-kRVanVAE.js";function i(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>n(e)).filter(e=>e!==null).join(" • "));c(()=>document.title=o.value)}export{i as u};
+//# sourceMappingURL=usePageTitle-BMNt4v5o.js.map
