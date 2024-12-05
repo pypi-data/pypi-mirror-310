b2423e3a1494c35de3b58294148fcfaead2693ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:21 +0000

    Add package nacwrap/0.1.9/nacwrap-0.1.9-py3-none-any.whl

---
+++ packages/nacwrap/nacwrap-0.1.9-py3-none-any.whl/nacwrap/workflows.py
@@ -0,0 +1,25 @@
+"""
+This module contains functions to interact with workflows
+"""
+
+# TODO List workflows
+
+# TODO List Workflow permissions
+
+# TODO Update workflow permissions
+
+# TODO Get a workflow design
+
+# TODO Delete a workflow
+
+# TODO Deactivate a workflow
+
+# TODO Activate a workflow
+
+# TODO Add tags to a workflow
+
+# TODO Export a draft workflow
+
+# TODO Export a published workflow
+
+# TODO Import a workflow
