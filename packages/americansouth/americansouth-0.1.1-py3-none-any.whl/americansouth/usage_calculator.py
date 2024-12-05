81539f2e8202a1c2a98c76a55c24493e5b7f7878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:39 +0000

    Add package americansouth/0.1.1/americansouth-0.1.1-py3-none-any.whl

---
+++ packages/americansouth/americansouth-0.1.1-py3-none-any.whl/americansouth/usage_calculator.py
@@ -0,0 +1,21 @@
+import datetime
+
+
+class UsageCalculator:
+    @staticmethod
+    def calc_daily_limit(
+        amount: float,
+        total: float,
+        hours_remaining: float,
+    ) -> float:
+        remaining_gb: float = total - amount
+        days_remaining: float = hours_remaining / 24
+        if days_remaining == 0:
+            return 0.0
+        return remaining_gb / days_remaining
+
+    @staticmethod
+    def calc_hours_remaining(
+        current_time: datetime.datetime, next_billing: datetime.datetime
+    ) -> float:
+        return (next_billing - current_time).total_seconds() / 3600
