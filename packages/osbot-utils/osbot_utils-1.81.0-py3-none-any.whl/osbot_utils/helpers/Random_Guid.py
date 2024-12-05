dc44abb172a2b7de0fb16aa192d423cbd9f0ad26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:39 +0000

    Add package osbot-utils/1.81.0/osbot_utils-1.81.0-py3-none-any.whl

---
+++ packages/osbot-utils/osbot_utils-1.81.0-py3-none-any.whl/osbot_utils/helpers/Random_Guid.py
@@ -0,0 +1,12 @@
+from osbot_utils.utils.Misc import random_guid, is_guid
+
+class Random_Guid(str):
+    def __new__(cls, value=None):
+        if value is None:
+            value = random_guid()
+        if is_guid(value):
+            return str.__new__(cls, value)
+        raise ValueError(f'in Random_Guid: value provided was not a Guid: {value}')
+
+    def __str__(self):
+        return self
