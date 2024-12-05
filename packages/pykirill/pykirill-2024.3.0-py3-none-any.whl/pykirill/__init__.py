749843d43774f1aef27d3d241f0d9206c9c37798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:55 +0000

    Add package pykirill/2024.3.0/pykirill-2024.3.0-py3-none-any.whl

---
+++ packages/pykirill/pykirill-2024.3.0-py3-none-any.whl/pykirill/__init__.py
@@ -0,0 +1,8 @@
+"""
+This is my personal Python package, `pykirill`,
+which includes a collection of utilities and functions that I frequently use during scientific exploration.
+This package is especially designed to be portable,
+making it suitable for environments like Google Colab where setup needs to be minimal
+"""
+
+from .version import VERSION as __version__  # noqa: F401
