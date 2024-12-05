cae459314486589f666a9eff903bc485008a8a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:55 +0000

    Add package botolib/0.0.11/botolib-0.0.11-py3-none-any.whl

---
+++ packages/botolib/botolib-0.0.11-py3-none-any.whl/botolib/services/sso.py
@@ -0,0 +1,28 @@
+from . import AWSService
+from ..utils.common import remove_none_values
+
+class SSO(AWSService):
+    __servicename__ = 'sso'
+
+    def list_accounts(self, access_token, next_token = None):
+        request_params = remove_none_values({
+            'accessToken':access_token,
+            'nextToken':next_token
+        })
+
+        return self.client.list_accounts(**request_params)
+    
+    def list_accounts_with_paginator(self, access_token):
+        return self.get_result_from_paginator('list_accounts','accountList', accessToken=access_token)
+
+    def list_account_roles(self, access_token, account_id, next_token = None):
+        request_params = remove_none_values({
+            'accessToken':access_token,
+            'accountId':account_id,
+            'nextToken':next_token
+        })
+
+        return self.client.list_account_roles(**request_params)
+    
+    def list_account_roles_with_paginator(self, access_token, account_id):
+        return self.get_result_from_paginator('list_account_roles', 'roleList', accessToken=access_token, accountId=account_id)
