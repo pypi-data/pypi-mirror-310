34c4f65f409919f4fdad52416b087d577c16c21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:03:24 +0000

    Add package pistacker/1.0.1/pistacker-1.0.1.tar.gz

---
+++ packages/pistacker/pistacker-1.0.1.tar.gz/pistacker-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Function to read the requirements.txt file
+def read_requirements():
+    with open('requirements.txt') as req_file:
+        return req_file.read().splitlines()
+
+setup(
+    name="pistacker",
+    author="Eric D. Sakkas",
+    author_email="esakkas@wesleyan.edu",
+    description="Analyzes Pi-Stacking in Molecular Dynamics Trajectories",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/esakkas24/stacker",
+    version="1.0.1",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+        ],
+    python_requires=">=3.5",
+    entry_points={
+        'console_scripts': [
+            'stacker=stacker.__init__:run_python_command', 
+        ],
+    },
+    install_requires=read_requirements(), 
+)
