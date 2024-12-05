e82df0d391042357a4d644b1c71f93cc30066688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:54 +0000

    Add package rgwml/0.0.341/rgwml-0.0.341-py3-none-any.whl

---
+++ packages/rgwml/rgwml-0.0.341-py3-none-any.whl/rgwml/__init__.py
@@ -0,0 +1,9 @@
+from .p import *
+from .d import *
+from .f import *
+from .m import *
+from .resources.backend import *
+from .resources.frontend import *
+from .resources.frontend_assets import *
+
+
