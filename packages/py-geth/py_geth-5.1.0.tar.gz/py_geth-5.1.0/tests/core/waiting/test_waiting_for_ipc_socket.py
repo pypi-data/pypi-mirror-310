d668652eea664dee08377ffc122f131e3e422ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:12 +0000

    Add package py-geth/5.1.0/py_geth-5.1.0.tar.gz

---
+++ packages/py-geth/py_geth-5.1.0.tar.gz/py_geth-5.1.0/tests/core/waiting/test_waiting_for_ipc_socket.py
@@ -0,0 +1,25 @@
+from flaky import (
+    flaky,
+)
+import pytest
+
+from geth import (
+    DevGethProcess,
+)
+from geth.utils.timeout import (
+    Timeout,
+)
+
+
+def test_waiting_for_ipc_socket(base_dir):
+    with DevGethProcess("testing", base_dir=base_dir) as geth:
+        assert geth.is_running
+        geth.wait_for_ipc(timeout=20)
+
+
+@flaky(max_runs=3)
+def test_timeout_waiting_for_ipc_socket(base_dir):
+    with DevGethProcess("testing", base_dir=base_dir) as geth:
+        assert geth.is_running
+        with pytest.raises(Timeout):
+            geth.wait_for_ipc(timeout=0.01)
