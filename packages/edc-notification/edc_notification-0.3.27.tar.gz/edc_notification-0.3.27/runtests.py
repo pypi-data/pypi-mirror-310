d89f1dea5c30aa9f7ebf66e32d2a32077a8b0cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:43 +0000

    Add package edc-notification/0.3.27/edc_notification-0.3.27.tar.gz

---
+++ packages/edc-notification/edc_notification-0.3.27.tar.gz/edc_notification-0.3.27/runtests.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import logging
+from pathlib import Path
+
+from edc_test_utils import DefaultTestSettings, func_main
+
+app_name = "edc_notification"
+base_dir = Path(__file__).absolute().parent
+
+project_settings = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
+    EDC_AUTH_CODENAMES_WARN_ONLY=True,
+    SILENCED_SYSTEM_CHECKS=[
+        "sites.E101",
+        "edc_navbar.E002",
+        "edc_navbar.E003",
+        "edc_consent.E001",
+    ],
+    EMAIL_ENABLED=True,
+    EMAIL_CONTACTS={
+        "ae_reports": "aereports@example.com",
+        "data_manager": "data_manager@example.com",
+        "tmg": "tmg@example.com",
+    },
+    EDC_SITES_REGISTER_DEFAULT=True,
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "django_crypto_fields.apps.AppConfig",
+        "django_revision.apps.AppConfig",
+        "edc_action_item.apps.AppConfig",
+        "edc_appointment.apps.AppConfig",
+        "edc_auth.apps.AppConfig",
+        "edc_data_manager.apps.AppConfig",
+        "edc_device.apps.AppConfig",
+        "edc_facility.apps.AppConfig",
+        "edc_form_runners.apps.AppConfig",
+        "edc_identifier.apps.AppConfig",
+        "edc_lab.apps.AppConfig",
+        "edc_label.apps.AppConfig",
+        "edc_locator.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        "edc_notification.apps.AppConfig",
+        "edc_offstudy.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        "edc_timepoint.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
+        "edc_appconfig.apps.AppConfig",
+    ],
+    RANDOMIZATION_LIST_PATH=str(base_dir / app_name / "tests" / "test_randomization_list.csv"),
+    add_dashboard_middleware=True,
+).settings
+
+
+def main():
+    func_main(project_settings, f"{app_name}.tests")
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
