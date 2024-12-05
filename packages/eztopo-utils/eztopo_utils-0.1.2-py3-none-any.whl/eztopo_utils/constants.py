c5f61948783444a0f29491b49b74385f458e9316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:48 +0000

    Add package eztopo-utils/0.1.2/eztopo_utils-0.1.2-py3-none-any.whl

---
+++ packages/eztopo-utils/eztopo_utils-0.1.2-py3-none-any.whl/eztopo_utils/constants.py
@@ -0,0 +1,12 @@
+# Chopper
+CHOPPER_PORT = 50051
+FRAME_TIME_INTERVAL = 2
+'''Number of frames to wait before capturing the next frame. Increasing this value will increase performance as less frames will be saved and thus processed later down the line.'''
+# Redis
+REDIS_PORT = 6379
+REDIS_HOST = "10.108.148.45"
+# Minio
+MINIO_HOST = "minio-proj.minio-ns.svc.cluster.local:9000"
+MINIO_USER = "rootuser"
+MINIO_PASSWORD = "rootpass123"
+
