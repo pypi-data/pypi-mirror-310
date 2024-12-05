eefe631cf32299ba2ecd95c0fa593b988cdc660e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:03 +0000

    Add package kaithem/0.86.2/kaithem-0.86.2-py3-none-any.whl

---
+++ packages/kaithem/kaithem-0.86.2-py3-none-any.whl/kaithem/src/chandler/html/cue-countdown.vue
@@ -0,0 +1,31 @@
+<style scoped></style>
+
+
+<template id="cue-cd">
+    <div style="position: relative; width: fit-content;">
+        <span v-if="group.active && cue && (cue.length || cue.relLength)"
+        :data-count-ref="group.enteredCue"
+        :data-count-bpm="group.bpm"
+        :data-count-len="group.cuelen">
+        </span>
+    </div>
+</template>
+
+
+
+<script>
+import { formatInterval} from "./utils.mjs?cache_version=452dc529-8f57-41e0-8fb3-c485ce1dfd61";
+
+// See boardapi update_countdowns function to actually make it work
+
+
+
+module.exports = {
+    template: '#cue-cd',
+    props: ['cue', 'group'],
+    data: function () {
+        return ({ 'formatInterval': formatInterval })
+    }
+}
+
+</script>
