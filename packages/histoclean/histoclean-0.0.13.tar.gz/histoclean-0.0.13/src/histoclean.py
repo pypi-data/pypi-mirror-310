cb8cbef0d87060105931eb621fa2302971f79cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:32 +0000

    Add package histoclean/0.0.13/histoclean-0.0.13.tar.gz

---
+++ packages/histoclean/histoclean-0.0.13.tar.gz/histoclean-0.0.13/src/histoclean.py
@@ -0,0 +1,45 @@
+import os
+from tkinter import filedialog
+import tkinter as tk
+from back import image_patching, balance_module, whitespace_filter, image_normalisation, augmentation, patch_finder as pf
+import initialisation
+
+
+def SaveData():
+    global Active_Module
+    DefaultFile = [('HistoClean File', '*.hc')]
+    SavePath = filedialog.asksaveasfilename(initialdir=os.getcwd(), filetypes=DefaultFile, defaultextension=DefaultFile)
+    print(f"Saving data to {SavePath}")
+    Data = []
+
+    if Active_Module == "Patch":
+        Data.append("Patch")
+        # Data.append(SettingValues)
+
+
+def run():
+    tk.mainloop()
+
+
+def patch_image(imageFolder, saveLocation, outputTileSize, magnification, outputExtension):
+    image_patching.ImagePatching.patch_image(imageFolder, saveLocation, outputTileSize, magnification, outputExtension)
+
+
+def white_filter(imgFolder, minTissueCoverage, method, blur, keepSubThresholdImagesSeparate=1, createBinaryMasks=1):
+    #  Needs the checkboxes at bottom added as parameters
+    # This is done in gui using KeepCheckVar, which updates on checkbox update (needs checked)
+    whitespace_filter.WhitespaceFilter.WS_Thread_Threading(imgFolder, minTissueCoverage, method, blur,
+                                                           keepSubThresholdImagesSeparate, createBinaryMasks)
+
+def balance(imgFolders, balance, folderVal):
+    balance_module.Balance.Adjust_ProgressBar(imgFolders, balance, folderVal)
+
+def image_normalisation(targetImage, targetFolder, method, newSaveFolder=None):
+    image_normalisation.ImageNormalisation.Norm_Thread_Threading(targetImage, targetFolder, method, newSaveFolder)
+
+def augment_images(imgFolder, augments, applySize, applyMethod):
+    augmentation.Augmentation.augment(imgFolder, augments, applySize, applyMethod)
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/histoclean/histoclean-0.0.13.tar.gz/histoclean-0.0.13/tests/test_master.py
@@ -0,0 +1,10 @@
+import unittest
+
+from augmentation_test import *
+from balance_test import *
+from image_normalisation_test import *
+from image_patching_test import *
+from whitespace_filter_test import *
+
+if __name__ == '__main__':
+    unittest.main()
