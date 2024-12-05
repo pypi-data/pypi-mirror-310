d5fbe4fea1dd4a77a62fc3e8e39b3cbf3f97a810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:04:24 +0000

    Add package vital-ai-chat/0.1.17/vital_ai_chat-0.1.17.tar.gz

---
+++ packages/vital-ai-chat/vital_ai_chat-0.1.17.tar.gz/vital_ai_chat-0.1.17/ai_chat_domain/model/HaleyChatInterAgentCommand.pyi
@@ -0,0 +1,9 @@
+
+import datetime
+from com_vitalai_haley_domain.model.HaleyRequestMessage import HaleyRequestMessage
+
+
+class HaleyChatInterAgentCommand(HaleyRequestMessage):
+        callingInteractionURI: str
+        respondingInteractionURI: str
+
