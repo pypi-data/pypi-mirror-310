8c2e96c5cae0566712274488e1a619473e2a8078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:16 +0000

    Add package foxapi/1.1.2/foxapi-1.1.2.tar.gz

---
+++ packages/foxapi/foxapi-1.1.2.tar.gz/foxapi-1.1.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name='foxapi',
+    version="1.1.2",
+    url='https://github.com/ThePhoenix78/FoxAPI',
+    download_url='https://github.com/ThePhoenix78/FoxAPI/tarball/master',
+    license='MIT',
+    author='ThePhoenix78',
+    author_email='thephoenix788@gmail.com',
+    description='A wrapper for the foxhole API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=[
+        'foxhole',
+        'foxapi',
+        'foxhole-game'
+    ],
+    install_requires=[
+        'requests',
+        'pillow',
+    ],
+    setup_requires=[
+        'wheel'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    package_data={
+        'foxapi.Images.MapHex': ["*.png"],
+        'foxapi.Images.MapIcons': ["*.png"]
+    },
+    include_package_data=True,
+    # packages=["sdist", "bdist_wheel"]
+    # python_requires='>=3.6',
+)
