1ffd50a590370ed3e8e9ef739db9c8044c036c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:06:00 +0000

    Add package googletrans-di/4.0.1/googletrans_di-4.0.1.tar.gz

---
+++ packages/googletrans-di/googletrans_di-4.0.1.tar.gz/googletrans_di-4.0.1/googletrans_di/__init__.py
@@ -0,0 +1,7 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = 'Translator',
+__version__ = '4.0.1'
+
+
+from googletrans_di.client import Translator
+from googletrans_di.constants import LANGCODES, LANGUAGES  # noqa
