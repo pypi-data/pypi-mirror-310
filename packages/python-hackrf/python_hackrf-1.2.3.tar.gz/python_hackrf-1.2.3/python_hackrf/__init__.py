83f975eb3352d871a3e657f82df014ae364fed88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:45 +0000

    Add package python-hackrf/1.2.3/python_hackrf-1.2.3.tar.gz

---
+++ packages/python-hackrf/python_hackrf-1.2.3.tar.gz/python_hackrf-1.2.3/python_hackrf/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.2.3'
+
+from python_hackrf.pylibhackrf import pyhackrf  # noqa F401
+from python_hackrf.pyhackrf_tools import (  # noqa F401
+    pyhackrf_operacake,
+    pyhackrf_transfer,
+    pyhackrf_sweep,
+    pyhackrf_info,
+)
