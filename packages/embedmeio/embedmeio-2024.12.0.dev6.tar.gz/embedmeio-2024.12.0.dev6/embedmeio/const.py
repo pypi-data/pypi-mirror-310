27efb4b7e65a8af7affc08dd79cdcc8e36111401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:05:32 +0000

    Add package embedmeio/2024.12.0.dev6/embedmeio-2024.12.0.dev6.tar.gz

---
+++ packages/embedmeio/embedmeio-2024.12.0.dev6.tar.gz/embedmeio-2024.12.0.dev6/embedmeio/const.py
@@ -0,0 +1,12 @@
+"""Constants used by embedme."""
+
+__version__ = "2024.12.0-dev6"
+
+from pathlib import Path
+
+DEFAULT_EMBEDME_PATH = Path.home() / ".embedmeio" / __version__
+
+def get_embedme_package():
+    if "dev" in __version__:
+        return f"esphome@git+https://github.com/EmbedMe-io/embedme.git@dev"
+    return f"esphome@git+https://github.com/EmbedMe-io/embedme.git@{__version__}"
