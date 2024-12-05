73594d34389bc74109ce27311d3b66f42b74a4c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:36:14 +0000

    Add package streamlit-nightly/1.40.2.dev20241120/streamlit_nightly-1.40.2.dev20241120.tar.gz

---
+++ packages/streamlit-nightly/streamlit_nightly-1.40.2.dev20241120.tar.gz/streamlit_nightly-1.40.2.dev20241120/streamlit/static/static/js/Hooks.Dd8RMECJ.js
@@ -0,0 +1,2 @@
+import{r as t}from"./index.CrjRiJXs.js";const o=r=>{const e=t.useRef();return t.useEffect(()=>{e.current=r},[r]),e.current};export{o as u};
+//# sourceMappingURL=Hooks.Dd8RMECJ.js.map
