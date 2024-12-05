cb5327108e1bcb42242cdd14207e958e7cec9c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:22:49 +0000

    Add package propy-alamstein/0.1.2/propy_alamstein-0.1.2.tar.gz

---
+++ packages/propy-alamstein/propy_alamstein-0.1.2.tar.gz/propy_alamstein-0.1.2/README.md
@@ -0,0 +1,24 @@
+# Small Language Model
+
+Final project for Matt Harrison / MetaSnake's [Professional Python](https://store.metasnake.com/propy) course.
+
+The core code in this repo is the class `SmallLanguageModel` in the file `small_language_model.py`. However, that code is largely irrelevant, and exists just as a vehicle for us to explore modern tools used in Python development:
+
+  * `uv` instead of `pip` for installing packages
+  * LLMs to write code
+  * Automated tests with `pytest`
+  * Type Annotations
+  * pre-commit and GitHub Actions to automate testing, linting with `ruff`, etc.
+  * Deploying a package to PyPI
+
+If, despite the above warning, you still want to run the code in this repo, you can do so like this:
+
+```python
+from propy_alamstein.small_language_model import SmallLanguageModel
+
+slm = SmallLanguageModel()
+slm.train("The quick brown fox jumped over the lazy dogs")
+
+slm.predict_next("a")
+slm.get_character_training_frequency()
+```
---
+++ packages/propy-alamstein/propy_alamstein-0.1.2.tar.gz/propy_alamstein-0.1.2/tests/test_small_language_model.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import pytest
+from collections import Counter
+
+# Presumably there is a better way to do this, but I just don't know it!
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from propy_alamstein.small_language_model import SmallLanguageModel
+
+
+def test_initializes_with_empty_dict() -> None:
+    slm = SmallLanguageModel()
+
+    assert len(slm.char_map) == 0
+
+
+@pytest.fixture
+def initialized_slm() -> SmallLanguageModel:
+    slm = SmallLanguageModel()
+    slm.train("Hello")
+
+    return slm
+
+
+def test_train(initialized_slm):
+    correct_H = Counter({"e": 1})
+    assert initialized_slm.char_map["H"] == correct_H
+
+    correct_e = Counter({"l": 1})
+    assert initialized_slm.char_map["e"] == correct_e
+
+    correct_l = Counter({"l": 1, "o": 1})
+    assert initialized_slm.char_map["l"] == correct_l
+
+    # There should not be any extra letters in the dict.
+    # Note the final "o" in "Hello" should not be in the dict.
+    assert set(initialized_slm.char_map.keys()) == set("Hell")
+
+
+def test_predict(initialized_slm: SmallLanguageModel) -> None:
+    assert initialized_slm.predict_next("H") == "e"
+
+    assert initialized_slm.predict_next("e") == "l"
+
+
+# This mark + the function below are intended to be equal to the test_predict method above
+@pytest.mark.parametrize("input, expected_prediction", [("H", "e"), ("e", "l")])
+def test_predicted_expected(
+    initialized_slm: SmallLanguageModel, input: str, expected_prediction: str
+) -> None:
+    assert initialized_slm.predict_next(input) == expected_prediction
+
+
+def test_edge_cases(initialized_slm: SmallLanguageModel) -> None:
+    assert initialized_slm.predict_next("o") is None
+    assert initialized_slm.predict_next("z") is None
