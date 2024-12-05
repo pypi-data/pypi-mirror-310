55b14a494f2abce44b327235367de9f095787a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:05:05 +0000

    Add package django-pagination-utils/0.1.0/django_pagination_utils-0.1.0.tar.gz

---
+++ packages/django-pagination-utils/django_pagination_utils-0.1.0.tar.gz/django_pagination_utils-0.1.0/django_pagination_utils/mixins/query_filter_order_mixin.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from django.db.models import Model
+from .query_filter_mixin import QueryFilterMixin
+from .query_order_mixin import QueryOrderMixin
+
+
+class QueryFilterOrderMixin(ABC, QueryFilterMixin, QueryOrderMixin):
+
+    @property
+    @abstractmethod
+    def model(self) -> Model:
+        raise NotImplementedError
+
+    def __init__(self):
+        self.init_filterset_defaults()
+        self.init_order_defaults()
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+    def get_complete_queryset(self, params: dict):
+        queryset = self.get_queryset()
+        queryset = self.apply_filterset_to_queryset(queryset, params)
+        queryset = self.apply_order_to_queryset(queryset, params)
+        return queryset
+
+    def get_defaults(self):
+        return {
+            'order_by': self.default_order_by,
+            'order_direction': self.default_order_direction,
+            'fields_order': self.allowed_fields_order,
+            'field_time': self.default_field_time
+        }
