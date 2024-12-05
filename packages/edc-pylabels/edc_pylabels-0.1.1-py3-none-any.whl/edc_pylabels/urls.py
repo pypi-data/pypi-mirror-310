75a8d32bcc47ae9c0849748a546c7f04e5928e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:18 +0000

    Add package edc-pylabels/0.1.1/edc_pylabels-0.1.1-py3-none-any.whl

---
+++ packages/edc-pylabels/edc_pylabels-0.1.1-py3-none-any.whl/edc_pylabels/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import RedirectView
+
+app_name = "edc_pylabels"
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url=f"/{app_name}/admin/"), name="home_url"),
+    # path("print_labels/", PrintLabelsView.as_view(), name="print-barcode"),
+    # path("", HomeView.as_view(), name="home"),
+]
