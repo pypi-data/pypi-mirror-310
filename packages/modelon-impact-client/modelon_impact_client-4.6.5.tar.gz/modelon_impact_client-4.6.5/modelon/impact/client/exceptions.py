83c91c1d71cb8d7bb3824ec73cf7dcdba1b7080d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:04:08 +0000

    Add package modelon-impact-client/4.6.5/modelon_impact_client-4.6.5.tar.gz

---
+++ packages/modelon-impact-client/modelon_impact_client-4.6.5.tar.gz/modelon_impact_client-4.6.5/modelon/impact/client/exceptions.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+
+from typing import Any
+
+
+class Error(Exception):
+    pass
+
+
+class UnsupportedSemanticVersionError(Error):
+    pass
+
+
+class NoAssignedLicenseError(Error):
+    pass
+
+
+class OperationTimeOutError(Error):
+    pass
+
+
+class OperationFailureError(Error):
+    @classmethod
+    def for_operation(cls, operation_name: str) -> OperationFailureError:
+        return cls(
+            f"{operation_name} was cancelled before completion! "
+            f"Log file generated for cancelled {operation_name} is empty!"
+        )
+
+
+class OperationNotCompleteError(Error):
+    @classmethod
+    def for_operation(
+        cls, operation_name: str, status: Any
+    ) -> OperationNotCompleteError:
+        return cls(
+            f"{operation_name} is still in progress! Status : {status}."
+            f" Please call the wait() method on the {operation_name} operation"
+            " to wait until completion!"
+        )
+
+
+class OperationCompleteError(Error):
+    pass
+
+
+class IllegalProjectImport(Error):
+    pass
+
+
+class IllegalWorkspaceImport(Error):
+    pass
+
+
+class IllegalWorkspaceExport(Error):
+    pass
+
+
+class IllegalWorkspaceConversion(Error):
+    pass
+
+
+class IllegalContentImport(Error):
+    pass
+
+
+class IllegalCustomArtifactImport(Error):
+    pass
+
+
+class IllegalCaseResultImport(Error):
+    pass
+
+
+class IllegalFMUImport(Error):
+    pass
+
+
+class ExternalResultUploadError(Error):
+    pass
+
+
+class NoSuchCustomArtifactError(Error):
+    pass
+
+
+class NoAssociatedPublishedWorkspaceError(Error):
+    pass
+
+
+class RemotePublishedWorkspaceLinkError(Error):
+    pass
+
+
+class AuthenticationError(Error):
+    pass
