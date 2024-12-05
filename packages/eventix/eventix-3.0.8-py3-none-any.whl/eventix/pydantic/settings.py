43228aa92d6216c822c2867c8c8f9020cb16cf6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:49 +0000

    Add package eventix/3.0.8/eventix-3.0.8-py3-none-any.whl

---
+++ packages/eventix/eventix-3.0.8-py3-none-any.whl/eventix/pydantic/settings.py
@@ -0,0 +1,15 @@
+import os
+from typing import Literal
+
+from pydantic_settings import BaseSettings
+
+BackendNames = Literal["mongodb", "couchdb"]
+
+
+class EventixSettings(BaseSettings):
+    eventix_backend: BackendNames = "mongodb"
+    eventix_relay_config: str = os.path.abspath(
+        os.path.join(__file__, "../../../relay.yaml")
+    )
+    eventix_trigger_config_directory: str = ""
+    eventix_url: str = ""
