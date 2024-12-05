dcccae6ca457ee0610f515498571a9562de4355e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:04 +0000

    Add package edc-form-validators/0.3.41/edc_form_validators-0.3.41.tar.gz

---
+++ packages/edc-form-validators/edc_form_validators-0.3.41.tar.gz/edc_form_validators-0.3.41/runtests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import logging
+from pathlib import Path
+
+from edc_test_utils import DefaultTestSettings, func_main
+
+app_name = "edc_form_validators"
+base_dir = Path(__file__).absolute().parent
+
+project_settings = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.messages",
+        "django.contrib.sessions",
+        "django.contrib.sites",
+        "django.contrib.staticfiles",
+        "edc_form_validators.apps.AppConfig",
+        "form_validators_app.apps.AppConfig",
+    ],
+).settings
+
+
+def main():
+    func_main(project_settings, f"{app_name}.tests")
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
