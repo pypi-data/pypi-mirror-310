c208edd9a9e75ec97cb62116cc0b55818b0c04a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:05:06 +0000

    Add package DocumentAI-std/0.3.2.dev3/documentai_std-0.3.2.dev3.tar.gz

---
+++ packages/DocumentAI-std/documentai_std-0.3.2.dev3.tar.gz/documentai_std-0.3.2.dev3/setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="DocumentAI_std",
+    version="0.3.2-dev3",
+    packages=find_packages(exclude=["DocumentAI_std.tests"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=required,
+    url="https://github.com/LATIS-DocumentAI-Group/DocumentAI-std",
+    license="MIT",
+    author="Hamza Gbada",
+    author_email="hamza.gbada@gmail.com",
+    python_requires=">=3.11, <3.13",
+    description="The main standards for Latis Document AI project",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
