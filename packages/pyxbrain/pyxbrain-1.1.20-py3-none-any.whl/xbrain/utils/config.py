d49408c5896ccc26ec3cc0b9ab9bfb44a0a89335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:35:58 +0000

    Add package pyxbrain/1.1.20/pyxbrain-1.1.20-py3-none-any.whl

---
+++ packages/pyxbrain/pyxbrain-1.1.20-py3-none-any.whl/xbrain/utils/config.py
@@ -0,0 +1,40 @@
+import os
+class Config:
+    def __init__(self):
+        self.user_home_path = os.path.expanduser("~")  # 获取用户主目录路径
+        self.config_path = os.path.join(self.user_home_path, "xbrain", "config.yaml")  # 使用用户目录下的配置文件
+        self.config = self.load_config()
+        self.OPENAI_BASE_URL = self.config["openai"]["base_url"]
+        self.OPENAI_API_KEY = self.config["openai"]["api_key"]
+        self.OPENAI_MODEL = self.config["openai"]["model"]
+
+
+    def load_config(self):
+        import yaml
+        # 确保目录存在
+        os.makedirs(os.path.dirname(self.config_path), exist_ok=True)
+        if not os.path.exists(self.config_path):
+            # 如果文件不存在，创建文件并写入默认配置
+            default_config = {
+                'openai': {
+                    'base_url': 'https://api.openai.com/v1',
+                    'api_key': '',
+                    'model': 'gpt-4o-2024-08-06'
+                }
+            }
+            with open(self.config_path, 'w') as f:
+                yaml.dump(default_config, f, default_flow_style=False)
+            return default_config
+        else:
+            # 如果文件存在，正常加载配置
+            with open(self.config_path, "r") as f:
+                return yaml.load(f, Loader=yaml.FullLoader)
+            
+
+class Constants:
+    def __init__(self):
+        self.CONFIG_NAME = "config.yaml"
+        self.XBRAIN_DIR = ".xbrain"
+        self.VERSION = "1.1.20"
+
+
