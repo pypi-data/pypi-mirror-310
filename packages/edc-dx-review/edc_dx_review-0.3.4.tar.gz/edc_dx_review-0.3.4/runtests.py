ed0adf9269ff68ec404b5a1df51eda653443e5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:06 +0000

    Add package edc-dx-review/0.3.4/edc_dx_review-0.3.4.tar.gz

---
+++ packages/edc-dx-review/edc_dx_review-0.3.4.tar.gz/edc_dx_review-0.3.4/runtests.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import logging
+from pathlib import Path
+
+from edc_constants.constants import CHOL, DM, HIV, HTN
+from edc_test_utils import DefaultTestSettings, func_main
+
+app_name = "edc_dx_review"
+
+base_dir = Path(__file__).absolute().parent
+
+project_settings = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
+    SUBJECT_SCREENING_MODEL="edc_metadata.subjectscreening",
+    SUBJECT_CONSENT_MODEL="edc_metadata.subjectconsent",
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
+    SUBJECT_VISIT_MISSED_MODEL="edc_metadata.subjectvisitmissed",
+    SUBJECT_REQUISITION_MODEL="edc_metadata.subjectrequisition",
+    EDC_SITES_REGISTER_DEFAULT=True,
+    LIST_MODEL_APP_LABEL="edc_dx_review",
+    EDC_DX_LABELS={
+        HIV: "HIV",
+        DM: "Diabetes",
+        HTN: "Hypertension",
+        CHOL: "High Cholesterol",
+    },
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "django_crypto_fields.apps.AppConfig",
+        "django_revision.apps.AppConfig",
+        "multisite",
+        "edc_appointment.apps.AppConfig",
+        "edc_action_item.apps.AppConfig",
+        "edc_consent.apps.AppConfig",
+        "edc_device.apps.AppConfig",
+        "edc_facility.apps.AppConfig",
+        "edc_lab.apps.AppConfig",
+        "edc_list_data.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        "edc_crf.apps.AppConfig",
+        "edc_offstudy.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
+        "edc_identifier.apps.AppConfig",
+        "edc_notification.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        "edc_timepoint.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
+        "edc_dx_review.apps.AppConfig",
+    ],
+    RANDOMIZATION_LIST_PATH=str(base_dir / app_name / "tests" / "test_randomization_list.csv"),
+    add_dashboard_middleware=True,
+    use_test_urls=True,
+).settings
+
+
+def main():
+    func_main(project_settings, f"{app_name}.tests")
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
