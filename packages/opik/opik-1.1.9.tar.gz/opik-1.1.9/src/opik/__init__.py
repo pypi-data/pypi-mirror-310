3eb666d134af7bbc495023a9f0d60bdf32b9084f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:43 +0000

    Add package opik/1.1.9/opik-1.1.9.tar.gz

---
+++ packages/opik/opik-1.1.9.tar.gz/opik-1.1.9/src/opik/__init__.py
@@ -0,0 +1,28 @@
+from .api_objects.prompt import Prompt
+from .decorator.tracker import track, flush_tracker
+from .api_objects.opik_client import Opik
+from .api_objects.trace import Trace
+from .api_objects.span import Span
+from .api_objects.dataset import Dataset
+from . import _logging
+from .configurator.configure import configure
+from . import package_version
+from .plugins.pytest.decorator import llm_unit
+from .evaluation import evaluate
+
+_logging.setup()
+
+__version__ = package_version.VERSION
+__all__ = [
+    "__version__",
+    "evaluate",
+    "track",
+    "flush_tracker",
+    "Opik",
+    "Trace",
+    "Span",
+    "Dataset",
+    "llm_unit",
+    "configure",
+    "Prompt",
+]
