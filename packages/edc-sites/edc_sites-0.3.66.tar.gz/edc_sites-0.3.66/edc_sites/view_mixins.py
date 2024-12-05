6420319cce4d70923a66904425a53da95caa682b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 21:23:12 +0000

    Add package edc-sites/0.3.66/edc_sites-0.3.66.tar.gz

---
+++ packages/edc-sites/edc_sites-0.3.66.tar.gz/edc_sites-0.3.66/edc_sites/view_mixins.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import SiteProfile
+from .site import SiteNotRegistered, sites
+
+
+class SiteViewMixin:
+    def get_context_data(self, **kwargs) -> dict:
+        try:
+            site_profile = SiteProfile.objects.get(site__id=self.request.site.id)
+        except ObjectDoesNotExist:
+            site_profile = None
+        kwargs.update(site_profile=site_profile)
+        try:
+            kwargs.update(site_title=site_profile.title)
+        except AttributeError:
+            if not sites.all():
+                raise SiteNotRegistered(
+                    "Unable to determine site profile 'title'. No sites have been registered! "
+                )
+            raise
+        return super().get_context_data(**kwargs)
