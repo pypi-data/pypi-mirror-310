41016651c6a3c96fc8fba8a36eeebb5271df6c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 23:03:32 +0000

    Add package ixbrlparse/0.10.0/ixbrlparse-0.10.0-py3-none-any.whl

---
+++ packages/ixbrlparse/ixbrlparse-0.10.0-py3-none-any.whl/ixbrlparse/components/context.py
@@ -0,0 +1,61 @@
+import datetime
+from copy import deepcopy
+from typing import Any, Optional
+
+
+class ixbrlContext:  # noqa: N801
+    """Class to represent an ixbrl context.
+
+    The context should either have an instant date or a start and end date.
+
+    Attributes:
+        id: The id of the context.
+        entity: A dictionary of the entity information.
+        segments: A list of dictionaries of the segment information.
+        instant: The instant date of the context.
+        startdate: The start date of the context.
+        enddate: The end date of the context."""
+
+    def __init__(
+        self,
+        _id: str,
+        entity: dict[str, Optional[str]],
+        segments: Optional[list[dict]],
+        instant: Optional[str],
+        startdate: Optional[str],
+        enddate: Optional[str],
+    ):
+        self.id = _id
+        self.entity = entity
+        self.segments = segments
+        self.instant: Optional[datetime.date] = None
+        self.startdate: Optional[datetime.date] = None
+        self.enddate: Optional[datetime.date] = None
+
+        date_fields = {
+            "instant": instant,
+            "startdate": startdate,
+            "enddate": enddate,
+        }
+        for field, value in date_fields.items():
+            if value:
+                datevalue = datetime.datetime.strptime(value.strip(), "%Y-%m-%d").astimezone().date()
+                setattr(self, field, datevalue)
+
+    def __repr__(self) -> str:
+        if self.startdate and self.enddate:
+            datestr = f"{self.startdate} to {self.enddate}"
+        else:
+            datestr = str(self.instant)
+
+        segmentstr = " (with segments)" if self.segments else ""
+
+        return f"<IXBRLContext {self.id} [{datestr}]{segmentstr}>"
+
+    def to_json(self) -> dict[str, list[dict[str, Any]]]:
+        """Convert the object to a JSON serialisable dictionary."""
+        values = deepcopy(self.__dict__)
+        for i in ["startdate", "enddate", "instant"]:
+            if isinstance(values[i], datetime.date):
+                values[i] = str(values[i])
+        return values
