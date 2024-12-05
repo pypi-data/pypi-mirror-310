0661495189e5b7a1b0ef5b2293dcd7620de12c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-05 22:05:52 +0000

    Add package sshtmux/0.1.6/sshtmux-0.1.6.tar.gz

---
+++ packages/sshtmux/sshtmux-0.1.6.tar.gz/sshtmux-0.1.6/sshtmux/sshm/ssh_group.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from .ssh_host import SSH_Host
+
+
+@dataclass
+class SSH_Group:
+    """Class for SSH Group config structure"""
+
+    name: str
+    desc: str = ""
+    info: list = field(default_factory=list)
+    hosts: List[SSH_Host] = field(default_factory=list)
+    patterns: List[SSH_Host] = field(default_factory=list)
+
+    print_style: str = ""
+
+    def __rich__(self):
+        pass
