b3fbdbcd3e6cc405268b5b938feba85fcc5aff7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:09:33 +0000

    Add package aegis-framework/0.1.12/aegis_framework-0.1.12-py3-none-any.whl

---
+++ packages/aegis-framework/aegis_framework-0.1.12-py3-none-any.whl/aegis_framework/__init__.py
@@ -0,0 +1,10 @@
+"""
+Aegis Framework - A framework for creating multi-agent colonies
+"""
+
+__version__ = "0.1.11"
+__author__ = "Metis Analytics"
+__email__ = "cjohnson@metisos.com"
+
+from aegis_framework.core.master_agent import *
+from aegis_framework.core.ollama_model import *
---
+++ packages/aegis-framework/aegis_framework-0.1.12-py3-none-any.whl/aegis_framework/core/__init__.py
@@ -0,0 +1,4 @@
+from aegis_framework_dev.core.master_agent import MasterAIAgent
+from aegis_framework_dev.core.ollama_model import OllamaLocalModel
+
+__all__ = ["MasterAIAgent", "OllamaLocalModel"]
