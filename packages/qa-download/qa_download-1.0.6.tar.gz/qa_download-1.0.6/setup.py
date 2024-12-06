197df9ef26266acc994acc2e75c9a7ca29c416a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:58 +0000

    Add package qa-download/1.0.6/qa_download-1.0.6.tar.gz

---
+++ packages/qa-download/qa_download-1.0.6.tar.gz/qa_download-1.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qa_download",
+    version="1.0.6",
+    author="Ahmed Qaddoura",
+    author_email="aqaddora96@gmail.com",
+    description="A tool for downloading music with embedded metadata and lyrics.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AQaddora/music_downloader",
+    packages=find_packages(),
+    install_requires=[
+        "yt-dlp",
+        "requests",
+        "beautifulsoup4",
+        "mutagen",
+    ],
+    entry_points={
+        "console_scripts": [
+            "music-downloader=music_downloader.downloader:main",
+        ],
+    },
+    python_requires=">=3.7",
+)
