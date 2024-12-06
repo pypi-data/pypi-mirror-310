1e578f42e2e16d580cab9e23e6d35a9aeb56c0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:28 +0000

    Add package beet-observer/0.4.0/beet_observer-0.4.0.tar.gz

---
+++ packages/beet-observer/beet_observer-0.4.0.tar.gz/beet_observer-0.4.0/beet_observer/plugin.py
@@ -0,0 +1,44 @@
+from beet import Context, run_beet
+
+from .data_pack import *
+from .resource_pack import *
+
+
+def beet_default(ctx: Context):
+    if "observer" not in ctx.meta:
+        return
+    # get default directories
+    if "default_dir" not in ctx.meta["observer"]:
+        # default dir not defined
+        ctx.meta["observer"]["default_dir_dp"] = "default_overlay"
+        ctx.meta["observer"]["default_dir_rp"] = "default_overlay"
+    elif isinstance(ctx.meta["observer"]["default_dir"], str):
+        # default dir is the same for dp and rp
+        ctx.meta["observer"]["default_dir_dp"] = ctx.meta["observer"]["default_dir"]
+        ctx.meta["observer"]["default_dir_rp"] = ctx.meta["observer"]["default_dir"]
+    else:
+        # default dir is different for dp and rp
+        ctx.meta["observer"]["default_dir_dp"] = ctx.meta["observer"]["default_dir"][
+            "dp"
+        ]
+        ctx.meta["observer"]["default_dir_rp"] = ctx.meta["observer"]["default_dir"][
+            "rp"
+        ]
+    # loop through all overlays
+    for overlay in ctx.meta["observer"]["overlays"]:
+        # create relative path
+        path = f"{ctx.directory}/{overlay['process']}"
+        # generate context for overlay pack
+        with run_beet(
+            config={"data_pack": {"load": "."}, "resource_pack": {"load": "."}},
+            directory=path,
+        ) as ctx_overlay:
+            if "directory" not in overlay:
+                dp_dir = f"overlay_{ctx_overlay.data.pack_format}"
+                rp_dir = f"overlay_{ctx_overlay.assets.pack_format}"
+            else:
+                dp_dir = overlay["directory"]
+                rp_dir = overlay["directory"]
+            # compare build pack and overlay pack
+            gen_dp_overlays(ctx, ctx_overlay, dp_dir)
+            gen_rp_overlays(ctx, ctx_overlay, rp_dir)
