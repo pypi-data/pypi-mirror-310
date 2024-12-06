2e178c25e8d73f03c0439a2204cc0caf261a6318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:55 +0000

    Add package ceo-py/0.7.1rc0/ceo_py-0.7.1rc0-py3-none-any.whl

---
+++ packages/ceo-py/ceo_py-0.7.1rc0-py3-none-any.whl/ceo/prompt/executor_prompt.py
@@ -0,0 +1,46 @@
+import logging
+from collections.abc import Iterator
+
+from langchain_core.language_models import BaseChatModel
+
+from ceo.ability.ability import Ability
+from ceo.prompt.prompt import Prompt
+
+log = logging.getLogger('ceo.prompt')
+
+
+class ExecutorPrompt(Prompt):
+    def __init__(self, params: dict, action: Ability, ext_context: str = ''):
+        self.action = action
+        self.params = params
+        prompt = ('Precondition: Below is a tool and your choice(params) for the tool.\n'
+                  'Task: Explain what you are going to do.\n'
+                  'Output format: text.\n'
+                  'Example output: I am trying to open calculator.\n'
+                  f'Tool: {action}\n'
+                  f'Params(choice): {params}\n')
+        super().__init__(prompt, ext_context)
+        log.debug(f'Executor prompt(before): {self.prompt}')
+
+    def explain(self, model: BaseChatModel, stream: bool = False) -> str | Iterator:
+        if stream:
+            return model.stream(self.prompt)
+        return model.invoke(self.prompt).content
+
+    def invoke(self, model: BaseChatModel, stream: bool = False) -> str | Iterator:
+        result = self.action.function(**self.params)
+        prompt = ('Precondition: Below is a tool, your choice(params) for the tool, '
+                  'and the result of your using the tool.\n'
+                  'Task: Explain what you have done, and write down the result detailed. '
+                  '(The result is shown below at [Result])\n'
+                  'Output format: text.\n'
+                  'Output contains: {the_choice_you_made}, {what_you_have_done}\n'
+                  'Output example: I wrote a wechat message which says "Bonjour". The result is "success"\n'
+                  f'Tool: {self.action}\n'
+                  f'Params(choice): {self.params}\n'
+                  f'Result: {result}\n')
+        prompt = f'{self.ext_context}{self.seperator}{prompt}'
+        log.debug(f'Executor prompt(after): {prompt}')
+        if stream:
+            return model.stream(prompt)
+        return model.invoke(prompt).content
---
+++ packages/ceo-py/ceo_py-0.7.1rc0-py3-none-any.whl/ceo/prompt/self_introduce.py
@@ -0,0 +1,23 @@
+import logging
+from collections.abc import Iterator
+
+from langchain_core.language_models import BaseChatModel
+
+from ceo.prompt.prompt import Prompt
+
+log = logging.getLogger('ceo.prompt')
+
+
+class SelfIntroducePrompt(Prompt):
+    def __init__(self, agent: any, ext_context: str = ''):
+        prompt = ('Task: Introduce yourself briefly base on below information.(Only tell what you exactly '
+                  'can do base on your abilities)\n'
+                  f'Information: {agent.__repr__()}\n'
+                  f'Output format: My name is <name>. What can i do: ...\n')
+        super().__init__(prompt, ext_context)
+        log.debug(f'SelfIntroducePrompt: {self.prompt}')
+
+    def invoke(self, model: BaseChatModel, stream: bool = False) -> str | Iterator:
+        if stream:
+            return model.stream(self.prompt)
+        return model.invoke(self.prompt).content
