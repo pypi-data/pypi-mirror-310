636e090da68f54ac1e4617851d4b5911ba62f14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:41 +0000

    Add package onyeka-gauss-dist/0.1/onyeka_gauss_dist-0.1.tar.gz

---
+++ packages/onyeka-gauss-dist/onyeka_gauss_dist-0.1.tar.gz/onyeka_gauss_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='onyeka_gauss_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gauss_dist'],
+      zip_safe=False)
