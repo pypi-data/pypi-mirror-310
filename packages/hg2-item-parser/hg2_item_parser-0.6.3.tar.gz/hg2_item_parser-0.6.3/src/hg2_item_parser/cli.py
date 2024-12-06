ddc5463723adb1aa19e03d14df39d43201ea888d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:06:32 +0000

    Add package hg2-item-parser/0.6.3/hg2_item_parser-0.6.3.tar.gz

---
+++ packages/hg2-item-parser/hg2_item_parser-0.6.3.tar.gz/hg2_item_parser-0.6.3/src/hg2_item_parser/cli.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import Annotated
+
+import typer
+
+from .item_parser import ItemParser
+
+app = typer.Typer()
+
+
+@app.command(help="Prints an item to a console")
+def check(
+    item_id: int, data_dir_path: Annotated[Path, typer.Argument()] = Path("extracted")
+) -> None:
+    parser = ItemParser(data_dir_path)
+    item = parser.parse_item(item_id)
+    typer.echo(item)
+
+
+@app.command(help="Parses items in range")
+def parse_from_to(
+    first_item_id: int,
+    last_item_id: int,
+    output_file_path: Annotated[Path, typer.Argument()] = Path("parsed/items.txt"),
+    data_dir_path: Annotated[Path, typer.Argument()] = Path("extracted"),
+) -> None:
+    parser = ItemParser(data_dir_path)
+    parser.parse_and_write_items_from_to(
+        first_item_id, last_item_id, output_file_path, progressbar=True
+    )
+
+
+@app.command(help="Parses an item")
+def parse(
+    item_id: int,
+    output_file_path: Annotated[Path, typer.Argument()] = Path("parsed/items.txt"),
+    data_dir_path: Annotated[Path, typer.Argument()] = Path("extracted"),
+) -> None:
+    parser = ItemParser(data_dir_path)
+    parser.parse_and_write_item(item_id, output_file_path)
+
+
+def main() -> None:
+    app()
