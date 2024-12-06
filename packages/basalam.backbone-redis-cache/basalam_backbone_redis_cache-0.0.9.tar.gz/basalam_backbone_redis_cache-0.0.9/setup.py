f3027a884a6395779c6ef6290c91f8e9f7b91357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:04:58 +0000

    Add package basalam.backbone-redis-cache/0.0.9/basalam_backbone_redis_cache-0.0.9.tar.gz

---
+++ packages/basalam.backbone-redis-cache/basalam_backbone_redis_cache-0.0.9.tar.gz/basalam_backbone_redis_cache-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+setuptools.setup(
+    name="basalam.backbone-redis-cache",
+    author="Mojtabaa Habibain",
+    author_email="mojtabaa.hn@gmail.com",
+    description="Python Utilities & Basalam Micro-Services SDK",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    setuptools_git_versioning={"enabled": True},
+    setup_requires=["setuptools-git-versioning"],
+    install_requires=[
+        "redis==4.4.4",
+        "aioredis==2.0.1"
+    ]
+)
