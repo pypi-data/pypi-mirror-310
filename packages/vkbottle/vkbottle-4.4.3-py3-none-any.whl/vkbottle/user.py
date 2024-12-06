2f1f238612279581c585a3a09aea840f16c40586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:04:57 +0000

    Add package vkbottle/4.4.3/vkbottle-4.4.3-py3-none-any.whl

---
+++ packages/vkbottle/vkbottle-4.4.3-py3-none-any.whl/vkbottle/user.py
@@ -0,0 +1,31 @@
+from typing_extensions import deprecated  # type: ignore
+
+from vkbottle.framework.labeler import UserLabeler
+
+from .dispatch.rules import base as rules
+from .dispatch.views import user as views
+from .framework.user import User, UserBlueprint, run_multibot
+from .tools.mini_types.user import MessageMin
+
+Message = MessageMin
+
+
+@deprecated(
+    "Blueprints was deprecated and will be removed in future releases, "
+    "read about new code separation method in documentation: \n"
+    "https://vkbottle.rtfd.io/ru/latest/tutorial/code-separation/",
+    category=FutureWarning,
+    stacklevel=0,
+)
+class Blueprint(UserBlueprint): ...
+
+
+__all__ = (
+    "Blueprint",
+    "Message",
+    "User",
+    "UserLabeler",
+    "rules",
+    "run_multibot",
+    "views",
+)
