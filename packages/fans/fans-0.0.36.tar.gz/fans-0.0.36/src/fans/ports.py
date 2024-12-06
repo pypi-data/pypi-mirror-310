751d7d97b1e8475916cd5bfc966a9042c5569836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:06:15 +0000

    Add package fans/0.0.36/fans-0.0.36.tar.gz

---
+++ packages/fans/fans-0.0.36.tar.gz/fans-0.0.36/src/fans/ports.py
@@ -0,0 +1,12 @@
+class ports:
+
+    auth            = 6560
+    fme             = 6561
+    quantix         = 6562
+    doy             = 6563
+
+    stome           = 6565
+
+    stome_desktop   = 6660
+
+    fansjs_dev      = 16501
