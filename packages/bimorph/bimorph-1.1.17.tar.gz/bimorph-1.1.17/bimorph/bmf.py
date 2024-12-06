e822eceb18dbfbbddf088959ef81462a1df10498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:32 +0000

    Add package bimorph/1.1.17/bimorph-1.1.17.tar.gz

---
+++ packages/bimorph/bimorph-1.1.17.tar.gz/bimorph-1.1.17/bimorph/bmf.py
@@ -0,0 +1,18 @@
+def hello(name):
+	print("Привет 5 тебе foo " +name +"!")
+
+def run(name):
+	#import a_module
+	print("Запуск2 ./bmf sqlite://tmp.sqlite")
+	#print( dir( fastfilepackage ) );
+	#import os
+	#print(os.system('./bmf sqlite://test.sqlite'))
+
+	#import subprocess
+	#print(subprocess.check_output(['./bmf', 'sqlite://tmp.sqlite']))
+	#print(subprocess.check_output(['pwd']))
+
+	#import subprocess
+	#subprocess.call(["./bmf sqlite://test.sqlite"])
+	#process = subprocess.run(['ls', '-l', '-a'])
+
