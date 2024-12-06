7af913a43a6cd4ef92a3313f4be2aa2047a95bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:21 +0000

    Add package HubNLP/0.1/HubNLP-0.1.tar.gz

---
+++ packages/HubNLP/HubNLP-0.1.tar.gz/HubNLP-0.1/HubNLP/__init__.py
@@ -0,0 +1,1 @@
+from .core import print_developer_info, print_library_tagline
---
+++ packages/HubNLP/HubNLP-0.1.tar.gz/HubNLP-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HubNLP",  # Package name
+    version="0.1",  # Initial version
+    author="Self-nasu",
+    author_email="nexiotech.2024@gmail.com",
+    description="A simple NLP utility library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Self-nasu/HubNLP",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
