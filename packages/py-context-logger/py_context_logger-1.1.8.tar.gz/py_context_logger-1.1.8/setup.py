4c9da58ba890dfb312da512ca98cb19a5a4e9073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:11 +0000

    Add package py-context-logger/1.1.8/py_context_logger-1.1.8.tar.gz

---
+++ packages/py-context-logger/py_context_logger-1.1.8.tar.gz/py_context_logger-1.1.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_context_logger',
+    version='1.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "google-cloud-logging==3.11.3",
+        "google-auth==2.36.0"
+    ],
+    license='MIT',
+    description='A python context logger with thread-local storage and context propagation for Python applications.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/RajeshKumar1490/py-context-logger',
+    author='Rajesh Ganjikunta',
+    author_email='rajeshkumarganjikunta90@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
