5f8b9a728c113d45e51ded8d9bc4ac36a5fe68c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:06:02 +0000

    Add package parvathimodule/0.3.0/parvathimodule-0.3.0.tar.gz

---
+++ packages/parvathimodule/parvathimodule-0.3.0.tar.gz/parvathimodule-0.3.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = 'parvathimodule',  # Package name
+    version = '0.3.0'      ,  # Version
+    description = 'A sample Python module',
+    author = 'Parvathi Vishnu',
+    author_email= 'parvathi.r.vishnu@gmail.com',
+    packages=find_packages(), #Automatically find modules
+    install_requires=[] , # Dependencies (if any)
+    python_requires = '>=3.6' #Supported python versions
+
+)
+
+# pip install setuptools wheel -- for creating a package
+# We need to change to the location where setup.py is located ie; cd parvathimodule
+# python setup.py sdist bdist_wheel --- for creating supporting files of python to locally install our module
+# pip install dist/parvathimodule-0.1.0-py3-none-any.whl -- for installing our created modulepython setup.py sdist bdist_wheel 
