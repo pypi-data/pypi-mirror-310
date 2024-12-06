a7982d15c3213a8be77734d83be537284fe127ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:05:12 +0000

    Add package chinus-emotions/2.2.0/chinus_emotions-2.2.0.tar.gz

---
+++ packages/chinus-emotions/chinus_emotions-2.2.0.tar.gz/chinus_emotions-2.2.0/src/emotions/__init__.py
@@ -0,0 +1,8 @@
+from .emotions import get_rand_id, get_mean, get_word
+
+__all__ = [
+    'get_word',
+    'get_mean',
+    'get_rand_id'
+]
+
---
+++ packages/chinus-emotions/chinus_emotions-2.2.0.tar.gz/chinus_emotions-2.2.0/src/emotions/db_connect.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+
+def db_connect(query: str) -> list[tuple]:
+    """
+    Connects to the database and returns a list of tuples.
+
+    :param query:
+    :return:
+    """
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.abspath(os.path.join(base_dir, 'emotions.db'))
+
+    # db 연결
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute(query)
+            result = cursor.fetchall()
+        finally:
+            cursor.close()
+
+
+    return result
