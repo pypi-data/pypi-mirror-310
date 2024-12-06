96fe0db6d32238974d64b9e943df8e7e55da9659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:39 +0000

    Add package nxopentse/0.0.1a20/nxopentse-0.0.1a20.tar.gz

---
+++ packages/nxopentse/nxopentse-0.0.1a20.tar.gz/nxopentse-0.0.1a20/src/nxopentse/cad/__init__.py
@@ -0,0 +1,19 @@
+from .code import   nx_hello,\
+                    get_all_bodies_in_part,\
+                    get_all_bodies_in_component,\
+                    get_faces_of_type, \
+                    get_all_points,\
+                    get_all_features,\
+                    get_feature_by_name,\
+                    get_all_point_features,\
+                    get_point_with_feature_name, \
+                    create_cylinder_between_two_points, \
+                    create_intersect_feature, \
+                    get_faces_of_body, \
+                    get_area_faces_with_color, \
+                    get_area_faces_with_color, \
+                    create_point, \
+                    create_line_between_two_points, \
+                    delete_feature,\
+                    get_named_datum_planes,\
+                    create_bounding_box
