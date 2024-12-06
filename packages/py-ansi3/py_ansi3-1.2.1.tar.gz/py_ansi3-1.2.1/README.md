44fb98b7181ed6ce73319c50f4a0054cb9286c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:09 +0000

    Add package py-ansi3/1.2.1/py_ansi3-1.2.1.tar.gz

---
+++ packages/py-ansi3/py_ansi3-1.2.1.tar.gz/py_ansi3-1.2.1/README.md
@@ -0,0 +1,31 @@
+# PyANSI3
+
+PyANSI3 is a simple Python library to apply ANSI colors to text in the terminal. It allows you to easily print colored text directly in your terminal by using placeholders like `{red}`, `{green}`, etc.
+
+## Full colors
+
+        black
+        red
+        green
+        yellow
+        blue
+        magenta
+        cyan
+        white
+        reset
+        bold
+        underline
+        black_bg
+        red_bg
+        green_bg
+        yellow_bg
+        blue_bg
+        magenta_bg
+        cyan_bg
+        white_bg
+
+### Installation
+
+You can install PyANSI3 via pip:
+
+pip install py-ansi3
---
+++ packages/py-ansi3/py_ansi3-1.2.1.tar.gz/py_ansi3-1.2.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-ansi3",
+    version="1.2.1",
+    packages=find_packages(),
+    url="https://github.com/mohamedlunar/PyANSI3",
+    author="MohamedLunar",
+    author_email="contact.mohamedlunardev@gmail.com",
+    description="🔗 Package Guide On https://github.com/mohamedlunar/PyANSI3",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        # Your dependencies here
+    ],
+)
