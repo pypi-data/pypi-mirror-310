71ed13d7aeba276d008a1dfeb62341a829319a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:24 +0000

    Add package distributions-jaaf/1.1/distributions_jaaf-1.1.tar.gz

---
+++ packages/distributions-jaaf/distributions_jaaf-1.1.tar.gz/distributions_jaaf-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_jaaf',
+      version='1.1',
+      description='Gaussian distributions JAAF',
+      packages=['distributions'],
+      zip_safe=False)
