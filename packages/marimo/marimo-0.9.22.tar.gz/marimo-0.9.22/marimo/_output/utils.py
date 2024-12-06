ffdcbe4c34c01f12812593b405d101ce5ad41530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:10:17 +0000

    Add package marimo/0.9.22/marimo-0.9.22.tar.gz

---
+++ packages/marimo/marimo-0.9.22.tar.gz/marimo-0.9.22/marimo/_output/utils.py
@@ -0,0 +1,32 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+import urllib.parse
+from typing import Optional, Union
+
+from marimo._messaging.mimetypes import KnownMimeType
+
+
+def build_data_url(mimetype: KnownMimeType, data: bytes) -> str:
+    assert mimetype is not None
+    # `data` must be base64 encoded
+    str_repr = data.decode("utf-8").replace("\n", "")
+    return f"data:{mimetype};base64,{str_repr}"
+
+
+def flatten_string(text: str) -> str:
+    return "".join([line.strip() for line in text.split("\n")])
+
+
+def create_style(
+    pairs: dict[str, Union[str, int, float, None]],
+) -> Optional[str]:
+    if not pairs:
+        return None
+
+    return ";".join([f"{k}: {v}" for k, v in pairs.items() if v is not None])
+
+
+def uri_encode_component(code: str) -> str:
+    """Equivalent to `encodeURIComponent` in JavaScript."""
+    return urllib.parse.quote(code, safe="~()*!.'")
---
+++ packages/marimo/marimo-0.9.22.tar.gz/marimo-0.9.22/marimo/_static/assets/channel-CURbiOR9.js
@@ -0,0 +1,1 @@
+import{U as a,C as r}from"./mermaid-U-xJ2OxK.js";const s=(s,o)=>a.lang.round(r.parse(s)[o]);export{s as c};
