d66292a4afbbefccd2f0e3564e610896d24f269e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:36 +0000

    Add package asktable/3.3.0/asktable-3.3.0.tar.gz

---
+++ packages/asktable/asktable-3.3.0.tar.gz/asktable-3.3.0/src/asktable/types/sys/project_update_params.py
@@ -0,0 +1,16 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import TypedDict
+
+__all__ = ["ProjectUpdateParams"]
+
+
+class ProjectUpdateParams(TypedDict, total=False):
+    locked: Optional[bool]
+    """是否锁定"""
+
+    name: Optional[str]
+    """项目名称"""
