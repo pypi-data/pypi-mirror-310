5c7f443a852f14829a6c76be4af959d90289d4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:07:24 +0000

    Add package Mensajes-chrishiguera97/6.0/mensajes_chrishiguera97-6.0.tar.gz

---
+++ packages/Mensajes-chrishiguera97/mensajes_chrishiguera97-6.0.tar.gz/mensajes_chrishiguera97-6.0/tests/test_hola.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy as np
+from mensajes.hola.saludos import generar_array
+
+
+class PruebasHola(unittest.TestCase):
+    def test_generar_array(self):
+        np.testing.assert_array_equal(
+            np.array([0,1,2,3,4,5]),
+            generar_array(6)
+        )
+
+"""
+from mensajes.hola.saludos import *
+from mensajes.adios.despedidas import *
+
+saludar()
+Saludo()
+
+despedir()
+despedida()
+
+"""
