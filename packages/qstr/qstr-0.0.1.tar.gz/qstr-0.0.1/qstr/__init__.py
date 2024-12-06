b5dcff68f20a14f32f12ddd4b22723954ee0ebf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:55 +0000

    Add package qstr/0.0.1/qstr-0.0.1.tar.gz

---
+++ packages/qstr/qstr-0.0.1.tar.gz/qstr-0.0.1/qstr/__init__.py
@@ -0,0 +1,137 @@
+def capitalize(s):
+    return s.capitalize()
+
+def casefold(s):
+    return s.casefold()
+
+def center(s, width, fillchar=' '):
+    return s.center(width, fillchar)
+
+def count(s, sub, start=None, end=None):
+    return s.count(sub, start, end)
+
+def encode(s, encoding='utf-8', errors='strict'):
+    return s.encode(encoding, errors)
+
+def endswith(s, suffix, start=None, end=None):
+    return s.endswith(suffix, start, end)
+
+def expandtabs(s, tabsize=8):
+    return s.expandtabs(tabsize)
+
+def find(s, sub, start=None, end=None):
+    return s.find(sub, start, end)
+
+def format(s, *args, **kwargs):
+    return s.format(*args, **kwargs)
+
+def format_map(s, mapping):
+    return s.format_map(mapping)
+
+def index(s, sub, start=None, end=None):
+    return s.index(sub, start, end)
+
+def isalnum(s):
+    return s.isalnum()
+
+def isalpha(s):
+    return s.isalpha()
+
+def isascii(s):
+    return s.isascii()
+
+def isdecimal(s):
+    return s.isdecimal()
+
+def isdigit(s):
+    return s.isdigit()
+
+def isidentifier(s):
+    return s.isidentifier()
+
+def islower(s):
+    return s.islower()
+
+def isnumeric(s):
+    return s.isnumeric()
+
+def isprintable(s):
+    return s.isprintable()
+
+def isspace(s):
+    return s.isspace()
+
+def istitle(s):
+    return s.istitle()
+
+def isupper(s):
+    return s.isupper()
+
+def join(s, iterable):
+    return s.join(iterable)
+
+def ljust(s, width, fillchar=' '):
+    return s.ljust(width, fillchar)
+
+def lower(s):
+    return s.lower()
+
+def lstrip(s, chars=None):
+    return s.lstrip(chars)
+
+def partition(s, sep):
+    return s.partition(sep)
+
+def removeprefix(s, prefix):
+    return s.removeprefix(prefix)
+
+def removesuffix(s, suffix):
+    return s.removesuffix(suffix)
+
+def replace(s, old, new, count=None):
+    return s.replace(old, new, count)
+
+def rfind(s, sub, start=None, end=None):
+    return s.rfind(sub, start, end)
+
+def rindex(s, sub, start=None, end=None):
+    return s.rindex(sub, start, end)
+
+def rjust(s, width, fillchar=' '):
+    return s.rjust(width, fillchar)
+
+def rpartition(s, sep):
+    return s.rpartition(sep)
+
+def rsplit(s, sep=None, maxsplit=-1):
+    return s.rsplit(sep, maxsplit)
+
+def rstrip(s, chars=None):
+    return s.rstrip(chars)
+
+def split(s, sep=None, maxsplit=-1):
+    return s.split(sep, maxsplit)
+
+def splitlines(s, keepends=False):
+    return s.splitlines(keepends)
+
+def startswith(s, prefix, start=None, end=None):
+    return s.startswith(prefix, start, end)
+
+def strip(s, chars=None):
+    return s.strip(chars)
+
+def swapcase(s):
+    return s.swapcase()
+
+def title(s):
+    return s.title()
+
+def translate(s, table):
+    return s.translate(table)
+
+def upper(s):
+    return s.upper()
+
+def zfill(s, width):
+    return s.zfill(width)
