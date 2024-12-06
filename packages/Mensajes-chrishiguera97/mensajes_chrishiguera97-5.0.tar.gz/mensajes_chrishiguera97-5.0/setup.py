4d43ab42f38d1d918f5595d0dc234f79a7dbc434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:06:31 +0000

    Add package Mensajes-chrishiguera97/5.0/mensajes_chrishiguera97-5.0.tar.gz

---
+++ packages/Mensajes-chrishiguera97/mensajes_chrishiguera97-5.0.tar.gz/mensajes_chrishiguera97-5.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Mensajes-chrishiguera97',
+    version='5.0',
+    description='Un paquete para saludar y despedir',
+    long_description=open('readme.md').read(),
+    long_description_content_type='text/markdown',
+    author='Christian Higuera Gómez',
+    author_email='christianhiguera2007@hotmail.com',
+    url='https://www.christian.dev',
+    license_files=['LICENSE'],
+    packages=find_packages(),
+    #scripts=['test.py'],
+    scripts=[],
+    test_suite='tests',
+    install_requires=[paquete.strip() for paquete in open("requirements.txt").readlines()],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Utilities'
+    ]
+)
