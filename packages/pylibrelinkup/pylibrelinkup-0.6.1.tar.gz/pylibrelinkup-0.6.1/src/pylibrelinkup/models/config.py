1c6d64127f8d86d1680609d9fbd031ac4377a013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:49 +0000

    Add package pylibrelinkup/0.6.1/pylibrelinkup-0.6.1.tar.gz

---
+++ packages/pylibrelinkup/pylibrelinkup-0.6.1.tar.gz/pylibrelinkup-0.6.1/src/pylibrelinkup/models/config.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic.alias_generators import to_camel
+
+from pylibrelinkup.models.data import F, H, L, Nd, Std
+
+__all__ = ["AlarmRules", "FixedLowAlarmValues"]
+
+
+class AlarmRules(BaseModel):
+    """AlarmRules class to store alarm rules data."""
+
+    model_config = ConfigDict(
+        str_strip_whitespace=True,
+        alias_generator=to_camel,
+        populate_by_name=True,
+        from_attributes=True,
+    )
+
+    c: bool = Field(default=False)
+    h: H
+    f: F
+    l: L
+    nd: Nd
+    p: int
+    r: int
+    std: Std
+
+
+class FixedLowAlarmValues(BaseModel):
+    """FixedLowAlarmValues class to store fixed alarm values."""
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        from_attributes=True,
+    )
+
+    mgdl: int
+    mmoll: float
