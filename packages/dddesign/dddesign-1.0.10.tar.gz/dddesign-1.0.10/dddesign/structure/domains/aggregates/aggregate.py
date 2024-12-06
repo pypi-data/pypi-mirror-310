eac773f62a5b8cdc59a7211434ed4375f77a5b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:54 +0000

    Add package dddesign/1.0.10/dddesign-1.0.10.tar.gz

---
+++ packages/dddesign/dddesign-1.0.10.tar.gz/dddesign-1.0.10/dddesign/structure/domains/aggregates/aggregate.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class Aggregate(BaseModel):
+    model_config = ConfigDict(validate_assignment=True, arbitrary_types_allowed=True)
---
+++ packages/dddesign/dddesign-1.0.10.tar.gz/dddesign-1.0.10/dddesign/utils/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    'base_model',
+    'convertors',
+    'class_helpers',
+    'function_exceptions_extractor',
+    'module_getter',
+    'sequence_helpers',
+    'annotation_helpers',
+    'function_helpers',
+)
