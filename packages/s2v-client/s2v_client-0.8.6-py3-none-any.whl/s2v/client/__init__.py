00d7b72006d293144f00603d8a5010c8e0664411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:10:36 +0000

    Add package s2v-client/0.8.6/s2v_client-0.8.6-py3-none-any.whl

---
+++ packages/s2v-client/s2v_client-0.8.6-py3-none-any.whl/s2v/client/__init__.py
@@ -0,0 +1,4 @@
+from s2v.client.cli import main
+from s2v.client.lib import S2VClient, ValidationFailure, ValidationResult, ValidationSuccess
+
+__all__ = ["main", "S2VClient", "ValidationFailure", "ValidationResult", "ValidationSuccess"]
