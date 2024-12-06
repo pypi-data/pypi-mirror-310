bff3866c203610567ff5501cfa5f4134b6146ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:54 +0000

    Add package xpip.mirror/0.2.2/xpip.mirror-0.2.2-py2.py3-none-any.whl

---
+++ packages/xpip.mirror/xpip.mirror-0.2.2-py2.py3-none-any.whl/xpip_mirror/attribute.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from urllib.parse import urljoin
+
+__project__ = "xpip.mirror"
+__version__ = "0.2.2"
+__description__ = "pip mirror management"
+__url_home__ = "https://github.com/bondbox/xpip/"
+__url_code__ = __url_home__
+__url_docs__ = __url_home__
+__url_bugs__ = urljoin(__url_home__, "issues")
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
