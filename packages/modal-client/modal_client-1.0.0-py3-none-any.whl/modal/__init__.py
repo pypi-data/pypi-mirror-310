1b8fb861307a0c6a2c97e9422d556b7f9abcbbb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:07:53 +0000

    Add package modal-client/1.0.0/modal_client-1.0.0-py3-none-any.whl

---
+++ packages/modal-client/modal_client-1.0.0-py3-none-any.whl/modal/__init__.py
@@ -0,0 +1,7 @@
+error = '''
+######################################################################
+# The legacy `modal-client` PyPI package is no longer being updated. #
+# Please install the `modal` package instead (`pip install modal`).  #
+######################################################################
+'''
+raise Exception(error)
