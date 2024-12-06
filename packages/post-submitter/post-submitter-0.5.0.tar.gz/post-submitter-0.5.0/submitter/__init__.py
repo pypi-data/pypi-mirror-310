fa33013c99d2dc8744ffd7f72048e6cdc79dfdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:00 +0000

    Add package post-submitter/0.5.0/post-submitter-0.5.0.tar.gz

---
+++ packages/post-submitter/post-submitter-0.5.0.tar.gz/post-submitter-0.5.0/submitter/__init__.py
@@ -0,0 +1,34 @@
+from .client import ApiException, Client, OpenAPI, Result, Session, basic_auth
+from .model import (
+    Blog,
+    BlogFilter,
+    BlogQuery,
+    Filter,
+    PatchBody,
+    RequestLog,
+    Role,
+    Task,
+    Test,
+    Tests,
+    User,
+)
+
+__all__ = [
+    "ApiException",
+    "Blog",
+    "BlogFilter",
+    "BlogQuery",
+    "Client",
+    "Filter",
+    "OpenAPI",
+    "PatchBody",
+    "RequestLog",
+    "Result",
+    "Role",
+    "Session",
+    "Task",
+    "Test",
+    "Tests",
+    "User",
+    "basic_auth",
+]
