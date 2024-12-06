30ba1c6e9cdf0922797ee179784ad86a18298fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:12 +0000

    Add package message-formatter/0.1.0/message_formatter-0.1.0.tar.gz

---
+++ packages/message-formatter/message_formatter-0.1.0.tar.gz/message_formatter-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='message_formatter',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A lightweight package for formatting and serializing messages.',
+    author='mani',
+    author_email='mani.dasari3027@gmail.com',
+    url='https://github.com/manidasari27/message_formatter',
+    install_requires=[],
+)
