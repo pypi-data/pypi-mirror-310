77d06d4501d23c9eef6de933de2a0aa729ea1785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:10 +0000

    Add package asifjamali/0.0.1/asifjamali-0.0.1.tar.gz

---
+++ packages/asifjamali/asifjamali-0.0.1.tar.gz/asifjamali-0.0.1/README.md
@@ -0,0 +1,22 @@
+# MusibaT #
+
+
+# What is this repository for? #
+
+* Learning Perpose
+* Version 18.9.0
+* [Learn iSsues](https://bitbucket.org/shahzain83/musibat/issues)
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contact Us ###
+
+* musibat_bots
+* +923032939576
---
+++ packages/asifjamali/asifjamali-0.0.1.tar.gz/asifjamali-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='asifjamali',
+    version='0.0.1',
+    author='Shahzain Khan',
+    author_email='technomusibat@hotmail.com',
+    description='Team Musibat Bots',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://bitbucket.org/shahzain83/musibat',
+    project_urls = {
+        "Bug Tracker": "https://bitbucket.org/shahzain83/musibat/issues"
+    },
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+)
