4a6c2eead0669f4a1a0ba86b3b0e0a743bee5f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:26:30 +0000

    Add package trolo/0.1.0.dev2/trolo-0.1.0.dev2-py3-none-any.whl

---
+++ packages/trolo/trolo-0.1.0.dev2-py3-none-any.whl/trolo/models/dfine/maps.py
@@ -0,0 +1,42 @@
+MODEL_CONFIG_MAP = {
+    # Base model names (short aliases)
+    "dfine-n": "dfine/dfine_hgnetv2_n_coco.yml",
+    "dfine-s": "dfine/dfine_hgnetv2_s_coco.yml", 
+    "dfine-m": "dfine/dfine_hgnetv2_m_coco.yml",
+    "dfine-l": "dfine/dfine_hgnetv2_l_coco.yml",
+    "dfine-x": "dfine/dfine_hgnetv2_x_coco.yml",
+
+    # Full config file names also supported
+    "dfine_hgnetv2_n_coco.yml": "dfine/dfine_hgnetv2_n_coco.yml",
+    "dfine_hgnetv2_s_coco.yml": "dfine/dfine_hgnetv2_s_coco.yml",
+    "dfine_hgnetv2_m_coco.yml": "dfine/dfine_hgnetv2_m_coco.yml", 
+    "dfine_hgnetv2_l_coco.yml": "dfine/dfine_hgnetv2_l_coco.yml",
+    "dfine_hgnetv2_x_coco.yml": "dfine/dfine_hgnetv2_x_coco.yml",
+
+    # CrowdHuman variants
+    "dfine_hgnetv2_n_ch.yml": "dfine/crowdhuman/dfine_hgnetv2_n_ch.yml",
+    "dfine_hgnetv2_s_ch.yml": "dfine/crowdhuman/dfine_hgnetv2_s_ch.yml",
+    "dfine_hgnetv2_m_ch.yml": "dfine/crowdhuman/dfine_hgnetv2_m_ch.yml",
+    "dfine_hgnetv2_x_ch.yml": "dfine/crowdhuman/dfine_hgnetv2_x_ch.yml",
+    "dfine_n_coco_dummy.yml": "dfine/dfine_n_coco_dummy.yml",
+
+    # Objects365 variants  
+    "dfine_hgnetv2_n_obj365.yml": "dfine/objects365/dfine_hgnetv2_n_obj365.yml",
+    "dfine_hgnetv2_m_obj365.yml": "dfine/objects365/dfine_hgnetv2_m_obj365.yml",
+    "dfine_hgnetv2_l_obj365.yml": "dfine/objects365/dfine_hgnetv2_l_obj365.yml",
+    "dfine_hgnetv2_x_obj365.yml": "dfine/objects365/dfine_hgnetv2_x_obj365.yml",
+
+    # Custom dataset variants
+    "dfine_hgnetv2_n_custom.yml": "dfine/custom/dfine_hgnetv2_n_custom.yml",
+    "dfine_hgnetv2_s_custom.yml": "dfine/custom/dfine_hgnetv2_s_custom.yml", 
+    "dfine_hgnetv2_m_custom.yml": "dfine/custom/dfine_hgnetv2_m_custom.yml",
+    "dfine_hgnetv2_l_custom.yml": "dfine/custom/dfine_hgnetv2_l_custom.yml",
+    "dfine_hgnetv2_x_custom.yml": "dfine/custom/dfine_hgnetv2_x_custom.yml",
+
+    # .pth file names also supported
+    "dfine_n_coco.pth": "dfine/dfine_hgnetv2_n_coco.yml",
+    "dfine_s_coco.pth": "dfine/dfine_hgnetv2_s_coco.yml",
+    "dfine_m_coco.pth": "dfine/dfine_hgnetv2_m_coco.yml",
+    "dfine_l_coco.pth": "dfine/dfine_hgnetv2_l_coco.yml",
+    "dfine_x_coco.pth": "dfine/dfine_hgnetv2_x_coco.yml",
+}
