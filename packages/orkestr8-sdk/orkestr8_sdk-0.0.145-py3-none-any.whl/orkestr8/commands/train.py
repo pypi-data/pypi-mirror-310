adffe9f7b9b10a1dca88c526667674f9f771154b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:13 +0000

    Add package orkestr8-sdk/0.0.145/orkestr8_sdk-0.0.145-py3-none-any.whl

---
+++ packages/orkestr8-sdk/orkestr8_sdk-0.0.145-py3-none-any.whl/orkestr8/commands/train.py
@@ -0,0 +1,37 @@
+import importlib
+import os
+from dataclasses import dataclass
+from multiprocessing import Process
+from pathlib import Path
+
+from .base import Command
+
+
+def _get_pid_save_location() -> Path:
+    base_runs_dir = Path("~/runs")
+    os.makedirs(str(base_runs_dir), exist_ok=True)
+    return base_runs_dir
+
+
+@dataclass
+class TrainArgs:
+    model_module: str
+
+
+class TrainCommand(Command[TrainArgs]):
+    @staticmethod
+    def parse(args) -> TrainArgs:
+        return TrainArgs(args.model_module)
+
+    @staticmethod
+    def _run(func):
+        child_id = os.getpid()
+        with open(str(_get_pid_save_location() / "run_id.txt"), "w") as f:
+            f.write(f"PID: {child_id}")
+        func()
+
+    def run(self):
+        """Imports model training module and invokes 'train' function"""
+        m = importlib.import_module(self.args.model_module)
+        p = Process(target=self._run, args=(m.train,))
+        p.start()
