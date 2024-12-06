2d4eaebfa14403c808c25d222e35d4a1e9037ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:23 +0000

    Add package ugraph/0.0.421/ugraph-0.0.421.tar.gz

---
+++ packages/ugraph/ugraph-0.0.421.tar.gz/ugraph-0.0.421/src/ugraph/_abc/_link.py
@@ -0,0 +1,23 @@
+from abc import ABC
+from dataclasses import dataclass
+from enum import IntEnum, unique
+from typing import NewType
+
+from ugraph._abc._node import NodeId
+
+EndNodeIdPair = NewType("EndNodeIdPair", tuple[NodeId, NodeId])
+
+
+@unique
+class BaseLinkType(IntEnum):
+    pass
+
+
+@dataclass(frozen=True, slots=True)
+class LinkABC(ABC):
+    link_type: BaseLinkType
+
+
+@unique
+class SpeedCategory(IntEnum):
+    pass
