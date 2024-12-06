92d794ac589c87f5553a3a89639be566294da4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:21 +0000

    Add package brainiac-model/0.1.0/brainiac_model-0.1.0.tar.gz

---
+++ packages/brainiac-model/brainiac_model-0.1.0.tar.gz/brainiac_model-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="brainiac-model",
+    version="0.1.0",
+    author="Divyanshu",
+    description="A 3D ResNet50 model for brain MRI analysis",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://huggingface.co/Divytak/brainiac",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.9.0",
+        "transformers>=4.20.0",
+        "monai>=1.0.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
