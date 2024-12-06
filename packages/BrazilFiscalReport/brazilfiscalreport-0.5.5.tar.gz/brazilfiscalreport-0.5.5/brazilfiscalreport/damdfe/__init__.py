044b4792a6fc756a9a88236d0e25c955bc2e0d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:28 +0000

    Add package BrazilFiscalReport/0.5.5/brazilfiscalreport-0.5.5.tar.gz

---
+++ packages/BrazilFiscalReport/brazilfiscalreport-0.5.5.tar.gz/brazilfiscalreport-0.5.5/brazilfiscalreport/damdfe/__init__.py
@@ -0,0 +1,10 @@
+from .config import DamdfeConfig, DecimalConfig, FontType, Margins
+from .damdfe import Damdfe
+
+__all__ = [
+    "Damdfe",
+    "DamdfeConfig",
+    "DecimalConfig",
+    "FontType",
+    "Margins",
+]
