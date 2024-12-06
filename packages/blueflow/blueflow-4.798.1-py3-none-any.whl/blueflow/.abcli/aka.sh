54aa4612d9ebfe94fa3ac91c6680c5bf11ebe73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:31 +0000

    Add package blueflow/4.798.1/blueflow-4.798.1-py3-none-any.whl

---
+++ packages/blueflow/blueflow-4.798.1-py3-none-any.whl/blueflow/.abcli/aka.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+function nbs() {
+    notebooks_and_scripts "$@"
+}
+
+function notebooks() {
+    abcli_notebooks "$@"
+}
+
+function jupyter-notebook() {
+    abcli_notebooks "$@"
+}
