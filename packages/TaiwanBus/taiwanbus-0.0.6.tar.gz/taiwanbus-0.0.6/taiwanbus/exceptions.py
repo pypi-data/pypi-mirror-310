0e15eb5d9fba25ba086a6c11835ee4b045044594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:19 +0000

    Add package TaiwanBus/0.0.6/taiwanbus-0.0.6.tar.gz

---
+++ packages/TaiwanBus/taiwanbus-0.0.6.tar.gz/taiwanbus-0.0.6/taiwanbus/exceptions.py
@@ -0,0 +1,18 @@
+# My code is shit.
+# Custom exceptions for TaiwanBus.
+
+
+class DatabaseNotFoundError(Exception):
+    pass
+
+
+class RouteNotFoundError(Exception):
+    pass
+
+
+class StopNotFoundError(Exception):
+    pass
+
+
+class UnsupportedDatabaseError(Exception):
+    pass
