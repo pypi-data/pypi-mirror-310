3ca365a64850af4567821eda159e7704736b66c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:04:56 +0000

    Add package ataraxis-time/3.0.0/ataraxis_time-3.0.0-cp312-abi3-win_amd64.whl

---
+++ packages/ataraxis-time/ataraxis_time-3.0.0-cp312-abi3-win_amd64.whl/ataraxis_time/precision_timer/__init__.pyi
@@ -0,0 +1,4 @@
+from .timer_class import PrecisionTimer as PrecisionTimer
+from .timer_benchmark import benchmark as benchmark
+
+__all__ = ["PrecisionTimer", "benchmark"]
---
+++ packages/ataraxis-time/ataraxis_time-3.0.0-cp312-abi3-win_amd64.whl/ataraxis_time/time_helpers/__init__.pyi
@@ -0,0 +1,7 @@
+from .helper_functions import (
+    convert_time as convert_time,
+    get_timestamp as get_timestamp,
+    extract_timestamp_from_bytes as extract_timestamp_from_bytes,
+)
+
+__all__ = ["convert_time", "get_timestamp", "extract_timestamp_from_bytes"]
