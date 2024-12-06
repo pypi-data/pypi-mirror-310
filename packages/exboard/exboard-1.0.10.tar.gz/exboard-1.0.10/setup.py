4c77e543efc96e5d9a02c6374d9112e9d3e62237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:35 +0000

    Add package exboard/1.0.10/exboard-1.0.10.tar.gz

---
+++ packages/exboard/exboard-1.0.10.tar.gz/exboard-1.0.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='exboard',
+    version='1.0.10',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+    ],
+)
