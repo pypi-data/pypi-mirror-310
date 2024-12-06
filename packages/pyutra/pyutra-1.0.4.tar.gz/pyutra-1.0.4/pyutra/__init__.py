ecec6c20095e62a3883d3683e15a3ee933114b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:13 +0000

    Add package pyutra/1.0.4/pyutra-1.0.4.tar.gz

---
+++ packages/pyutra/pyutra-1.0.4.tar.gz/pyutra-1.0.4/pyutra/__init__.py
@@ -0,0 +1,19 @@
+# General Class crap
+from pyutra import ColorText, JsonConfig, CoolDebugging, Extras, pastelib, discord
+
+# Json Library
+from pyutra.JsonConfig import JsonConfig, EasyJsonConfig
+
+# Color Text Library
+from pyutra.ColorText import ColorText
+
+# Extras
+from pyutra.Extras import Extras
+
+# Discord Library
+from pyutra.discord import Webhooks
+
+# Pastebin Library (Ported): https://github.com/thediamondog/pastebinlib
+from pyutra.pastelib.Pastelib import Pastelib
+from pyutra.pastelib.User import User
+from pyutra.pastelib import User, Pastelib
