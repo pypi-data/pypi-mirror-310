4eb4ed142fdb73801b5232a7dfba17fcc5cfa5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:49 +0000

    Add package nonebot-plugin-fishspeech-tts/0.3.5/nonebot_plugin_fishspeech_tts-0.3.5.tar.gz

---
+++ packages/nonebot-plugin-fishspeech-tts/nonebot_plugin_fishspeech_tts-0.3.5.tar.gz/nonebot_plugin_fishspeech_tts-0.3.5/nonebot_plugin_fishspeech_tts/config.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import Optional, Literal
+from nonebot import get_plugin_config
+
+"""
+用于定义配置文件类, 用于配置插件参数
+"""
+
+
+class Config(BaseModel):
+    # 基础配置
+    tts_is_online: bool = True
+    tts_chunk_length: Literal["normal", "short", "long"] = "normal"
+    tts_audio_path: str = "./data/参考音频"
+    tts_prefix: Optional[str] = None
+
+    # 区分配置
+    online_authorization: Optional[str] = "xxxxx"
+    online_model_first: bool = True
+    # 设置代理地址
+    online_api_proxy: Optional[str] = None
+
+    offline_api_url: str = "http://127.0.0.1:8080"
+
+
+config = get_plugin_config(Config)
