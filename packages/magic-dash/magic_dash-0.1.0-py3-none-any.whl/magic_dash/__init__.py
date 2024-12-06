a74e87ee167a1040d9fab80b0640ea7c14a27237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:06:09 +0000

    Add package magic-dash/0.1.0/magic_dash-0.1.0-py3-none-any.whl

---
+++ packages/magic-dash/magic_dash-0.1.0-py3-none-any.whl/magic_dash/__init__.py
@@ -0,0 +1,51 @@
+import os
+import click
+import shutil
+
+
+@click.group(name="magic-dash")
+def magic_dash():
+    """magic-dash命令行工具"""
+
+    pass
+
+
+@click.command(name="list")
+def _list():
+    """列出当前可生成的全部Dash应用项目模板"""
+    click.echo("内置Dash应用项目模板：\n")
+    # 为magic-dash输出添加高亮颜色
+    click.echo(click.style("- magic-dash    基础多页面应用模板", fg="blue"))
+
+
+@click.command(name="create")
+@click.option("--name", required=True, type=click.STRING, help="Dash应用项目模板名称")
+@click.option("--path", type=click.STRING, default=".", help="项目生成目标路径")
+def _create(name, path):
+    """生成指定Dash应用项目模板到指定目录"""
+
+    # 复制项目模板到指定目录
+    shutil.copytree(
+        src=os.path.join(
+            # magic-dash实际位置
+            os.path.dirname(os.path.abspath(__file__)),
+            "templates",
+            name,
+        ),
+        dst=os.path.join(path, name),
+    )
+
+    click.echo(
+        click.style(
+            "已成功生成项目{}至目录{}".format(name, os.path.join(path, name)),
+            fg="green",
+        )
+    )
+
+
+# 令子命令生效
+magic_dash.add_command(_list)
+magic_dash.add_command(_create)
+
+if __name__ == "__main__":
+    magic_dash()
