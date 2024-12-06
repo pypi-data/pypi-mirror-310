4d8524656066dd84f261c689f728611339808fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:36 +0000

    Add package ezformer/0.0.1/ezformer-0.0.1.tar.gz

---
+++ packages/ezformer/ezformer-0.0.1.tar.gz/ezformer-0.0.1/ezformer/models.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+
+
+#Performer model code
+class Performer:
+    pass
+
+#Enformer model code
+class Enformer:
+
+    def __init__(self, tfhub_url):
+        self._model = hub.load(tfhub_url).model
+
+    def predict_on_batch(self, inputs):
+        predictions = self._model.predict_on_batch(inputs)
+        # return a dictionary of the predictions
+        return {k: v.numpy() for k, v in predictions.items()}
+
+    def contribution_input_grad(self, input_sequence, target_mask, output_head='human'):
+        input_sequence = input_sequence[tf.newaxis]
+
+        target_mask_mass = tf.reduce_sum(target_mask)
+        with tf.GradientTape() as tape:
+            tape.watch(input_sequence)
+            prediction = tf.reduce_sum(
+                target_mask[tf.newaxis] *
+                self._model.predict_on_batch(input_sequence)[output_head]) / target_mask_mass
+
+        input_grad = tape.gradient(prediction, input_sequence) * input_sequence
+        input_grad = tf.squeeze(input_grad, axis=0)
+        return tf.reduce_sum(input_grad, axis=-1)
