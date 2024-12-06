c7a3e4383d11c5c5efe0a9bef1f7946fd6449bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:38 +0000

    Add package msgraph-py/1.2.0/msgraph_py-1.2.0-py3-none-any.whl

---
+++ packages/msgraph-py/msgraph_py-1.2.0-py3-none-any.whl/msgraph/__init__.py
@@ -0,0 +1,28 @@
+import logging
+
+from .devices import (  # noqa: F401
+    delete_device,
+    get_device,
+    get_laps_password,
+    list_owned_devices,
+)
+from .groups import (  # noqa: F401
+    add_group_member,
+    get_group,
+    list_group_members,
+    remove_group_member,
+)
+from .identity import (  # noqa: F401
+    delete_auth_method,
+    get_signin,
+    get_user,
+    get_user_risk,
+    list_auth_methods,
+    reset_strong_auth,
+    revoke_refresh_tokens,
+)
+from .mail import (  # noqa: F401
+    send_mail,
+)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
