1257a58a11e8bb25f70ad723b6190ca7e2573ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:18:11 +0000

    Add package chelydra/0.0.8rc1732469059/chelydra-0.0.8rc1732469059.tar.gz

---
+++ packages/chelydra/chelydra-0.0.8rc1732469059.tar.gz/chelydra-0.0.8rc1732469059/tests/test_success.py
@@ -0,0 +1,9 @@
+from chelydra import backup
+
+BACKUP_FOLDER = "./backup"
+SOURCE_FOLDER = "./source_folder"
+TARGET_FOLDER = "./target_folder"
+
+# Not a real test, just setting this dummy test for validatinf pytest
+def test_success():
+    assert 1 == 1
