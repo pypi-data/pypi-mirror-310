534d9e10a25ac986c75fee8c4d99509ecba9981d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:09 +0000

    Add package mk1pass/0.0.3/mk1pass-0.0.3.tar.gz

---
+++ packages/mk1pass/mk1pass-0.0.3.tar.gz/mk1pass-0.0.3/mk1pass/CHECKSUM-SHA3-256.py
@@ -0,0 +1,6 @@
+'''
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/mk1pass_guru/head/public/script/mk1pass.py)= 714c2ce8267c5765e6e9c5ca775a42019820ac61abaad3f155e87d4c027323de
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/mk1pass_guru/head/public/script/printdv.py)= 04dfe92722c09eb6217a46e0d7807d7f16523ad6fb0c9817929c6069db1f46fd
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/mk1pass_guru/head/private/pypi/mk1pass/__init__.py)= 8790065240230bc3ff1544177625f795f21d26ad8cbe78bfdc8ea940f4b7d53c
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/mk1pass_guru/head/private/pypi/mk1pass/__main__.py)= c7788d251902160152633f061e85fe769e414a7307901134d641afa4e3c0a5ae
+'''
