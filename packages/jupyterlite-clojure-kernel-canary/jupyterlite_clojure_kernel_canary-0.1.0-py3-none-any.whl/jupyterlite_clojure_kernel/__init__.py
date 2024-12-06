51166286d21380dfbd78507637842840587a0a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:51 +0000

    Add package jupyterlite-clojure-kernel-canary/0.1.0/jupyterlite_clojure_kernel_canary-0.1.0-py3-none-any.whl

---
+++ packages/jupyterlite-clojure-kernel-canary/jupyterlite_clojure_kernel_canary-0.1.0-py3-none-any.whl/jupyterlite_clojure_kernel/__init__.py
@@ -0,0 +1,9 @@
+"""A JupyterLite kernel powered by Clojure."""
+
+from ._version import __version__
+
+__all__ = ["__version__", "_jupyter_labextension_paths"]
+
+
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": "jupyterlite-clojure-kernel"}]
