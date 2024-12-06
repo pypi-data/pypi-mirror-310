10d5eb06e497ae6cb8935ac6ee5ebaea74f769c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:39 +0000

    Add package swak/0.2.9/swak-0.2.9.tar.gz

---
+++ packages/swak/swak-0.2.9.tar.gz/swak-0.2.9/swak/pd/__init__.py
@@ -0,0 +1,27 @@
+"""Pandas utilities and partials of dataframe method calls.
+
+Parameters that are known at program start are used to initialize the classes
+so that, at runtime, dataframes can flow through a preconfigured processing
+pipe of callable objects.
+
+"""
+
+from .read import ParquetReader
+from .frame import (
+    AsType,
+    ColumnSelector,
+    ColumnsSelector,
+    ColumnMapper,
+    RowsSelector
+)
+
+__all__ = [
+    'ParquetReader',
+    'AsType',
+    'ColumnSelector',
+    'ColumnsSelector',
+    'ColumnMapper',
+    'RowsSelector'
+]
+
+# ToDo: Replicate for pola-rs in separate "po" subpackage!
