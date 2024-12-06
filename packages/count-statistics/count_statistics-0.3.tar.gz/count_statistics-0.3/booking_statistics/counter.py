ad01aef922740ba1b65db2c3acc5768a75b99854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:01 +0000

    Add package count-statistics/0.3/count_statistics-0.3.tar.gz

---
+++ packages/count-statistics/count_statistics-0.3.tar.gz/count_statistics-0.3/booking_statistics/counter.py
@@ -0,0 +1,9 @@
+from bookings.models import Booking
+
+class BookingCounter:
+    def __init__(self, user):
+        self.user = user
+
+    def count_bookings(self):
+        return Booking.objects.filter(user=self.user)
+        
