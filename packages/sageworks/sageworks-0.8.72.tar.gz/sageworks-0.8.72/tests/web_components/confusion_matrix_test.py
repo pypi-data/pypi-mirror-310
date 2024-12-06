6b8bdbdf676bebc59b52e4cb9849c97e038d7230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:18 +0000

    Add package sageworks/0.8.72/sageworks-0.8.72.tar.gz

---
+++ packages/sageworks/sageworks-0.8.72.tar.gz/sageworks-0.8.72/tests/web_components/confusion_matrix_test.py
@@ -0,0 +1,29 @@
+"""Tests for confusion_matrix web component"""
+
+# SageWorks Imports
+from sageworks.web_components.confusion_matrix import ConfusionMatrix
+from sageworks.api.model import Model
+
+
+def test_confusion_matrix():
+    """Test the ConfusionMatrix class"""
+    # Instantiate model
+    m = Model("wine-classification")
+    inference_run = "auto_inference"
+
+    # Instantiate the ConfusionMatrix class
+    cm = ConfusionMatrix()
+
+    # Generate the figure
+    fig = cm.update_properties(m, inference_run)
+
+    # Apply dark theme
+    fig.update_layout(template="plotly_dark")
+
+    # Show the figure
+    fig.show()
+
+
+if __name__ == "__main__":
+    # Run the tests
+    test_confusion_matrix()
