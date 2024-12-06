b9fb0b909d4a0c35dd03505cd9933f9f09ef049f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:06 +0000

    Add package py-ansi3/1.2.0/py_ansi3-1.2.0.tar.gz

---
+++ packages/py-ansi3/py_ansi3-1.2.0.tar.gz/py_ansi3-1.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-ansi3",
+    version="1.2.0",
+    packages=find_packages(),
+    url="https://github.com/mohamedlunar/PyANSI3",
+    author="MohamedLunar",
+    author_email="contact.mohamedlunardev@gmail.com",
+    description="pip install py-ansi3",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        # Your dependencies here
+    ],
+)
