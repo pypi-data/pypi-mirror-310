0b387b1f2e1f4c6780ca84a4dea323fd712da5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:05 +0000

    Add package neuronic/0.1.4/neuronic-0.1.4-py3-none-any.whl

---
+++ packages/neuronic/neuronic-0.1.4-py3-none-any.whl/neuronic/__init__.py
@@ -0,0 +1,7 @@
+"""Neuronic - AI-powered data transformation library"""
+
+__version__ = "0.1.4"
+
+from .core import Neuronic, OutputType, NeuronicError, APIKeyError, TransformationError
+
+__all__ = ["Neuronic", "OutputType", "NeuronicError", "APIKeyError", "TransformationError"] 
