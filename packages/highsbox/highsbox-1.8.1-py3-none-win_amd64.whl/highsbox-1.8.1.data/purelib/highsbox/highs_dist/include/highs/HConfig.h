db45d4de321cb328c09b74a686e54807337ab9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:34 +0000

    Add package highsbox/1.8.1/highsbox-1.8.1-py3-none-win_amd64.whl

---
+++ packages/highsbox/highsbox-1.8.1-py3-none-win_amd64.whl/highsbox-1.8.1.data/purelib/highsbox/highs_dist/include/highs/HConfig.h
@@ -0,0 +1,21 @@
+#ifndef HCONFIG_H_
+#define HCONFIG_H_
+
+#define FAST_BUILD
+/* #undef ZLIB_FOUND */
+/* #undef CUPDLP_CPU */
+#define CMAKE_BUILD_TYPE "Release"
+#define CMAKE_INSTALL_PREFIX "D:/a/highsbox/highsbox/HiGHS/highs_dist"
+/* #undef HIGHSINT64 */
+/* #undef HIGHS_NO_DEFAULT_THREADS */
+#define HIGHS_HAVE_MM_PAUSE
+/* #undef HIGHS_HAVE_BUILTIN_CLZ */
+#define HIGHS_HAVE_BITSCAN_REVERSE
+
+#define HIGHS_GITHASH "4a7f24a"
+#define HIGHS_VERSION_MAJOR 1
+#define HIGHS_VERSION_MINOR 8
+#define HIGHS_VERSION_PATCH 1
+#define HIGHS_DIR "D:/a/highsbox/highsbox/HiGHS"
+
+#endif /* HCONFIG_H_ */
