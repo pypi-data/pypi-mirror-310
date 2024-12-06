bec82b38f47d1a581af6f72b442b44ecf94bad7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:12:11 +0000

    Add package apiazure/1.0/apiazure-1.0.tar.gz

---
+++ packages/apiazure/apiazure-1.0.tar.gz/apiazure-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiazure',
+    version='1.0',
+    packages=find_packages(),
+    description='Libreria para uso de apis de azure',
+    author='Carlos Castillo',
+    author_email='ccastillo@ext.noatum.com',
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.12.3'
+)
