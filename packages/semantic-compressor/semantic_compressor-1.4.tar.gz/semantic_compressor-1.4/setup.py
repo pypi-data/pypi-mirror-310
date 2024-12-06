d3a7f5dca569caa55f3178d13340625786d66e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:20:32 +0000

    Add package semantic-compressor/1.4/semantic_compressor-1.4.tar.gz

---
+++ packages/semantic-compressor/semantic_compressor-1.4.tar.gz/semantic_compressor-1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='semantic_compressor',
+    version='1.4',
+    author='Carlo Moro',
+    author_email='cnmoro@gmail.com',
+    description="Semantic text compression",
+    packages=find_packages(),
+    package_data={
+        "compressor": ["resources/*"]
+    },
+    include_package_data=True,
+    install_requires=[
+        "numpy<2",
+        "nltk",
+        "scikit-learn",
+        "fasttext",
+        "onnxruntime",
+        "onnxruntime-extensions",
+        "pyspellchecker"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
