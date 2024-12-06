085f125c5f7a5c56d122cec8f9695637568d4de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:30 +0000

    Add package mfthree/8.20.7/mfthree-8.20.7.tar.gz

---
+++ packages/mfthree/mfthree-8.20.7.tar.gz/mfthree-8.20.7/three/MF/V3/Settings/RemoveVertices.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class RemoveVertices:
+    # Remove vertices.
+    def __init__(self, scans: List[int] = None):
+        # The scans indexes for which vertices are removed.
+        self.scans = scans
+
+
