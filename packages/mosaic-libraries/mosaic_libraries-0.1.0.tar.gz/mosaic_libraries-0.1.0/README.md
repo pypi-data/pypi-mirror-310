6dccaa167d2e465058135581301a9e5f8f86166f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:18 +0000

    Add package mosaic-libraries/0.1.0/mosaic_libraries-0.1.0.tar.gz

---
+++ packages/mosaic-libraries/mosaic_libraries-0.1.0.tar.gz/mosaic_libraries-0.1.0/README.md
@@ -0,0 +1,3 @@
+# mosaic-libraries
+
+When the internal logic for this package is implemented, more detail will be added.
---
+++ packages/mosaic-libraries/mosaic_libraries-0.1.0.tar.gz/mosaic_libraries-0.1.0/mosaic/__meta__.py
@@ -0,0 +1,5 @@
+from importlib import metadata
+
+__name__ = __package__ = "mosaic"
+__project__ = "mosaic-libraries"
+__version__ = metadata.version(__project__)
