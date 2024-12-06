e84dd650f590dc0b2d789b9332700ab93ecaa305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:05:58 +0000

    Add package llamator/1.0.2/llamator-1.0.2.tar.gz

---
+++ packages/llamator/llamator-1.0.2.tar.gz/llamator-1.0.2/src/llamator/attack_provider/attack_loader.py
@@ -0,0 +1,16 @@
+from ..attacks import (  # noqa
+    aim,
+    base64_injection,
+    complimentary_transition,
+    dan,
+    ethical_compliance,
+    harmful_behavior,
+    past_tense,
+    ru_dan,
+    ru_typoglycemia,
+    ru_ucar,
+    sycophancy,
+    translation,
+    typoglycemia,
+    ucar,
+)
