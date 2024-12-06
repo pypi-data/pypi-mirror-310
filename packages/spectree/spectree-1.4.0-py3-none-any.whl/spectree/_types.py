a1f002deb8f3c5397e7f1e43382d3ee246288ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:18:06 +0000

    Add package spectree/1.4.0/spectree-1.4.0-py3-none-any.whl

---
+++ packages/spectree/spectree-1.4.0-py3-none-any.whl/spectree/_types.py
@@ -0,0 +1,46 @@
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterator,
+    List,
+    Mapping,
+    Optional,
+    Sequence,
+    Type,
+    TypeVar,
+    Union,
+)
+
+from typing_extensions import Protocol
+
+from ._pydantic import BaseModel
+
+BaseModelSubclassType = TypeVar("BaseModelSubclassType", bound=BaseModel)
+ModelType = Type[BaseModelSubclassType]
+OptionalModelType = Optional[ModelType]
+NamingStrategy = Callable[[ModelType], str]
+NestedNamingStrategy = Callable[[str, str], str]
+
+
+class MultiDict(Protocol):
+    def get(self, key: str) -> Optional[str]:
+        pass
+
+    def getlist(self, key: str) -> List[str]:
+        pass
+
+    def __iter__(self) -> Iterator[str]:
+        pass
+
+
+class FunctionDecorator(Protocol):
+    resp: Any
+    tags: Sequence[Any]
+    security: Union[None, Dict, List[Any]]
+    deprecated: bool
+    path_parameter_descriptions: Optional[Mapping[str, str]]
+    _decorator: Any
+
+
+JsonType = Union[None, int, str, bool, List["JsonType"], Dict[str, "JsonType"]]
