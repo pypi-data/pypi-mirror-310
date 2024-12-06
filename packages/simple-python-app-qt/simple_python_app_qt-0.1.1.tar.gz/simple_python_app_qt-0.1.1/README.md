cf7f616cfc0831eb74213117faa45e7c8aeda23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:34:05 +0000

    Add package simple-python-app-qt/0.1.1/simple_python_app_qt-0.1.1.tar.gz

---
+++ packages/simple-python-app-qt/simple_python_app_qt-0.1.1.tar.gz/simple_python_app_qt-0.1.1/README.md
@@ -0,0 +1,17 @@
+[![Build status](https://github.com/twyleg/simple_python_app_qt/actions/workflows/tests.yaml/badge.svg)]()
+[![GitHub latest commit](https://badgen.net/github/last-commit/twyleg/simple_python_app_qt)](https://GitHub.com/twyleg/simple_python_app_qt/commit/)
+[![PyPI download month](https://img.shields.io/pypi/dm/simple-python-app-qt)](https://pypi.python.org/pypi/template-project-python-qt/)
+[![PyPi version](https://badgen.net/pypi/v/simple-python-app-qt/)](https://pypi.org/project/simple-python-app-qt)
+
+
+# simple_python_app_qt
+
+A small framework to simplify Python Qt applications. Based on [simple_python_app](https://github.com/twyleg/simple_python_app).
+
+## Dev dependencies
+
+* xvfb (to run Qt unit tests headless)
+
+## Examples
+
+All examples can be found in the [examples/](https://github.com/twyleg/simple_python_app/tree/master/examples) directory.
---
+++ packages/simple-python-app-qt/simple_python_app_qt-0.1.1.tar.gz/simple_python_app_qt-0.1.1/setup.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2024 twyleg
+import versioneer
+from pathlib import Path
+from setuptools import find_packages, setup
+
+
+def read(relative_filepath):
+    return open(Path(__file__).parent / relative_filepath).read()
+
+
+def read_long_description() -> str:
+    return read("README.md")
+
+
+# fmt: off
+setup(
+    name="simple_python_app_qt",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Torsten Wylegala",
+    author_email="mail@twyleg.de",
+    description="Small framework based on simple_python_app that simplifies Python Qt app creation.",
+    license="GPL 3.0",
+    keywords="framework boilerplate logging argparse config qt qml pyside",
+    url="https://github.com/twyleg/simple_python_app_qt",
+    packages=find_packages(),
+    long_description=read_long_description(),
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=[
+        "simple_python_app~=0.4.1",
+        "pyside6~=6.7.2"
+    ],
+    entry_points={},
+)
+# fmt: on
