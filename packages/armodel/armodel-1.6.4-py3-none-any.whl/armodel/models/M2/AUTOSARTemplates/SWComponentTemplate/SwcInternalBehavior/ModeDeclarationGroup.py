d39dbde5aed165d139b224a6318430f8867d7593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:34:25 +0000

    Add package armodel/1.6.4/armodel-1.6.4-py3-none-any.whl

---
+++ packages/armodel/armodel-1.6.4-py3-none-any.whl/armodel/models/M2/AUTOSARTemplates/SWComponentTemplate/SwcInternalBehavior/ModeDeclarationGroup.py
@@ -0,0 +1,38 @@
+from .....M2.AUTOSARTemplates.SWComponentTemplate.RPTScenario import ModeAccessPointIdent
+from .....M2.AUTOSARTemplates.SWComponentTemplate.Components.InstanceRefs import PModeGroupInAtomicSwcInstanceRef, RModeGroupInAtomicSWCInstanceRef
+from .....M2.AUTOSARTemplates.SWComponentTemplate.SwcInternalBehavior.AccessCount import AbstractAccessPoint
+from .....M2.AUTOSARTemplates.GenericStructure.GeneralTemplateClasses.ArObject import ARObject
+
+class ModeAccessPoint(ARObject):
+    def __init__(self):
+        super().__init__()
+
+        self.ident = None                   # type: ModeAccessPointIdent
+        self.modeGroupIRef = None           # type: RModeGroupInAtomicSWCInstanceRef
+
+    def getIdent(self):
+        return self.ident
+
+    def setIdent(self, value):
+        self.ident = value
+        return self
+
+    def getModeGroupIRef(self):
+        return self.modeGroupIRef
+
+    def setModeGroupIRef(self, value):
+        self.modeGroupIRef = value
+        return self
+
+class ModeSwitchPoint(AbstractAccessPoint):
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+
+        self.modeGroupIRef = None           # type: PModeGroupInAtomicSwcInstanceRef
+
+    def getModeGroupIRef(self):
+        return self.modeGroupIRef
+
+    def setModeGroupIRef(self, value):
+        self.modeGroupIRef = value
+        return self
