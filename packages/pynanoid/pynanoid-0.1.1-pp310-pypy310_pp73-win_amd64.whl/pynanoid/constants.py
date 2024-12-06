381b131d2adb01c089b7cdc91c2d7b7062cc3d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:43 +0000

    Add package pynanoid/0.1.1/pynanoid-0.1.1-pp310-pypy310_pp73-win_amd64.whl

---
+++ packages/pynanoid/pynanoid-0.1.1-pp310-pypy310_pp73-win_amd64.whl/pynanoid/constants.py
@@ -0,0 +1,5 @@
+#: The default alphabet used for generating nanoid strings.
+ALPHABET = "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+#: The default size of the generated nanoid strings.
+SIZE = 21
