b822170722b504c454098b52440c66ad2c9fdb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:38 +0000

    Add package chinus-tools/1.0.7/chinus_tools-1.0.7-py3-none-any.whl

---
+++ packages/chinus-tools/chinus_tools-1.0.7-py3-none-any.whl/chinus_tools/git/status.py
@@ -0,0 +1,20 @@
+import subprocess
+
+
+def get_modified_items() -> bool:
+    """
+    Git 상태를 확인하여 변경 사항의 존재 유무에 따라 True/False를 반환합니다
+    """
+    try:
+        # 'git status --porcelain'으로 변경 사항이 있는지 확인
+        result = subprocess.run(['git', 'status', '--porcelain'], capture_output=True, text=True, check=True)
+        # 출력 결과가 있으면 True (변경 사항 있음), 없으면 False (변경 사항 없음)
+        return bool(result.stdout.strip())
+    except subprocess.CalledProcessError as e:
+        # Git 명령 실행 실패 시 예외 처리
+        print(f"Git 명령 실행 중 오류 발생: {e}")
+        return False
+    except FileNotFoundError:
+        # Git 명령이 없는 경우 예외 처리
+        print("Git이 설치되어 있지 않거나 경로에 없습니다.")
+        return False
---
+++ packages/chinus-tools/chinus_tools-1.0.7-py3-none-any.whl/chinus_tools/paths/get/absolute_path.py
@@ -0,0 +1,8 @@
+import os
+
+from chinus_tools.paths.get.project_root import get_project_root
+from pathlib import Path
+
+
+def get_absolute_path(project_relative_path: str, root_identifiers: set = None) -> str:
+    return str(Path(os.path.join(get_project_root(root_identifiers), project_relative_path)))
