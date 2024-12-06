facdb59bcf2e436efaa4e602f9c815b8d8a0b77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:31:31 +0000

    Add package NLP-NLTK/0.3.1/nlp_nltk-0.3.1.tar.gz

---
+++ packages/NLP-NLTK/nlp_nltk-0.3.1.tar.gz/nlp_nltk-0.3.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NLP_NLTK',  # Your library name
+    version='0.3.1',  # Initial version
+    author='Naxio Technology',  # Your name
+    author_email='naxiotech.2024@gmail.com',  # Your email
+    description='A library for NLP tasks using NLTK',  # Short description
+    long_description=open('README.md').read(),  # Detailed description from README.md
+    long_description_content_type='text/markdown',
+    url='',  # GitHub repository link
+    packages=find_packages(),  # Automatically find and include packages
+    install_requires=[  # Dependencies
+        'nltk',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
