5d3f4ecd272f54f1e817f3724ee7d037c4ef8211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:32 +0000

    Add package open-webui/0.4.3/open_webui-0.4.3-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.4.3-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/clone.BOiTFlx8.js
@@ -0,0 +1,2 @@
+import{b as r}from"./graph.DmRaynjm.js";var e=4;function a(o){return r(o,e)}export{a as c};
+//# sourceMappingURL=clone.BOiTFlx8.js.map
---
+++ packages/open-webui/open_webui-0.4.3-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.B9r_yWOG.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.BSwnwacT.js";export{t as start};
+//# sourceMappingURL=start.B9r_yWOG.js.map
