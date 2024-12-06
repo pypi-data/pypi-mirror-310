d866b6d644edb6632d3d3e342063fc2293a49a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:21 +0000

    Add package optimal-regressors/0.3.0/optimal_regressors-0.3.0.tar.gz

---
+++ packages/optimal-regressors/optimal_regressors-0.3.0.tar.gz/optimal_regressors-0.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open("readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="optimal-regressors",  
+    version="0.3.0",  
+    author="Rehan Taneja",
+    author_email="rehan.taneja4321@gmail.com",
+    description="A Python library to find optimal configurations for regressors",
+    long_description=long_description,  
+    long_description_content_type="text/markdown",  
+    url="https://github.com/RehanTaneja/OptimalRegressors.git",  
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "scikit-learn",  
+        "xgboost",
+    ],
+)
