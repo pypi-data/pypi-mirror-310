09b516f6db61f5edf7c90c08d6ffe4c5925f2c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:07:52 +0000

    Add package spotipyio/1.0.0/spotipyio-1.0.0.tar.gz

---
+++ packages/spotipyio/spotipyio-1.0.0.tar.gz/spotipyio-1.0.0/spotipyio/testing/components/chapters/chapters_info_test_component.py
@@ -0,0 +1,19 @@
+from spotipyio.logic.consts.spotify_consts import CHAPTERS
+from spotipyio.logic.consts.typing_consts import Json
+from spotipyio.logic.internal_models import ChunkSize
+from spotipyio.testing.infra.base_chunks_test_component import BaseChunksTestComponent
+from spotipyio.testing.spotify_mock_factory import SpotifyMockFactory
+
+
+class ChaptersInfoTestComponent(BaseChunksTestComponent):
+    @property
+    def _route(self) -> str:
+        return f"/{CHAPTERS}"
+
+    @property
+    def _chunk_size(self) -> ChunkSize:
+        return ChunkSize.CHAPTERS
+
+    @staticmethod
+    def _random_valid_response() -> Json:
+        return SpotifyMockFactory.several_chapters()
