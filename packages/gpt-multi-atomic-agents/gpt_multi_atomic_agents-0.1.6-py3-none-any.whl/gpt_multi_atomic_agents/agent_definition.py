b50a358c5e467601e269c19c1dbae139e448b290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:08:57 +0000

    Add package gpt-multi-atomic-agents/0.1.6/gpt_multi_atomic_agents-0.1.6-py3-none-any.whl

---
+++ packages/gpt-multi-atomic-agents/gpt_multi_atomic_agents-0.1.6-py3-none-any.whl/gpt_multi_atomic_agents/agent_definition.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from atomic_agents.agents.base_agent import (
+    BaseIOSchema,
+)
+from pydantic import Field
+
+from . import util_output
+from .blackboard import Blackboard
+from .config import Config
+
+from .functions_dto import FunctionAgentInputSchema, FunctionAgentOutputSchema, FunctionSpecSchema
+
+@dataclass
+class AgentDefinition:
+    """
+    Defines one agent. NOT for direct use with LLM.
+    """
+    agent_name: str
+    description: str
+    accepted_functions: list[FunctionSpecSchema]
+    input_schema: type[FunctionAgentInputSchema]
+    initial_input: FunctionAgentInputSchema
+    output_schema: type[FunctionAgentOutputSchema]
+    topics: list[str] = Field(description="This agent ONLY generates if user mentioned one of these topics")
+    # TODO: could add custom prompt/prompt-extension if needed
+
+    def build_input(self, user_input: str, blackboard: Blackboard, config: Config) -> BaseIOSchema:
+        initial_input = self.initial_input
+        initial_input.user_input = user_input
+
+        initial_input.previously_generated_functions = blackboard.get_generated_functions_matching(self.get_accepted_function_names())
+        util_output.print_debug(f"[{self.agent_name}] Previously generated funs: {initial_input.previously_generated_functions}", config)
+
+        return initial_input
+
+    def get_accepted_function_names(self) -> list[str]:
+        return [f.function_name for f in self.accepted_functions]
