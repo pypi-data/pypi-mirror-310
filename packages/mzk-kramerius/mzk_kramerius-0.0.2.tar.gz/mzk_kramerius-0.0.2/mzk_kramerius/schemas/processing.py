c6df5054be7dcb5d43555f574c350acf05f8fcdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:23 +0000

    Add package mzk-kramerius/0.0.2/mzk_kramerius-0.0.2.tar.gz

---
+++ packages/mzk-kramerius/mzk_kramerius-0.0.2.tar.gz/mzk_kramerius-0.0.2/mzk_kramerius/schemas/processing.py
@@ -0,0 +1,69 @@
+from pydantic import BaseModel
+from datetime import datetime
+from ..datatypes import (
+    ProcessType,
+    ProcessState,
+    PathType,
+    IndexationType,
+    License,
+    Pid,
+)
+
+
+class EmptyParams(BaseModel):
+    pass
+
+
+class ImportParams(BaseModel):
+    inputDataDir: str
+    startIndexer: bool
+    license: License
+    collections: str
+    pathtype: PathType
+
+
+class ImportMetsParams(ImportParams):
+    policy: str
+    useIIPServer: bool
+
+
+class IndexParams(BaseModel):
+    type: IndexationType
+    pid: Pid
+    ignoreInconsistentObjects: bool
+
+
+class AddLicenseParams(BaseModel):
+    pid: Pid
+    license: License
+
+
+class RemoveLicenseParams(AddLicenseParams):
+    pass
+
+
+class DeleteTreeParams(BaseModel):
+    pid: Pid
+    ignoreIncosistencies: bool
+
+
+ProcessParams = (
+    ImportParams
+    | ImportMetsParams
+    | IndexParams
+    | AddLicenseParams
+    | RemoveLicenseParams
+    | DeleteTreeParams
+    | EmptyParams
+)
+
+
+class KrameriusProcess(BaseModel):
+    uuid: str
+    name: str
+    defid: ProcessType
+    state: ProcessState
+    planned: datetime
+    started: datetime
+    finished: datetime
+    params: ProcessParams
