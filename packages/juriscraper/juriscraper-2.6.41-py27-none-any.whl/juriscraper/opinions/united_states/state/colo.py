87d89dcd9fe5c598fbee0297e06cf3081e07b0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:31:55 +0000

    Add package juriscraper/2.6.41/juriscraper-2.6.41-py27-none-any.whl

---
+++ packages/juriscraper/juriscraper-2.6.41-py27-none-any.whl/juriscraper/opinions/united_states/state/colo.py
@@ -0,0 +1,141 @@
+"""Scraper for Colorado Supreme Court
+CourtID: colo
+Court Short Name: Colo.
+Author: Philip Ardery
+Reviewer: mlr
+Date created: 2016-06-03
+History:
+    - 2022-01-31: Updated by William E. Palin
+    - 2023-01-05: Updated by WEP
+    - 2023-11-19: Drop Selenium by WEP
+    - 2023-12-20: Updated with citations, judges and summaries, Palin
+    - 2024-07-04: Update to new site, grossir
+"""
+
+from datetime import date, datetime
+from typing import Tuple
+from urllib.parse import urlencode
+
+from juriscraper.AbstractSite import logger
+from juriscraper.OpinionSiteLinear import OpinionSiteLinear
+
+
+class Site(OpinionSiteLinear):
+    base_url = "https://research.coloradojudicial.gov/search.json"
+    detail_url = "https://research.coloradojudicial.gov/vid/{}.json?include=abstract%2Cparent%2Cmeta%2Cformats%2Cchildren%2Cproperties_with_ids%2Clibrary%2Csource&fat=1&locale=en&hide_ct6=true&t={}"
+    days_interval = 30
+    first_opinion_date = datetime(2010, 1, 1)
+    api_court_code = "14024_01"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.court_id = self.__module__
+        self.params = {
+            "product_id": "WW",
+            "jurisdiction": "US",
+            "content_type": "2",
+            "court": self.api_court_code,
+            "bypass_rabl": "true",
+            "include": "parent,abstract,snippet,properties_with_ids",
+            "per_page": "30",  # Server breaks down when per_page=500, returns 503
+            "page": "1",
+            "sort": "date",
+            "include_local_exclusive": "true",
+            "cbm": "6.0|361.0|5.0|9.0|4.0|2.0=0.01|400.0|1.0|0.001|1.5|0.2",
+            "locale": "en",
+            "hide_ct6": "true",
+            "t": str(datetime.now().timestamp())[:10],
+            "type": "document",
+        }
+        self.url = f"{self.base_url}?{urlencode(self.params)}"
+
+        # Request won't work without some of these X- headers
+        self.request["headers"].update(
+            {
+                "X-Requested-With": "XMLHttpRequest",
+                "X-Root-Account-Email": "colorado@vlex.com",
+                "X-User-Email": "colorado@vlex.com",
+                "X-Webapp-Seed": "9887408",
+            }
+        )
+        self.expected_content_types = ["text/html"]
+        self.make_backscrape_iterable(kwargs)
+
+    def _process_html(self) -> None:
+        search_json = self.html
+        logger.info(
+            "Number of results %s; %s in page",
+            search_json["count"],
+            len(search_json["results"]),
+        )
+
+        for result in search_json["results"]:
+            timestamp = str(datetime.now().timestamp())[:10]
+            url = self.detail_url.format(result["id"], timestamp)
+
+            if self.test_mode_enabled():
+                # we have manually nested detail JSONs to
+                # to be able to have a test file
+                detail_json = result["detail_json"]
+            else:
+                # Full case name and docket number are only available
+                # on the detail page
+                self._request_url_get(url)
+                detail_json = self.request["response"].json()
+
+            # Reset variables to prevent sticking previous values
+            # when a value is missing
+            docket_number, case_name_full, date_filed = "", "", ""
+
+            # Example of parallel citation:
+            # https://research.coloradojudicial.gov/vid/907372624
+            citation, parallel_citation = "", ""
+            for p in detail_json["properties"]:
+                label = p["property"]["label"]
+                if label == "Docket Number":
+                    docket_number = p["values"][0]
+                elif label == "Parties":
+                    case_name_full = p["values"][0]
+                elif label == "Decision Date":
+                    # Note that json['published_at'] is not the date_filed
+                    date_filed = p["values"][0]
+                elif label == "Citation":
+                    citation = p["values"][0]
+                    if len(p["values"]) > 1:
+                        parallel_citation = p["values"][1]
+
+            case = {
+                "date": date_filed,
+                "docket": docket_number,
+                "name": case_name_full,
+                "url": f"{detail_json['public_url']}/content",
+                "status": "Published" if citation else "Unknown",
+                "citation": citation,
+                "parallel_citation": parallel_citation,
+            }
+
+            self.cases.append(case)
+
+    def _download_backwards(self, dates: Tuple[date]) -> None:
+        """Make custom date range request
+
+        :param dates: (start_date, end_date) tuple
+        :return None
+        """
+        logger.info("Backscraping for range %s %s", *dates)
+        start = dates[0].strftime("%Y-%m-%d")
+        end = dates[1].strftime("%Y-%m-%d")
+        timestamp = str(datetime.now().timestamp())[:10]
+        params = {**self.params}
+        params.update(
+            {
+                "date": f"{start}..{end}",
+                # These are duplicated by the frontend too
+                "locale": ["en", "en"],
+                "hide_ct6": ["true", "true"],
+                "t": [timestamp, timestamp],
+            }
+        )
+        self.url = f"{self.base_url}?{urlencode(params)}"
+        self.html = self._download()
+        self._process_html()
