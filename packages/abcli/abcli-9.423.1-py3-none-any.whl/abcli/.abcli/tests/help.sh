907eaed8c70a8c5e97d5e96336a84ae3ee64d35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:23 +0000

    Add package abcli/9.423.1/abcli-9.423.1-py3-none-any.whl

---
+++ packages/abcli/abcli-9.423.1-py3-none-any.whl/abcli/.abcli/tests/help.sh
@@ -0,0 +1,105 @@
+#! /usr/bin/env bash
+
+function test_abcli_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "@pypi" \
+        "@pypi browse" \
+        "@pypi build" \
+        "@pypi install" \
+        \
+        "@pytest" \
+        \
+        "@batch browse" \
+        "@batch cat" \
+        "@batch eval" \
+        "@batch list" \
+        "@batch submit" \
+        \
+        "@browse" \
+        \
+        "@docker browse " \
+        "@docker build " \
+        "@docker clear " \
+        "@docker eval " \
+        "@docker push " \
+        "@docker run " \
+        "@docker seed " \
+        "@docker source " \
+        \
+        "@env" \
+        "@env backup" \
+        "@env backup list" \
+        "@env dot" \
+        "@env dot cat" \
+        "@env dot cat" \
+        "@env dot cp" \
+        "@env dot edit" \
+        "@env dot get" \
+        "@env dot list" \
+        "@env dot load" \
+        "@env dot set" \
+        \
+        "@git" \
+        "@git browse" \
+        "@git checkout" \
+        "@git clone" \
+        "@git create_branch" \
+        "@git create_pull_request" \
+        "@git get_branch" \
+        "@git get_repo_name" \
+        "@git increment_version" \
+        "@git pull" \
+        "@git push" \
+        "@git recreate_ssh" \
+        "@git reset" \
+        "@git review" \
+        "@git status" \
+        "@git sync_fork" \
+        \
+        "@gpu status get" \
+        "@gpu status show" \
+        "@gpu validate" \
+        \
+        "@init" \
+        \
+        "@latex" \
+        "@latex build" \
+        "@latex install" \
+        \
+        "@plugins get_module_name" \
+        "@plugins install" \
+        "@plugins list_of_external" \
+        "@plugins list_of_installed" \
+        "@plugins transform" \
+        \
+        "@repeat" \
+        \
+        "@seed" \
+        "@seed eject" \
+        "@seed list" \
+        \
+        "@sleep" \
+        \
+        "@test" \
+        "@test list" \
+        \
+        "@terraform" \
+        "@terraform cat" \
+        "@terraform disable" \
+        "@terraform enable" \
+        \
+        "@watch" \
+        \
+        "abcli"; do
+        abcli_eval ,$options \
+            abcli_help $module
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_hr
+    done
+
+    return 0
+}
