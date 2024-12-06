c9dfccb77fd9f3fa32ff8db66a4c165b2b3fc1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:04:18 +0000

    Add package verinfast/0.6.17/verinfast-0.6.17.tar.gz

---
+++ packages/verinfast/verinfast-0.6.17.tar.gz/verinfast-0.6.17/src/verinfast/cloud/aws/costs.py
@@ -0,0 +1,81 @@
+import json
+import os
+import subprocess
+from verinfast.cloud.aws.get_profile import find_profile
+
+
+def runAws(targeted_account, start, end, path_to_output, log,
+           profile=None, dry=False):
+
+    def _get_costs_and_usage(profile: str, aws_output_file: str):
+        cmd = f'''
+            aws ce get-cost-and-usage \
+            --time-period Start={start},End={end} \
+            --granularity=DAILY \
+            --metrics "BlendedCost" \
+            --group-by Type=DIMENSION,Key=SERVICE \
+            --profile="{profile}" \
+            --output=json | cat
+        '''
+
+        try:
+            results = subprocess.run(
+                cmd,
+                shell=True,
+                stdout=subprocess.PIPE,
+                check=True
+            )
+
+        except subprocess.CalledProcessError:
+            log(msg="Error getting data from AWS CLI get-cost-and-usage",
+                tag="AWS CLI")
+            raise Exception("Error getting aws cli data.")
+
+        text = results.stdout.decode()
+
+        if not text or not isinstance(text, str):
+            log(msg="No data returned from AWS CLI get-cost-and-usage",
+                tag="AWS CLI")
+            return None
+
+        obj = json.loads(text)
+        results_by_time = obj["ResultsByTime"]
+        charges = []
+        for charge in results_by_time:
+            if charge["Groups"]:
+                for group in charge["Groups"]:
+                    newCharge = {
+                        "Date": charge["TimePeriod"]["Start"],
+                        "Group": group["Keys"][0],
+                        "Cost": group["Metrics"]["BlendedCost"]["Amount"],
+                        "Currency": group["Metrics"]["BlendedCost"]["Unit"]
+                    }
+                    charges.append(newCharge)
+        upload = {
+            "metadata": {
+                "provider": "aws",
+                "account": str(targeted_account)
+            },
+            "data": charges
+        }
+
+        with open(aws_output_file, 'w') as outfile:
+            outfile.write(json.dumps(upload, indent=4))
+        return aws_output_file
+
+    if profile is None:
+        profile = find_profile(targeted_account=targeted_account, log=log)
+
+    if profile is None:
+        log(msg="No matching profiles found",
+            tag=targeted_account)
+        return None
+
+    output_file = os.path.join(
+        path_to_output,
+        f'aws-cost-{targeted_account}.json'
+    )
+    if not dry:
+        _get_costs_and_usage(profile, output_file)
+
+    return output_file
