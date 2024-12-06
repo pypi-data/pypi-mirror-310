496f5c933a56d1ef62a559afcb90e8f1ac157491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:06 +0000

    Add package ansys-rocky-core/0.3.0/ansys_rocky_core-0.3.0-py3-none-any.whl

---
+++ packages/ansys-rocky-core/ansys_rocky_core-0.3.0-py3-none-any.whl/ansys/rocky/core/launcher.py
@@ -0,0 +1,143 @@
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Module that exposes functions to launch a Rocky application session."""
+import contextlib
+import os
+from pathlib import Path
+import subprocess
+import time
+from typing import Optional, Union
+
+from Pyro5.errors import CommunicationError
+
+from ansys.rocky.core.client import DEFAULT_SERVER_PORT, RockyClient, connect_to_rocky
+from ansys.rocky.core.exceptions import RockyLaunchError
+
+_CONNECT_TO_SERVER_TIMEOUT = 60
+
+
+def launch_rocky(
+    rocky_exe: Optional[Union[Path, str]] = None,
+    *,
+    headless: bool = True,
+    server_port: int = DEFAULT_SERVER_PORT,
+    close_existing: bool = False,
+) -> RockyClient:
+    """
+    Launch the Rocky executable with the PyRocky server enabled.
+
+    This method waits for Rocky to start up and then returns a
+    ```RockyClient`` instance.
+
+    Parameters
+    ----------
+    rocky_exe : Optional[Path], optional
+        Path to the Rocky executable. If a path is not specified, this method
+        tries to find the path using ``AWP_ROOT*`` environment variables.
+    headless : bool, optional
+        Whether to launch Rocky in headless mode. The default is ``True``.
+    server_port: int, optional
+        Set the port for Rocky RPC server.
+    close_existing: bool, optional
+        Checks if a session exists under the given server_port and closes it
+        before attempting to launch a new session.
+
+    Returns
+    -------
+    RockyClient
+        Rocky client instance connected to the launched Rocky app.
+    """
+    if isinstance(rocky_exe, str):
+        rocky_exe = Path(rocky_exe)
+
+    if _is_port_busy(server_port):
+        if close_existing:
+            # Will try to connect to an existing session using the
+            # given server port and attempt to close it.
+            client = connect_to_rocky(port=server_port)
+            try:
+                client.close()
+            except CommunicationError:
+                # Maybe the session closed in the meantime so we just pass
+                pass
+        else:
+            raise RockyLaunchError(f"Port {server_port} is already in use.")
+
+    if rocky_exe is None:
+        awp_roots = [k for k in os.environ.keys() if k.startswith("AWP_ROOT")]
+        for awp_root in sorted(awp_roots, reverse=True):
+            rocky_exe = Path(os.environ[awp_root]) / "Rocky/bin/Rocky.exe"
+            if rocky_exe.is_file():
+                break
+        else:
+            raise FileNotFoundError("Rocky executable is not found.")
+    else:
+        if not rocky_exe.is_file():
+            raise FileNotFoundError(f"Rocky executable is not found at {rocky_exe}.")
+
+    cmd = [rocky_exe, "--pyrocky", "--pyrocky-port", str(server_port)]
+    if headless:
+        cmd.append("--headless")
+    with contextlib.suppress(subprocess.TimeoutExpired):
+        rocky_process = subprocess.Popen(cmd)
+        rocky_process.wait(timeout=3)
+
+    # Rocky.exe call returned to soon, something happen
+    if rocky_process.returncode is not None:
+        raise RockyLaunchError(f"Error launching Rocky:\n  {' '.join(cmd)}")
+
+    client = connect_to_rocky(port=server_port)
+
+    # TODO: A more elegant way to find out that Rocky Pyro server started.
+    now = time.time()
+    while (time.time() - now) < _CONNECT_TO_SERVER_TIMEOUT:
+        try:
+            client.api.GetProject()
+        except CommunicationError:
+            time.sleep(1)
+        else:
+            break
+    else:
+        raise RockyLaunchError("Could not connect Rocky remote server: timed out")
+
+    client._process = rocky_process
+    return client
+
+
+def _is_port_busy(port: int) -> bool:
+    """
+    Check if there is already a Rocky server running.
+
+    Parameters
+    ----------
+    port : int
+        Port to check.
+
+    Returns
+    -------
+    bool
+        ``True`` if the port is busy, ``False`` otherwise.
+    """
+    import socket
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(("localhost", port)) == 0
