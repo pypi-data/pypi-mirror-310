87f8ae81e4cd779cca725f2c681f37f0ec19b8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:36 +0000

    Add package joke-gen/0.1.4/joke_gen-0.1.4-py3-none-any.whl

---
+++ packages/joke-gen/joke_gen-0.1.4-py3-none-any.whl/joke_gen/translator.py
@@ -0,0 +1,14 @@
+from translate import Translator
+
+
+class JokeTranslator:
+    def __init__(self):
+        # Set up a translator with a default language (for example, Spanish)
+        self.default_lang = "es"
+
+    def translate(self, text, target_language=None):
+        """Translates the text to the target language (defaults to Spanish)."""
+        if target_language is None:
+            target_language = self.default_lang
+        translator = Translator(to_lang=target_language)
+        return translator.translate(text)
