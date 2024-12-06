02d568dbc91ec035cd9e6bc09e3031801021f279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:04 +0000

    Add package texfont/0.1.5/texfont-0.1.5.tar.gz

---
+++ packages/texfont/texfont-0.1.5.tar.gz/texfont-0.1.5/README.md
@@ -0,0 +1,7 @@
+## Texfont
+
+This is a tool that helps you to download common font for latex project.
+
+```shell
+poetry config pypi-token.pypi
+```
---
+++ packages/texfont/texfont-0.1.5.tar.gz/texfont-0.1.5/texfont/font/install.py
@@ -0,0 +1,57 @@
+import json
+import os
+import platform
+import requests
+from shutil import copyfileobj
+from importlib import resources  # Python 3.9+ 可用
+
+
+def download_and_install_font(font_name, font_url, install_dir):
+    """
+    Download and install a font from the given URL.
+    """
+    try:
+        print(f"Downloading {font_name}...")
+        response = requests.get(font_url, stream=True)
+        response.raise_for_status()
+
+        # 保存字体文件到本地
+        font_path = os.path.join(
+            install_dir, f"{font_name}.ttf"
+        )  # 或者使用 .otf 根据实际情况
+        with open(font_path, "wb") as f:
+            copyfileobj(response.raw, f)
+
+        print(f"{font_name} installed successfully at {install_dir}!")
+    except Exception as e:
+        print(f"Failed to install {font_name}: {e}")
+
+
+def install_fonts():
+    """
+    Install predefined fonts.
+    """
+    try:
+        with resources.open_text("texfont", "font_list.json") as f:
+            fonts = json.load(f)
+    except Exception as e:
+        print(f"Failed to load font list: {e}")
+        return
+
+    # 根据操作系统确定字体安装目录
+    system = platform.system()
+    if system == "Linux":
+        install_dir = os.path.expanduser("~/.fonts")
+    elif system == "Darwin":  # macOS
+        install_dir = os.path.expanduser("~/Library/Fonts")
+    elif system == "Windows":
+        install_dir = os.path.join(os.getenv("C:\Windows"), "Fonts")
+    else:
+        raise OSError("Unsupported operating system")
+
+    # 创建字体目录（如果不存在的话）
+    os.makedirs(install_dir, exist_ok=True)
+
+    # 下载并安装每个字体
+    for font_name, font_url in fonts.items():
+        download_and_install_font(font_name, font_url, install_dir)
