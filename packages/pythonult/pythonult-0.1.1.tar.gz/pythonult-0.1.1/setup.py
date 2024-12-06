4a4829fb52b68b51bf1e25160453d3360dbd4c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:55 +0000

    Add package pythonult/0.1.1/pythonult-0.1.1.tar.gz

---
+++ packages/pythonult/pythonult-0.1.1.tar.gz/pythonult-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pythonult",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.1',    
+        'numpy',
+        'pytest',
+        'gputil',
+        'py-cpuinfo',
+        'psutil',
+        'pythonutb'
+    ],
+    author="lampadaire",
+    author_email="ilovelampadaire@gmail.com",
+    description="A collection of utilities.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lamps-dev/utl",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
