825ca8b90cef355c5b9db0c526ffb1f10f9c3691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:13 +0000

    Add package kamu-jp-modern/10.0.2/kamu_jp_modern-10.0.2.tar.gz

---
+++ packages/kamu-jp-modern/kamu_jp_modern-10.0.2.tar.gz/kamu_jp_modern-10.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kamu-jp-modern',  # パッケージ名
+    version='10.0.2',  # バージョン
+    packages=find_packages(),
+    description='Modern is a simple package for logging and progress bar and More!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='DiamondGotCat',
+    author_email='chii@kamu.jp',
+    url='https://github.com/DiamondGotCat/Modern',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
