ebb546aa2bb1796132437947f370f828c5f9b3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:13 +0000

    Add package phosphorus/0.8.2/phosphorus-0.8.2.tar.gz

---
+++ packages/phosphorus/phosphorus-0.8.2.tar.gz/phosphorus-0.8.2/src/phosphorus/subcommands/base.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, cast
+
+from phosphorus._seven import toml_parser
+from phosphorus.lib.metadata import Metadata
+
+if TYPE_CHECKING:
+    from argparse import Namespace
+
+
+class BaseCommand:
+    __slots__ = ("meta", "verbosity")
+
+    def __init__(self, args: Namespace, /) -> None:
+        self.verbosity = args.verbosity
+        self.meta = Metadata.from_path()
+
+    def run(self) -> None:
+        msg = f"{self.__class__.__qualname__} must implement run"
+        raise NotImplementedError(msg)
+
+    def get_current_hash(self) -> str:
+        with self.meta.lockfile.open("rb") as file:
+            return cast(str, toml_parser(file)["$meta"]["hash"])
