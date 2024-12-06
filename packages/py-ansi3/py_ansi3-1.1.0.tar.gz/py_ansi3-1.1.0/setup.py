7fc25600f362d7821383773ae4f273224f7efea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:19 +0000

    Add package py-ansi3/1.1.0/py_ansi3-1.1.0.tar.gz

---
+++ packages/py-ansi3/py_ansi3-1.1.0.tar.gz/py_ansi3-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-ansi3',
+    version='1.1.0',
+    description='A simple library to apply ANSI colors to text in terminal',
+    author='MohamedLunar',
+    author_email='contact.mohamedlunardev@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
