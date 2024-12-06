5e8636e868387b64d386aa869d2b151dbd9280b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:16 +0000

    Add package bovine/0.5.14/bovine-0.5.14.tar.gz

---
+++ packages/bovine/bovine-0.5.14.tar.gz/bovine-0.5.14/bovine/crypto/test_digest.py
@@ -0,0 +1,55 @@
+import pytest
+
+from .digest import validate_digest
+
+
+@pytest.mark.parametrize(
+    "headers",
+    [
+        {"digest": "sha-256=MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k="},
+        {"content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:"},
+        {
+            "content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:;mooooo"
+        },
+        {
+            "content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:;sound=mooo;mooo"
+        },
+        {
+            "content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:,sha-new=:xx==:"
+        },
+        {
+            "content-digest": "sha-512=:S3whoWr+QjPjztVYoyMrjTHT69+7rL7lKcuL72oBsgxRE396sAM+WNtjrE8cWloUUTlkKSoSDaGCaNGqBBj3qQ==:"
+        },
+        {
+            "content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:,sha-new=:xx==:,sha-512=:S3whoWr+QjPjztVYoyMrjTHT69+7rL7lKcuL72oBsgxRE396sAM+WNtjrE8cWloUUTlkKSoSDaGCaNGqBBj3qQ==:"
+        },
+    ],
+)
+def test_validate_digest_success(headers):
+    assert validate_digest(headers, b'{"cows": "good"}')
+
+
+@pytest.mark.parametrize(
+    "headers",
+    [
+        {"digest": "sha-256=MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k="},
+        {"content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:"},
+        {
+            "content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:;mooooo"
+        },
+        {
+            "content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:;sound=mooo;mooo"
+        },
+        {
+            "content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:,sha-new=:xx==:"
+        },
+        {
+            "content-digest": "sha-512=:S3whoWr+QjPjztVYoyMrjTHT69+7rL7lKcuL72oBsgxRE396sAM+WNtjrE8cWloUUTlkKSoSDaGCaNGqBBj3qQ==:"
+        },
+        {
+            "content-digest": "sha-256=:MILb5lUDD6Z0pDSxhgxj+hMBEw0uTzP3g2qUJGHMp9k=:,sha-new=:xx==:,sha-512=:S3whoWr+QjPjztVYoyMrjTHT69+7rL7lKcuL72oBsgxRE396sAM+WNtjrE8cWloUUTlkKSoSDaGCaNGqBBj3qQ==:"
+        },
+    ],
+)
+def test_validate_digest_failure(headers):
+    assert not validate_digest(headers, b'{"cows": "bad"}')
