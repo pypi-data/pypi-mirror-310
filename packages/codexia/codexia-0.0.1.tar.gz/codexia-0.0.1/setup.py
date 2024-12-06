689d500d5f26224478894acbeb1e4f03b09b7220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:04:53 +0000

    Add package codexia/0.0.1/codexia-0.0.1.tar.gz

---
+++ packages/codexia/codexia-0.0.1.tar.gz/codexia-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="codexia",
+    version="0.0.1",
+    description="Coming soon - Code transformation tool",
+    author="Ben H",
+    author_email="ben.herbener@gmail.com",
+    packages=[],  
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+    ],
+)
