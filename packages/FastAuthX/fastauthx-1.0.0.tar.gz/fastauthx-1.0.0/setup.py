daa96ab4d9c3951af05c996153c18bd0f177484d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:07 +0000

    Add package FastAuthX/1.0.0/fastauthx-1.0.0.tar.gz

---
+++ packages/FastAuthX/fastauthx-1.0.0.tar.gz/fastauthx-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FastAuthX",
+    version="1.0.0",
+    author="Joseph Christopher",
+    author_email="joechristophersc@gmail.com",
+    description="A plug-and-play authentication system for FastAPI.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/emeraldlinks/FastAuthX",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "sqlalchemy",
+        "sqlmodel",
+        "passlib[bcrypt]",
+        "python-jose",
+        "uvicorn",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Framework :: FastAPI",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
