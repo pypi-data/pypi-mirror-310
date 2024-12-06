ac9a9b87232116969f2b0ab6abfa0a3ef78fa8a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:56 +0000

    Add package CVX2/0.1.0/CVX2-0.1.0.tar.gz

---
+++ packages/CVX2/CVX2-0.1.0.tar.gz/CVX2-0.1.0/cvx2/__init__.py
@@ -0,0 +1,72 @@
+import torch
+from torch import nn
+from .utils import auto_pad
+from ._cbam import ChannelAttention, SpatialAttention, CBAM
+from .do_conv import DOConv2d
+
+__all__ = [
+	'Conv',
+	'WidthBlock',
+	'ChannelAttention',
+	'SpatialAttention',
+	'CBAM',
+	'DOConv',
+	'DOConv2d'
+]
+
+
+class Conv(nn.Module):
+	"""Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+	
+	default_act = nn.SiLU()  # default activation
+	
+	def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+		"""Initialize Conv layer with given arguments including activation."""
+		super().__init__()
+		self.conv = nn.Conv2d(c1, c2, k, s, auto_pad(k, p, d), groups=g, dilation=d, bias=False)
+		self.bn = nn.BatchNorm2d(c2)
+		self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+	
+	def forward(self, x):
+		"""Apply convolution, batch normalization and activation to input tensor."""
+		return self.act(self.bn(self.conv(x)))
+	
+	def forward_fuse(self, x):
+		"""Perform transposed convolution of 2D data."""
+		return self.act(self.conv(x))
+
+
+class DOConv(nn.Module):
+	"""Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+	
+	default_act = nn.SiLU()  # default activation
+	
+	def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+		"""Initialize Conv layer with given arguments including activation."""
+		super().__init__()
+		self.conv = DOConv2d(c1, c2, k, s, auto_pad(k, p, d), groups=g, dilation=d, bias=False)
+		self.bn = nn.BatchNorm2d(c2)
+		self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+	
+	def forward(self, x):
+		"""Apply convolution, batch normalization and activation to input tensor."""
+		return self.act(self.bn(self.conv(x)))
+	
+	def forward_fuse(self, x):
+		"""Perform transposed convolution of 2D data."""
+		return self.act(self.conv(x))
+
+
+class WidthBlock(nn.Module):
+	
+	def __init__(self, c1, c2, kernel_sizes=(3, 5, 7), shortcut=True):
+		super().__init__()
+		self.convs = nn.ModuleList(
+			[Conv(c1, c2, k) for k in kernel_sizes]
+		)
+		self.final_conv = nn.Conv2d(len(kernel_sizes) * c2, c2, kernel_size=1)
+		self.add = shortcut and c1 == c2
+	
+	def forward(self, x: torch.Tensor):
+		y = torch.concatenate([conv(x) for conv in self.convs], dim=1)
+		return x + self.final_conv(y) if self.add else self.final_conv(y)
