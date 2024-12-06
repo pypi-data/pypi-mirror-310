0b00859df656ba4dec80603b336c1f49d9d40804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:41 +0000

    Add package django-cerberus/1.0.0/django_cerberus-1.0.0.tar.gz

---
+++ packages/django-cerberus/django_cerberus-1.0.0.tar.gz/django_cerberus-1.0.0/cerberus/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.utils.translation import gettext as _
+
+from cerberus.models import Lockout
+
+
+@admin.register(Lockout)
+class LockoutAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    list_display = ['ip_address', 'username', 'user_agent', 'failed_attempts', 'get_expiration_time_text',
+                    'is_locked', 'is_expired']
+    list_filter = ['is_locked', 'is_expired']
+    search_fields = ['ip_address', 'username', 'user_agent']
+    fieldsets = (
+        ('Main data', {
+            'fields': ('ip_address', 'username', 'is_locked', 'is_expired', 'failed_attempts')
+        }),
+        ('Data recollected', {
+            #'classes': ('collapse',),
+            'fields': ('user_agent', 'params_get', 'params_post')
+        }),
+    )
+    actions = ['lock', 'unlock']
+
+    def lock(self, request, queryset):
+        queryset.update(is_locked=True)
+    lock.short_description = _(u'Lock users')
+
+    def unlock(self, request, queryset):
+        queryset.update(is_locked=False)
+    unlock.short_description = _(u'Unlock users')
---
+++ packages/django-cerberus/django_cerberus-1.0.0.tar.gz/django_cerberus-1.0.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='django-cerberus',
+    version='1.0.0',
+    description='Django app that locks out users after too many failed login attempts.',
+    long_description=open('README.rst').read(),
+    author='Pragmatic Mates',
+    author_email='info@pragmaticmates.com',
+    maintainer='Pragmatic Mates',
+    maintainer_email='info@pragmaticmates.com',
+    url='https://github.com/PragmaticMates/django-cerberus',
+    packages=[
+        'cerberus',
+        'cerberus.migrations',
+    ],
+    include_package_data=True,
+    install_requires=('django>=3',),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 3 - Alpha'
+    ],
+    license='BSD License',
+    keywords = "django login auth cerberus",
+)
