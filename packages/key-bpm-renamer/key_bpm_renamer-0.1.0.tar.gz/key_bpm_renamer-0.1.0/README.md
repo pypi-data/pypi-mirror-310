f6dba34f95c1a44e3e98bbf40aab9bc14a951773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:13 +0000

    Add package key-bpm-renamer/0.1.0/key_bpm_renamer-0.1.0.tar.gz

---
+++ packages/key-bpm-renamer/key_bpm_renamer-0.1.0.tar.gz/key_bpm_renamer-0.1.0/README.md
@@ -0,0 +1,18 @@
+# My Audio Renamer
+
+`my_audio_renamer` is a Python tool designed to rename audio files based on their key and BPM (beats per minute). It supports MP3 files and uses popular libraries such as `eyed3` for MP3 metadata handling, `tinytag` for audio metadata extraction, and `pydub` for audio file processing.
+
+## Features
+
+- Extracts BPM and musical key from audio files.
+- Renames audio files based on their key and BPM.
+- Supports MP3 files for BPM tagging.
+- Command-line interface for easy usage.
+
+## Installation
+
+To install `my_audio_renamer`, run:
+
+```bash
+pip install my_audio_renamer
+```
---
+++ packages/key-bpm-renamer/key_bpm_renamer-0.1.0.tar.gz/key_bpm_renamer-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="key_bpm_renamer",  # The name of your package
+    version="0.1.0",  # The version of your package
+    packages=find_packages(),
+    install_requires=[
+        "eyed3", "tinytag", "pydub", "essentia",  # add any dependencies
+    ],
+    entry_points={
+        "console_scripts": [
+            "key_bpm_renamer=key_bpm_renamer.renamer:main",  # Adjust based on your script
+        ],
+    },
+    author="komly",
+    author_email="komly@yandex.ru",
+    description="A tool to rename audio files based on their key and BPM",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/komly/key_bpm_renamer",  # Change to your GitHub URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
