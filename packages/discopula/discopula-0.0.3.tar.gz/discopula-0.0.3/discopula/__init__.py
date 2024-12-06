607c429d551f661dfc1c5457b3d532551e476ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:19 +0000

    Add package discopula/0.0.3/discopula-0.0.3.tar.gz

---
+++ packages/discopula/discopula-0.0.3.tar.gz/discopula-0.0.3/discopula/__init__.py
@@ -0,0 +1,8 @@
+"""
+  Discopula is a Python package for the estimation of copula-based models for discrete data. 
+"""
+
+from discopula.checkerboard.copula import CheckerboardCopula
+
+__version__ = "0.0.3"
+__all__ = ["CheckerboardCopula"]
