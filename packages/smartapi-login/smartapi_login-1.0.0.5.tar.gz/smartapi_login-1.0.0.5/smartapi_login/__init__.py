074f812244a9649c9aa34cee23bf5366c4b2c971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:05:08 +0000

    Add package smartapi-login/1.0.0.5/smartapi_login-1.0.0.5.tar.gz

---
+++ packages/smartapi-login/smartapi_login-1.0.0.5.tar.gz/smartapi_login-1.0.0.5/smartapi_login/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+AutoSmartAPI Package
+This package contains modules for interacting with the SmartAPI, fetching historical data,
+trading sessions, and other financial data.
+
+Author: Mahesh Kumar
+Email: maheshrajbhar90@gmail.com
+Version: 1.0.0.4
+"""
+
+# Import the main classes and functions that should be accessible when the package is imported
+from .main import SmartAPIHelper  # Assuming your main class is in a file named 'smartapi.py'
+
+# You can also define version number here if needed
+__version__ = '1.0.0.5'
+# import logging
+# logging.basicConfig(level=logging.INFO)
+# logging.info("smartapi_login package loaded successfully!")
+
