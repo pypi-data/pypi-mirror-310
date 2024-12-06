1e8b29f712480626b59d99287d2fb2b9199b5021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:22 +0000

    Add package ml-algorithms/0.1.1/ml_algorithms-0.1.1.tar.gz

---
+++ packages/ml-algorithms/ml_algorithms-0.1.1.tar.gz/ml_algorithms-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ml_algorithms",
+    version="0.1.1",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["matplotlib>=3.9.2", "numpy>=2.1.3"],
+)
