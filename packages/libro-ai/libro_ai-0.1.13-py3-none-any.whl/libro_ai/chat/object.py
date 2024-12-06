d4f03d34f22686c6fe14029b83f5332b4c233d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:31 +0000

    Add package libro-ai/0.1.13/libro_ai-0.1.13-py3-none-any.whl

---
+++ packages/libro-ai/libro_ai-0.1.13-py3-none-any.whl/libro_ai/chat/object.py
@@ -0,0 +1,35 @@
+from typing import List, Optional
+from abc import ABC, abstractmethod
+from pydantic import BaseModel
+from typing import Callable
+
+from .source import CHAT_SOURCE
+from .executor import ChatExecutor
+
+
+class ChatObject(BaseModel):
+    name: str = None
+    type: str = CHAT_SOURCE["CUSTOM"]
+    order: int = 0
+    to_executor: Callable[[], ChatExecutor] = None
+
+    @property
+    def key(self):
+        return '%s:%s' % (self.type, self.name)
+
+    def model_dump(self):
+        '''Dump to dict'''
+        return {
+            **super().model_dump(exclude="to_executor"),
+            "key": self.key
+        }
+
+
+class ChatObjectProvider(BaseModel, ABC):
+    name: str = "custom"
+    is_system_provider: bool = False
+    api_key: Optional[str] = None
+
+    @abstractmethod
+    def list(self) -> List[ChatObject]:
+        """List chat executors."""
---
+++ packages/libro-ai/libro_ai-0.1.13-py3-none-any.whl/libro_ai/chat/qwen_chat_executor.py
@@ -0,0 +1,79 @@
+from typing import Optional
+from pydantic import Field
+
+from .executor import LLMChat
+from ..utils import is_langchain_installed
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.messages import HumanMessage, SystemMessage,AIMessage
+from langchain_core.prompt_values import StringPromptValue
+from IPython.display import display
+from libro_core.config import libro_config
+
+
+class TongyiChat(LLMChat):
+    name: str = "tongyi"
+    model: str = Field(default="qwen-max")
+    chat: ChatTongyi = None
+    api_key: Optional[str] = None
+
+    def load(self):
+        if is_langchain_installed():
+            extra_params = {}
+            if self.api_key:
+                extra_params["api_key"] = self.api_key
+            self.chat = ChatTongyi(model_name=self.model, **extra_params)
+            return True
+        return False
+
+    def run(self, value, language = None,stream=False, sync=True, system_prompt = None, **kwargs):
+        if not self.chat:
+            self.load()
+
+        if isinstance(value, list):
+            input = (
+                [SystemMessage(content=system_prompt)] + value
+                if system_prompt is not None
+                else value
+            )
+        else:
+            input = [
+                SystemMessage(content=system_prompt),
+                HumanMessage(content=value.text),
+            ] if system_prompt is not None else [HumanMessage(content=value.text)]
+        
+        if stream:
+            try:
+                if not self.chat:
+                    raise Exception("Chat model not loaded")
+                chat = self.chat
+                if sync:
+                    result = chat.stream(input, streaming = True,**kwargs)
+                    return result
+                else:
+
+                    result = chat.astream(input, streaming = True, **kwargs)
+                    return result
+            except Exception as e:
+
+                return ""
+        else:
+            try:
+                if not self.chat:
+                    raise Exception("Chat model not loaded")
+                chat = self.chat
+                if sync:
+                    result = chat.invoke(input, **kwargs)
+                    return result
+                else:
+                    result = chat.ainvoke(input, **kwargs)
+                    return result
+            except Exception as e:
+                return ""
+
+    def display(self, value, **kwargs):
+        if isinstance(value, str):
+            data = {"application/vnd.libro.prompt+json": value}
+            display(data, raw=True)
+        if isinstance(value, AIMessage):
+            data = {"application/vnd.libro.prompt+json": value.content}
+            display(data, raw=True)
