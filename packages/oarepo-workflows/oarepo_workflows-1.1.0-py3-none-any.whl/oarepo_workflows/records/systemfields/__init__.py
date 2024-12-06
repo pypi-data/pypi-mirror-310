769a9ba5b32148ca8df63dccde475adf5693b968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:55 +0000

    Add package oarepo-workflows/1.1.0/oarepo_workflows-1.1.0-py3-none-any.whl

---
+++ packages/oarepo-workflows/oarepo_workflows-1.1.0-py3-none-any.whl/oarepo_workflows/records/systemfields/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2024 CESNET z.s.p.o.
+#
+# oarepo-workflows is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+#
+"""Record layer, system fields."""
+
+from .state import RecordStateField
+from .workflow import WorkflowField
+
+__all__ = (
+    "RecordStateField",
+    "WorkflowField",
+)
