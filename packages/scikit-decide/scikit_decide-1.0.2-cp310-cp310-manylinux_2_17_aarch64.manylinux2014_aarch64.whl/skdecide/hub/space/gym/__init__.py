bc092562397ef367830f7b6717f454cb5e578cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:52 +0000

    Add package scikit-decide/1.0.2/scikit_decide-1.0.2-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/scikit-decide/scikit_decide-1.0.2-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/skdecide/hub/space/gym/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) AIRBUS and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .gym import (
+    BoxSpace,
+    DataSpace,
+    DictSpace,
+    DiscreteSpace,
+    EnumSpace,
+    GymSpace,
+    ListSpace,
+    MultiBinarySpace,
+    MultiDiscreteSpace,
+    SetSpace,
+    TupleSpace,
+)
