5bc826c7dbe8719f455ebfa831365b72b3107779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:05:54 +0000

    Add package ststools/3.0.0/ststools-3.0.0.tar.gz

---
+++ packages/ststools/ststools-3.0.0.tar.gz/ststools-3.0.0/ststools/__init__.py
@@ -0,0 +1,11 @@
+from .base import Display,Save_data,Hist_plot,Grid_plot,Open_files,Hist_metal,Hist_molecule
+
+print('Software de visualizacao de arquivos de STS Nanosurf e Omicron v3.0.0')
+print('By Rafael Reis Barreto, contato rafinhareis17@gmail.com')
+print('Github com o codigo fonte https://github.com/rafinhareis/ststools')
+print('Repositorio Pypi https://pypi.org/project/ststools/ (nele voce pode conferir qual a versao mais atual)')
+print('Arquivos Nanosurf tipo csv (x,y,z) ')
+print('Aquivos Omicron tipo .txt, .asc ou .ibw (IGOR) para grid. NAO MUDAR O NOME DO ARQUIVO PADRAO')
+print('Se sentir no fundo do coracaozinho, poe meu nome no artigo =D')
+
+
