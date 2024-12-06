912f89d66058f612d9623d626aa369abea3db448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:16 +0000

    Add package tfprob/0.0.1/tfprob-0.0.1-py3-none-any.whl

---
+++ packages/tfprob/tfprob-0.0.1-py3-none-any.whl/tfprob/__init__.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tenorflow import keras as tfk
+
+import tensorflow_probability as tfp
+from tensorflow_probability import layers as tfl
+from tensorflow_probability import distributions as tfd
+from tensorflow_probability import bijectors as tfb
