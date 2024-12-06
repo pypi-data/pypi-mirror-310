ea81b8b122c1653d5c2f316d8f7cbb7ca8162a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:46 +0000

    Add package ai-benchmarks/0.1.0/ai_benchmarks-0.1.0-py3-none-any.whl

---
+++ packages/ai-benchmarks/ai_benchmarks-0.1.0-py3-none-any.whl/inference/demo.py
@@ -0,0 +1,31 @@
+import os
+import asyncio
+import time
+from openai import AsyncOpenAI
+
+input_text = "http://127.0.0.1:8011/v1"
+
+client = AsyncOpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+    base_url=input_text
+)
+
+
+async def main(stream: bool) -> None:
+    start_time = time.time()
+
+    chat_completion = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "介绍一下地球",
+            }
+        ],
+        model="llama3.1:8b",
+        stream=stream
+    )
+    models = await client.models.list()
+    print(models.data[0].id)
+
+
+asyncio.run(main(stream=True))  # 或者 main(stream=False)
