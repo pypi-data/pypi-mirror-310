e40906b4308dc71f14ac32a111387471b80cfbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:18:33 +0000

    Add package telegram-markdown-text/0.5.0/telegram_markdown_text-0.5.0.tar.gz

---
+++ packages/telegram-markdown-text/telegram_markdown_text-0.5.0.tar.gz/telegram_markdown_text-0.5.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="telegram-markdown-text",
+    version="0.5.0",
+    packages=find_packages(),
+    description="Simple MarkdownV2 string builder for the Telegram API",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/mrvayzard/telegram-markdown-text",
+    author="Denys Yablonskyi",
+    keywords=["telegram", "tg", "markdown", "markdownV2", "text"],
+)
