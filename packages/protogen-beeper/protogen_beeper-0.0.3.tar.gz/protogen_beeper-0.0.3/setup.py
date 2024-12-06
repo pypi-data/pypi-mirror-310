003ebef3a3ea114b5851b148b0428ed7bdbabcfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:29 +0000

    Add package protogen-beeper/0.0.3/protogen_beeper-0.0.3.tar.gz

---
+++ packages/protogen-beeper/protogen_beeper-0.0.3.tar.gz/protogen_beeper-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name = "protogen_beeper",
+    version = "0.0.3",
+    author = "Prootzel",
+    author_email = "lirkel123@gmail.com",
+    description = "A library to test how to develop modules",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Prootzel/Protogen_Test_Library",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
