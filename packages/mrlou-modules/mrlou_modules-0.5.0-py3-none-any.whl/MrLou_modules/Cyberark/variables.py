6151e5f9e97b8c2c04a3996d48d787d688d5856b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:07:15 +0000

    Add package mrlou-modules/0.5.0/mrlou_modules-0.5.0-py3-none-any.whl

---
+++ packages/mrlou-modules/mrlou_modules-0.5.0-py3-none-any.whl/MrLou_modules/Cyberark/variables.py
@@ -0,0 +1,14 @@
+# variables.py
+
+# CyberArk API configurations
+URI = ""
+AppID = ""
+Safe = ""
+Folder = ""
+Username = ""
+Object = f"{Username}"
+
+# Certificate paths
+ca_cert = r''
+client_cert = r''
+client_key = r''
