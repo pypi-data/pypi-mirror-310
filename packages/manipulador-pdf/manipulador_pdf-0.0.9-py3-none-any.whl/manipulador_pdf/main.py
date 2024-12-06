b9402072df3bf8c82b0c11b36d9fba3ef9334485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:20 +0000

    Add package manipulador-pdf/0.0.9/manipulador_pdf-0.0.9-py3-none-any.whl

---
+++ packages/manipulador-pdf/manipulador_pdf-0.0.9-py3-none-any.whl/manipulador_pdf/main.py
@@ -0,0 +1,13 @@
+
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Usuario\PycharmProjects\PyInstaller\tesseract.exe'
+
+
+# Substitui o primeiro item da lista.
+help_msg = '\n'.join(['\n 0: Retornar '] + main_msg.split('\n')[2:])
+
+
+
+
+
+
