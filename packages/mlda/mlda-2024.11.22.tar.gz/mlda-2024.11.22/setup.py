f1e8539fda111f55fa61c9be1f7e324f319852c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:38 +0000

    Add package mlda/2024.11.22/mlda-2024.11.22.tar.gz

---
+++ packages/mlda/mlda-2024.11.22.tar.gz/mlda-2024.11.22/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='mlda',  # required
+    version='2024.11.22',
+    description='mlda: A Python package for Machine Learning-base Data Assimilation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Feng Zhu, Weimin Si',
+    author_email='fengzhu@ucar.edu, weimin_si@brown.edu',
+    url='https://github.com/fzhu2e/mlda',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD-3',
+    zip_safe=False,
+    keywords=['Machine Learning', 'Data Assimilation'],
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.12',
+    ],
+    install_requires=[
+        'netCDF4',
+        'xarray',
+        'dask',
+        'nc-time-axis',
+        'colorama',
+        'tqdm',
+        'x4c-exp',
+    ],
+)
