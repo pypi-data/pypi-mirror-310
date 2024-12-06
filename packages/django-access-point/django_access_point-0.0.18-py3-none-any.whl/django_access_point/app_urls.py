829a474e32105035f514e70bed3dcb30c9fe8516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:08 +0000

    Add package django-access-point/0.0.18/django_access_point-0.0.18-py3-none-any.whl

---
+++ packages/django-access-point/django_access_point-0.0.18-py3-none-any.whl/django_access_point/app_urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from .auth import LoginView, ForgotPasswordView, ResetPasswordView
+from .views import PlatformUser, PlatformUserCustomField
+
+router = DefaultRouter()
+router.register(r"custom-fields/platform/users",PlatformUserCustomField,basename="platform.user.custom-fields")
+router.register(r"platform/users", PlatformUser, basename="platform.user")
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
+    path('reset-password/<str:uidb64>/<str:token>/', ResetPasswordView.as_view(), name='reset-password'),
+]
+
+urlpatterns += router.urls
