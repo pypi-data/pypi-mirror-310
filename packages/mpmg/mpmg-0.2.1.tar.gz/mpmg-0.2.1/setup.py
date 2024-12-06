4d977c4136011a875cdaf66ca3d9caa2e21bfeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:19 +0000

    Add package mpmg/0.2.1/mpmg-0.2.1.tar.gz

---
+++ packages/mpmg/mpmg-0.2.1.tar.gz/mpmg-0.2.1/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mpmg',  
+    version='0.2.1',  
+    description='A Minimum Price Markov Game modular environment', 
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Igor Sadoune',
+    author_email='igor.sadoune@polymtl.ca',
+    url='https://github.com/IgorSadoune/mpmg-python-package',
+    license='MIT', 
+    install_requires=[
+        'numpy',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.10.12',
+)
