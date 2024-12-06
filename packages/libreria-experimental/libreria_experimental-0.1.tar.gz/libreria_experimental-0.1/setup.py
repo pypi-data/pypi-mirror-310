5d5af9af55ae5b6a72d552048c3c9f41d1b296d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:06:10 +0000

    Add package libreria-experimental/0.1/libreria_experimental-0.1.tar.gz

---
+++ packages/libreria-experimental/libreria_experimental-0.1.tar.gz/libreria_experimental-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+with open("README.md", "r") as f:
+    description_md=f.read()
+setup(name='libreria_experimental',
+version='0.1',
+packages=find_packages(),
+install_requires=["sympy",
+"matplotlib",
+"numpy",
+"statistics",
+"scipy",
+"mathematica"],
+long_description=description_md,
+long_description_content_type="text/markdown")
+
