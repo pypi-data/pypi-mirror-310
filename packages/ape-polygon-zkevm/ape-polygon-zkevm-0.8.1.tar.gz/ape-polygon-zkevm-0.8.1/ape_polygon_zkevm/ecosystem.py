029e3d08dbcaa32ef765c1ee33f15b12c1b30926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:25 +0000

    Add package ape-polygon-zkevm/0.8.1/ape-polygon-zkevm-0.8.1.tar.gz

---
+++ packages/ape-polygon-zkevm/ape-polygon-zkevm-0.8.1.tar.gz/ape-polygon-zkevm-0.8.1/ape_polygon_zkevm/ecosystem.py
@@ -0,0 +1,26 @@
+from typing import cast
+
+from ape_ethereum.ecosystem import (
+    BaseEthereumConfig,
+    Ethereum,
+    NetworkConfig,
+    create_network_config,
+)
+
+NETWORKS = {
+    # chain_id, network_id
+    "mainnet": (1101, 1101),
+    "cardona": (2442, 2442),
+}
+
+
+class PolygonZkEVMConfig(BaseEthereumConfig):
+    mainnet: NetworkConfig = create_network_config(block_time=2, required_confirmations=1)
+    cardona: NetworkConfig = create_network_config(block_time=2, required_confirmations=1)
+
+
+class PolygonZkEVM(Ethereum):
+    @property
+    def config(self) -> PolygonZkEVMConfig:  # type: ignore
+        cfg = self.config_manager.get_config("polygon-zkevm")
+        return cast(PolygonZkEVMConfig, cfg)
