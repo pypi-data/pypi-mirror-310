2a0189d1dbb17a8276d62a7b54907fff9dd057d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:20 +0000

    Add package toolini/0.0.1/toolini-0.0.1.tar.gz

---
+++ packages/toolini/toolini-0.0.1.tar.gz/toolini-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='toolini',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.32.3'
+    ]
+)
