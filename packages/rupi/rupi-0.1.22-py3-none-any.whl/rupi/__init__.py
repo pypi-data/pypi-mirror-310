798bd19a4dc96132552ce6d4eb8c596f7022ea31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:06 +0000

    Add package rupi/0.1.22/rupi-0.1.22-py3-none-any.whl

---
+++ packages/rupi/rupi-0.1.22-py3-none-any.whl/rupi/__init__.py
@@ -0,0 +1,13 @@
+from .flames import flames_game as flames
+
+__all__ = ["flames"]
+
+# Define the 'about' attribute
+about = """
+Rupi - A fun Python library to play the FLAMES game!
+
+Email: tanujairam.v@gmail.com
+Email: tanujairamtanu@gmail.com
+
+Description: Use `flames_game(name1, name2)` to find the relationship between two names.
+"""
