44f266b1ce3b144de5022418824cced6a4487367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:15 +0000

    Add package py2openai/0.9.6/py2openai-0.9.6.tar.gz

---
+++ packages/py2openai/py2openai-0.9.6.tar.gz/py2openai-0.9.6/src/py2openai/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.9.6"
+
+from py2openai.executable import create_executable, ExecutableFunction
+from py2openai.functionschema import FunctionType, create_schema
+from py2openai.schema_generators import (
+    create_schemas_from_module,
+    create_schemas_from_class,
+)
+from py2openai.typedefs import OpenAIFunctionDefinition, OpenAIFunctionTool
+
+__all__ = [
+    "ExecutableFunction",
+    "FunctionType",
+    "OpenAIFunctionDefinition",
+    "OpenAIFunctionTool",
+    "create_executable",
+    "create_schema",
+    "create_schemas_from_class",
+    "create_schemas_from_module",
+]
