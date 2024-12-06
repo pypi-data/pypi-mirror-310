8e3e114dcbf9cf1ab076396a56f4544aef44de9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:41 +0000

    Add package source-translator/1.0.1/source_translator-1.0.1-py3-none-any.whl

---
+++ packages/source-translator/source_translator-1.0.1-py3-none-any.whl/source_translator-1.0.1.data/scripts/source_translate.py
@@ -0,0 +1,53 @@
+#!python
+
+import sys
+import pathlib
+import argparse
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent / "src"))
+
+from source_translator import SourceCode, c_like
+from source_translator.langs import cpp, ts, php
+from source_translator.naming import snake_to_lower_camel
+
+translators = {
+    "cpp": cpp.CppTranslator(),
+    "ts": ts.TypeScriptTranslator(),
+    "js": ts.TypeScriptTranslator(False),
+    "php": php.PhpTranslator(),
+}
+translators["c++"] = translators["cpp"]
+
+
+styles = {
+    "allman": c_like.AllmanStyle(),
+    "k&r": c_like.KandRStyle(),
+    "whitesmiths": c_like.WhitesmithsStyle(),
+    "gnu": c_like.GnuStyle()
+}
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", type=pathlib.Path)
+parser.add_argument("--language", "-x", choices=translators.keys())
+parser.add_argument("--style", "-s", choices=styles.keys())
+parser.add_argument("--indent-width", "-w", type=int, default=4)
+parser.add_argument("--camel", action="store_true")
+args = parser.parse_args()
+
+with open(args.file) as f:
+    source = SourceCode(f.read())
+
+translator = translators[args.language]
+
+if args.style:
+    translator.indent_style = styles[args.style]
+
+if args.indent_width:
+    translator.indent_style.spaces = args.indent_width
+
+
+translator.naming_style = snake_to_lower_camel if args.camel else str
+
+output = translator.convert(source)
+print(output)
