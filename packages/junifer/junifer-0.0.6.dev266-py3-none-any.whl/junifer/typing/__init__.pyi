f3af71295d18db186e208438cfbc61937eada3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:46 +0000

    Add package junifer/0.0.6.dev266/junifer-0.0.6.dev266-py3-none-any.whl

---
+++ packages/junifer/junifer-0.0.6.dev266-py3-none-any.whl/junifer/typing/__init__.pyi
@@ -0,0 +1,25 @@
+__all__ = [
+    "DataGrabberLike",
+    "PreprocessorLike",
+    "MarkerLike",
+    "StorageLike",
+    "PipelineComponent",
+    "Dependencies",
+    "ConditionalDependencies",
+    "ExternalDependencies",
+    "MarkerInOutMappings",
+    "DataGrabberPatterns",
+]
+
+from ._typing import (
+    DataGrabberLike,
+    PreprocessorLike,
+    MarkerLike,
+    StorageLike,
+    PipelineComponent,
+    Dependencies,
+    ConditionalDependencies,
+    ExternalDependencies,
+    MarkerInOutMappings,
+    DataGrabberPatterns,
+)
