6c3a6e7a01690feb5270f7108b4724cfecf711e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:53 +0000

    Add package dddesign/1.0.10/dddesign-1.0.10-py3-none-any.whl

---
+++ packages/dddesign/dddesign-1.0.10-py3-none-any.whl/dddesign/structure/services/service.py
@@ -0,0 +1,11 @@
+from abc import ABCMeta, abstractmethod
+
+from pydantic import BaseModel, ConfigDict
+
+
+class Service(BaseModel, metaclass=ABCMeta):
+    model_config = ConfigDict(frozen=True, arbitrary_types_allowed=True)
+
+    @abstractmethod
+    def handle(self):
+        ...
