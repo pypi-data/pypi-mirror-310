f7e2f6184b0cc8dc850650baa84d678eea4a6cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:02 +0000

    Add package pycrowmodule/0.12/pycrowmodule-0.12.tar.gz

---
+++ packages/pycrowmodule/pycrowmodule-0.12.tar.gz/pycrowmodule-0.12/setup.py
@@ -0,0 +1,18 @@
+"""Crow/AAP Alarm IP / Serial Module package."""
+from setuptools import setup, find_packages
+
+setup(
+    name='pycrowmodule',
+    version='0.12',
+    description='Library for Crow/AAP Alarm IP / Serial Module',
+    url='https://github.com/osotechie/pycrowmodule',
+    author='osotechie',
+    author_email='owen@demooy.co.nz',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords='Crow/AAP Alarm IP / Serial Module for Home Assistant',
+    include_package_data=True,
+    zip_safe=False
+)
+
