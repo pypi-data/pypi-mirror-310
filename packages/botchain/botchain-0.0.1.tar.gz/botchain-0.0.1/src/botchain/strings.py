707515ab70d91efaf53b1302c01c31b0189a77c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:06:30 +0000

    Add package botchain/0.0.1/botchain-0.0.1.tar.gz

---
+++ packages/botchain/botchain-0.0.1.tar.gz/botchain-0.0.1/src/botchain/strings.py
@@ -0,0 +1,5 @@
+bot_system_template = "You are a bot named {name} with an objective to {objective}.\n\n{instructions}"
+
+bot_doc_template = "A bot named {name} with an objective to {objective}.\n\nThis bot will: {instructions}"
+
+tool_template = "    Tool name: {name}\n    Description: {description}"
