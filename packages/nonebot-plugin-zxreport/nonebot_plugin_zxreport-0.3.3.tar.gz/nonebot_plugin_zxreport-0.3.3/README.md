fc6b77960b02d66a9bafa241417d8360f3f17aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:06:11 +0000

    Add package nonebot-plugin-zxreport/0.3.3/nonebot_plugin_zxreport-0.3.3.tar.gz

---
+++ packages/nonebot-plugin-zxreport/nonebot_plugin_zxreport-0.3.3.tar.gz/nonebot_plugin_zxreport-0.3.3/README.md
@@ -0,0 +1,73 @@
+<div align=center>
+
+<img width="250" height="312" src="https://github.com/HibiKier/nonebot-plugin-zxreport/blob/main/docs_image/tt.jpg"/>
+
+</div>
+
+<div align="center">
+
+<p>
+  <img src="https://raw.githubusercontent.com/lgc-NB2Dev/readme/main/template/plugin.svg" alt="NoneBotPluginText">
+</p>
+
+# nonebot-plugin-zxreport
+
+_✨ 基于 [NoneBot2](https://github.com/nonebot/nonebot2) 的一个插件 ✨_
+
+![python](https://img.shields.io/badge/python-v3.9%2B-blue)
+![nonebot](https://img.shields.io/badge/nonebot-v2.1.3-yellow)
+![onebot](https://img.shields.io/badge/onebot-v11-black)
+[![license](https://img.shields.io/badge/license-AGPL3.0-FE7D37)](https://github.com/HibiKier/zhenxun_bot/blob/main/LICENSE)
+
+</div>
+
+## 📖 介绍
+
+[小真寻](https://github.com/HibiKier/zhenxun_bot)记者为你献上今天报道！
+
+> [!NOTE]
+>
+> <div align="center"><b>小真寻也很可爱呀，也会很喜欢你！</b></div>
+>
+> <div align="center"><img width="235" height="235" src="https://github.com/HibiKier/nonebot-plugin-zxpm/blob/main/docs_image/tt3.png"/><img width="235" height="235" src="https://github.com/HibiKier/nonebot-plugin-zxpm/blob/main/docs_image/tt1.png"/><img width="235" height="235" src="https://github.com/HibiKier/nonebot-plugin-zxpm/blob/main/docs_image/tt2.png"/></div>
+
+## 💿 安装
+
+```python
+pip install nonebot-plugin-zxreport
+```
+
+```python
+nb plugin install nonebot-plugin-zxreport
+```
+
+| 配置                    | 类型 |            默认值             | 说明                                                             |
+| :---------------------- | :--: | :---------------------------: | ---------------------------------------------------------------- |
+| alapi_token          | str  |           ""           |  60s看世界api替换，当存在该配置时，使用alapi早报api，在https://admin.alapi.cn/user/login登录后获取token                                                |
+| auto_send          | bool  |           true           |  每日9点自动发送日报    |
+
+
+## 🎁 使用
+
+
+```python
+真寻日报
+
+
+超级用户:
+
+  开启/关闭指定群真寻日报发送:
+    开启真寻日报 123123123 31232333 ...(群号)
+    关闭真寻日报 123123123 31232333 ...(群号)
+
+  重置真寻日报: 删除当前日报文件
+```·
+
+## 🎁 效果图
+
+![_](https://github.com/HibiKier/nonebot-plugin-zxreport/blob/main/docs_image/1.png)
+
+## ❤ 感谢
+
+- 可爱的小真寻 Bot [`zhenxun_bot`](https://github.com/HibiKier/zhenxun_bot): 我谢我自己，桀桀桀
+- [mahou](https://github.com/spirit1431007/mahou): 参考ui以及数据源
---
+++ packages/nonebot-plugin-zxreport/nonebot_plugin_zxreport-0.3.3.tar.gz/nonebot_plugin_zxreport-0.3.3/nonebot_plugin_zxreport/__init__.py
@@ -0,0 +1,159 @@
+import asyncio
+import random
+from datetime import datetime
+
+from nonebot import get_bots, require
+from nonebot.log import logger
+from nonebot.permission import SUPERUSER
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+from playwright.async_api import TimeoutError
+
+require("nonebot_plugin_alconna")
+require("nonebot_plugin_htmlrender")
+require("nonebot_plugin_localstore")
+require("nonebot_plugin_apscheduler")
+require("nonebot_plugin_uninfo")
+
+from nonebot_plugin_alconna import (
+    Alconna,
+    Args,
+    Arparma,
+    Image,
+    MultiVar,
+    Option,
+    Query,
+    Target,
+    UniMessage,
+    on_alconna,
+    store_true,
+)
+from nonebot_plugin_apscheduler import scheduler
+from nonebot_plugin_uninfo import Uninfo, get_interface
+
+from .config import REPORT_PATH, Conifg, config
+from .data_source import Report, group_manager
+
+__plugin_meta__ = PluginMetadata(
+    name="真寻日报",
+    description="嗨嗨，这里是小记者真寻哦",
+    usage="""
+    指令：
+        真寻日报
+    """.strip(),
+    type="application",
+    config=Conifg,
+    homepage="https://github.com/HibiKier/nonebot-plugin-zxreport",
+    supported_adapters=inherit_supported_adapters(
+        "nonebot_plugin_alconna",
+    ),
+    extra={"author": "HibiKier", "version": "0.3"},
+)
+
+
+_matcher = on_alconna(Alconna("真寻日报"), priority=5, block=True)
+
+_status_matcher = on_alconna(
+    Alconna(
+        "report-status",
+        Args["gid_list", MultiVar(str)],
+        Option("--close", action=store_true, help_text="关闭"),
+    ),
+    priority=5,
+    block=True,
+    permission=SUPERUSER,
+)
+
+_status_matcher.shortcut(
+    "开启真寻日报",
+    command="report-status",
+    arguments=["{%0}"],
+    prefix=True,
+)
+
+_status_matcher.shortcut(
+    "关闭真寻日报",
+    command="report-status",
+    arguments=["--close", "{%0}"],
+    prefix=True,
+)
+
+_reset_matcher = on_alconna(
+    Alconna("重置真寻日报"), priority=5, block=True, permission=SUPERUSER
+)
+
+
+@_status_matcher.handle()
+async def _(
+    arparma: Arparma,
+    gid_list: Query[tuple[str, ...]] = Query("gid_list"),
+):
+    for gid in gid_list.result:
+        if arparma.find("close"):
+            group_manager.add(gid)
+        else:
+            group_manager.remove(gid)
+    group_manager.save()
+    await UniMessage(
+        f"已{'关闭' if arparma.find('close') else '开启'}以上群组日报状态!"
+    ).send(reply_to=True)
+    logger.info(
+        f"{'关闭' if arparma.find('close') else '开启'}真寻日报: {gid_list.result}"
+    )
+
+
+@_reset_matcher.handle()
+async def _():
+    file = REPORT_PATH / f"{datetime.now().date()}.png"
+    if file.exists():
+        file.unlink()
+        logger.info("重置真寻日报")
+    await UniMessage("真寻日报已重置!").send()
+
+
+@_matcher.handle()
+async def _():
+    try:
+        path = await Report.get_report_image()
+        await UniMessage(Image(raw=path)).send()
+        logger.info("查看真寻日报")
+    except TimeoutError:
+        await UniMessage("真寻日报生成超时...").send(at_sender=True)
+        logger.error("真寻日报生成超时")
+
+
+@scheduler.scheduled_job(
+    "cron",
+    hour=0,
+    minute=1,
+)
+async def _():
+    for _ in range(3):
+        try:
+            await Report.get_report_image()
+            logger.info("自动生成日报成功...")
+            break
+        except TimeoutError:
+            logger.warning("自动生成日报失败...")
+
+
+@scheduler.scheduled_job(
+    "cron",
+    hour=9,
+    minute=1,
+)
+async def _():
+    if not config.auto_send:
+        return
+    file = await Report.get_report_image()
+    for _, bot in get_bots().items():
+        if interface := get_interface(bot):
+            scenes = [
+                s
+                for s in await interface.get_scenes()
+                if s.is_group and not group_manager.check(s.id)
+            ]
+            for scene in scenes:
+                await UniMessage(Image(raw=file)).send(bot=bot, target=Target(scene.id))
+                rand = random.randint(1, 5)
+                await asyncio.sleep(rand)
+    logger.info("每日真寻日报发送完成...")
