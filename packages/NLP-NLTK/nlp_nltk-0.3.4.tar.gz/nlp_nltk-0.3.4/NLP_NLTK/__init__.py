9c36c0248eb36e4925f31dbbf945e8f52fbf1a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:20:04 +0000

    Add package NLP-NLTK/0.3.4/nlp_nltk-0.3.4.tar.gz

---
+++ packages/NLP-NLTK/nlp_nltk-0.3.4.tar.gz/nlp_nltk-0.3.4/NLP_NLTK/__init__.py
@@ -0,0 +1,3 @@
+from .main import load_conll_data, word_features, extract_entities, evaluate_dataset
+from .main import preprocess_data, QADataset, prepare_QADataset
+from .main import AttentionLayer, build_model
---
+++ packages/NLP-NLTK/nlp_nltk-0.3.4.tar.gz/nlp_nltk-0.3.4/README.md
@@ -0,0 +1,8 @@
+# NLP-NLTK
+
+A Python library for NLP tasks using NLTK.
+
+## Installation
+
+```bash
+pip install NLP-NLTK
