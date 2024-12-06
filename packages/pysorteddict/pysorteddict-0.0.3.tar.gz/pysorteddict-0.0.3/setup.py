84a9a5fe9d5598a54cd9d22b1dff880a7a6ab13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:42 +0000

    Add package pysorteddict/0.0.3/pysorteddict-0.0.3.tar.gz

---
+++ packages/pysorteddict/pysorteddict-0.0.3.tar.gz/pysorteddict-0.0.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    ext_modules=[
+        setuptools.Extension(
+            name="pysorteddict",
+            extra_compile_args=["-std=c++20"],
+            sources=["src/pysorteddict/pysorteddict.cc"],
+            py_limited_api=True,
+        )
+    ]
+)
