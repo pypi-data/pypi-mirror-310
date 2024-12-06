5a5302e37495828b8652bef1780c5f73b530e318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:43 +0000

    Add package hitant/0.1.0/hitant-0.1.0.tar.gz

---
+++ packages/hitant/hitant-0.1.0.tar.gz/hitant-0.1.0/tests/test_analyzer.py
@@ -0,0 +1,15 @@
+from hitant.analyzer import crawl_website
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15'
+}
+
+url = "https://www.naver.com"
+blocked = crawl_website(url, headers)
+print(blocked)
+
+# 예측 수행
+if blocked:
+    print(f'URL: {url}, RESULT: 피싱 사이트')
+else:
+    print(f'URL: {url}, RESULT: 안전 사이트')
