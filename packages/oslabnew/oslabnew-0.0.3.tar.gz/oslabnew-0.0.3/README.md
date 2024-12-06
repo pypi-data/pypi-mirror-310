1874ccbb94b5cef84cc9a278d768a92011767cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:11 +0000

    Add package oslabnew/0.0.3/oslabnew-0.0.3.tar.gz

---
+++ packages/oslabnew/oslabnew-0.0.3.tar.gz/oslabnew-0.0.3/README.md
@@ -0,0 +1,12 @@
+# cnslab
+
+`oslabnew` is a Python library for os lab.
+
+## Installation
+
+You can install `oslabnew` via pip:
+
+
+
+
+This README provides a brief overview of the library, installation instructions, usage examples, and information about the license.
---
+++ packages/oslabnew/oslabnew-0.0.3.tar.gz/oslabnew-0.0.3/oslabnew/__init__.py
@@ -0,0 +1,9 @@
+from .programs import f1,f2,f3,f4,f5,f6,fcfs,sjf,fork,sleep,orphan,shared1,shared2,client,server,pipe, threads, fifo, lru, contiguous, indexed, linked, producer_consumer, dining_philosopher, bankers, shell
+
+
+def list_functions():
+    print('''
+f1,f2,f3,f4,f5,f6,fcfs,sjf,fork,sleep,orphan,shared1,shared2,client,server,pipe
+          
+threads,  fifo, lru, contiguous, indexed, linked, producer_consumer, dining_philosopher, bankers, shell
+''')
