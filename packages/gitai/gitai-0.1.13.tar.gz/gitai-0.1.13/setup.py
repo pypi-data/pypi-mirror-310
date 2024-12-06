cc9deec71789439f8d157aa9cbe9b8877c6797df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:42 +0000

    Add package gitai/0.1.13/gitai-0.1.13.tar.gz

---
+++ packages/gitai/gitai-0.1.13.tar.gz/gitai-0.1.13/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import yaml
+import re
+
+setup(
+    name='gitai',
+    version='0.1.13',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+		'langchain',
+		'python-dotenv',
+		'click',
+		'langchain-openai',
+		'langchainhub',
+		'asyncio',
+	],
+    entry_points='''
+        [console_scripts]
+        gitai=gitai.main:main
+		gitai-agent=gitai.agent:main
+    ''',
+)
