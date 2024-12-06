8de54ab5d396ae8fd6f10afe9920af44d5e6c2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:24 +0000

    Add package pyterrier-alpha/0.9.2/pyterrier_alpha-0.9.2-py3-none-any.whl

---
+++ packages/pyterrier-alpha/pyterrier_alpha-0.9.2-py3-none-any.whl/pyterrier_alpha/__init__.py
@@ -0,0 +1,27 @@
+"""``pyterrier-alpha`` provides a set of utilities for working with PyTerrier that are currently under development.
+
+Functionality provided by this package is subject to change in future versions.
+"""
+
+__version__ = '0.9.2'
+
+from pyterrier_alpha import artifact, colab, inspect, io, utils, validate
+from pyterrier_alpha.artifact import Artifact, ArtifactBuilder
+from pyterrier_alpha.dataframe_builder import DataFrameBuilder
+from pyterrier_alpha.rbo import RBO, rbo
+from pyterrier_alpha.transformer_repr import transformer_repr
+
+__all__ = [
+    'Artifact',
+    'ArtifactBuilder',
+    'DataFrameBuilder',
+    'RBO',
+    'artifact',
+    'colab',
+    'inspect',
+    'io',
+    'rbo',
+    'transformer_repr',
+    'utils',
+    'validate',
+]
