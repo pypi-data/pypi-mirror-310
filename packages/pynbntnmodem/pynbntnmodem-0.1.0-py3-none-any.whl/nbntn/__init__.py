1cc2276184df67901ef13921e83a3c83d2c2eb8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:19 +0000

    Add package pynbntnmodem/0.1.0/pynbntnmodem-0.1.0-py3-none-any.whl

---
+++ packages/pynbntnmodem/pynbntnmodem-0.1.0-py3-none-any.whl/nbntn/__init__.py
@@ -0,0 +1,18 @@
+from .modem import (Module, RegInfo, SigInfo, NtnLocation, NtnOpMode,
+                    ModuleModel, PdpContext, PdpType, TransportType,
+                    RegistrationState, UrcType, get_model)
+
+__all__ = [
+    'Module',
+    'RegInfo',
+    'SigInfo',
+    'ModuleModel',
+    'NtnLocation',
+    'NtnOpMode',
+    'PdpContext',
+    'PdpType',
+    'TransportType',
+    'RegistrationState',
+    'UrcType',
+    'get_model',
+]
