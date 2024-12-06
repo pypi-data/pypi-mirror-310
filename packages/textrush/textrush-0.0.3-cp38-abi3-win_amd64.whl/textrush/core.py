ed8195b522abca1efbf0e645db861571ed7a4ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:12 +0000

    Add package textrush/0.0.3/textrush-0.0.3-cp38-abi3-win_amd64.whl

---
+++ packages/textrush/textrush-0.0.3-cp38-abi3-win_amd64.whl/textrush/core.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typing import Dict
+from textrush.librush import PyKeywordProcessor
+
+__all__ = [
+    "KeywordProcessor",
+]
+
+
+class KeywordProcessor:
+    def __init__(self, case_sensitive: bool = False):
+        self._kp = PyKeywordProcessor(case_sensitive)
+
+    def add_keyword(self, keyword: str, clean_name: str = None):
+        self._kp.add_keyword(keyword, clean_name)
+
+    def add_keywords_from_dict(
+        self, dictionary: Dict[str, str], errors: str = "ignore"
+    ):
+        items = list(dictionary.items())
+        try:
+            self._kp.add_keywords_with_clean_name_from_iter(items)
+        except ValueError as e:
+            if errors == "ignore":
+                return
+            raise e
+
+    def extract_keywords(self, text: str, span_info: bool = False):
+        if span_info:
+            return self._kp.extract_keywords_with_span(text)
+        return self._kp.extract_keywords(text)
