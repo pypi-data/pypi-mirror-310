910b9ab430673eec099f30e28836c8f8327eb46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:32 +0000

    Add package pygame-shaders-setup/0.0.7/pygame_shaders_setup-0.0.7.tar.gz

---
+++ packages/pygame-shaders-setup/pygame_shaders_setup-0.0.7.tar.gz/pygame_shaders_setup-0.0.7/pygame_shaders_setup/__init__.py
@@ -0,0 +1,7 @@
+from .pygame_shaders import Shader
+from .pygame_shaders import DefaultScreenShader
+from .pygame_shaders import ComputeShader
+from .pygame_shaders import DEFAULT_FRAGMENT_SHADER
+from .pygame_shaders import DEFAULT_VERTEX_SHADER
+from .texture import Texture
+from .shader_setup import Window
---
+++ packages/pygame-shaders-setup/pygame_shaders_setup-0.0.7.tar.gz/pygame_shaders_setup-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pygame_shaders_setup",              # Replace with your package name
+    version="0.0.7",                # Package version
+    packages=find_packages(),       # Automatically find sub-packages
+    description="Basic window class for pygame_shaders", # Short description
+    python_requires=">=3.6",
+    install_requires= [
+        "moderngl==5.8.2",
+        "numpy>=2.0.0,<3.0.0",
+    ]
+)
