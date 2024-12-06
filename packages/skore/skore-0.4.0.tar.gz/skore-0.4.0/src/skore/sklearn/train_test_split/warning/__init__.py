68dd642643c7765205fd7de4487b77cfca1c7177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:42 +0000

    Add package skore/0.4.0/skore-0.4.0.tar.gz

---
+++ packages/skore/skore-0.4.0.tar.gz/skore-0.4.0/src/skore/sklearn/train_test_split/warning/__init__.py
@@ -0,0 +1,32 @@
+"""Train-test split warnings.
+
+This module implements the warnings that can be reported to the user when using
+`train_test_split`.
+"""
+
+from .high_class_imbalance_too_few_examples_warning import (
+    HighClassImbalanceTooFewExamplesWarning,
+)
+from .high_class_imbalance_warning import (
+    HighClassImbalanceWarning,
+)
+from .random_state_unset_warning import RandomStateUnsetWarning
+from .shuffle_true_warning import ShuffleTrueWarning
+from .stratify_is_set_warning import StratifyWarning
+
+TRAIN_TEST_SPLIT_WARNINGS = [
+    HighClassImbalanceTooFewExamplesWarning,
+    HighClassImbalanceWarning,
+    StratifyWarning,
+    RandomStateUnsetWarning,
+    ShuffleTrueWarning,
+]
+
+__all__ = [
+    "TRAIN_TEST_SPLIT_WARNINGS",
+    "HighClassImbalanceTooFewExamplesWarning",
+    "HighClassImbalanceWarning",
+    "StratifyWarning",
+    "RandomStateUnsetWarning",
+    "ShuffleTrueWarning",
+]
