f31d21e191e73f35144fb1b03cb62a630971366d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:07:39 +0000

    Add package pretty-jupyter/2.0.8/pretty_jupyter-2.0.8.tar.gz

---
+++ packages/pretty-jupyter/pretty_jupyter-2.0.8.tar.gz/pretty_jupyter-2.0.8/src/pretty_jupyter/__init__.py
@@ -0,0 +1,16 @@
+from pretty_jupyter.magics import JinjaMagics
+
+# these imports are here for conf.json to work
+from pretty_jupyter.preprocessors import (
+    HtmlNbMetadataPreprocessor,
+    NbMetadataPreprocessor,
+    TokenCleaningPreprocessor,
+    TokenPreprocessor,
+)
+
+
+def load_ipython_extension(ipython):
+    # The `ipython` argument is the currently active `InteractiveShell`
+    # instance, which can be used in any way. This allows you to register
+    # new magics or aliases, for example.
+    ipython.register_magics(JinjaMagics)
