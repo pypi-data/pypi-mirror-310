39baf161af21ff00e952d37d2a44080bc989c894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:16:39 +0000

    Add package ataraxis-automation/4.0.1/ataraxis_automation-4.0.1.tar.gz

---
+++ packages/ataraxis-automation/ataraxis_automation-4.0.1.tar.gz/ataraxis_automation-4.0.1/src/ataraxis_automation/__init__.py
@@ -0,0 +1,7 @@
+"""This library exposes functions used by tox tasks to automate project development and provides a cli-interface for
+calling these functions.
+
+See https://github.com/Sun-Lab-NBB/ataraxis-automation for more details.
+API documentation: https://ataraxis-automation-api-docs.netlify.app/
+Author: Ivan Kondratyev (Inkaros)
+"""
