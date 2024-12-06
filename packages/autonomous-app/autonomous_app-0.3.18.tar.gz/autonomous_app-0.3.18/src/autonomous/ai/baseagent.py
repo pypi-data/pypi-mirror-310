24cf4d05fc06ded94180a368e1d449c6c892ac90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:16 +0000

    Add package autonomous-app/0.3.18/autonomous_app-0.3.18.tar.gz

---
+++ packages/autonomous-app/autonomous_app-0.3.18.tar.gz/autonomous_app-0.3.18/src/autonomous/ai/baseagent.py
@@ -0,0 +1,38 @@
+from autonomous import log
+from autonomous.model.autoattr import ReferenceAttr
+from autonomous.model.automodel import AutoModel
+
+from .models.openai import OpenAIModel
+
+
+class BaseAgent(AutoModel):
+    meta = {"abstract": True, "allow_inheritance": True, "strict": False}
+
+    client = ReferenceAttr(choices=[OpenAIModel])
+
+    _ai_model = OpenAIModel
+
+    def delete(self):
+        if self.client:
+            self.client.delete()
+        return super().delete()
+
+    def get_agent_id(self):
+        return self.get_client().id
+
+    def get_client(self):
+        if self.client is None:
+            self.client = self._ai_model(
+                name=self.name,
+                instructions=self.instructions,
+                description=self.description,
+            )
+            self.client.save()
+            self.save()
+        return self.client
+
+    def clear_files(self, file_id=None):
+        return self.client.clear_files(file_id)
+
+    def attach_file(self, file_contents, filename="dbdata.json"):
+        return self.client.attach_file(file_contents, filename)
