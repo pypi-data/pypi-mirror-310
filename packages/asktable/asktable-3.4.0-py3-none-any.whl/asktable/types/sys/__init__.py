c037b4003665e145651b65f9444f8baa5921e368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:28 +0000

    Add package asktable/3.4.0/asktable-3.4.0-py3-none-any.whl

---
+++ packages/asktable/asktable-3.4.0-py3-none-any.whl/asktable/types/sys/__init__.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .api_key import APIKey as APIKey
+from .project import Project as Project
+from .project_list_params import ProjectListParams as ProjectListParams
+from .project_create_params import ProjectCreateParams as ProjectCreateParams
+from .project_update_params import ProjectUpdateParams as ProjectUpdateParams
