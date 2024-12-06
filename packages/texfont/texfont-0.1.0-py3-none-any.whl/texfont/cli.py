3568bf177f100e400185ba270555db8639be8093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:06:08 +0000

    Add package texfont/0.1.0/texfont-0.1.0-py3-none-any.whl

---
+++ packages/texfont/texfont-0.1.0-py3-none-any.whl/texfont/cli.py
@@ -0,0 +1,23 @@
+"""Console script for texfont."""
+
+import typer
+from rich.console import Console
+
+from font.install import install_fonts
+
+app = typer.Typer()
+console = Console()
+
+
+@app.command()
+def main(
+        cmd: str = typer.Argument(..., help="The command to execute (e.g., 'install').")
+):
+    if cmd == "install":
+        install_fonts()
+    if cmd == "clean":
+        print("clean")
+
+
+if __name__ == "__main__":
+    app()
