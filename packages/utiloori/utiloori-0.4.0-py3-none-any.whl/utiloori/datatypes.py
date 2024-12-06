7b7ad09f4da3f9f79a5c0a00fd3b26b4ca21ce74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:11:34 +0000

    Add package utiloori/0.4.0/utiloori-0.4.0-py3-none-any.whl

---
+++ packages/utiloori/utiloori-0.4.0-py3-none-any.whl/utiloori/datatypes.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2023-present Oori Data <info@oori.dev>
+# SPDX-License-Identifier: Apache-2.0
+# utiloori.datatypes
+'''
+Tools for various data types
+'''
+from uuid import UUID
+
+def validate_uuid(uuid_str):
+    ''' Validate UUID string '''
+    try:
+        UUID(uuid_str)
+        return True
+    except ValueError:
+        return False
