4100247dab939c29e897fd98eb75dd8cce9f4674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:16 +0000

    Add package dkdc-util/0.0.1/dkdc_util-0.0.1.tar.gz

---
+++ packages/dkdc-util/dkdc_util-0.0.1.tar.gz/dkdc_util-0.0.1/readme.md
@@ -0,0 +1,14 @@
+# dkdc-util
+
+Utility functions.
+
+> [!WARNING]
+> Work in progress. Not intended for use yet.
+
+## why?
+
+simplicity.
+
+## setup
+
+TODO
---
+++ packages/dkdc-util/dkdc_util-0.0.1.tar.gz/dkdc_util-0.0.1/src/dkdc_util/__init__.py
@@ -0,0 +1,10 @@
+from dkdc_util.main import now, uuid_parts, uuid, uuid_to_parts, get_dkdc_dir, load_env
+
+__all__ = [
+    "now",
+    "uuid_parts",
+    "uuid",
+    "uuid_to_parts",
+    "get_dkdc_dir",
+    "load_env",
+]
