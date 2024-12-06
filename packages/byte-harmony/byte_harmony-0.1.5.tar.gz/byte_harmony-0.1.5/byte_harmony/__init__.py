655d41c63b37c04be04831c71bcb961cc428107b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:11 +0000

    Add package byte-harmony/0.1.5/byte_harmony-0.1.5.tar.gz

---
+++ packages/byte-harmony/byte_harmony-0.1.5.tar.gz/byte_harmony-0.1.5/byte_harmony/__init__.py
@@ -0,0 +1,6 @@
+# export AWS_ACCESS_KEY_ID=AKIA4YQKVLRVPCDNIYMI
+# export AWS_SECRET_ACCESS_KEY=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
+# export AWS_DEFAULT_REGION=us-east-1
+
+def main():
+    return "test"
---
+++ packages/byte-harmony/byte_harmony-0.1.5.tar.gz/byte_harmony-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byte_harmony",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[],
+    author="jdev",
+    author_email="jdev@radioframenetworks.com",
+    description="Python love and robots",
+    keywords="",
+    url="https://radioframenetworks.com/jacknote",
+)
