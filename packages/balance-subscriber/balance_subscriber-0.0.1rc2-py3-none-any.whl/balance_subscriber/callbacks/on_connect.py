bc79c090d341ecad9bac176eb365dd79ebebf7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:10 +0000

    Add package balance-subscriber/0.0.1rc2/balance_subscriber-0.0.1rc2-py3-none-any.whl

---
+++ packages/balance-subscriber/balance_subscriber-0.0.1rc2-py3-none-any.whl/balance_subscriber/callbacks/on_connect.py
@@ -0,0 +1,17 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    """
+    The callback for when the client receives a CONNACK response from the server.
+    https://eclipse.dev/paho/files/paho.mqtt.python/html/client.html#paho.mqtt.client.Client.on_connect
+    """
+    logger.info(f"Connected with result code {reason_code}")
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    for topic in userdata["topics"]:
+        client.subscribe(topic)
+        logger.info("Subscribed to %s", topic)
---
+++ packages/balance-subscriber/balance_subscriber-0.0.1rc2-py3-none-any.whl/balance_subscriber/callbacks/on_message.py
@@ -0,0 +1,38 @@
+import csv
+import datetime
+import logging
+from pathlib import Path
+
+import paho.mqtt.client
+
+logger = logging.getLogger(__name__)
+
+
+def on_message(
+        client: paho.mqtt.client.Client, userdata: dict, msg: paho.mqtt.client.MQTTMessage
+):
+    """
+    The callback for when a PUBLISH message is received from the server.
+
+    on_message callback
+    https://eclipse.dev/paho/files/paho.mqtt.python/html/client.html#paho.mqtt.client.Client.on_message
+
+    MQTT message class
+    https://eclipse.dev/paho/files/paho.mqtt.python/html/client.html#paho.mqtt.client.MQTTMessage
+    """
+    # Build file path
+    # Create a directory based on topic name
+    # Remove the path root (we want to make a subdirectory in our data directory)
+    # E.g. '/plant/PL-f15320/Network' becomes 'plant/PL-f15320/Network.csv'
+    topic_path = Path(msg.topic.lstrip('/') + '.csv')
+    path = Path(userdata["data_dir"]) / topic_path
+
+    # Ensure directory exits
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    timestamp = datetime.datetime.fromtimestamp(msg.timestamp, datetime.timezone.utc)
+
+    # Append to CSV file
+    with path.open("a") as file:
+        writer = csv.writer(file)
+        writer.writerow((timestamp.isoformat(), msg.payload))
