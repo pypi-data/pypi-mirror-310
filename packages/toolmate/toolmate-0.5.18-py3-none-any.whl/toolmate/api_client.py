899025713cd397ef1e270fbed4782a438439fba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:22 +0000

    Add package toolmate/0.5.18/toolmate-0.5.18-py3-none-any.whl

---
+++ packages/toolmate/toolmate-0.5.18-py3-none-any.whl/toolmate/api_client.py
@@ -0,0 +1,53 @@
+import requests, argparse, json, re
+from toolmate import config
+
+# Create the parser
+parser = argparse.ArgumentParser(description="ToolMate AI API client cli options")
+# Add arguments
+parser.add_argument("default", nargs="?", default=None, help="instruction sent to ToolMate API server")
+parser.add_argument('-a', '--all', action='store', dest='all', help="display the whole conversation; either 'plain' or 'json'")
+parser.add_argument('-k', '--key', action='store', dest='key', help="API key")
+parser.add_argument('-p', '--port', action='store', dest='port', help="server port")
+parser.add_argument('-s', '--server', action='store', dest='server', help="server address; 'http://localhost' by default")
+parser.add_argument('-t', '--tools', action='store', dest='tools', help="search enabled tools; '@' to display all; regex pattern to filter")
+# Parse arguments
+args = parser.parse_args()
+
+def main():
+    if args.tools is not None and args.tools.strip():
+        query = args.tools.strip().lower()
+        endpoint = f"{args.server if args.server else config.toolmate_api_client_host}:{args.port if args.port else config.toolmate_api_client_port}/api/tools"
+
+        url = f"""{endpoint}?query={query}"""
+        headers = {
+            "Content-Type": "application/json",
+            "X-API-Key": args.key if args.key else config.toolmate_api_client_key,
+        }
+        response = requests.post(url, headers=headers)
+        print(json.loads(response.json())["tools"])
+
+    elif args.default is not None and args.default.strip():
+        query = args.default.strip()
+        endpoint = f"{args.server if args.server else config.toolmate_api_client_host}:{args.port if args.port else config.toolmate_api_client_port}/api/toolmate"
+
+        url = f"""{endpoint}?query={query}"""
+        headers = {
+            "Content-Type": "application/json",
+            "X-API-Key": args.key if args.key else config.toolmate_api_client_key,
+        }
+        response = requests.post(url, headers=headers)
+
+        #response = requests.post(endpoint, json=prompt)
+        if args.all and args.all.lower() in ("plain", "json"):
+            if args.all.lower() == "plain":
+                for i in json.loads(response.json()):
+                    role = i.get("role", "")
+                    content = i.get("content", "")
+                    print(f"```{role}\n{content.rstrip()}\n```")
+            elif args.all.lower() == "json":
+                print(response.json())
+        else:
+            print(json.loads(response.json())[-1]["content"])
+
+if __name__ == '__main__':
+    main()
