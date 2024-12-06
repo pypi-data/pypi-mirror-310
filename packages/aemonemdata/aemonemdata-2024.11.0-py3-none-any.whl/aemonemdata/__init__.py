28f3e4bce85d84a606aeca7bfb953e216298491a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:26 +0000

    Add package aemonemdata/2024.11.0/aemonemdata-2024.11.0-py3-none-any.whl

---
+++ packages/aemonemdata/aemonemdata-2024.11.0-py3-none-any.whl/aemonemdata/__init__.py
@@ -0,0 +1,14 @@
+from .constants import (
+    EndPoint,
+    BaseUrl,
+    REGIONS,
+    AUTH_ERROR_CODES
+)
+from .aemonem import (
+    AemoNemData,
+    )
+
+from .utils import (
+    current_30min_window
+)
+__all__ = ["EndPoint","BaseUrl","REGIONS","AUTH_ERROR_CODES","AemoNemData","current_30min_window"]
