4267e6d6e8fda96d6c7f900b7908ddeab82ea180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:05:03 +0000

    Add package packageimporter/1.4.1/packageimporter-1.4.1.tar.gz

---
+++ packages/packageimporter/packageimporter-1.4.1.tar.gz/packageimporter-1.4.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='packageimporter',
+    version='1.4.1',
+    packages=find_packages(),
+    description='A really simple package that allows you to import packages to your code',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ziyan Zhou',
+    author_email='unknownuserfrommars@protonmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
