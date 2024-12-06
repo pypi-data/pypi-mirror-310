6a37f493748df6d880bd975f7147ec077d202d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:05:10 +0000

    Add package honeybee-REVIVE/0.0.15/honeybee_REVIVE-0.0.15-py3-none-any.whl

---
+++ packages/honeybee-REVIVE/honeybee_REVIVE-0.0.15-py3-none-any.whl/honeybee_revive/cli/graph.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# -*- Python Version: 3.10 -*-
+
+"""Honeybee-REVIVE result parsing commands."""
+
+import click
+import sys
+import logging
+
+
+@click.group(help="Commands for Graphing Results.")
+def graph():
+    pass
+
+
+@graph.command("winter-html-graphs")
+@click.argument("result-sql", type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True))
+@click.argument("output-name", type=str)
+@click.argument("output-file", type=click.File("w"))
+def winter_html_graphs(result_sql: str, output_name: str, output_file: click.utils.LazyFile):
+    """Create HTML graphs for the winter resilience results."""
+    raise NotImplementedError("This command is not yet implemented.....")
+    sys.exit(1)
