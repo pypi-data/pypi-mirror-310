44d73ba975c60d1de6f99312feadcbd2581b0adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:35 +0000

    Add package silverwyyapi/0.3/silverwyyapi-0.3-py3-none-any.whl

---
+++ packages/silverwyyapi/silverwyyapi-0.3-py3-none-any.whl/cloudmusic/music.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+"""
+Music object
+"""
+
+
+class Song(object):
+    def __init__(self, song_id):
+        self.song_id = song_id
+        self.name = None
+        self.artists = ()
+        self.album = None
+
+
+class Playlist(object):
+    def __init__(self, playlist_id):
+        self.playlist_id = playlist_id
+        self.name = None
+        self.creator = None
+        self.track_count = -1
+        self.tracks = ()
+
+
+class Artist(object):
+    def __init__(self, artist_id):
+        self.artist_id = artist_id
+        self.name = None
+        self.music_size = -1
+        self.album_size = -1
+        self.mv_size = -1
+        # self.alias = None
+        self.brief_desc = None
+        self.hot_songs = ()
+
+
+class Album(object):
+    def __init__(self, album_id):
+        self.album_id = album_id
+        self.name = None
+        self.artists = ()
+        self.size = -1
+        self.songs = ()
+
+
+class User(object):
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.nickname = None
+        self.gender = None
+        self.signature = None
+        self.avatar_url = None
+
+
+class CommentSet(object):
+    def __init__(self, song_id):
+        self.song_id = song_id
+        self.comments = []
+        self.hot_comments = []
+        self.more = False
+        self.more_hot = False
+        self.total = -1
+        self.offset = -1
+
+
+class HotCommentSet(object):
+    def __init__(self, song_id):
+        self.song_id = song_id
+        self.has_more = False
+        self.hot_comments = []
+        self.total = -1
+        self.offset = -1
+
+
+class Comment(object):
+    def __init__(self, comment_id, song_id):
+        self.comment_id = comment_id
+        self.song_id = song_id
+        self.be_replied = None
+        self.content = None
+        self.liked = False
+        self.liked_count = -1
+        self.time = -1
+        self.user = None
+
+
+class Lyric(object):
+    def __init__(self, song_id):
+        self.song_id = song_id
+        self.song = None
+        self.lyric = None
+        self.tlyric = None
