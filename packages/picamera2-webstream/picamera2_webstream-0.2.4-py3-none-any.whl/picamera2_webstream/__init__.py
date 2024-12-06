458cfe1b13202ba29db33739df4d6e4dacbaae90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:30 +0000

    Add package picamera2-webstream/0.2.4/picamera2_webstream-0.2.4-py3-none-any.whl

---
+++ packages/picamera2-webstream/picamera2_webstream-0.2.4-py3-none-any.whl/picamera2_webstream/__init__.py
@@ -0,0 +1,5 @@
+from .stream_picamera import VideoStream, create_app as create_picamera_app
+from .stream_ffmpeg import FFmpegStream, create_app as create_ffmpeg_app
+
+__version__ = '0.2.4'
+__all__ = ['VideoStream', 'FFmpegStream', 'create_picamera_app', 'create_ffmpeg_app']
