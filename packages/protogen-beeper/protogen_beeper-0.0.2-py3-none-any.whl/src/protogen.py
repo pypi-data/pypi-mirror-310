2151a83137b79fe2efde041c72f43b004d911cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:28 +0000

    Add package protogen-beeper/0.0.2/protogen_beeper-0.0.2-py3-none-any.whl

---
+++ packages/protogen-beeper/protogen_beeper-0.0.2-py3-none-any.whl/src/protogen.py
@@ -0,0 +1,15 @@
+def beep() -> None:
+    """Beeps"""
+    print("Beep :3")
+    
+def boop() -> None:
+    """Boops"""
+    print ("Boop :3")
+    
+def boop_nose() -> None:
+    """Gives noseboop"""
+    print (">w<")
+    
+def head_pat() -> None:
+    """Headpats"""
+    print("uwu")
