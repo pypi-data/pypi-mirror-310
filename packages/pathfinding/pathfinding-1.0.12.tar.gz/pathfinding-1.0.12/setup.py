88896c504663b363e8c8d67588f7da278e8a6a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:35 +0000

    Add package pathfinding/1.0.12/pathfinding-1.0.12.tar.gz

---
+++ packages/pathfinding/pathfinding-1.0.12.tar.gz/pathfinding-1.0.12/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pathfinding",
+    description="Pathfinding algorithms (based on Pathfinding.JS)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brean/python-pathfinding",
+    version="1.0.12",
+    license="MIT",
+    author="Andreas Bresser",
+    packages=find_packages(),
+    tests_require=["numpy", "pandas"],
+    include_package_data=True,
+    install_requires=[],
+)
