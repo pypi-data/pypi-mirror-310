e6503301ae97d73597fcd52b734d6f46fc6c8e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:55 +0000

    Add package langgraph-api/0.0.2/langgraph_api-0.0.2.tar.gz

---
+++ packages/langgraph-api/langgraph_api-0.0.2.tar.gz/langgraph_api-0.0.2/langgraph_api/js/src/hooks.mjs
@@ -0,0 +1,12 @@
+// This hook is to ensure that @langchain/langgraph package
+// found in /api folder has precendence compared to user-provided package
+// found in /deps. Does not attempt to semver check for too old packages.
+export async function resolve(specifier, context, nextResolve) {
+  const parentURL = new URL("./graph.mts", import.meta.url).toString();
+
+  if (specifier.startsWith("@langchain/langgraph")) {
+    return nextResolve(specifier, { ...context, parentURL });
+  }
+
+  return nextResolve(specifier, context);
+}
