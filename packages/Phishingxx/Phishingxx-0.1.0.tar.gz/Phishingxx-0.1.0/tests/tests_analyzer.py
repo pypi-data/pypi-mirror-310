d40cf88e5b2ccd5cf3c19a831c871a248383a1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:22 +0000

    Add package Phishingxx/0.1.0/Phishingxx-0.1.0.tar.gz

---
+++ packages/Phishingxx/Phishingxx-0.1.0.tar.gz/Phishingxx-0.1.0/tests/tests_analyzer.py
@@ -0,0 +1,14 @@
+from phishingxx.analyzer import crawl_website
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15'
+}
+
+url = "https://www.naver.com"
+blocked = crawl_website(url, headers)
+
+# 예측 수행
+if blocked:
+    print(f'URL: {url}, RESULT: 피싱 사이트')
+else:
+    print(f'URL: {url}, RESULT: 안전 사이트')
