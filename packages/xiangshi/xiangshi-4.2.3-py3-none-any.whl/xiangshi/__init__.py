1237183f5b31a600b7071798321b4662504d90cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:37 +0000

    Add package xiangshi/4.2.3/xiangshi-4.2.3-py3-none-any.whl

---
+++ packages/xiangshi/xiangshi-4.2.3-py3-none-any.whl/xiangshi/__init__.py
@@ -0,0 +1,12 @@
+from xiangsi import Functions
+from xiangsi import Calculator
+
+fun = Functions()
+cal = Calculator()
+
+weight = fun.weight
+construct = fun.construct
+cossim = cal.cossim
+minhash = cal.minhash
+simhash = cal.simhash
+jaccard = cal.jaccard
