d9dfd0514bd98ea2b5a0a432a09a211ecd8fe9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:18 +0000

    Add package petextgrid/0.0.1/petextgrid-0.0.1.tar.gz

---
+++ packages/petextgrid/petextgrid-0.0.1.tar.gz/petextgrid-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='petextgrid',
+    version='0.0.1',
+    author='blank',
+    author_email='shanandone@qq.com',
+    packages=['textgrid'],
+    test_suite='tests',
+    description='Praat TextGrid manipulation.'
+)
