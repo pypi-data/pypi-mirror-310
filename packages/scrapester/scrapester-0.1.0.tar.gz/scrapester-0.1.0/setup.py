49e76ba79c64473b9d8c5ee7fcbbc2ddfb60e9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:11:36 +0000

    Add package scrapester/0.1.0/scrapester-0.1.0.tar.gz

---
+++ packages/scrapester/scrapester-0.1.0.tar.gz/scrapester-0.1.0/setup.py
@@ -0,0 +1,35 @@
+# setup.py
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="scrapester",
+    version="0.1.0",
+    author="Naquiao",
+    author_email="ignacio@bugster.app",
+    description="Python SDK for Scrapester API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bugsterapp/scrapester",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+)
