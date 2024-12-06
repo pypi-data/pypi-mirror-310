0a74d3899180fe04d6e798665e0682af4e23507f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:39 +0000

    Add package falco-cli/0.27.0a6/falco_cli-0.27.0a6.tar.gz

---
+++ packages/falco-cli/falco_cli-0.27.0a6.tar.gz/falco_cli-0.27.0a6/src/falco_cli/config.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+from typing import TypedDict, cast
+
+try:
+    from typing import Unpack
+except ImportError:
+    from typing_extensions import Unpack
+
+import tomlkit
+
+
+class FalcoConfig(TypedDict, total=False):
+    revision: str
+    blueprint: str
+    skip: list[str]
+
+
+def parse_crud_config_from_pyproject(values: dict) -> dict:
+    return {key.lower().replace("-", "_"): value for key, value in values.items()}
+
+
+def parse_crud_config_to_pyproject(values: dict) -> dict:
+    return {key.lower().replace("_", "-"): value for key, value in values.items()}
+
+
+def write_falco_config(pyproject_path: Path, **kwargs: Unpack[TypedDict]) -> None:
+    new_falco_config = kwargs
+    new_crud_config = parse_crud_config_to_pyproject(new_falco_config.pop("crud", {}))
+
+    # pyproject = tomlkit.parse(pyproject_path.read_text()) if pyproject_path.exists() else {}
+    if pyproject_path.exists():  # noqa
+        pyproject = tomlkit.parse(pyproject_path.read_text(encoding="utf-8"))
+    else:
+        pyproject = {}
+    existing_falco_config = pyproject.get("tool", {}).get("falco", {})
+
+    existing_falco_config.update(new_falco_config)
+
+    tool = pyproject.get("tool", {})
+    tool.update({"falco": existing_falco_config})
+    pyproject["tool"] = tool
+    pyproject_path.write_text(tomlkit.dumps(pyproject), encoding="utf-8")
+
+
+def read_falco_config(pyproject_path: Path) -> FalcoConfig:
+    pyproject = tomlkit.parse(pyproject_path.read_text(encoding="utf-8"))
+    falco_config = pyproject.get("tool", {}).get("falco", {})
+    crud_config = falco_config.pop("crud", {})
+    crud_config = parse_crud_config_from_pyproject(crud_config)
+    return cast(FalcoConfig, {**falco_config, "crud": crud_config})
---
+++ packages/falco-cli/falco_cli-0.27.0a6.tar.gz/falco_cli-0.27.0a6/tests/conftest.py
@@ -0,0 +1,115 @@
+import os
+import shutil
+import subprocess
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+
+import pytest
+from cappa.testing import CommandRunner
+
+from falco_cli.__main__ import Falco
+
+
+@pytest.fixture(autouse=True)
+def change_test_dir(monkeypatch, tmp_path):
+    monkeypatch.chdir(tmp_path)
+
+
+@pytest.fixture
+def runner():
+    return CommandRunner(Falco)
+
+
+@pytest.fixture
+def django_project(tmp_path):
+    project_dir = tmp_path / "myproject"
+    subprocess.run(["django-admin", "startproject", "myproject"], check=True)
+    os.chdir(project_dir)
+
+    # Create a new Django app
+    subprocess.run(["python", "manage.py", "startapp", "blog"], check=True)
+
+    # Create a basic model in the app
+    model_code = """
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    """
+    (project_dir / "blog" / "models.py").write_text(model_code)
+
+    # Register the app in the project's settings
+    settings_file = project_dir / "myproject" / "settings.py"
+    settings_content = settings_file.read_text()
+    settings_file.write_text(
+        settings_content + "\n" + "INSTALLED_APPS += ['blog', 'django_extensions']\n"
+    )
+
+    # create a pyproject.toml
+    (project_dir / "pyproject.toml").write_text(
+        """
+[project]
+name = "myproject"
+version = "0.1.0"
+"""
+    )
+
+    yield project_dir
+    os.chdir(tmp_path)
+
+
+@pytest.fixture
+def set_git_repo_to_clean():
+    def mock_run(args, **kwargs):
+        if args == ["git", "status", "--porcelain"]:
+            mock = MagicMock()
+            mock.returncode = 0
+            mock.stdout = ""
+            return mock
+        return original_run(args, **kwargs)
+
+    original_run = subprocess.run
+
+    with patch("subprocess.run", side_effect=mock_run):
+        yield
+
+
+@pytest.fixture
+def pyproject_toml(tmp_path):
+    pyproject_toml = tmp_path / "pyproject.toml"
+    pyproject_toml.write_text(
+        """
+        [project]
+        name = "myproject"
+        version = "0.1.0"
+        """
+    )
+    yield pyproject_toml
+    pyproject_toml.unlink()
+
+
+@pytest.fixture
+def git_user_infos():
+    name = "John Doe"
+    email = "johndoe@example.com"
+
+    def mock_run(args, **kwargs):
+        if args == ["git", "config", "--global", "--get", "user.name"]:
+            mock = MagicMock()
+            mock.returncode = 0
+            mock.stdout = name
+            return mock
+        if args == ["git", "config", "--global", "--get", "user.email"]:
+            mock = MagicMock()
+            mock.returncode = 0
+            mock.stdout = email
+            return mock
+
+        return original_run(args, **kwargs)
+
+    original_run = subprocess.run
+
+    with patch("subprocess.run", side_effect=mock_run):
+        yield name, email
