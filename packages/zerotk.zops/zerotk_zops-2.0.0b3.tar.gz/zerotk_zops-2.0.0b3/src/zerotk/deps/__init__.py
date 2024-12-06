ae18f661953fc0da8b3c0d613a14840b15c043a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:07:30 +0000

    Add package zerotk.zops/2.0.0b3/zerotk_zops-2.0.0b3.tar.gz

---
+++ packages/zerotk.zops/zerotk_zops-2.0.0b3.tar.gz/zerotk_zops-2.0.0b3/src/zerotk/deps/__init__.py
@@ -0,0 +1,25 @@
+from ._deps import (
+    Declarations,
+    Dependency,
+    Deps,
+    Command,
+    Singleton,
+    Factory,
+    Attribute,
+    define,
+    field,
+    MetaClass,
+)
+
+__all__ = (
+    Declarations,
+    Dependency,
+    Deps,
+    Command,
+    Singleton,
+    Factory,
+    Attribute,
+    define,
+    field,
+    MetaClass,
+)
