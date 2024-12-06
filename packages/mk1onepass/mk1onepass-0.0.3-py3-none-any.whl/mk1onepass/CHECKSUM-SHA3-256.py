95d0b1545228fb282b0d0fe084012d3bcc7ff7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:32 +0000

    Add package mk1onepass/0.0.3/mk1onepass-0.0.3-py3-none-any.whl

---
+++ packages/mk1onepass/mk1onepass-0.0.3-py3-none-any.whl/mk1onepass/CHECKSUM-SHA3-256.py
@@ -0,0 +1,5 @@
+'''
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/mk1onepass/head/public/script/mk1onepass.py)= 9c8ca9fcaec95189812679d013782dbadf867f1d05d551ab1ad32dde784cf6f2
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/mk1onepass/head/public/pypi/mk1onepass/__init__.py)= 7d881727c533aded9702e43a3834331f92b631619b31f20fb26bd78e439fede4
+SHA3-256(/Volumes/thder_data/home_mike/svnwork/current/mk1onepass/head/public/pypi/mk1onepass/__main__.py)= 677776840335531583f3f4e5af239b64665dff74218f7b4e749ee942ac344920
+'''
