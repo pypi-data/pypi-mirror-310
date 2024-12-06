31590799b417f762ff62f72ec549bd56fe64f209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:38 +0000

    Add package dsms-sdk/2.1.0.dev0/dsms_sdk-2.1.0.dev0-py3-none-any.whl

---
+++ packages/dsms-sdk/dsms_sdk-2.1.0.dev0-py3-none-any.whl/dsms/knowledge/properties/annotations.py
@@ -0,0 +1,42 @@
+"""Annotation property of a KItem"""
+
+from typing import TYPE_CHECKING, Optional
+
+from pydantic import Field
+
+from dsms.knowledge.properties.base import KItemProperty, KItemPropertyList
+from dsms.knowledge.utils import _make_annotation_schema
+
+if TYPE_CHECKING:
+    from typing import Any, Callable, Dict
+
+
+class Annotation(KItemProperty):
+    """KItem annotation model"""
+
+    iri: str = Field(..., description="IRI of the annotation")
+    label: str = Field(..., description="Label of the annotation")
+    namespace: str = Field(..., description="Namespace of the annotation")
+    description: Optional[str] = Field(
+        None, description="Description of the annotation"
+    )
+
+
+class AnnotationsProperty(KItemPropertyList):
+    """KItemPropertyList for annotations"""
+
+    # OVERRIDE
+    @property
+    def k_property_item(cls) -> "Callable":
+        """Annotation data model"""
+        return Annotation
+
+    @property
+    def k_property_helper(cls) -> None:
+        """Not defined for Affiliations"""
+        return _make_annotation_schema
+
+    @property
+    def by_iri(cls) -> "Dict[str, Any]":
+        """Return dict of annotations per IRI"""
+        return {annotation.iri: annotation for annotation in cls}
---
+++ packages/dsms-sdk/dsms_sdk-2.1.0.dev0-py3-none-any.whl/dsms/knowledge/properties/external_links.py
@@ -0,0 +1,31 @@
+"""ExternalLink property of a KItem"""
+
+from typing import TYPE_CHECKING
+
+from pydantic import AnyUrl, Field
+
+from dsms.knowledge.properties.base import KItemProperty, KItemPropertyList
+
+if TYPE_CHECKING:
+    from typing import Callable
+
+
+class ExternalLink(KItemProperty):
+    """External link of a KItem."""
+
+    label: str = Field(..., description="Label of the external link")
+    url: AnyUrl = Field(..., description="URL of the external link")
+
+
+class ExternalLinksProperty(KItemPropertyList):
+    """KItemPropertyList for external links"""
+
+    # OVERRIDE
+    @property
+    def k_property_item(cls) -> "Callable":
+        return ExternalLink
+
+    # OVERRIDE
+    @property
+    def k_property_helper(cls) -> None:
+        """Not defined for External links"""
