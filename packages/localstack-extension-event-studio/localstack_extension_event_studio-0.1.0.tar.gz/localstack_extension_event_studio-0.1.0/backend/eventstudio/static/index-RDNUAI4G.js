672fe29f0026bcae3fd4565f0dc3bae07707a1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:18:57 +0000

    Add package localstack-extension-event-studio/0.1.0/localstack_extension_event_studio-0.1.0.tar.gz

---
+++ packages/localstack-extension-event-studio/localstack_extension_event_studio-0.1.0.tar.gz/localstack_extension_event_studio-0.1.0/backend/eventstudio/static/index-RDNUAI4G.js
@@ -0,0 +1,5 @@
+(() => {
+  // html:/Users/max/Documents/3.1_Code_Repository_Local/3.2_LocalStack_Projects/localstack-extension-event-studio/frontend/src/index.html
+  var src_default = "./index-UUISF6VF.html";
+})();
+//# sourceMappingURL=index-RDNUAI4G.js.map
