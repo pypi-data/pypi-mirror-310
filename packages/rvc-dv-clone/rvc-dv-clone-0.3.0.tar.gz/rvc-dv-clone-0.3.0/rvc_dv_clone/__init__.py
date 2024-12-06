85c83cc03762092c1821260833d06bcfefe831cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:18 +0000

    Add package rvc-dv-clone/0.3.0/rvc-dv-clone-0.3.0.tar.gz

---
+++ packages/rvc-dv-clone/rvc-dv-clone-0.3.0.tar.gz/rvc-dv-clone-0.3.0/rvc_dv_clone/__init__.py
@@ -0,0 +1,3 @@
+from .rvc_infer import *
+
+__all__ = ["rvc_infer"]
---
+++ packages/rvc-dv-clone/rvc-dv-clone-0.3.0.tar.gz/rvc-dv-clone-0.3.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="rvc-dv-clone",
+    version="0.3.0",
+    description="Retrieval-based Voice Conversion library",
+    author="Original: RVC-Project, Package: Alakxender",
+    author_email="alakxender@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=2.0.0",
+        "torchaudio>=2.0.0",
+        "numpy",
+        "scipy",
+        "librosa",
+        "fairseq",
+        "ffmpeg-python",
+        "soundfile",
+        "pyworld",
+        "faiss-cpu",
+        "torchcrepe",
+        "pyyaml",
+        "praat-parselmouth",
+    ],
+    python_requires=">=3.8",
+    include_package_data=True,
+    package_data={
+        "rvc_dv_clone": ["*.yaml", "*.json", "configs/*"],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
