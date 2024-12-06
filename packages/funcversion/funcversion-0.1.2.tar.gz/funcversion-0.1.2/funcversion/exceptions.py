97987d72e25589dd608bc892aac6adb5ade08d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:10 +0000

    Add package funcversion/0.1.2/funcversion-0.1.2.tar.gz

---
+++ packages/funcversion/funcversion-0.1.2.tar.gz/funcversion-0.1.2/funcversion/exceptions.py
@@ -0,0 +1,22 @@
+# funcversion/exceptions.py
+
+
+class VersionNotFoundError(Exception):
+    """Exception raised when a specified version is not found."""
+
+    pass
+
+class NoVersionsFoundError(Exception):
+    """Exception raised when no versions are found."""
+
+    pass
+
+class InvalidVersionError(Exception):
+    """Exception raised when an invalid version is provided."""
+
+    pass
+
+class VersionExistsError(Exception):
+    """Exception raised when a version already exists."""
+
+    pass
