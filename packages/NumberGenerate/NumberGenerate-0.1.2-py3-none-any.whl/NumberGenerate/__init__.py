9d491da3691850b94bade4ea4a8dab4061ea709b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:03:52 +0000

    Add package NumberGenerate/0.1.2/NumberGenerate-0.1.2-py3-none-any.whl

---
+++ packages/NumberGenerate/NumberGenerate-0.1.2-py3-none-any.whl/NumberGenerate/__init__.py
@@ -0,0 +1,4 @@
+from .IDCard import IDCardGenerate
+from .Name import NameGenerate
+from .Phone import PhoneGenerate
+from .SaveFile import SaveFile
---
+++ packages/NumberGenerate/NumberGenerate-0.1.2-py3-none-any.whl/NumberGenerate/errors.py
@@ -0,0 +1,2 @@
+class NumberValueError(ValueError):
+    """号码传入参数有误引起此报错"""
