d54615d9d999f4f7277f440b5a49d2211b42dac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:04:30 +0000

    Add package siphon-cli/1.2.1/siphon_cli-1.2.1.tar.gz

---
+++ packages/siphon-cli/siphon_cli-1.2.1.tar.gz/siphon_cli-1.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='siphon-cli',
+    version='1.2.1',
+    author='Morgan Joyce',
+    author_email='morganj2k@gmail.com',
+    description='A tool to efficiently extract and compress Git repository contents for LLMs.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/atxtechbro/siphon',
+    packages=find_packages(),
+    py_modules=['siphon'],
+    install_requires=[
+        'gitpython',
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'si=siphon:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
