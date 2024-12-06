5b479d69ee3e5a63dd3a19850ceef5f7ed022860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:16 +0000

    Add package xy-file/1.0.7/xy_file-1.0.7-py3-none-any.whl

---
+++ packages/xy-file/xy_file-1.0.7-py3-none-any.whl/xy_file/Object/Permission.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+__author__ = "余洋"
+__doc__ = "Permission"
+"""
+  * @File    :   Permission.py
+  * @Time    :   2024/11/06 20:27:11
+  * @Author  :   余洋
+  * @Version :   0.0.1
+  * @Contact :   yuyangit.0515@qq.com
+  * @License :   (C)Copyright 2019-2024, 希洋 (Ship of Ocean)
+  * @Desc    :   
+"""
+
+
+class Permission:
+    pass
---
+++ packages/xy-file/xy_file-1.0.7-py3-none-any.whl/xy_file/execute/Main.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+__author__ = "余洋"
+__doc__ = "Main"
+"""
+  * @File    :   Main.py
+  * @Time    :   2024/11/05 21:45:08
+  * @Author  :   余洋
+  * @Version :   0.0.1
+  * @Contact :   yuyangit.0515@qq.com
+  * @License :   (C)Copyright 2019-2024, 希洋 (Ship of Ocean)
+  * @Desc    :   
+"""
+
+from xy_file.Runner import Runner
+
+
+def main():
+    runner = Runner()
+    runner.main()
+
+
+if __name__ == "__main__":
+    main()
