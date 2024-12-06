42f4214b541842ab25ad5a77880bee1d31f62796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:01 +0000

    Add package arb/0.1/arb-0.1.tar.gz

---
+++ packages/arb/arb-0.1.tar.gz/arb-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="arb",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "ccxt",
+        "colorama==0.4.6",
+        "requests==2.31.0",
+       " pytz==2023.3",
+    ],
+    entry_points={
+        "console_scripts": [
+            "arb_runner=arb.run:main",  # Optional CLI
+        ],
+    },
+)
