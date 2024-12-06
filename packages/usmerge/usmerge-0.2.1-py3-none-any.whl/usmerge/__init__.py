f72606d2489d8f9fa9ec71a2a626dcc066ce4188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:40 +0000

    Add package usmerge/0.2.1/usmerge-0.2.1-py3-none-any.whl

---
+++ packages/usmerge/usmerge-0.2.1-py3-none-any.whl/usmerge/__init__.py
@@ -0,0 +1,18 @@
+from .usmerge import (
+    equal_wid_merge,
+    equal_fre_merge,
+    kmeans_merge,
+    som_k_merge,
+    fcm_merge,
+    kernel_density_merge,
+    information_merge,
+    gaussian_mixture_merge,
+    hierarchical_density_merge,
+    jenks_breaks_merge,
+    quantile_merge,
+    dbscan_1d_merge
+)
+
+__version__ = "0.2.1"
+__author__ = "HuiWang"
+__email__ = "huiw1128@gmail.com"
