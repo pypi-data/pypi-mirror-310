792b7b616ac68b7985422955eb4ab06b157de248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:10:01 +0000

    Add package data-safe-haven/5.1.0/data_safe_haven-5.1.0.tar.gz

---
+++ packages/data-safe-haven/data_safe_haven-5.1.0.tar.gz/data_safe_haven-5.1.0/data_safe_haven/infrastructure/components/__init__.py
@@ -0,0 +1,51 @@
+from .composite import (
+    EntraApplicationComponent,
+    EntraDesktopApplicationProps,
+    EntraWebApplicationProps,
+    LinuxVMComponentProps,
+    LocalDnsRecordComponent,
+    LocalDnsRecordProps,
+    MicrosoftSQLDatabaseComponent,
+    MicrosoftSQLDatabaseProps,
+    NFSV3BlobContainerComponent,
+    NFSV3BlobContainerProps,
+    PostgresqlDatabaseComponent,
+    PostgresqlDatabaseProps,
+    VMComponent,
+)
+from .dynamic import (
+    BlobContainerAcl,
+    BlobContainerAclProps,
+    FileShareFile,
+    FileShareFileProps,
+    SSLCertificate,
+    SSLCertificateProps,
+)
+from .wrapped import (
+    WrappedLogAnalyticsWorkspace,
+    WrappedNFSV3StorageAccount,
+)
+
+__all__ = [
+    "BlobContainerAcl",
+    "BlobContainerAclProps",
+    "EntraApplicationComponent",
+    "EntraDesktopApplicationProps",
+    "EntraWebApplicationProps",
+    "FileShareFile",
+    "FileShareFileProps",
+    "LinuxVMComponentProps",
+    "LocalDnsRecordComponent",
+    "LocalDnsRecordProps",
+    "MicrosoftSQLDatabaseComponent",
+    "MicrosoftSQLDatabaseProps",
+    "NFSV3BlobContainerComponent",
+    "NFSV3BlobContainerProps",
+    "WrappedNFSV3StorageAccount",
+    "PostgresqlDatabaseComponent",
+    "PostgresqlDatabaseProps",
+    "SSLCertificate",
+    "SSLCertificateProps",
+    "VMComponent",
+    "WrappedLogAnalyticsWorkspace",
+]
