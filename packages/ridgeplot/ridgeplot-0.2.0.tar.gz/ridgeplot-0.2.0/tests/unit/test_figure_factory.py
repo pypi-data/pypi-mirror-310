aac4155ada203387669ddd8f2a88bab2aa42c952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:25 +0000

    Add package ridgeplot/0.2.0/ridgeplot-0.2.0.tar.gz

---
+++ packages/ridgeplot/ridgeplot-0.2.0.tar.gz/ridgeplot-0.2.0/tests/unit/test_figure_factory.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from ridgeplot._figure_factory import create_ridgeplot
+
+if TYPE_CHECKING:
+    from ridgeplot._types import Densities
+
+
+class TestCreateRidgeplot:
+
+    @pytest.mark.parametrize(
+        "densities",
+        [
+            [],
+            [1, 2, 3],
+            [[1, 2, 3]],
+            [(1, 2)],
+            [[(1, 2)]],
+        ],
+    )
+    def test_densities_must_be_4d(self, densities: Densities) -> None:
+        with pytest.raises(ValueError, match="Expected a 4D array of densities"):
+            create_ridgeplot(
+                densities=densities,
+                colorscale=...,  # type: ignore[arg-type]
+                opacity=...,  # type: ignore[arg-type]
+                colormode=...,  # type: ignore[arg-type]
+                trace_labels=...,  # type: ignore[arg-type]
+                line_color=...,  # type: ignore[arg-type]
+                line_width=...,  # type: ignore[arg-type]
+                spacing=...,  # type: ignore[arg-type]
+                show_yticklabels=...,  # type: ignore[arg-type]
+                xpad=...,  # type: ignore[arg-type]
+            )
