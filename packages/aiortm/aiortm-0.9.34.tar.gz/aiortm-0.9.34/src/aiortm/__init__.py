5c636ffc0278b28ff131bbb678ca55e4a4c51e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:05:51 +0000

    Add package aiortm/0.9.34/aiortm-0.9.34.tar.gz

---
+++ packages/aiortm/aiortm-0.9.34.tar.gz/aiortm-0.9.34/src/aiortm/__init__.py
@@ -0,0 +1,26 @@
+"""Use the Remember the Milk API with aiohttp."""
+
+from .client import AioRTMClient, Auth
+from .exceptions import (
+    AioRTMError,
+    APIAuthError,
+    APIResponseError,
+    AuthError,
+    ResponseError,
+    TransportAuthError,
+    TransportResponseError,
+)
+
+__version__ = "0.9.34"
+
+__all__ = [
+    "AioRTMClient",
+    "Auth",
+    "AioRTMError",
+    "APIAuthError",
+    "APIResponseError",
+    "AuthError",
+    "ResponseError",
+    "TransportAuthError",
+    "TransportResponseError",
+]
