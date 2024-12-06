47fed8d9c4255037147c00a018df7a0c70d65966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 09:04:50 +0000

    Add package any-llm-client/1.2.0/any_llm_client-1.2.0.tar.gz

---
+++ packages/any-llm-client/any_llm_client-1.2.0.tar.gz/any_llm_client-1.2.0/examples/pydantic-settings.py
@@ -0,0 +1,26 @@
+import asyncio  # noqa: INP001
+import os
+
+import pydantic_settings
+
+import any_llm_client
+
+
+class Settings(pydantic_settings.BaseSettings):
+    llm_model: any_llm_client.AnyLLMConfig
+
+
+os.environ["LLM_MODEL"] = """{
+    "api_type": "openai",
+    "url": "http://127.0.0.1:11434/v1/chat/completions",
+    "model_name": "qwen2.5-coder:1.5b"
+}"""
+settings = Settings()
+
+
+async def main() -> None:
+    async with any_llm_client.get_client(settings.llm_model) as client:
+        print(await client.request_llm_message("Кек, чо как вообще на нарах?"))  # noqa: T201
+
+
+asyncio.run(main())
