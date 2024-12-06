3243460fa734d9f86800e6dd53e89f24ac24a080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:41 +0000

    Add package ampel-interface/0.10.2/ampel_interface-0.10.2-py3-none-any.whl

---
+++ packages/ampel-interface/ampel_interface-0.10.2-py3-none-any.whl/ampel/cli/config.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File:                Ampel-interface/ampel/cli/config.py
+# License:             BSD-3-Clause
+# Author:              valery brinnel <firstname.lastname@gmail.com>
+# Date:                20.08.2022
+# Last Modified Date:  20.08.2022
+# Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
+
+from os import environ, makedirs, path
+
+from platformdirs import user_data_dir
+
+
+def get_user_data_config_path() -> str:
+	"""
+	:returns: full path to yaml ampel conf
+	"""
+
+	if env := environ.get('AMPEL_CONFIG'):
+		return env
+
+	if env := (environ.get('CONDA_PREFIX') or environ.get('VIRTUAL_ENV')):
+
+		app_path = path.join(env, 'share')
+		if not path.exists(app_path):
+			raise ValueError('Conda path does not contain a shared folder?')
+
+		app_path = path.join(app_path, 'ampel')
+		if not path.exists(app_path):
+			makedirs(app_path)
+
+		# ex: /Users/hu/miniconda3/envs/myCondaEnv/share/ampel/conf.yml
+		return path.join(app_path, "conf.yml")
+
+	app_path = user_data_dir("ampel")
+	if not path.exists(app_path):
+		makedirs(app_path)
+
+	app_path = path.join(app_path, "conf")
+	if not path.exists(app_path):
+		makedirs(app_path)
+
+	# ex: /Users/hu/Library/Application Support/ampel/conf.yml
+	return path.join(app_path, "conf.yml")
