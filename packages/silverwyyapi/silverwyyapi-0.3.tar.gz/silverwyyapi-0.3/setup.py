0f3a918cc7d8682afb486e86e4d00951b29d7224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:35 +0000

    Add package silverwyyapi/0.3/silverwyyapi-0.3.tar.gz

---
+++ packages/silverwyyapi/silverwyyapi-0.3.tar.gz/silverwyyapi-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="silverwyyapi",
+    version="0.3",
+    packages=["cloudmusic"],
+    package_dir={"": "src"},
+    zip_safe=False,
+    install_requires=[
+        "requests~=2.31.0",
+        "urllib3~=2.0.7",
+        "pycryptodomex~=3.21.0",
+    ],
+
+    url="https://github.com/SilverW0o0W",
+    author="Silver",
+    author_email="silver.codingcat@gmail.com",
+    license='MIT',
+    description='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    entry_points={
+    }
+)
