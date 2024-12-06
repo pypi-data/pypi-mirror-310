addbef252d61ea75f4637ef2ef550d3d6eca5f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:41 +0000

    Add package inf-rvc-py/1.0.0/inf_rvc_py-1.0.0.tar.gz

---
+++ packages/inf-rvc-py/inf_rvc_py-1.0.0.tar.gz/inf_rvc_py-1.0.0/setup.py
@@ -0,0 +1,41 @@
+import os
+import platform
+import pkg_resources
+from setuptools import find_packages, setup
+
+setup(
+    name="inf-rvc-py",
+    version="1.0.0",
+    description="Python wrapper for fast inference with rvc",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    readme="README.md",
+    python_requires=">=3.10",
+    author="kindahex",
+    url="https://github.com/kindahex/rvc-py",
+    packages=find_packages(),
+    package_data={'': ['*.txt', '*.md', '*.rep', '*.pickle']},
+    install_requires=[
+        "torch",
+        "torchaudio",
+        "gradio",
+        "praat-parselmouth>=0.4.3",
+        "pyworld==0.3.2",
+        "faiss-cpu==1.7.3",
+        "torchcrepe==0.0.20",
+        "ffmpeg-python>=0.2.0",
+        "fairseq==0.12.2",
+        "typeguard==4.2.0",
+        "soundfile",
+        "librosa",
+        "numpy",
+        "yt_dlp",
+        "audio-separator[gpu]",
+    ],
+    include_package_data=True,
+    extras_require={"all": [
+        "scipy",
+        "numba==0.56.4",
+        "edge-tts"
+        ]},
+)
