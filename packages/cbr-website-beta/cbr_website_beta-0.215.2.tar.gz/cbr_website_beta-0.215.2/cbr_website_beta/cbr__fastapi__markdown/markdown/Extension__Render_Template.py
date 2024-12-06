b6592142402219105b346415633b814e7edfdff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:23 +0000

    Add package cbr-website-beta/0.215.2/cbr_website_beta-0.215.2.tar.gz

---
+++ packages/cbr-website-beta/cbr_website_beta-0.215.2.tar.gz/cbr_website_beta-0.215.2/cbr_website_beta/cbr__fastapi__markdown/markdown/Extension__Render_Template.py
@@ -0,0 +1,9 @@
+from markdown import Extension
+
+from osbot_markdown.markdown.md__pre_processors.Pre_Processor__Render_Template import Pre_Processor__Render_Template
+
+# todo: SECURITY: check for the security implications of this extension (since although in principle there should be no sensitive data in the
+#       render_template views (all sensitive data should be provided to the view), we need to double check this)
+class Extension__Render_Template(Extension):
+    def extendMarkdown(self, md):
+        md.preprocessors.register(Pre_Processor__Render_Template(md), 'flask_template', 175)
