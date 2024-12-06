c22072fca4d63783ee105d667fe5cd4fa5149e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:06:03 +0000

    Add package akshmodeone/0.2.0/akshmodeone-0.2.0.tar.gz

---
+++ packages/akshmodeone/akshmodeone-0.2.0.tar.gz/akshmodeone-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+setup(
+    name='akshmodeone',
+    version='0.2.0',
+    description='A samlpe python module',
+    author='akash c abhilash',
+    author_email='akash@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+)
+
+
+
