9794e48b7ac9a70841bd00382239f20a16f3641a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:32 +0000

    Add package retx/0.4/retx-0.4.tar.gz

---
+++ packages/retx/retx-0.4.tar.gz/retx-0.4/retx/__main__.py
@@ -0,0 +1,40 @@
+#!python
+import sys
+import argparse
+from u import csv_deserialize, csv_serialize
+from u import json_deserialize, json_serialize
+
+parser = argparse.ArgumentParser(
+    description='A bit of extra help to transform stuff in the command line',
+    epilog='Your feedback is appreciated: https://github.com/andyil/retx')
+
+parser.add_argument('-i', '--input', action='store', nargs='*', default=['stdin'])
+parser.add_argument('-o', '--output', action='store', default='stdout', nargs='?')
+parser.add_argument('-c', '--cmd', action='store', choices=('j2c', 'c2j'))
+
+
+i = parser.parse_args()
+cmd = i.cmd
+if cmd == 'j2c':
+    serializer = csv_serialize
+    deserializer = json_deserialize
+
+else: #cmd == 'c2j'
+    serializer = json_serialize
+    deserializer = csv_deserialize
+
+if i.output == 'stdout':
+    outfile = sys.stdout
+elif i.output == 'stderr':
+    outfile = sys.stderr
+else:
+    outfile = open(i.output, 'w')
+
+for inp in i.input:
+    if inp == 'stdin':
+        s = sys.stdin
+    else:
+        s = open(inp, 'r')
+    serializer(deserializer(s), outfile)
+
+
