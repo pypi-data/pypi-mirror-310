3a6b4279a7e5ad06da86a02f6916e15b1a466037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:15 +0000

    Add package neuromeka/3.3.2.1/neuromeka-3.3.2.1-py3-none-any.whl

---
+++ packages/neuromeka/neuromeka-3.3.2.1-py3-none-any.whl/neuromeka/proto/__init__.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+generated_files_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(generated_files_path)
+
+path_to_remove1 = "/home/user/dev/runtest/Release/IndyDeployment/PythonMiddleware/interfaces/impl"
+path_to_remove2 = "/home/user/release/IndyDeployment/PythonMiddleware/interfaces/impl"
+if path_to_remove1 in sys.path: sys.path.remove(path_to_remove1)
+if path_to_remove2 in sys.path: sys.path.remove(path_to_remove2)
+
+from .ethercat_pb2_grpc import *
+from .control_pb2_grpc import *
+from .device_pb2_grpc import *
+from .config_pb2_grpc import *
+from .rtde_pb2_grpc import *
+from .moby_pb2_grpc import *
+from .cri_pb2_grpc import *
+from .hri_pb2_grpc import *
+
+from . import ethercat_msgs_pb2 as ethercat_msgs
+from . import common_msgs_pb2 as common_msgs
+from . import control_msgs_pb2 as control_msgs
+from . import config_msgs_pb2 as config_msgs
+from . import device_msgs_pb2 as device_msgs
+from . import rtde_msgs_pb2 as rtde_msgs
+from . import moby_msgs_pb2 as moby_msgs
+from . import cri_pb2 as cri_msgs
+from . import hri_msgs_pb2 as hri_msgs
+from . import shared_msgs_pb2 as shared_msgs
