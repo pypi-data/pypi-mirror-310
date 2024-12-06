09b1084536f760a29c6809c0aa39f9c5a57ee63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 13:25:42 +0000

    Add package deepview-converter/2.6.9/deepview_converter-2.6.9-cp311-cp311-win_amd64.whl

---
+++ packages/deepview-converter/deepview_converter-2.6.9-cp311-cp311-win_amd64.whl/deepview/converter/__main__.py
@@ -0,0 +1,16 @@
+# Copyright 2018 by Au-Zone Technologies.  All Rights Reserved.
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential.
+#
+# This source code is provided solely for runtime interpretation by Python.
+# Modifying or copying any source code is explicitly forbidden.
+
+import deepview.converter.convert as deepview_converter
+
+def main():
+    deepview_converter.main()
+    
+if __name__ == "__main__":
+    main()
+    
