eca9b48891e2eef3fe02d2600d6ea7873eb5ba75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:38 +0000

    Add package gpu-active/0.1.0/gpu_active-0.1.0-py3-none-any.whl

---
+++ packages/gpu-active/gpu_active-0.1.0-py3-none-any.whl/gpu_active/__init__.py
@@ -0,0 +1,4 @@
+from .get_gpu import get_default_device, to_device, DeviceDataLoader
+
+# Optional: Define what gets imported with `from get_gpu import *`
+__all__ = ['get_default_device', 'to_device', 'DeviceDataLoader']
