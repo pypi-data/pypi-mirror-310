2e16b34043f6784e454aeb192eece0c3aa1bdfd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:35 +0000

    Add package zfs-replicate/4.0.0/zfs_replicate-4.0.0-py3-none-any.whl

---
+++ packages/zfs-replicate/zfs_replicate-4.0.0-py3-none-any.whl/zfs/replicate/filesystem/__init__.py
@@ -0,0 +1,16 @@
+"""ZFS FileSystem Operations."""
+
+from .create import create as create  # noqa: F401 # pylint: disable=C0414
+from .destroy import destroy as destroy  # noqa: F401 # pylint: disable=C0414
+from .type import FileSystem as FileSystem  # pylint: disable=C0414
+from .type import filesystem as filesystem  # pylint: disable=C0414
+
+
+def remote_dataset(remote: FileSystem, local: FileSystem) -> FileSystem:
+    """Remote dataset for the remote and local pair."""
+    return filesystem(name=remote.name + "/" + local.dataset)
+
+
+def remote_filesystem(remote: FileSystem, local: FileSystem) -> FileSystem:
+    """Remote filesystem for the remote and local pair."""
+    return filesystem(name=remote.name + "/" + local.name)
---
+++ packages/zfs-replicate/zfs_replicate-4.0.0-py3-none-any.whl/zfs/replicate/list.py
@@ -0,0 +1,36 @@
+"""Common List Functions."""
+
+from typing import List, Sequence, Tuple, TypeVar
+
+__all__ = ("inits", "venn")
+
+T = TypeVar("T")
+
+
+def inits(elements: Sequence[T]) -> Sequence[Sequence[T]]:
+    """All initial segments of the given list.
+
+    The shortest lists are first.
+
+    >>> inits(["a", "b", "c"])
+    [[], ['a'], ['a', 'b'], ['a', 'b', 'c']]
+    """
+    return [elements[:n] for n in range(len(elements) + 1)]
+
+
+def venn(
+    lefts: List[T],
+    rights: List[T],
+) -> Tuple[List[T], List[T], List[T]]:
+    """Calculate venn diagram of the two sequences.
+
+    A venn diagram shows the elements both sets contain individually as
+    well as the elements they have in common.  We return a 3-tuple with
+    the left only, common, and right only elements in its respective
+    components.
+    """
+    return (
+        [x for x in lefts if x not in rights],
+        [x for x in lefts if x in rights],
+        [x for x in rights if x not in lefts],
+    )
