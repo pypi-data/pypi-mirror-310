533282425911b2f7643b2d362213d4f5dd7ae43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:04:56 +0000

    Add package pydia3/0.1.0/pydia3-0.1.0.tar.gz

---
+++ packages/pydia3/pydia3-0.1.0.tar.gz/pydia3-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import platform
+
+setup(
+    name="pydia3",
+    version="0.1.0",
+    description="Python interface for Microsoft's Debug Interface Access (DIA) SDK.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Michael K. Steinberg",
+    author_email="m.kuper.steinberg@gmail.com",
+    url="https://github.com/Michael-K-Stein/pydia",
+    license="MIT",  # Update with your license
+    packages=find_packages(),
+    package_data={
+        "pydia": ["*.pyd"] if platform.system() == "Windows" else ["*.so"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.12",
+)
