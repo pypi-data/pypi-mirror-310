1914597230078ca1efb583a2c5935debc1ce1bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:30 +0000

    Add package dify-oapi/0.0.2/dify_oapi-0.0.2.tar.gz

---
+++ packages/dify-oapi/dify_oapi-0.0.2.tar.gz/dify_oapi-0.0.2/dify_oapi/api/dify/v1/version.py
@@ -0,0 +1,12 @@
+from dify_oapi.core.model.config import Config
+
+from .resource import Audio, File, Message, Meta, Parameter
+
+
+class V1:
+    def __init__(self, config: Config):
+        self.meta: Meta = Meta(config)
+        self.file: File = File(config)
+        self.audio: Audio = Audio(config)
+        self.parameter: Parameter = Parameter(config)
+        self.message: Message = Message(config)
