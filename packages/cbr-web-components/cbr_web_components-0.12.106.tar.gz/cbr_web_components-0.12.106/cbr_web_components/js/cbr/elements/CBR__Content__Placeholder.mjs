e90f296a55f6981ff3044f0b78edecaceb7b2699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:55 +0000

    Add package cbr-web-components/0.12.106/cbr_web_components-0.12.106.tar.gz

---
+++ packages/cbr-web-components/cbr_web_components-0.12.106.tar.gz/cbr_web_components-0.12.106/cbr_web_components/js/cbr/elements/CBR__Content__Placeholder.mjs
@@ -0,0 +1,25 @@
+import Div from "../../core/Div.mjs"
+import Raw_Html from "../../core/Raw_Html.mjs"
+
+export default class CBR__Content__Placeholder extends Div {
+    constructor({id, ...kwargs}={}) {
+        kwargs.class = `content-placeholder ${kwargs.class || ''}`
+        super({id, ...kwargs})
+
+        const div_container = new Div({ id: 'placeholder-container', class: 'placeholder-container'                                    })
+
+        this.add_element(div_container)
+
+
+    }
+
+    static css_rules() {
+        return {
+            ".placeholder-container": { height          : '100%',
+                                        width           : '100%',
+                                        backgroundColor : "#eef5f9",                          // Light base color
+                                        boxShadow       : "inset 10px 20px 30px rgba(0,0,0,0.07)",  // Soft shadow
+            }
+        }
+    }
+}
