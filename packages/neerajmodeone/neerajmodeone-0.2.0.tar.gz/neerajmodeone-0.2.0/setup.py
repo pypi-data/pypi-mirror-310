0022330b1fb339f32384f640b1ba2aad8b2376c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:06:02 +0000

    Add package neerajmodeone/0.2.0/neerajmodeone-0.2.0.tar.gz

---
+++ packages/neerajmodeone/neerajmodeone-0.2.0.tar.gz/neerajmodeone-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = 'neerajmodeone',     # package
+    version= '0.2.0',           # version
+    description= 'A sample Python module',
+    author= 'Neeraj V N',
+    author_email= 'weldernaru@gmail.com',
+    package = find_packages(),         # Automathically find modules
+    install_requires = [],             # Dependencies (if any)
+    python_requires = '>=3.6',          # Supported Python versions
+)
+
