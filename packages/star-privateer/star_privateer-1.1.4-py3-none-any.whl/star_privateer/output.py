27eb9c0ed9a447a4f7b2b8bcb2d7e976b033366c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:46 +0000

    Add package star-privateer/1.1.4/star_privateer-1.1.4-py3-none-any.whl

---
+++ packages/star-privateer/star_privateer-1.1.4-py3-none-any.whl/star_privateer/output.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Copyright 2023 Sylvain Breton
+
+This file is part of star-privateer.
+
+star-privateer is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+star-privateer is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+star-privateer. If not, see <https://www.gnu.org/licenses/>.  
+'''
+
+def write_output (filename, dp_name, dp) :
+  '''
+  Write a data product into a csv file. 
+  '''
+  return
