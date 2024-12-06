cb555be780b335dd5b3d55a4e204eb770ddd2d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:41 +0000

    Add package source-translator/1.0.1/source_translator-1.0.1.tar.gz

---
+++ packages/source-translator/source_translator-1.0.1.tar.gz/source_translator-1.0.1/src/source_translator/__init__.py
@@ -0,0 +1,7 @@
+from .python_source import IndentationManager, Range, SourceCode, AstTranslator, IndentationStyle
+from .c_like import AllmanStyle, KandRStyle, WhitesmithsStyle, GnuStyle, CLike
+
+__all__ = [
+    "IndentationManager", "Range", "SourceCode", "AstTranslator", "IndentationStyle",
+    "AllmanStyle", "KandRStyle", "WhitesmithsStyle", "GnuStyle", "CLike",
+]
