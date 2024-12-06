1ed0ea407031eec18494205265c1a1570f4923ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:17:54 +0000

    Add package lib-mx-colab/0.2.1/lib_mx_colab-0.2.1.tar.gz

---
+++ packages/lib-mx-colab/lib_mx_colab-0.2.1.tar.gz/lib_mx_colab-0.2.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='lib_mx_colab',  # Cambia por el nombre de tu librería
+    version='0.2.1',
+    author='IA (Sistema de Intres)',
+    author_email='system.ai.of.interest@gmail.com',
+    description='IA Sistema de Intres',
+    #long_description=open('README.md').read(),  # Asegúrate de tener este archivo
+    #long_description_content_type='text/markdown',
+    url='https://www.youtube.com/@IA.Sistema.de.Interes',  # Opcional
+    packages=find_packages(),  # Encuentra todos los paquetes y módulos
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+        'base64',
+        'IPython',
+    ],
+)
+
