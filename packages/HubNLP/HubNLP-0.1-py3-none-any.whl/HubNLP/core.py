7f2a7cbc747b973eb65d1377c1b7f216fa6dd704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:32:21 +0000

    Add package HubNLP/0.1/HubNLP-0.1-py3-none-any.whl

---
+++ packages/HubNLP/HubNLP-0.1-py3-none-any.whl/HubNLP/core.py
@@ -0,0 +1,12 @@
+def print_developer_info():
+    """
+    Prints the developer's name and library details.
+    """
+    print("Library Name: HubNLP")
+    print("Developed by: Nexio Tech")
+
+def print_library_tagline():
+    """
+    Prints a tagline for the library.
+    """
+    print("HubNLP - Simplifying NLP for Everyone!")
