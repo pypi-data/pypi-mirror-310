b48b69d8912e9cfe43c795b08b01f9be98f57c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:11:40 +0000

    Add package laneatt/2.4.3/laneatt-2.4.3.tar.gz

---
+++ packages/laneatt/laneatt-2.4.3.tar.gz/laneatt-2.4.3/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+
+setup(
+  name = 'laneatt',
+  packages = ['laneatt', 'laneatt.utils'],
+  package_data={'laneatt': ['config/backbones.yaml']},
+  include_package_data=True,
+  version = '2.4.3',
+  license='MIT',
+  description = 'A package to detect lane lines in images and videos',
+  author = 'Paolo Reyes',
+  author_email = 'paolo.alfonso.reyes@gmail.com',
+  url = 'https://github.com/PaoloReyes/RealTime-LaneATT',
+  download_url = 'https://github.com/PaoloReyes/RealTime-LaneATT/archive/refs/tags/LaneATT-v2.4.tar.gz',
+  keywords = ['lanes', 'AI', 'greenhouse', 'detection', 'machine learning', 'laneatt', 'delimitations'],
+  install_requires=[
+          'filelock',
+          'fsspec',
+          'Jinja2',
+          'joblib',
+          'MarkupSafe',
+          'mpmath',
+          'matplotlib',
+          'networkx',
+          'numpy',
+          'opencv-python',
+          'pillow',
+          'PyYAML',
+          'scikit-learn',
+          'scipy',
+          'setuptools',
+          'sympy',
+          'threadpoolctl',
+          'torch',
+          'torchaudio',
+          'torchvision',
+          'tqdm',
+          'typing_extensions',
+          'wheel',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+
+    'Intended Audience :: Developers', # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License', # License
+
+    'Programming Language :: Python :: 3', # Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.12',
+  ],
+)
