a759ae57833695eff0b47efefc67fb60bec1ab00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:11 +0000

    Add package liteflask/7.2/liteflask-7.2.tar.gz

---
+++ packages/liteflask/liteflask-7.2.tar.gz/liteflask-7.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='liteflask',
+    version='7.2',
+    packages=find_packages(),
+    install_requires=[],
+    package_data={'liteflask': ['*.py','*.txt','*.sh']},
+)
