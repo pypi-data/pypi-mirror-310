11a8f17588d8fe56fd9b3f6074ef3a2887fcd652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 07:04:39 +0000

    Add package eolymp/0.12.7/eolymp-0.12.7-py3-none-any.whl

---
+++ packages/eolymp/eolymp-0.12.7-py3-none-any.whl/eolymp/atlas/statement_pb2.pyi
@@ -0,0 +1,26 @@
+from eolymp.ecm import content_pb2 as _content_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Statement(_message.Message):
+    __slots__ = ["author", "automatic", "content", "download_link", "id", "locale", "source", "title"]
+    AUTHOR_FIELD_NUMBER: _ClassVar[int]
+    AUTOMATIC_FIELD_NUMBER: _ClassVar[int]
+    CONTENT_FIELD_NUMBER: _ClassVar[int]
+    DOWNLOAD_LINK_FIELD_NUMBER: _ClassVar[int]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    LOCALE_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_FIELD_NUMBER: _ClassVar[int]
+    TITLE_FIELD_NUMBER: _ClassVar[int]
+    author: str
+    automatic: bool
+    content: _content_pb2.Content
+    download_link: str
+    id: str
+    locale: str
+    source: str
+    title: str
+    def __init__(self, id: _Optional[str] = ..., locale: _Optional[str] = ..., automatic: bool = ..., title: _Optional[str] = ..., content: _Optional[_Union[_content_pb2.Content, _Mapping]] = ..., download_link: _Optional[str] = ..., author: _Optional[str] = ..., source: _Optional[str] = ...) -> None: ...
