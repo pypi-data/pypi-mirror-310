0206a01c00fd553f3194a0f9ca3c6389ea126504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:54 +0000

    Add package rs232-to-pdu/1.1.0/rs232_to_pdu-1.1.0-py3-none-any.whl

---
+++ packages/rs232-to-pdu/rs232_to_pdu-1.1.0-py3-none-any.whl/rs232_to_pdu/parsers/__init__.py
@@ -0,0 +1,7 @@
+"""
+rs232-to-pdu
+Copyright (C) 2024 InkBridge Networks (legal@inkbridge.io)
+
+rs232-to-pdu © 2024 by InkBridge is licensed under CC BY-NC 4.0. To view a copy
+of this license, visit https://creativecommons.org/licenses/by-nc/4.0/
+"""
