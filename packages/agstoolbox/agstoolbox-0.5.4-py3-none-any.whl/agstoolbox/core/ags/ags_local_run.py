7b3858a5199e83589ac0d52d0468082fab578b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 20:05:49 +0000

    Add package agstoolbox/0.5.4/agstoolbox-0.5.4-py3-none-any.whl

---
+++ packages/agstoolbox/agstoolbox-0.5.4-py3-none-any.whl/agstoolbox/core/ags/ags_local_run.py
@@ -0,0 +1,25 @@
+from __future__ import annotations  # for python 3.8
+
+from agstoolbox.core.ags.ags_editor import LocalAgsEditor
+from agstoolbox.core.ags.game_project import GameProject
+from agstoolbox.core.utils.file import get_absolute_path
+from agstoolbox.core.utils.run import run_exe_params
+
+
+def ags_editor_start(editor: LocalAgsEditor, block: bool = False) -> int:
+    return run_exe_params(editor.path, block)
+
+
+def ags_editor_load(editor: LocalAgsEditor, project: GameProject, block: bool = False) -> int:
+    project_path = get_absolute_path(project.path)
+    return run_exe_params(editor.path, block, [project_path], )
+
+
+def ags_editor_build(editor: LocalAgsEditor, project: GameProject, block: bool = False) -> int:
+    project_path = get_absolute_path(project.path)
+    return run_exe_params(editor.path, block, ['/compile', project_path])
+
+
+def ags_editor_template_build(editor: LocalAgsEditor, project: GameProject, block: bool = False) -> int:
+    project_path = get_absolute_path(project.path)
+    return run_exe_params(editor.path, block, ['/maketemplate', project_path])
