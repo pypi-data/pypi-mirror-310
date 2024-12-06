ff39db334d861a1aa91d128ca4735fd8c4ee1ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:28 +0000

    Add package svg-ultralight/0.33.0/svg_ultralight-0.33.0.tar.gz

---
+++ packages/svg-ultralight/svg_ultralight-0.33.0.tar.gz/svg_ultralight-0.33.0/tests/conftest.py
@@ -0,0 +1,11 @@
+"""Test configuration for pytest.
+
+:author: Shay Hill
+:created: 7/2/2019
+"""
+
+
+def pytest_assertrepr_compare(config, op, left, right):
+    """See full error diffs."""
+    if op in ("==", "!="):
+        return [f"{left} {op} {right}"]
