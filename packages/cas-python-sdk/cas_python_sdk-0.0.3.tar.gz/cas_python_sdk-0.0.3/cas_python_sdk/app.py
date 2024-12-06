7e30bc95b21ba63268c4c7a1401d6354edc80369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 18:12:26 +0000

    Add package cas-python-sdk/0.0.3/cas_python_sdk-0.0.3.tar.gz

---
+++ packages/cas-python-sdk/cas_python_sdk-0.0.3.tar.gz/cas_python_sdk-0.0.3/cas_python_sdk/app.py
@@ -0,0 +1,4 @@
+import cas_python_sdk
+
+def testing():
+    return cas_python_sdk.sum_as_string(1, 2)
---
+++ packages/cas-python-sdk/cas_python_sdk-0.0.3.tar.gz/cas_python_sdk-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cas_python_sdk',              # Replace with your project name
+    version='0.0.3',                # Version of your package
+    description='A Python package that provides an abstraction layer to the Rust Crypto suite of algorithms',
+    author='Mike Mulchrone',
+    author_email='',
+    url='https://github.com/yourusername/my_project',  # Project URL
+    packages=find_packages(),       # Automatically find packages
+    install_requires=[              # List of dependencies
+        'requests'            # Example
+    ],
+    classifiers=[                   # Optional classifiers (helps PyPI categorize your project)
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',         # Minimum Python version required
+)
