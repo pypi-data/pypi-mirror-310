11cd78d2d65d6654f386a229bed3c281efd93c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:11:28 +0000

    Add package colab-notebook-id/0.1/colab_notebook_id-0.1.tar.gz

---
+++ packages/colab-notebook-id/colab_notebook_id-0.1.tar.gz/colab_notebook_id-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="colab_notebook_id",
+    version="0.1",
+    description="Retrieve Google Colab notebook ID dynamically",
+    author="Xtrail AI ",
+    author_email="info@xtrail.in",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
