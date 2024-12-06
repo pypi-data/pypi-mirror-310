3859ef54bbb851ce758b1c54bfb4b8b25c64eae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:38 +0000

    Add package ex4nicegui/0.8.4/ex4nicegui-0.8.4.tar.gz

---
+++ packages/ex4nicegui/ex4nicegui-0.8.4.tar.gz/ex4nicegui-0.8.4/ex4nicegui/reactive/officials/radio.py
@@ -0,0 +1,68 @@
+from typing import (
+    Any,
+    Callable,
+    List,
+    Optional,
+    TypeVar,
+    Dict,
+    Union,
+)
+from ex4nicegui.reactive.services.reactive_service import ParameterClassifier
+from ex4nicegui.utils.signals import (
+    TGetterOrReadonlyRef,
+    _TMaybeRef as TMaybeRef,
+    to_value,
+)
+from nicegui import ui
+from ex4nicegui.reactive.mixins.value_element import ValueElementMixin
+from .base import BindableUi
+
+T = TypeVar("T")
+
+
+class RadioBindableUi(BindableUi[ui.radio], ValueElementMixin[Any]):
+    def __init__(
+        self,
+        options: Union[TMaybeRef[List], TMaybeRef[Dict]],
+        *,
+        value: TMaybeRef[Any] = None,
+        on_change: Optional[Callable[..., Any]] = None,
+    ) -> None:
+        pc = ParameterClassifier(
+            locals(),
+            maybeRefs=[
+                "options",
+                "value",
+            ],
+            v_model=("value", "on_change"),
+            events=["on_change"],
+        )
+
+        value_kws = pc.get_values_kws()
+
+        element = ui.radio(**value_kws)
+        super().__init__(element)  # type: ignore
+
+        for key, value in pc.get_bindings().items():
+            self.bind_prop(key, value)  # type: ignore
+
+    @property
+    def value(self):
+        return self.element.value
+
+    def bind_prop(self, prop: str, value: TGetterOrReadonlyRef):
+        if ValueElementMixin._bind_specified_props(self, prop, value):
+            return self
+
+        if prop == "options":
+            return self.bind_options(value)
+
+        return super().bind_prop(prop, value)
+
+    def bind_options(self, options: TGetterOrReadonlyRef):
+        @self._ui_signal_on(options, deep=True)
+        def _():
+            ParameterClassifier.mark_event_source_as_internal(self.element)
+            self.element.set_options(to_value(options))
+
+        return self
---
+++ packages/ex4nicegui/ex4nicegui-0.8.4.tar.gz/ex4nicegui-0.8.4/ex4nicegui/reactive/systems/reactive_system.py
@@ -0,0 +1,21 @@
+from typing import Callable, Dict
+from ex4nicegui.utils.signals import to_value, to_raw
+
+
+def convert_kws_ref2value(kws: Dict) -> Dict:
+    return {key: to_raw(to_value(value)) for key, value in kws.items()}
+
+
+def inject_method(obj, method_name: str, new_handle: Callable):
+    if hasattr(obj, method_name):
+        original_method = getattr(obj, method_name)
+
+        def injected_method(*args, **kwargs):
+            new_handle(*args, **kwargs)
+            return original_method(*args, **kwargs)
+
+        setattr(obj, method_name, injected_method)
+    else:
+        raise AttributeError(
+            f"'{type(obj).__name__}' object has no attribute '{method_name}'."
+        )
