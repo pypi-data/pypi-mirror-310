b57c3c86e15dedb15feb063689026b2c9023197d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:32:00 +0000

    Add package QuotesEngine/1.0.0/quotesengine-1.0.0.tar.gz

---
+++ packages/QuotesEngine/quotesengine-1.0.0.tar.gz/quotesengine-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="QuotesEngine",
+    version="1.0.0",
+    description="A Python library to generate some motivation quotes.",
+    author="Sridharan",
+    author_email="sridharansri2312@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/QuotesEngine/quotesengine-1.0.0.tar.gz/quotesengine-1.0.0/tests/test.py
@@ -0,0 +1,2 @@
+from QuotesEngine import Motivate
+
