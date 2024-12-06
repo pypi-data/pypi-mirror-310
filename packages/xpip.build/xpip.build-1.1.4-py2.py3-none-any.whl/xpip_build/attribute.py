022a8c327f725485aebd54b6e2716c9380d54461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 05:05:54 +0000

    Add package xpip.build/1.1.4/xpip.build-1.1.4-py2.py3-none-any.whl

---
+++ packages/xpip.build/xpip.build-1.1.4-py2.py3-none-any.whl/xpip_build/attribute.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from urllib.parse import urljoin
+
+__project__ = "xpip.build"
+__version__ = "1.1.4"
+__description__ = "Python package. Build. Install."
+__url_home__ = "https://github.com/bondbox/xpip/"
+__url_code__ = __url_home__
+__url_docs__ = __url_home__
+__url_bugs__ = urljoin(__url_home__, "issues")
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
---
+++ packages/xpip.build/xpip.build-1.1.4-py2.py3-none-any.whl/xpip_upload/attribute.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from urllib.parse import urljoin
+
+__project__ = "xpip.upload"
+__version__ = "1.1.1"
+__description__ = "upload python package via twine"
+__url_home__ = "https://github.com/bondbox/xpip/"
+__url_code__ = __url_home__
+__url_docs__ = __url_home__
+__url_bugs__ = urljoin(__url_home__, "issues")
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
