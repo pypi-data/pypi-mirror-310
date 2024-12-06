c49df5b1e6dbb5693c531611fdbda5f1c7413bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:18:05 +0000

    Add package pymultifit/0.1.4/pymultifit-0.1.4-py3-none-any.whl

---
+++ packages/pymultifit/pymultifit-0.1.4-py3-none-any.whl/pymultifit/distributions/__init__.py
@@ -0,0 +1,12 @@
+"""Created on Aug 03 20:07:36 2024"""
+
+from typing import Optional
+
+from .gaussian_d import GaussianDistribution
+from .laplace_d import LaplaceDistribution
+from .logNorm_d import LogNormalDistribution
+from .others import cubic, line, linear, nth_polynomial, quadratic
+from .skewNorm_d import SkewedNormalDistribution
+from .powerLaw_d import PowerLawDistribution
+
+oFloat = Optional[float]
