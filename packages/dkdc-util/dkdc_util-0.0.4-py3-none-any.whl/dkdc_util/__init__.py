5fd3188b7bdeecf15c13fe38b58c5afe2759dd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 22:05:15 +0000

    Add package dkdc-util/0.0.4/dkdc_util-0.0.4-py3-none-any.whl

---
+++ packages/dkdc-util/dkdc_util-0.0.4-py3-none-any.whl/dkdc_util/__init__.py
@@ -0,0 +1,17 @@
+from dkdc_util.main import (
+    now,
+    uuid_parts,
+    uuid,
+    uuid_to_parts,
+    get_dkdc_dir,
+    get_config_toml,
+)
+
+__all__ = [
+    "now",
+    "uuid_parts",
+    "uuid",
+    "uuid_to_parts",
+    "get_dkdc_dir",
+    "get_config_toml",
+]
