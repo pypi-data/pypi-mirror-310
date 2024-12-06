172271ee935fd1752e257b0d6094d62666a77a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 04:07:00 +0000

    Add package fundamentum-edge-proto/1.5.0/fundamentum_edge_proto-1.5.0.tar.gz

---
+++ packages/fundamentum-edge-proto/fundamentum_edge_proto-1.5.0.tar.gz/fundamentum_edge_proto-1.5.0/fundamentum_edge_proto/__init__.py
@@ -0,0 +1,91 @@
+__all__ = (
+    "add_ActionsServicer_to_server",
+    "add_ConfigurationServicer_to_server",
+    "add_FirmwareUpdateServicer_to_server",
+    "add_ProvisioningServicer_to_server",
+    "add_StatesEventServicer_to_server",
+    "add_TelemetryServicer_to_server",
+    "build_actions_stub",
+    "build_configuration_stub",
+    "build_firmware_update_stub",
+    "build_provisioning_stub",
+    "build_states_event_stub",
+    "build_telemetry_stub",
+    "ActionRequest",
+    "ActionResponse",
+    "ACTIONS_DESCRIPTOR",
+    "ActionsServicer",
+    "ActionsStub",
+    "ConfigData",
+    "CONFIGURATION_DESCRIPTOR",
+    "ConfigurationServicer",
+    "ConfigurationStub",
+    "FIRMWARE_UPDATE_DESCRIPTOR",
+    "FirmwareUpdateRequest",
+    "FirmwareUpdateResponse",
+    "FirmwareUpdateServicer",
+    "FirmwareUpdateStub",
+    "PROVISIONING_DESCRIPTOR",
+    "ProvisioningServicer",
+    "ProvisioningStub",
+    "ProvisionRequest",
+    "ProvisionResponse",
+    "Qos",
+    "StateJsonData",
+    "STATES_DESCRIPTOR",
+    "StatesEventServicer",
+    "StatesEventStub",
+    "TELEMETRY_DESCRIPTOR",
+    "TelemetryRequest",
+    "TelemetryServicer",
+    "TelemetryStub",
+    "UpdateData",
+)
+
+from .actions_pb2 import DESCRIPTOR as ACTIONS_DESCRIPTOR
+from .actions_pb2 import ActionRequest, ActionResponse
+from .actions_pb2_grpc import ActionsServicer, ActionsStub, add_ActionsServicer_to_server
+from .configuration_pb2 import DESCRIPTOR as CONFIGURATION_DESCRIPTOR
+from .configuration_pb2 import ConfigData, UpdateData
+from .configuration_pb2_grpc import (
+    ConfigurationServicer,
+    ConfigurationStub,
+    add_ConfigurationServicer_to_server,
+)
+from .provisioning_pb2 import DESCRIPTOR as PROVISIONING_DESCRIPTOR
+from .provisioning_pb2 import ProvisionRequest, ProvisionResponse
+from .provisioning_pb2_grpc import (
+    ProvisioningServicer,
+    ProvisioningStub,
+    add_ProvisioningServicer_to_server,
+)
+from .qos_pb2 import Qos
+from .states_pb2 import DESCRIPTOR as STATES_DESCRIPTOR
+from .states_pb2 import StateJsonData
+from .states_pb2_grpc import (
+    StatesEventServicer,
+    StatesEventStub,
+    add_StatesEventServicer_to_server,
+)
+from .telemetry_pb2 import DESCRIPTOR as TELEMETRY_DESCRIPTOR
+from .telemetry_pb2 import TelemetryRequest
+from .telemetry_pb2_grpc import (
+    TelemetryServicer,
+    TelemetryStub,
+    add_TelemetryServicer_to_server,
+)
+from .typed_stubs import (
+    build_actions_stub,
+    build_configuration_stub,
+    build_firmware_update_stub,
+    build_provisioning_stub,
+    build_states_event_stub,
+    build_telemetry_stub,
+)
+from .update_pb2 import DESCRIPTOR as FIRMWARE_UPDATE_DESCRIPTOR
+from .update_pb2 import FirmwareUpdateRequest, FirmwareUpdateResponse
+from .update_pb2_grpc import (
+    FirmwareUpdateServicer,
+    FirmwareUpdateStub,
+    add_FirmwareUpdateServicer_to_server,
+)
