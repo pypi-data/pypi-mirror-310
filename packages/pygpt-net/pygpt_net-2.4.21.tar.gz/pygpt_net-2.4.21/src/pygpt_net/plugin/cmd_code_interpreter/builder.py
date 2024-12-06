cb1730b597331f65bf181b2490b755df5a533726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 14:11:36 +0000

    Add package pygpt-net/2.4.21/pygpt_net-2.4.21.tar.gz

---
+++ packages/pygpt-net/pygpt_net-2.4.21.tar.gz/pygpt_net-2.4.21/src/pygpt_net/plugin/cmd_code_interpreter/builder.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.11.20 21:00:00                  #
+# ================================================== #
+
+from PySide6.QtCore import Slot, Signal, QObject
+
+from pygpt_net.plugin.base.worker import BaseWorker, BaseSignals
+from pygpt_net.utils import trans
+
+class Builder(QObject):
+    def __init__(self, plugin=None):
+        super(Builder, self).__init__()
+        self.plugin = plugin
+        self.worker = None
+
+    def build_image(self):
+        """Run IPython image build"""
+        try:
+            self.worker = Worker()
+            self.worker.plugin = self.plugin
+            self.worker.signals.build_finished.connect(self.handle_build_finished)
+            self.worker.signals.error.connect(self.handle_build_failed)
+            self.plugin.window.threadpool.start(self.worker)
+        except Exception as e:
+            self.plugin.window.ui.dialogs.alert(e)
+
+    @Slot()
+    def handle_build_finished(self):
+        """Handle build finished"""
+        self.plugin.window.ui.dialogs.alert(trans('ipython.docker.build.finish'))
+        self.plugin.window.update_status(trans('ipython.docker.build.finish'))
+
+    @Slot(object)
+    def handle_build_failed(self, error):
+        """Handle build failed"""
+        self.plugin.window.ui.dialogs.alert(str(error))
+        self.plugin.window.update_status(str(error))
+
+class WorkerSignals(BaseSignals):
+    build_finished = Signal()
+
+class Worker(BaseWorker):
+    def __init__(self, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.signals = WorkerSignals()
+        self.args = args
+        self.kwargs = kwargs
+        self.plugin = None
+
+    @Slot()
+    def run(self):
+        try:
+            self.plugin.ipython.build_image()
+            self.signals.build_finished.emit()
+        except Exception as e:
+            self.signals.error.emit(e)
---
+++ packages/pygpt-net/pygpt_net-2.4.21.tar.gz/pygpt_net-2.4.21/src/pygpt_net/ui/layout/toolbox/mode.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.11.17 03:00:00                  #
+# ================================================== #
+
+from PySide6.QtWidgets import QVBoxLayout, QWidget
+
+from pygpt_net.ui.widget.element.labels import TitleLabel
+from pygpt_net.ui.widget.lists.mode_combo import ModeCombo
+from pygpt_net.utils import trans
+
+
+class Mode:
+    def __init__(self, window=None):
+        """
+        Toolbox UI
+
+        :param window: Window instance
+        """
+        self.window = window
+        self.id = 'prompt.mode'
+
+    def setup(self) -> QWidget:
+        """
+        Setup mode
+
+        :return: QWidget
+        """
+        widget = QWidget()
+        widget.setLayout(self.setup_list())
+        return widget
+
+    def setup_list(self) -> QVBoxLayout:
+        """
+        Setup list
+
+        :return: QVBoxLayout
+        """
+        label_key = self.id + '.label'
+        self.window.ui.nodes[self.id] = ModeCombo(self.window, self.id)
+        self.window.ui.nodes[label_key] = TitleLabel(trans("toolbox.mode.label"))
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.window.ui.nodes[label_key])
+        layout.addWidget(self.window.ui.nodes[self.id])
+        layout.addStretch()
+
+        return layout
