904b473db87deadaf3b7847509248946a8d3f4c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 03:17:59 +0000

    Add package snowflake-ml-python/1.7.2/snowflake_ml_python-1.7.2.tar.gz

---
+++ packages/snowflake-ml-python/snowflake_ml_python-1.7.2.tar.gz/snowflake_ml_python-1.7.2/snowflake/cortex/_summarize.py
@@ -0,0 +1,33 @@
+from typing import Optional, Union, cast
+
+from snowflake import snowpark
+from snowflake.cortex._util import CORTEX_FUNCTIONS_TELEMETRY_PROJECT, call_sql_function
+from snowflake.ml._internal import telemetry
+
+
+@telemetry.send_api_usage_telemetry(
+    project=CORTEX_FUNCTIONS_TELEMETRY_PROJECT,
+)
+def Summarize(
+    text: Union[str, snowpark.Column],
+    session: Optional[snowpark.Session] = None,
+) -> Union[str, snowpark.Column]:
+    """Summarize calls into the LLM inference service to summarize the input text.
+
+    Args:
+        text: A Column of strings to summarize.
+        session: The snowpark session to use. Will be inferred by context if not specified.
+
+    Returns:
+        A column of string summaries.
+    """
+
+    return _summarize_impl("snowflake.cortex.summarize", text, session=session)
+
+
+def _summarize_impl(
+    function: str,
+    text: Union[str, snowpark.Column],
+    session: Optional[snowpark.Session] = None,
+) -> Union[str, snowpark.Column]:
+    return cast(Union[str, snowpark.Column], call_sql_function(function, session, text))
