f1af4a772ff07976f082d39a2526552b61b3b57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:38 +0000

    Add package brdata-rag-tools/0.1.5.1/brdata_rag_tools-0.1.5.1.tar.gz

---
+++ packages/brdata-rag-tools/brdata_rag_tools-0.1.5.1.tar.gz/brdata_rag_tools-0.1.5.1/src/brdata_rag_tools/databases.py
@@ -0,0 +1,339 @@
+import os
+from dataclasses import dataclass
+from typing import List, Type, Dict
+
+import faiss
+import numpy as np
+from pgvector.sqlalchemy import Vector
+from sqlalchemy import String, text, BLOB
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session, Mapped, mapped_column
+
+from .datastructures import Base, BaseClass
+from .embeddings import EmbeddingConfig
+
+
+class Database:
+    def __init__(self, user: str, database: str,
+                 password: str, host: str, port: int,
+                 verbose: bool = False, vector_type=None):
+        self.user = user
+        self.database = database
+        self.password = os.environ.get(
+            "DATABASE_PASSWORD") if password is None else password
+        self.host = host
+        self.port = port
+        self.verbose = verbose
+        self.engine = self._create_engine()
+        self.embedding_table = None
+        self.Base = Base
+        self.metadata = Base.metadata
+        self.vector_type = vector_type
+
+    def create_abstract_embedding_table(self, embed_type: EmbeddingConfig):
+        """
+        Create an abstract embedding table. Use it to inherit your own database tables to use with the PGVector class.
+
+        :param embed_type: The type of embedding to be used.
+        :return: The abstract embedding table class.
+        """
+
+        class EmbeddingTable(self.Base):
+            embedding_type = embed_type
+
+            __abstract__ = True
+            id: Mapped[str] = mapped_column(primary_key=True, unique=True)
+            embedding_source: Mapped[str] = mapped_column(String)
+            embedding: Mapped[np.array] = mapped_column(
+                self.vector_type(embedding_type.dimension)
+            )
+
+            def __repr__(self):
+                return self.id + ": " + self.embedding_source
+
+        return EmbeddingTable
+
+    def create_tables(self):
+        """
+        Create tables in the database based on the defined metadata.
+
+        :return: None
+        """
+        self.metadata.create_all(self.engine)
+
+    def _create_engine(self):
+        """
+        Create a SQLAlchemy engine instance for connecting to a PostgreSQL database.
+
+        :return: SQLAlchemy engine instance
+        """
+        raise NotImplementedError()
+
+    def session(self, **kw):
+        """
+        Initiates a new session with the database.
+        Any parameters will be passed directly to sqlalchemy's Session class.
+
+        :return: Session object
+        """
+        session = Session(self.engine, **kw)
+        return session
+
+    def drop_table(self, name: str):
+        """
+        Drop a table from the database.
+
+        :param name: The name of the table to be dropped.
+        :type name: str
+        :return: None
+        """
+        with self.session() as session:
+            session.execute(text(f"DROP TABLE IF EXISTS {name};"))
+            session.commit()
+
+    def retrieve_similar_content(self, prompt, table: Type[BaseClass],
+                                 embedding_type: EmbeddingConfig, limit: int = 50, threshold: float = None):
+        raise NotImplementedError()
+
+    def get_existing_row_ids(self, table: BaseClass):
+        """
+        Get the IDs from the given table using the provided embedding.
+
+        :param table: The table to query the IDs from.
+        :type table: BaseClass
+
+        :return: A list of IDs from the provided table.
+        :rtype: List[int]
+        """
+        with self.session() as session:
+            ids = session.query(table.id).all()
+
+        return [x.id for x in ids]
+
+    def retrieve_embedding(self, row_id: str, table: BaseClass) -> np.array:
+        raise NotImplementedError()
+
+    def write_rows(self, rows: List[Type[BaseClass]], create_embeddings: bool = True, expunge=False, expire=True):
+        """
+        Write rows to the database and optionally create embeddings for the rows.
+
+        :param rows: A list of rows to be written to the database. Rows must be instances of BaseClass or its subclasses.
+        :param create_embeddings: A boolean value indicating whether embeddings should be created for the rows.
+                                  Default value is True.
+        :return: None
+        """
+        table = type(rows[0])
+        if create_embeddings:
+            embedder = rows[0].embedding_type.model
+
+            rows_with_embedding = self.get_existing_row_ids(table)
+            rows_wo_embedding = [x for x in rows if x.id not in rows_with_embedding]
+
+            rows_to_write = embedder.create_embedding_bulk(rows_wo_embedding)
+        else:
+            existing_rows = self.get_existing_row_ids(table)
+            rows_to_write = [x for x in rows if x.id not in existing_rows]
+
+        with self.session(expire_on_commit=expire) as session:
+            session.add_all(rows_to_write)
+            session.commit()
+            if expunge:
+                session.expunge_all()
+
+@dataclass
+class IndexWrapper:
+    embeddings: faiss.METRIC_INNER_PRODUCT
+    ids: List[str]
+
+
+class FAISS(Database):
+    def __init__(self, user: str = None, database: str = None, password: str = None,
+                 host: str = None, port: int = None, verbose: bool = False):
+        super().__init__(user, database, password, host, port, verbose, vector_type=BLOB)
+
+        self.indices: Dict[str, IndexWrapper] = dict()
+
+    def write_rows(self, rows: List[Type[BaseClass]], create_embeddings: bool = True, expunge=True, expire=True):
+        """
+        Write rows to the database and optionally create embeddings for the rows.
+
+        :param rows: A list of rows to be written to the database. Rows must be instances of BaseClass or its subclasses.
+        :param create_embeddings: A boolean value indicating whether embeddings should be created for the rows.
+                                  Default value is True.
+        :return: None
+        """
+        table = type(rows[0])
+        embedder = rows[0].embedding_type.model
+
+        rows_with_embedding = self.get_existing_row_ids(table)
+        rows_wo_embedding = [x for x in rows if x.id not in rows_with_embedding]
+
+        newly_embedded = embedder.create_embedding_bulk(rows_wo_embedding)
+
+        for i, row in enumerate(newly_embedded):
+            # normalization necessary says documentation:
+            # https://github.com/facebookresearch/faiss/wiki/MetricType-and-distances
+            embedding = np.array([row.embedding]).astype("float32")
+            faiss.normalize_L2(embedding)  # inplace -.-
+            self.indices[table.__tablename__].embeddings.add(embedding)
+            self.indices[table.__tablename__].ids.append(row.id)
+
+        with self.session(expire_on_commit=expire) as session:
+            session.add_all(newly_embedded)
+            session.flush()
+            session.commit()
+            if expunge:
+                session.expunge_all()
+
+    def write_index(self):
+        raise NotImplementedError()
+
+    def _create_engine(self):
+        if self.database is None:
+            return create_engine("sqlite+pysqlite:///:memory:", echo=self.verbose)
+        else:
+            return create_engine(f"sqlite+pysqlite:///{self.database}",
+                                 echo=self.verbose)
+
+    def retrieve_similar_content(self, prompt, table: Type[BaseClass],
+                                 embedding_type: EmbeddingConfig,
+                                 limit: int = 50, max_dist: float = None) -> List:
+        """
+        Retrieve similar content based on a prompt. The function creates an embedding with the specified embedding type
+        and queries the associated database for the most similar matches.
+
+        :param prompt: The prompt for which similar content needs to be found.
+        :param table: The table in which the content is stored.
+        :param embedding_type: The type of embedding to be used.
+        :param limit: The maximum number of similar content to be retrieved (default: 50).
+        :return: A list of results containing similar content.
+        """
+        embedder = embedding_type.model
+        prompt_embedding = np.array([embedder.create_embedding(prompt)]).astype(
+            "float32")
+        faiss.normalize_L2(prompt_embedding)
+
+        sim_matrix, indices = self.indices[table.__tablename__].embeddings.search(
+            prompt_embedding, limit)
+
+        ids = []
+        distances = []
+
+        for i, index in enumerate(indices[0]):
+            if i != -1:
+                ids.append(self.indices[table.__tablename__].ids[index])
+                distances.append(sim_matrix[0][i])
+
+        with self.session() as session:
+            results = session.execute(select(table).where(table.id.in_(ids))).all()
+
+        dict_result = []
+
+        for i, row in enumerate(results):
+            results[i][0].embedding = np.frombuffer(row[0].embedding)
+            d = results[i]._asdict()
+            if max_dist is not None and distances[i] < max_dist:
+                d["cosine_dist"] = distances[i]
+                dict_result.append(d)
+
+        return dict_result
+
+    def retrieve_embedding(self, row_id: str, table: BaseClass) -> np.array:
+        raise NotImplementedError("retrieve_embedding is not implemented")
+
+    def create_tables(self):
+        self.metadata.create_all(self.engine)
+
+        metadata_tables = list(self.metadata.tables.keys())
+        existing = list(self.indices.keys())
+
+        for table in metadata_tables:
+            if table not in existing:
+                dimension = self.metadata.tables[table].columns["embedding"].type.length
+                # self.indices[table] = IndexWrapper(embeddings=faiss.IndexHNSWFlat(dimension, faiss.METRIC_INNER_PRODUCT), ids = [])
+                self.indices[table] = IndexWrapper(
+                    embeddings=faiss.IndexFlatL2(dimension), ids=[])
+
+        for table in existing:
+            if table not in metadata_tables:
+                self.indices.pop(table)
+
+
+class PGVector(Database):
+    """
+    This class represents a connection to a PostgreSQL database with the pgvector addon.
+
+    :param user: The username to connect to the database. Default is "postgres".
+    :type user: str
+    :param database: The name of the database to connect to. Default is "postgres".
+    :type database: str
+    :param password: The password to connect to the database. If not provided, it will use the value of the "DATABASE_PASSWORD" environment variable.
+    :type password: str
+    :param host: The host address of the database. Default is "localhost".
+    :type host: str
+    :param port: The port number of the database. Default is 5432.
+    :type port: int
+    :param verbose: Whether to enable verbose output. Default is False.
+    :type verbose: bool
+    """
+
+    def __init__(self, user: str = "postgres", database: str = "postgres",
+                 password: str = None, host: str = "localhost", port: int = 5432,
+                 verbose: bool = False):
+        super().__init__(user, database, password, host, port, verbose, vector_type=Vector)
+
+        with self.session() as session:
+            session.execute(text('CREATE EXTENSION IF NOT EXISTS vector;'))
+            session.commit()
+
+    def _create_engine(self):
+        """
+        Create a SQLAlchemy engine instance for connecting to a PostgreSQL database.
+
+        :return: SQLAlchemy engine instance
+        """
+        return create_engine(
+            f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}/{self.database}",
+            echo=self.verbose)
+
+    def retrieve_similar_content(self, prompt, table: Type[BaseClass],
+                                 embedding_type: EmbeddingConfig, limit: int = 50, max_dist: float = None):
+        """
+        Retrieve similar content based on a prompt. The function creates an embedding with the specified embedding type
+        and queries the associated database for the most similar matches.
+
+        :param prompt: The prompt for which similar content needs to be found.
+        :param table: The table in which the content is stored.
+        :param embedding_type: The type of embedding to be used.
+        :param limit: The maximum number of similar content to be retrieved (default: 50).
+        :return: A list of results containing similar content.
+        """
+        embedder = embedding_type.model
+        prompt_embedding = embedder.create_embedding(prompt)
+
+        with self.session() as session:
+            results = session.execute(select(table, table.embedding.cosine_distance(
+                prompt_embedding).label("cosine_dist")).order_by("cosine_dist").limit(
+                limit)).all()
+
+        for i in range(len(results)):
+            if max_dist <= results[i].cosine_dist:
+                results = results[:i]
+                break
+
+        return [x._asdict() for x in results]
+
+    def retrieve_embedding(self, row_id: str, table: BaseClass) -> np.array:
+        """
+        Retrieve embedding from the database for a given row ID and table.
+
+        :param row_id: The ID of the row to retrieve the embedding from.
+        :param table: The table containing the embedding column.
+        :return: The retrieved embedding as a numpy array.
+
+        """
+        with self.session() as session:
+            embedding = session.query(table.emebedding).where(
+                table.id == row_id).first()
+
+        return embedding
---
+++ packages/brdata-rag-tools/brdata_rag_tools-0.1.5.1.tar.gz/brdata_rag_tools-0.1.5.1/src/brdata_rag_tools/embeddings.py
@@ -0,0 +1,216 @@
+import os
+from aenum import Enum, extend_enum
+from typing import List, Optional, Type
+
+import numpy as np
+import requests
+import logging
+
+from .datastructures import BaseClass
+
+embedding_data = {
+    "sentence_transformers": {
+        "dimension": 1024
+    },
+}
+
+user_models = {}
+
+
+def register(model, name=None, dimension: int = None):
+    extend_enum(EmbeddingConfig, name.upper(), name)
+    user_models[name] = model
+
+    embedding_data[name] = {
+        "dimension": dimension
+    }
+
+    logging.info(f"Successfully registered model {name}.")
+
+
+class EmbeddingConfig(Enum):
+    """
+    The EmbeddingConfig class is an enumeration that represents different types of embeddings.
+    It is used to define the type of embedding model to be used in a text processing task.
+
+    ### Attributes:
+
+    - SENTENCE_TRANSFORMERS (EmbeddingConfig): Represents the Sentence Transformers embedding model.
+    - TF_IDF (EmbeddingConfig): Represents the TF-IDF embedding model.
+
+    Example Usage::
+
+        import test
+
+        embedding_type = EmbeddingConfig.SENTENCE_TRANSFORMERS
+        dimension = embedding_type.dimension
+        model = embedding_type.model
+
+    """
+    SENTENCE_TRANSFORMERS = "sentence_transformers"
+    TF_IDF = "tfidf"
+
+    @property
+    def dimension(self):
+        """
+        Get the dimension of the input.
+
+        :return: The dimension of the input.
+        :rtype: int
+        """
+        return embedding_data[self.value]["dimension"]
+
+    @property
+    def model(self):
+        """
+        :return: An instance of the used embedding model.
+        """
+        if self == self.SENTENCE_TRANSFORMERS:
+            return SentenceTransformer()
+        elif self == self.TF_IDF:
+            raise NotImplementedError()
+        else:
+            try:
+                return user_models[self.value]()
+            except KeyError:
+                raise KeyError(f"No embedding model with name {self.value} found.")
+
+
+class Embedder:
+    """
+    The Embedder class is responsible for creating embeddings for a list of objects.
+
+    :param endpoint: The endpoint for the embedding service.
+    :type endpoint: str
+    :param dimension: The dimension of the embeddings to be created.
+    :type dimension: int
+    :param token: The access auth_token for the embedding service (optional).
+    :type token: str, optional
+
+    :raises: NotImplementedError
+
+    :return: None
+    """
+
+    def __init__(self, endpoint: str, auth_token: Optional[str] = None):
+        self.endpoint = endpoint
+        self.auth_token = auth_token
+
+    def create_embedding_bulk(self, rows: List[Type[BaseClass]]) -> List[
+        Type[BaseClass]]:
+        raise NotImplementedError()
+
+    def create_embedding(self, text: str) -> np.array:
+        raise NotImplementedError()
+
+
+class SentenceTransformer(Embedder):
+    """
+    Class representing a sentence transformer for creating sentence embeddings.
+
+    Methods:
+        - create_embedding(text: str) -> np.array:
+            Takes a string of text and returns its corresponding sentence embedding.
+
+        - create_embedding_bulk(rows: List[Type[BaseClass]]) -> List[Type[BaseClass]]:
+            Takes a list of rows as input and creates sentence embeddings for each row. It updates
+            the 'embedding' attribute of each row in the list and returns the updated list.
+
+        Raises:
+            - NotImplementedError: Raised when more than 250 rows are passed to the create_embedding_bulk method.
+            - ValueError: Raised when there is an ID mismatch during the bulk embedding creation process.
+    """
+
+    def __init__(self):
+        super().__init__(endpoint=os.environ.get("SENTENCE_TRANSFORMER_ENDPOINT", "https://gbert-cosine-embeddings.brdata-dev.de"),
+                         auth_token=os.environ.get("SENTENCE_TRANSFORMER_TOKEN"))
+
+        if self.auth_token is None:
+            logging.info("No auth token for Sentence Transformer was provided.")
+
+    def create_embedding(self, text: str) -> np.array:
+        """
+        :param text: The text to be embedded.
+        :return: A numpy array representing the embedding of the text.
+        """
+        headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.auth_token}'
+        }
+
+        json_data = {
+            'content': [{
+                'id': '1',
+                'text': text,
+            }]
+        }
+
+        response = requests.post(f'{self.endpoint}/create_embeddings',
+                                 headers=headers, json=json_data).json()["content"]
+
+        return np.array(response[0]["embedding"])
+
+    def create_embedding_bulk(self, rows: List[Type[BaseClass]]) -> List[
+        Type[BaseClass]]:
+        """
+        Create embeddings for a list of rows.
+
+        :param rows: A list of rows to create embeddings for.
+        :type rows: List[Type[BaseClass]]
+        :return: A list of rows with embeddings added.
+        :rtype: List[Type[BaseClass]]
+        :raises NotImplementedError: If the number of rows is greater than 250.
+        :raises ValueError: If there is a mismatch in ID between the rows and the response.
+        """
+        if len(rows) > 250:
+            raise NotImplementedError(
+                "Sorry, only 250 rows may be processed at one time.")
+
+
+        headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.auth_token}'
+        }
+
+        content = []
+        for row in rows:
+
+            if row.id is None or row.embedding_source is None:
+                raise ValueError(
+                    "ID or embedding source is missing. Please provide an ID or embedding source in your table"
+                    "definition as described here with the declaration of podcast1: "
+                    "https://br-data.github.io/rag-tools-library/#augmenting-your-prompt.")
+
+            content.append({
+                'id': row.id,
+                'text': row.embedding_source,
+            })
+
+        json_data = {
+            'content': content
+        }
+
+        response = requests.post(f'{self.endpoint}/create_embeddings',
+                                 headers=headers, json=json_data)
+
+        if response.status_code != 200:
+            raise ConnectionError(f"Connection to the Embedder API returned status code {response.status_code}.")
+
+        try:
+            response = response.json()["content"]
+        except KeyError:
+            raise ValueError(
+                "The Embedding service did not return any results. Please make sure your auth_token is correct"
+                " and embedding_source is of a meaningful format.")
+
+        for i, text_element in enumerate(response):
+            if rows[i].id == text_element["id"]:
+                rows[i].embedding = np.array(text_element["embedding"])
+            else:
+                raise ValueError("ID mismatch.")
+
+        return rows
+
+
