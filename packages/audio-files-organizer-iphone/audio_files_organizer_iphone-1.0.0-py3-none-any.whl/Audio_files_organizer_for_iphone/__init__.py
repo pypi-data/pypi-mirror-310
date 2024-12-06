e8b3fe64f898ae189dfe3467e0c610512f4fe44a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 02:33:27 +0000

    Add package audio-files-organizer-iphone/1.0.0/audio_files_organizer_iphone-1.0.0-py3-none-any.whl

---
+++ packages/audio-files-organizer-iphone/audio_files_organizer_iphone-1.0.0-py3-none-any.whl/Audio_files_organizer_for_iphone/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# __init__.py
+"""
+Audio Organizer Package
+========================
+
+This package provides tools to organize and update audio file metadata
+to simplify syncing with iTunes or other platforms.
+
+Author: David Sandeep
+License: MIT
+"""
+
+from Audio_organizer import processit
+
+__version__ = "0.1.0"
+__author__ = "David Sandeep"
+
