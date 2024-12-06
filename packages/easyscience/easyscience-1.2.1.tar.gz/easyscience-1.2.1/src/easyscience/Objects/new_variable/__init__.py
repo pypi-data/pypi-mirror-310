5c7db0e186521b0d359fbd03d4e380e70bcea668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 00:09:29 +0000

    Add package easyscience/1.2.1/easyscience-1.2.1.tar.gz

---
+++ packages/easyscience/easyscience-1.2.1.tar.gz/easyscience-1.2.1/src/easyscience/Objects/new_variable/__init__.py
@@ -0,0 +1,11 @@
+from .descriptor_bool import DescriptorBool
+from .descriptor_number import DescriptorNumber
+from .descriptor_str import DescriptorStr
+from .parameter import Parameter
+
+__all__ = [
+    DescriptorBool,
+    DescriptorNumber,
+    DescriptorStr,
+    Parameter,
+]
