7f88e4af714d8dfacedf75e71124a52a98b9fd1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:52 +0000

    Add package utmail/0.1.8/utmail-0.1.8.tar.gz

---
+++ packages/utmail/utmail-0.1.8.tar.gz/utmail-0.1.8/utmail/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+from .utmail import UtMail
+from .utmail import Api
+from .api import *
+
+
+__version__ = "0.1.8"
+
+
+
---
+++ packages/utmail/utmail-0.1.8.tar.gz/utmail-0.1.8/utmail/api/__init__.py
@@ -0,0 +1,6 @@
+from .chacuo import ChacuoOption # 从chacuo.py导入ChacuoOption
+
+# 需要导入的类名
+__all__ = [
+    "ChacuoOption"
+]
