2d5894b579eb2c76f0a296dfe7a58287a95c7065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:32 +0000

    Add package chinus-emotions/3.0.1/chinus_emotions-3.0.1-py3-none-any.whl

---
+++ packages/chinus-emotions/chinus_emotions-3.0.1-py3-none-any.whl/chinus_emotions/src/main/main.py
@@ -0,0 +1,22 @@
+from chinus_emotions.src.main.classes.table import Table
+
+
+class Minds(Table):
+    """
+    emotions.db의 minds 테이블
+    """
+    _TABLE_NAME = 'minds'
+
+
+class Feelings(Table):
+    """
+    emotions.db의 feelings 테이블
+    """
+    _TABLE_NAME = 'feelings'
+
+
+class Senses(Table):
+    """
+    emotions.db의 senses 테이블
+    """
+    _TABLE_NAME = 'senses'
