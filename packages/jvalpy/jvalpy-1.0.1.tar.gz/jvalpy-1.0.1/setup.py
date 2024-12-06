1013ed56062dc4d07e207ab9ee40d5a69c993eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 08:09:57 +0000

    Add package jvalpy/1.0.1/jvalpy-1.0.1.tar.gz

---
+++ packages/jvalpy/jvalpy-1.0.1.tar.gz/jvalpy-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jvalpy',
+    version='1.0.1',    
+    description='py project validator',
+    url='https://github.com/yaty30/pyprjval',
+    author='James Yip',
+    author_email='contact@thejamesyip.com',
+    license='BSD 2-clause',
+    packages=find_packages(),
+    install_requires=['cryptography'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
+
+# pypi-AgEIcHlwaS5vcmcCJGQyMDRhMTg2LTY5NGMtNDYwMi05MTQ4LWQ2M2YzZjJmNTVlNAACKlszLCI4ZmIxZmEyNS0xYTdmLTQ2YzctYTFlMy03MWU2NzkxYzRhZjIiXQAABiA6pBrxMslvdNWA7EfrvpCBMr4w8KvDaBHrjbXOKZkALA
