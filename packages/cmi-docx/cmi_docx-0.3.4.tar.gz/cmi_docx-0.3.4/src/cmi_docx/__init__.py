c5a4859f1c38b4d712cc7bbd2ebeccc4a01ef9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:06:40 +0000

    Add package cmi-docx/0.3.4/cmi_docx-0.3.4.tar.gz

---
+++ packages/cmi-docx/cmi_docx-0.3.4.tar.gz/cmi_docx-0.3.4/src/cmi_docx/__init__.py
@@ -0,0 +1,7 @@
+""".. include:: ../../README.md"""  # noqa: D415
+
+from cmi_docx.document import ExtendDocument  # noqa: F401
+from cmi_docx.paragraph import ExtendParagraph, FindParagraph  # noqa: F401
+from cmi_docx.run import ExtendRun, FindRun  # noqa: F401
+from cmi_docx.styles import ParagraphStyle, RunStyle, TableStyle  # noqa: F401
+from cmi_docx.table import ExtendCell  # noqa: F401
