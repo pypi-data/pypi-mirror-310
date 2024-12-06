f8183b3865e467c37d6edb850e288f7c801b1c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:04:34 +0000

    Add package mfthree/8.20.11/mfthree-8.20.11.tar.gz

---
+++ packages/mfthree/mfthree-8.20.11.tar.gz/mfthree-8.20.11/three/MF/V3/Settings/Rectangle.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    # Rectangle settings.
+    def __init__(self, x: int, y: int, width: int, height: int):
+        # Rectangle x offset.
+        self.x = x
+        # Rectangle y offset.
+        self.y = y
+        # Rectangle width.
+        self.width = width
+        # Rectangle height.
+        self.height = height
+
+
