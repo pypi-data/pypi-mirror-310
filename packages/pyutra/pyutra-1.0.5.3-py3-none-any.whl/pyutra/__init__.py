ab1b3a1b557d90496111bc5f90dccb22d4a98e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:04:52 +0000

    Add package pyutra/1.0.5.3/pyutra-1.0.5.3-py3-none-any.whl

---
+++ packages/pyutra/pyutra-1.0.5.3-py3-none-any.whl/pyutra/__init__.py
@@ -0,0 +1,29 @@
+# General Class crap
+from pyutra import ColorText, JsonConfig, CoolDebugging, Extras, pastelib, discord
+
+# Json Library
+from pyutra.JsonConfig import JsonConfig, EasyJsonConfig
+
+# Color Text Library
+from pyutra.ColorText import ColorText
+
+# Extras
+from pyutra.Extras import Extras
+
+# Discord Library
+from pyutra.discord import Webhooks
+
+# Pastebin Library (Ported): https://github.com/thediamondog/pastebinlib
+from pyutra.pastelib.Pastelib import Pastelib
+from pyutra.pastelib.User import User
+from pyutra.pastelib import User, Pastelib
+
+# Internet Library
+from pyutra.internet import ip
+from pyutra.internet import web
+
+# Network Library
+from pyutra.network import Network
+
+# Blocksi Library (Ported): https://github.com/thediamondog/blocksicrap
+from pyutra.blocksi import admin_data, policy_data, user_data
