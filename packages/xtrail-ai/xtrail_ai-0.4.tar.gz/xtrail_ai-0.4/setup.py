3a1ef26ce98934f388d996f09482a96177ee2d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 01:34:15 +0000

    Add package xtrail-ai/0.4/xtrail_ai-0.4.tar.gz

---
+++ packages/xtrail-ai/xtrail_ai-0.4.tar.gz/xtrail_ai-0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xtrail_ai",  # New package name
+    version="0.4",
+    description="Retrieve machine details, monitor resources, and execute jobs dynamically.",
+    author="Harisha P C",
+    author_email="info@xtrail.in",
+    url="https://xtrail.in",  # Optional: Link to the source code or documentation
+    packages=find_packages(),
+    install_requires=[
+        "psutil>=5.9.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Use the appropriate license
+        "Operating System :: OS Independent",
+    ],
+    keywords="machine-id machine-learning resource-monitoring dynamic-execution",
+    python_requires=">=3.6",
+)
