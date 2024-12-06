9d314fc5545d036b489145b8367bbae1c1593ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 23:05:03 +0000

    Add package pybro-cli/0.1.4/pybro_cli-0.1.4.tar.gz

---
+++ packages/pybro-cli/pybro_cli-0.1.4.tar.gz/pybro_cli-0.1.4/chrome_profile_manager/utils/__init__.py
@@ -0,0 +1,7 @@
+# 📄 chrome_profile_manager/utils/__init__.py
+"""
+Utils package initialization
+"""
+from .chrome_scanner import ChromeProfileScanner, ChromeProfile
+
+__all__ = ['ChromeProfileScanner', 'ChromeProfile']
