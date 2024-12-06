43ce3f68a34153f538d85dbfa43833c27df428ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 11:04:32 +0000

    Add package meteocatpy/0.1.0/meteocatpy-0.1.0-py3-none-any.whl

---
+++ packages/meteocatpy/meteocatpy-0.1.0-py3-none-any.whl/meteocatpy/__init__.py
@@ -0,0 +1,6 @@
+"""Python Package to collect data from Meteocat API and interact with Meteocat Home Assistant Integration
+SPDX-License-Identifier: Apache-2.0
+
+For more details about this api, please refer to the documentation at
+https://gitlab.com/keatontaylor/alexapy
+"""
