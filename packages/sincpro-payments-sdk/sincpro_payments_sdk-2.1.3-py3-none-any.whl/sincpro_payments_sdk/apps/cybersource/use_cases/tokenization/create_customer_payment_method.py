3bd31df146d78fc4327d533c428f176b8b5f746f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 12:05:48 +0000

    Add package sincpro-payments-sdk/2.1.3/sincpro_payments_sdk-2.1.3-py3-none-any.whl

---
+++ packages/sincpro-payments-sdk/sincpro_payments_sdk-2.1.3-py3-none-any.whl/sincpro_payments_sdk/apps/cybersource/use_cases/tokenization/create_customer_payment_method.py
@@ -0,0 +1,33 @@
+from sincpro_payments_sdk.apps.cybersource.use_cases import (
+    DataTransferObject,
+    Feature,
+    cybersource,
+)
+
+
+class CommandCreateCustomerPaymentMethod(DataTransferObject):
+    external_id: str
+    email: str
+    card_number: str
+    card_type: str
+    month: str
+    year: str
+
+
+class ResponseCreateCustomerPaymentMethod(DataTransferObject):
+    raw_response: dict | None = None
+
+
+@cybersource.feature(CommandCreateCustomerPaymentMethod)
+class CreateCustomerPaymentMethod(Feature):
+    """
+    Create a customer payment method in CyberSource.
+    - Create a customer
+    - Create a card
+    - Associate card to customer
+    """
+
+    def execute(
+        self, dto: CommandCreateCustomerPaymentMethod
+    ) -> ResponseCreateCustomerPaymentMethod:
+        """Main execution"""
