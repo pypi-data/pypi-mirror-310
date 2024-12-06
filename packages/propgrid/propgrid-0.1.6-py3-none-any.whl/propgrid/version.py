bcfb98eec4660450fee2d1d0454e3e1a92a67200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 06:05:32 +0000

    Add package propgrid/0.1.6/propgrid-0.1.6-py3-none-any.whl

---
+++ packages/propgrid/propgrid-0.1.6-py3-none-any.whl/propgrid/version.py
@@ -0,0 +1,9 @@
+PROJECT_NAME     = 'propgrid'
+
+VER_MAJOR        = 0
+VER_MINOR        = 1
+VER_RELEASE      = 6
+
+VER_FLAGS        = ""
+
+__version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'
