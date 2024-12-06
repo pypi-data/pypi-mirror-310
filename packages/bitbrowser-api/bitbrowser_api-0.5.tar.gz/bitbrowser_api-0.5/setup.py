5911f91e9815c8fe60ec58b0bd1405599ef40fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 19:04:47 +0000

    Add package bitbrowser-api/0.5/bitbrowser_api-0.5.tar.gz

---
+++ packages/bitbrowser-api/bitbrowser_api-0.5.tar.gz/bitbrowser_api-0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+project_urls = {
+  'Source': 'https://github.com/embzheng/bitbrowser_api'
+}
+setuptools.setup(
+    name="bitbrowser_api",
+    version="0.5",
+    author="blue",
+    author_email="embzhengblue@gmail.com",
+    description="This is a bitbrowser operation tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ox-blueblue/bitbrowser_api",
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls = project_urls
+)
