295680b2365610a2cd048113f659d55337ca9226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 21:18:12 +0000

    Add package pretty-pie-log/0.2.1/pretty_pie_log-0.2.1-py3-none-any.whl

---
+++ packages/pretty-pie-log/pretty_pie_log-0.2.1-py3-none-any.whl/pretty_pie_log/examples.py
@@ -0,0 +1,20 @@
+from pretty_pie_log import PieLogger, PieLogLevel
+
+logger = PieLogger('Logger')
+logger.log(PieLogLevel.CRITICAL, 'This is a critical message', {'key': 'value'})
+logger.info('This is an info message', {'key': 'value'})
+logger.debug('This is a debug message', {'key': 'value'}, colorful=False)
+logger.info('This is an info message', {'key': 'value'}, colorful=False)
+logger.warning('This is a warning message', {'key': 'value'}, colorful=False)
+logger.error('This is an error message', {'key': 'value'}, colorful=False)
+logger.critical('This is a critical message', {'key': 'value'}, colorful=True)
+
+
+@logger.log_execution(print_args_at_start=True, print_result_at_end=True, start_message="Starting my task",
+                      start_message_log_level=PieLogLevel.DEBUG, end_message_log_level=PieLogLevel.INFO)
+def main(str):
+    logger.info("This is an info message", {'str': str})
+    return True
+
+
+main('here')
