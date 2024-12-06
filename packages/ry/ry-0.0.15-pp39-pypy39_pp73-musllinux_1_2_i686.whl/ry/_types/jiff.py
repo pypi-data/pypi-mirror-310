354494f887ad1c3da4fffe6523ba1e8da3eb49a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 15:05:27 +0000

    Add package ry/0.0.15/ry-0.0.15-pp39-pypy39_pp73-musllinux_1_2_i686.whl

---
+++ packages/ry/ry-0.0.15-pp39-pypy39_pp73-musllinux_1_2_i686.whl/ry/_types/jiff.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import Literal
+
+JIFF_UNIT_STRING = Literal[
+    "nanosecond",
+    "microsecond",
+    "millisecond",
+    "second",
+    "minute",
+    "hour",
+    "day",
+    "month",
+    "year",
+]
+
+JIFF_ROUND_MODE_STRING = Literal[
+    "ceil",
+    "floor",
+    "expand",
+    "trunc",
+    "half_ceil",
+    "half_floor",
+    "half_expand",
+    "half_trunc",
+    "half_even",
+]
