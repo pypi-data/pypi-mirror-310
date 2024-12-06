48930ba7231caea686efcaab532d902eae447e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:04:33 +0000

    Add package japanese-vocabulary-extractor/1.1.2/japanese_vocabulary_extractor-1.1.2-py3-none-any.whl

---
+++ packages/japanese-vocabulary-extractor/japanese_vocabulary_extractor-1.1.2-py3-none-any.whl/sample/dictionary.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Standard Library Imports
+import logging
+from jamdict import Jamdict
+
+jam = Jamdict()
+
+
+def get_word_info(word: str) -> dict:
+    result = jam.lookup(word)
+    if len(result.entries) == 0:
+        return {"definition": "", "kana": "", "is_real": False, "id": ""}
+    definitions = result.entries[0]
+    definition_text = ", ".join(sense.text() for sense in definitions.senses[:3])
+    kana_text = definitions.kana_forms[0].text
+    return {
+        "definition": definition_text,
+        "kana": kana_text,
+        "is_real": True,
+        "id": definitions.idseq,
+    }
