404ea31b962ccd04aecde3ea46fca0f734fc2102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 17:05:34 +0000

    Add package pysorteddict/0.0.5/pysorteddict-0.0.5.tar.gz

---
+++ packages/pysorteddict/pysorteddict-0.0.5.tar.gz/pysorteddict-0.0.5/setup.py
@@ -0,0 +1,14 @@
+import platform
+
+import setuptools
+
+setuptools.setup(
+    ext_modules=[
+        setuptools.Extension(
+            name="pysorteddict",
+            extra_compile_args=["/std:c++14" if platform.system() == "Windows" else "-std=c++14"],
+            sources=["src/pysorteddict/pysorteddict.cc"],
+            py_limited_api=True,
+        )
+    ]
+)
