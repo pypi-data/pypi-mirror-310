35628f97bccce05382d8830667553b6d1ed0f669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-06 16:04:57 +0000

    Add package opencal/3.8.3/opencal-3.8.3-py3-none-any.whl

---
+++ packages/opencal/opencal-3.8.3-py3-none-any.whl/opencal/__init__.py
@@ -0,0 +1,58 @@
+"""OpenCAL
+
+OpenCAL core library for Python
+
+Note:
+
+    This project is in beta stage.
+
+Viewing documentation using IPython
+-----------------------------------
+To see which functions are available in `opencal`, type ``opencal.<TAB>`` (where
+``<TAB>`` refers to the TAB key), or use ``opencal.*get_version*?<ENTER>`` (where
+``<ENTER>`` refers to the ENTER key) to narrow down the list.  To view the
+docstring for a function, use ``opencal.get_version?<ENTER>`` (to view the
+docstring) and ``opencal.get_version??<ENTER>`` (to view the source code).
+"""
+
+import importlib.metadata
+import logging
+
+import opencal.config
+import opencal.io.sqlitedb
+import opencal.path
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+# X.Y
+# X.Y.Z # For bugfix releases  
+# 
+# Admissible pre-release markers:
+# X.YaN # Alpha release
+# X.YbN # Beta release         
+# X.YrcN # Release Candidate   
+# X.Y # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+
+__version__ = importlib.metadata.version("opencal")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s:%(name)s:%(message)s',  # See https://docs.python.org/3/library/logging.html#logrecord-attributes
+    datefmt=r'%Y-%m-%dT%H:%M:%S%z'
+)
+
+def get_version():
+    return __version__
+
+
+cfg, config_path = opencal.config.get_config()
+
+
+__all__: list[str] = [
+    # 'get_version',
+]
