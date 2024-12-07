290ae3bf3a719b95b59912642204f2781f17b3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:56 +0000

    Add package wagtail-localize-git/0.15.0/wagtail_localize_git-0.15.0-py3-none-any.whl

---
+++ packages/wagtail-localize-git/wagtail_localize_git-0.15.0-py3-none-any.whl/wagtail_localize_git/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class WagtailLocalizeGitAppConfig(AppConfig):
+    label = "wagtail_localize_git"
+    name = "wagtail_localize_git"
+    verbose_name = "Wagtail Localize Git integration"
+    default_auto_field = "django.db.models.AutoField"
