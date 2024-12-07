6be73076ab24fb0a636fb2375a19e9a096c4f4fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:33:29 +0000

    Add package simple-package-yeonjin/0.1.0/simple_package_yeonjin-0.1.0.tar.gz

---
+++ packages/simple-package-yeonjin/simple_package_yeonjin-0.1.0.tar.gz/simple_package_yeonjin-0.1.0/README.md
@@ -0,0 +1,13 @@
+# My Simple Package
+
+이 패키지는 문자열을 뒤집는 간단한 함수를 제공합니다.
+
+## 설치
+
+pip install simple_package_yeonjin
+
+## 사용법
+```python
+from my_simple_package import reverse_string
+
+print(reverse_string("hello")) 
---
+++ packages/simple-package-yeonjin/simple_package_yeonjin-0.1.0.tar.gz/simple_package_yeonjin-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simple_package_yeonjin",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple package for reversing strings",
+    author="yeonjin",
+    author_email="yeonjin917n.n@gmail.com",
+    url="https://github.com/kangyeonjin/simple_package.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
