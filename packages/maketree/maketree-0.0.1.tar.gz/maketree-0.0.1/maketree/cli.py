db4ff253ba629b8e9d24517c7ea1f2e87336207f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:23 +0000

    Add package maketree/0.0.1/maketree-0.0.1.tar.gz

---
+++ packages/maketree/maketree-0.0.1.tar.gz/maketree-0.0.1/maketree/cli.py
@@ -0,0 +1,8 @@
+""" Frontend of the project (Argument handling and stuff) """
+
+PROGRAM = "maketree"
+VERSION = "0.0.1"
+
+
+def main():
+    print(PROGRAM, VERSION)
---
+++ packages/maketree/maketree-0.0.1.tar.gz/maketree-0.0.1/maketree/core/parser.py
@@ -0,0 +1,10 @@
+"""  Responsible for reading and parsing the structure file (in `.tree` format),
+that users provide to define the directory structure. """
+
+
+class Parser:
+
+    @classmethod
+    def parse_file(cls, filepath: str):
+        """Parse `filepath` .tree file and return the tree in a usable format (e.g, `dict` or `list`)"""
+        pass
