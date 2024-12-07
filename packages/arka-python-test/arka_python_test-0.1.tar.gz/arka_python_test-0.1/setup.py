90040f23f905de4b5dd73d43565f1a4a890d68ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:59 +0000

    Add package arka-python-test/0.1/arka_python_test-0.1.tar.gz

---
+++ packages/arka-python-test/arka_python_test-0.1.tar.gz/arka_python_test-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="arka_python_test",
+    version="0.1",
+    description="A Python package to interact with the Arka blockchain network",
+    author="Python",
+    author_email="chandini@vitwit.com",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "tqdm",
+    ],
+    python_requires=">=3.6",
+)
