fe3cc3973871da2db3668319138d5d7a05b36eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:37:03 +0000

    Add package opencv-statistical-tools/0.0.10/opencv_statistical_tools-0.0.10.tar.gz

---
+++ packages/opencv-statistical-tools/opencv_statistical_tools-0.0.10.tar.gz/opencv_statistical_tools-0.0.10/README.md
@@ -0,0 +1,10 @@
+# OpenCV Homography Utility
+This Python library provides a simplified and powerful interface for working with homography transformations using OpenCV. It is designed for tasks like perspective transformations, image stitching, planar mapping, and object alignment. The library abstracts complex OpenCV functionality into user-friendly methods for developers and researchers.
+
+Features
+
+Compute homography matrix from corresponding points.
+Apply perspective transformations to images.
+Visualize transformed images with overlays.
+Error calculation and optimization for homography accuracy.
+Export and import homography matrices for reuse.
---
+++ packages/opencv-statistical-tools/opencv_statistical_tools-0.0.10.tar.gz/opencv_statistical_tools-0.0.10/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open("app/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="opencv_statistical_tools",
+    version="0.0.10",
+    description="An OpenCV utility package for additional utility functions for probability and statistical operations.",
+    package_dir={"": "app"},
+    include_package_data=True,
+    package_data={'': ['Scenes/*']},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArjanCodes/2023-package",
+    author="Stephen Hudson",
+    author_email="arjan@arjancodes.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["opencv-python>=4.5.3", "numpy>=1.21.0"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.6",
+)
