7b5f983a0772c669acbc1e84861e65d2fe7f3f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:05 +0000

    Add package multiplespacolibrary/0.1.0/multiplespacolibrary-0.1.0.tar.gz

---
+++ packages/multiplespacolibrary/multiplespacolibrary-0.1.0.tar.gz/multiplespacolibrary-0.1.0/multiples/is_multiple_of_five.py
@@ -0,0 +1,3 @@
+def is_multiple_of_five(number):
+    """ Check if a number is a multiple of two. """
+    return number % 5 == 0
---
+++ packages/multiplespacolibrary/multiplespacolibrary-0.1.0.tar.gz/multiplespacolibrary-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="multiplespacolibrary",  # Replace with your package’s name
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author="pacobahena",
+    author_email="pakobahena72@gmail.com",
+    description="A library for checking multiples of 2 and 5.",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # License type
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
