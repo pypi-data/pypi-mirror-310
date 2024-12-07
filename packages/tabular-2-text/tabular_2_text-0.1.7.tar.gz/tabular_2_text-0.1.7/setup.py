2c0319cd63c8e644a9499ec312738ff34d4b8d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:30 +0000

    Add package tabular-2-text/0.1.7/tabular_2_text-0.1.7.tar.gz

---
+++ packages/tabular-2-text/tabular_2_text-0.1.7.tar.gz/tabular_2_text-0.1.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tabular_2_text",  # Your package name
+    version="0.1.7",  # updated readme with correct info
+    description="Take a df or csv and convert to text",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Sandeep Junnarkar",
+    author_email="sjnews@gmail.com",
+    url="https://github.com/sandeepmj/tabular_2_text",  # Repo URL or project homepage
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',  # Minimum Python version
+    install_requires=[  # Dependencies
+        "numpy>=1.21.0",
+        "pandas>=1.3.0"
+    ],
+)
