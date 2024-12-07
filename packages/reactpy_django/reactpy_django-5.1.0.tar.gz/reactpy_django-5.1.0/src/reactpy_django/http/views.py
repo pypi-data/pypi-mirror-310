3a6bb77a568457a5e3caa973e5e412a8f79d584c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:49 +0000

    Add package reactpy_django/5.1.0/reactpy_django-5.1.0.tar.gz

---
+++ packages/reactpy_django/reactpy_django-5.1.0.tar.gz/reactpy_django-5.1.0/src/reactpy_django/http/views.py
@@ -0,0 +1,64 @@
+import asyncio
+import os
+from urllib.parse import parse_qs
+
+from django.core.cache import caches
+from django.core.exceptions import SuspiciousOperation
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
+from reactpy.config import REACTPY_WEB_MODULES_DIR
+
+from reactpy_django.utils import create_cache_key, render_view
+
+
+async def web_modules_file(request: HttpRequest, file: str) -> HttpResponse:
+    """Gets JavaScript required for ReactPy modules at runtime. These modules are
+    returned from cache if available."""
+    from reactpy_django.config import REACTPY_CACHE
+
+    web_modules_dir = REACTPY_WEB_MODULES_DIR.current
+    path = os.path.abspath(web_modules_dir.joinpath(file))
+
+    # Prevent attempts to walk outside of the web modules dir
+    if str(web_modules_dir) != os.path.commonpath((path, web_modules_dir)):
+        raise SuspiciousOperation(
+            "Attempt to access a directory outside of REACTPY_WEB_MODULES_DIR."
+        )
+
+    # Fetch the file from cache, if available
+    last_modified_time = os.stat(path).st_mtime
+    cache_key = create_cache_key("web_modules", path)
+    file_contents = await caches[REACTPY_CACHE].aget(
+        cache_key, version=int(last_modified_time)
+    )
+    if file_contents is None:
+        with open(path, "r", encoding="utf-8") as fp:
+            file_contents = await asyncio.to_thread(fp.read)
+        await caches[REACTPY_CACHE].adelete(cache_key)
+        await caches[REACTPY_CACHE].aset(
+            cache_key, file_contents, timeout=604800, version=int(last_modified_time)
+        )
+
+    # TODO: Convert this to a StreamingHttpResponse
+    return HttpResponse(file_contents, content_type="text/javascript")
+
+
+async def view_to_iframe(request: HttpRequest, dotted_path: str) -> HttpResponse:
+    """Returns a view that was registered by reactpy_django.components.view_to_iframe."""
+    from reactpy_django.config import REACTPY_REGISTERED_IFRAME_VIEWS
+
+    # Get the view
+    registered_view = REACTPY_REGISTERED_IFRAME_VIEWS.get(dotted_path)
+    if not registered_view:
+        return HttpResponseNotFound()
+
+    # Get args and kwargs from the request
+    query = request.META.get("QUERY_STRING", "")
+    kwargs = {k: v if len(v) > 1 else v[0] for k, v in parse_qs(query).items()}
+    args = kwargs.pop("_args", [])
+
+    # Render the view
+    response = await render_view(registered_view, request, args, kwargs)
+
+    # Ensure page can be rendered as an iframe
+    response["X-Frame-Options"] = "SAMEORIGIN"
+    return response
---
+++ packages/reactpy_django/reactpy_django-5.1.0.tar.gz/reactpy_django-5.1.0/src/reactpy_django/models.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
+from reactpy_django.utils import get_pk
+
+
+class ComponentSession(models.Model):
+    """A model for storing component sessions."""
+
+    uuid = models.UUIDField(primary_key=True, editable=False, unique=True)  # type: ignore
+    params = models.BinaryField(editable=False)  # type: ignore
+    last_accessed = models.DateTimeField(auto_now=True)  # type: ignore
+
+
+class Config(models.Model):
+    """A singleton model for storing ReactPy configuration."""
+
+    cleaned_at = models.DateTimeField(auto_now_add=True)  # type: ignore
+
+    def save(self, *args, **kwargs):
+        """Singleton save method."""
+        self.pk = 1
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def load(cls):
+        obj, _ = cls.objects.get_or_create(pk=1)
+        return obj
+
+
+class UserDataModel(models.Model):
+    """A model for storing `user_state` data."""
+
+    # We can't store User as a ForeignKey/OneToOneField because it may not be in the same database
+    # and Django does not allow cross-database relations. Also, since we can't know the type of the UserModel PK,
+    # we store it as a string to normalize.
+    user_pk = models.CharField(max_length=255, unique=True)  # type: ignore
+    data = models.BinaryField(null=True, blank=True)  # type: ignore
+
+
+@receiver(pre_delete, sender=get_user_model(), dispatch_uid="reactpy_delete_user_data")
+def delete_user_data(sender, instance, **kwargs):
+    """Delete ReactPy's `UserDataModel` when a Django `User` is deleted."""
+    pk = get_pk(instance)
+
+    UserDataModel.objects.filter(user_pk=pk).delete()
