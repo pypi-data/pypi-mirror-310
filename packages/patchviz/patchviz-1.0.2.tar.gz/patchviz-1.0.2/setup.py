6cba20691b2a9a2b866180bbea0977d7c35a48cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:02 +0000

    Add package patchviz/1.0.2/patchviz-1.0.2.tar.gz

---
+++ packages/patchviz/patchviz-1.0.2.tar.gz/patchviz-1.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="patchviz",  # Your package name
+    version="1.0.2",  # Package version
+    description="Patch Viz: A library to test how well your model holds up against adversarial attacks",  # Short description
+    long_description=open("README.md").read(),  # Use README.md for detailed description
+    long_description_content_type="text/markdown",
+    author="Suneel Nadipalli",  # Replace with your name
+    author_email="nsuneel89@gmail.com",  # Replace with your email
+    url="https://github.com/suneel-nadipalli/patchviz",  # GitHub repo URL
+    packages=find_packages(),  # Automatically find all sub-packages
+    install_requires=[
+        "torch",
+        "torchvision",
+        "torchaudio",
+        "numpy",
+        "matplotlib",
+        "captum",
+        "tqdm",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
