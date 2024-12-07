3cef0cb040fe5c723e02f3c9cb714f0f30964145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:49 +0000

    Add package xgrammar/0.1.2/xgrammar-0.1.2-cp310-cp310-win_amd64.whl

---
+++ packages/xgrammar/xgrammar-0.1.2-cp310-cp310-win_amd64.whl/xgrammar/kernels/__init__.py
@@ -0,0 +1,4 @@
+"""The kernels for XGrammar."""
+
+from .apply_token_bitmask_inplace_cpu import apply_token_bitmask_inplace_cpu
+from .apply_token_bitmask_inplace_cuda import apply_token_bitmask_inplace_cuda
