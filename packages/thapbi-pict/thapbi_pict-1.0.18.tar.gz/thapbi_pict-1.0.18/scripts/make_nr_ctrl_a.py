5ecf64213d4dbbb6f6af09dac7e908509c6b2da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:12 +0000

    Add package thapbi-pict/1.0.18/thapbi_pict-1.0.18.tar.gz

---
+++ packages/thapbi-pict/thapbi_pict-1.0.18.tar.gz/thapbi_pict-1.0.18/scripts/make_nr_ctrl_a.py
@@ -0,0 +1,21 @@
+"""Make FASTA files non-redundant using Ctrl+A separator."""
+
+import sys
+from typing import Dict
+
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+
+CTRL_A = chr(1)
+
+seq_dict: Dict[str, set[str]] = {}
+for filename in sys.argv[1:]:
+    with open(filename) as handle:
+        for title, seq in SimpleFastaParser(handle):
+            for entry in title.split(CTRL_A):
+                if seq in seq_dict:
+                    seq_dict[seq].add(entry)
+                else:
+                    seq_dict[seq] = {entry}
+
+for seq in sorted(seq_dict):
+    print(f">{CTRL_A.join(sorted(seq_dict[seq]))}\n{seq}")
