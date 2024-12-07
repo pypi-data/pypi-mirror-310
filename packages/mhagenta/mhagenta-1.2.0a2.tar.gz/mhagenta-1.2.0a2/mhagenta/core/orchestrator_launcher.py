2993af6a73b53b8e2fc8683de539fd6bc491a99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:26 +0000

    Add package mhagenta/1.2.0a2/mhagenta-1.2.0a2.tar.gz

---
+++ packages/mhagenta/mhagenta-1.2.0a2.tar.gz/mhagenta-1.2.0a2/mhagenta/core/orchestrator_launcher.py
@@ -0,0 +1,12 @@
+import dill
+from typing import Any
+
+from mhagenta import Orchestrator
+
+
+if __name__ == "__main__":
+    with open('/mha-save/_orchestrator', 'rb') as f:
+        orchestrator: Orchestrator = dill.load(f)
+    with open('/mha-save/_params', 'rb') as f:
+        params: dict[str, Any] = dill.load(f)
+    orchestrator.run(**params)
