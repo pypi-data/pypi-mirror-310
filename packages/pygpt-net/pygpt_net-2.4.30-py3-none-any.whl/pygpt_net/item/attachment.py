10cce417ca67ce99bfb25bf0a5a7f248507bbac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:54 +0000

    Add package pygpt-net/2.4.30/pygpt_net-2.4.30-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.4.30-py3-none-any.whl/pygpt_net/item/attachment.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.11.23 00:00:00                  #
+# ================================================== #
+
+import json
+
+
+class AttachmentItem:
+    def __init__(self):
+        """
+        Attachment item
+        """
+        self.name = None
+        self.id = None
+        self.uuid = None
+        self.path = None
+        self.remote = None
+        self.vector_store_ids = []
+        self.meta_id = None
+        self.ctx = False
+        self.consumed = False
+        self.size = 0
+        self.send = False
+
+    def serialize(self) -> dict:
+        """
+        Serialize item to dict
+
+        :return: serialized item
+        """
+        return {
+            'id': self.id,
+            'uuid': self.uuid,
+            'name': self.name,
+            'path': self.path,
+            'size': self.size,
+            'remote': self.remote,
+            'ctx': self.ctx,
+            'vector_store_ids': self.vector_store_ids,
+            'meta_id': self.meta_id,
+            'send': self.send
+        }
+
+    def deserialize(self, data: dict):
+        """
+        Deserialize item from dict
+
+        :param data: serialized item
+        """
+        if 'id' in data:
+            self.id = data['id']
+        if 'uuid' in data:
+            self.uuid = data['uuid']
+        if 'name' in data:
+            self.name = data['name']
+        if 'path' in data:
+            self.path = data['path']
+        if 'size' in data:
+            self.size = data['size']
+        if 'remote_id' in data:
+            self.remote = data['remote']
+        if 'ctx' in data:
+            self.ctx = data['ctx']
+        if 'vector_store_ids' in data:
+            self.vector_store_ids = data['vector_store_ids']
+        if 'meta_id' in data:
+            self.meta_id = data['meta_id']
+        if 'send' in data:
+            self.send = data['send']
+
+    def dump(self) -> str:
+        """
+        Dump item to string
+
+        :return: serialized item
+        """
+        try:
+            return json.dumps(self.serialize())
+        except Exception as e:
+            pass
+        return ""
+
+    def __str__(self):
+        """To string"""
+        return self.dump()
