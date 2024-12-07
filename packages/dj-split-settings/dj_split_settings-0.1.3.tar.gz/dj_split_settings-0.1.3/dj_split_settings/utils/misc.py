0f586f0ed59670726dcd79ee1a22badedec223fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:37 +0000

    Add package dj-split-settings/0.1.3/dj_split_settings-0.1.3.tar.gz

---
+++ packages/dj-split-settings/dj_split_settings-0.1.3.tar.gz/dj_split_settings-0.1.3/dj_split_settings/utils/misc.py
@@ -0,0 +1,16 @@
+import yaml  # type: ignore
+
+
+def yaml_coerce(value):
+    if isinstance(value, str):
+        return yaml.load("dummy: " + value, Loader=yaml.SafeLoader)["dummy"]
+
+    return value
+
+
+def hex_to_bytes(hex_string: str) -> bytes:
+    return bytes.fromhex(hex_string)
+
+
+def bytes_to_hex(bytes_: bytes) -> str:
+    return bytes(bytes_).hex()
