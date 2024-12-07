d11201f02a3cceb81406f71a12cfab800ab9b6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:56 +0000

    Add package smallmodels/0.3.1/smallmodels-0.3.1-py3-none-any.whl

---
+++ packages/smallmodels/smallmodels-0.3.1-py3-none-any.whl/smallmodels/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from smallmodels.api.ggrfhvac_update_api import GGRFHVACUpdateApi
+from smallmodels.api.health_data_api import HealthDataApi
+from smallmodels.api.range_upgrade_api import RangeUpgradeApi
+
