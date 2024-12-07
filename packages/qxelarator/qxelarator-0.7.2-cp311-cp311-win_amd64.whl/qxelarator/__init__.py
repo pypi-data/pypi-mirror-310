15d1b365e154b00646e26d26197d1532efde9c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:33:36 +0000

    Add package qxelarator/0.7.2/qxelarator-0.7.2-cp311-cp311-win_amd64.whl

---
+++ packages/qxelarator/qxelarator-0.7.2-cp311-cp311-win_amd64.whl/qxelarator/__init__.py
@@ -0,0 +1,32 @@
+# Author Imran Ashraf
+
+
+# The import syntax changes slightly between python 2 and 3, so we
+# need to detect which version is being used:
+from sys import version_info
+if version_info[0] < 3:
+    raise EnvironmentError("sys.version_info refers to a version of "
+        "Python is not 3. This is not permitted. "
+        "sys.version_info = {}".format(version_info))
+
+class SimulationResult:
+    def __init__(self):
+        self.shots_requested = 0
+        self.shots_done = 0
+        self.results = {}
+        self.state = {}
+
+    def __repr__(self):
+        return f"""Shots requested: {self.shots_requested}
+Shots done: {self.shots_done}
+Measurements: {self.results}
+State: {self.state}"""
+
+class SimulationError:
+    def __init__(self, message):
+        self.message = message
+
+    def __repr__(self):
+        return f"Quantum simulation error: {self.message}"
+
+from .qxelarator import *
