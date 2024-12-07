51c5b177b89bda39a255361413d82df974c5dbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:53 +0000

    Add package arelle-release/2.35.10/arelle_release-2.35.10-py3-none-any.whl

---
+++ packages/arelle-release/arelle_release-2.35.10-py3-none-any.whl/tests/integration_tests/validation/conformance_suite_configurations/efm_current.py
@@ -0,0 +1,37 @@
+from pathlib import Path, PurePath
+
+from tests.integration_tests.validation.conformance_suite_config import (
+    AssetSource,
+    ConformanceSuiteAssetConfig,
+    ConformanceSuiteConfig,
+)
+
+CONFORMANCE_SUITE_ZIP_NAME = 'efm-71-240916.zip'
+
+config = ConformanceSuiteConfig(
+    additional_plugins_by_prefix=[(f'conf/{t}', frozenset({'EDGAR/render'})) for t in [
+        '612-presentation-syntax/612-09-presented-units-order',
+        '624-rendering/15-equity-changes',
+        '624-rendering/17-uncategorized-facts',
+        '626-rendering-syntax',
+        '902-sdr/efm/62421-sdr-multiple',
+    ]],
+    args=[
+        '--disclosureSystem', 'efm-pragmatic',
+        '--formula', 'run',
+    ],
+    assets=[
+        ConformanceSuiteAssetConfig.conformance_suite(
+            Path(CONFORMANCE_SUITE_ZIP_NAME),
+            entry_point=Path('conf/testcases.xml'),
+            public_download_url=f'https://www.sec.gov/files/edgar/{CONFORMANCE_SUITE_ZIP_NAME}',
+            source=AssetSource.S3_PUBLIC,
+        )
+    ],
+    cache_version_id='7RGPI61UxvM3f44.JpzkU2S3AU5bBVeF',
+    info_url='https://www.sec.gov/structureddata/osdinteractivedatatestsuite',
+    name=PurePath(__file__).stem,
+    plugins=frozenset({'EDGAR/validate', 'inlineXbrlDocumentSet'}),
+    shards=40,
+    test_case_result_options='match-any',
+)
