01f81407f86db2d5b1a03ae64f964fb25e5e0eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:00 +0000

    Add package datatrack/0.1.2/datatrack-0.1.2.tar.gz

---
+++ packages/datatrack/datatrack-0.1.2.tar.gz/datatrack-0.1.2/src/datatrack/config.py
@@ -0,0 +1,17 @@
+import os
+import tomllib
+from pathlib import Path
+
+
+default = {
+    "config": Path.home() / ".config",
+    "cache": Path.home() / ".local" / "share",
+}
+
+cfg_dir_path = Path(os.getenv("XDG_CONFIG_HOME", default["config"])) / __package__
+with open(cfg_dir_path / "config.toml", "rb") as f:
+    config = tomllib.load(f)
+
+if "cache" not in config:
+    data_dir =  Path(os.getenv("XDG_DATA_HOME", default["cache"])) / __package__
+    config["cache"] = data_dir / "cache"
