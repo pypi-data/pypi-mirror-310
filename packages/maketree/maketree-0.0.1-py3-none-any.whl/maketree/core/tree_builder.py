c145c5642d12ec0775a828af45d76df17229e5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:23 +0000

    Add package maketree/0.0.1/maketree-0.0.1-py3-none-any.whl

---
+++ packages/maketree/maketree-0.0.1-py3-none-any.whl/maketree/core/tree_builder.py
@@ -0,0 +1,2 @@
+""" Contains logic for creating the directory structure on the file system,
+based on the parsed data from the structure file. """
---
+++ packages/maketree/maketree-0.0.1-py3-none-any.whl/maketree/core/validator.py
@@ -0,0 +1,2 @@
+""" Responsible for validating the structure file and ensuring that the defined directory structure is correct and usable. """
+
