7d91d67b2002560d6a7f4a25805e4fead68ee057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:40 +0000

    Add package asyncly/0.3.3/asyncly-0.3.3-py3-none-any.whl

---
+++ packages/asyncly/asyncly-0.3.3-py3-none-any.whl/asyncly/srvmocker/handlers.py
@@ -0,0 +1,21 @@
+from collections.abc import Awaitable, Callable
+
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+
+from asyncly.srvmocker.models import MockService, RequestHistory
+
+
+def get_default_handler(handler_name: str) -> Callable[[Request], Awaitable[Response]]:
+    async def _handler(request: Request) -> Response:
+        history = RequestHistory(
+            request=request,
+            body=await request.read(),
+        )
+        context: MockService = request.app["service"]
+        context.history.append(history)
+        context.history_map[handler_name].append(history)
+        handler = context.handlers[handler_name]
+        return await handler.response(request)
+
+    return _handler
