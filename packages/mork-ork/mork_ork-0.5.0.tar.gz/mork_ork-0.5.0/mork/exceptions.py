53c7eec043ac1a3fd77d4dfaf2d9e0a07049ff0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:10:42 +0000

    Add package mork-ork/0.5.0/mork_ork-0.5.0.tar.gz

---
+++ packages/mork-ork/mork_ork-0.5.0.tar.gz/mork_ork-0.5.0/mork/exceptions.py
@@ -0,0 +1,25 @@
+"""Exceptions for Mork."""
+
+
+class EmailAlreadySent(Exception):
+    """Raised when an email has already been sent to this user."""
+
+
+class EmailSendError(Exception):
+    """Raised when an error occurs when sending an email."""
+
+
+class UserDeleteError(Exception):
+    """Raised when an error occurs when deleting a user."""
+
+
+class UserNotFound(Exception):
+    """Raised when a user has not been found."""
+
+
+class UserStatusError(Exception):
+    """Raised when an error occurs when checking a user status."""
+
+
+class UserProtected(Exception):
+    """Raised when a user is associated with an entry in a protected table."""
