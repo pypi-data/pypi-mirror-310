cce095a49e18172aab93ff5fff223b18273c931c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:22 +0000

    Add package wgpu/0.19.2/wgpu-0.19.2.tar.gz

---
+++ packages/wgpu/wgpu-0.19.2.tar.gz/wgpu-0.19.2/wgpu/__init__.py
@@ -0,0 +1,30 @@
+"""
+WebGPU for Python.
+"""
+
+# ruff: noqa: F401, F403
+
+from ._coreutils import logger
+from ._version import __version__, version_info
+from ._diagnostics import diagnostics, DiagnosticsBase
+from .flags import *
+from .enums import *
+from .classes import *
+from .gui import WgpuCanvasInterface
+from . import utils
+from . import backends
+from . import resources
+
+# The API entrypoint, from wgpu.classes - gets replaced when a backend loads.
+gpu = GPU()  # noqa: F405
+
+
+def rendercanvas_context_hook(canvas, present_methods):
+    import sys
+
+    backend_module = gpu.__module__
+    if backend_module in ("", "wgpu._classes"):
+        raise RuntimeError(
+            "A backend must be selected (e.g. with wgpu.gpu.request_adapter()) before canvas.get_context() can be called."
+        )
+    return sys.modules[backend_module].GPUCanvasContext(canvas, present_methods)
