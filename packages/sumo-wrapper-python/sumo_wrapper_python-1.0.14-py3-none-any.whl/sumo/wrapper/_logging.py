77199c1d651b164661660a4f9e233ff3dc2f343b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:42 +0000

    Add package sumo-wrapper-python/1.0.14/sumo_wrapper_python-1.0.14-py3-none-any.whl

---
+++ packages/sumo-wrapper-python/sumo_wrapper_python-1.0.14-py3-none-any.whl/sumo/wrapper/_logging.py
@@ -0,0 +1,33 @@
+import logging
+from datetime import datetime
+
+
+class LogHandlerSumo(logging.Handler):
+    def __init__(self, sumoClient):
+        logging.Handler.__init__(self)
+        self._sumoClient = sumoClient
+        return
+
+    def emit(self, record):
+        try:
+            dt = datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
+            json = {
+                "severity": record.levelname,
+                "message": record.getMessage(),
+                "timestamp": dt,
+                "source": record.name,
+                "pathname": record.pathname,
+                "funcname": record.funcName,
+                "linenumber": record.lineno,
+            }
+            if "objectUuid" in record.__dict__.keys():
+                json["objectUuid"] = record.__dict__.get("objectUuid")
+
+            self._sumoClient.post("/message-log/new", json=json)
+        except Exception:
+            # Never fail on logging
+            pass
+
+        return
+
+    pass
