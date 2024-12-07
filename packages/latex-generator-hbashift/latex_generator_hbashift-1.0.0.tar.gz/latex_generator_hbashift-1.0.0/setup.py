8ed0d491c38cdb619e8293fc3e9a8469774758ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:11:18 +0000

    Add package latex-generator-hbashift/1.0.0/latex_generator_hbashift-1.0.0.tar.gz

---
+++ packages/latex-generator-hbashift/latex_generator_hbashift-1.0.0.tar.gz/latex_generator_hbashift-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="latex_generator_hbashift",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+)
