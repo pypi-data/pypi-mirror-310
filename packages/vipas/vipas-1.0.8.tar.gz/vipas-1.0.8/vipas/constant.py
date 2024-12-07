996952fb81868762119fc9837daf68bfc837523d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:17 +0000

    Add package vipas/1.0.8/vipas-1.0.8.tar.gz

---
+++ packages/vipas/vipas-1.0.8.tar.gz/vipas-1.0.8/vipas/constant.py
@@ -0,0 +1,23 @@
+"""
+  Copyright (c) 2024 Vipas.AI
+
+  All rights reserved. This program and the accompanying materials
+  are made available under the terms of a proprietary license which prohibits
+  redistribution and use in any form, without the express prior written consent
+  of Vipas.AI.
+  
+  This code is proprietary to Vipas.AI and is protected by copyright and
+  other intellectual property laws. You may not modify, reproduce, perform,
+  display, create derivative works from, repurpose, or distribute this code or any portion of it
+  without the express prior written permission of Vipas.AI.
+  
+  For more information, contact Vipas.AI at legal@vipas.ai
+""" 
+# List of allowed model framework types
+ALLOWED_MODEL_FRAMEWORK_TYPES = [
+    "onnx",
+    "pytorch",
+    "tensorflow",
+    "sklearn",
+    "xgboost"
+]
