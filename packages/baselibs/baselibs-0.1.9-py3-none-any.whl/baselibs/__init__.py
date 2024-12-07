f84cccaaa2653a9c650cb542802d6f56dcbfbb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:34 +0000

    Add package baselibs/0.1.9/baselibs-0.1.9-py3-none-any.whl

---
+++ packages/baselibs/baselibs-0.1.9-py3-none-any.whl/baselibs/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#coding:utf-8
+
+__author__ = 'xmxoxo<xmxoxo@qq.com>'
+
+
+from .baselibs import *
+from .mtask_lib import MutliTask
+from .rediscache import RedisCache
+from .simple_timer import TimeCount
+
+VERSION = "0.1.9"
+__version__ = VERSION
+
+'''
+__all__ = [
+    'ChainMap',
+    'Counter',
+    'OrderedDict',
+    'UserDict',
+    'UserList',
+    'UserString',
+    'defaultdict',
+    'deque',
+    'namedtuple',
+]
+'''
+
+
+
