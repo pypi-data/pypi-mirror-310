233308cc83ab0f4028df460964db11f1f028273f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:04:59 +0000

    Add package clickpy-client/0.1.2/clickpy_client-0.1.2.tar.gz

---
+++ packages/clickpy-client/clickpy_client-0.1.2.tar.gz/clickpy_client-0.1.2/clickpy/queries/package_downloads_resume_query.py
@@ -0,0 +1,30 @@
+from typing import ClassVar
+
+from pydantic import BaseModel
+
+from clickpy.queries.base import BaseClickPyQuery
+
+
+class PackageDownloadsResumeModel(BaseModel):
+    day: int
+    week: int
+    month: int
+    year: int
+
+
+class PackageDownloadsResumeQuery(BaseClickPyQuery[PackageDownloadsResumeModel]):
+    QUERY: ClassVar = """
+SELECT 
+    sumIf(count, date > today() - toIntervalDay(1)) AS day,
+    sumIf(count, date > today() - toIntervalWeek(1)) AS week,
+    sumIf(count, date > today() - toIntervalMonth(1)) AS month,
+    sumIf(count, date > today() - toIntervalYear(1)) AS year,
+    sum(count) AS total
+FROM 
+    pypi.pypi_downloads_per_day 
+WHERE 
+    project = '{package}';
+"""
+
+    # Request params
+    package: str
