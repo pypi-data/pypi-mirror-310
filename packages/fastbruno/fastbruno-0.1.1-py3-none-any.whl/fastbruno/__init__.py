269f1d5c2ea9cd71c4b618d1a938fce0649daf29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:39 +0000

    Add package fastbruno/0.1.1/fastbruno-0.1.1-py3-none-any.whl

---
+++ packages/fastbruno/fastbruno-0.1.1-py3-none-any.whl/fastbruno/__init__.py
@@ -0,0 +1,5 @@
+from fastbruno.__internal.brunofy import ReqParam, Request, RouteInfo
+from fastbruno.__internal.cli import fastbruno_cli
+from fastbruno.__internal.fastbruno import FastBruno
+
+__all__ = ["FastBruno", "RouteInfo", "Request", "ReqParam", "fastbruno_cli"]
