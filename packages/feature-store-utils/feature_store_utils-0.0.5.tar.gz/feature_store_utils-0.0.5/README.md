9707cc8bd45e3ff7fb658c168159f30840428a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:36 +0000

    Add package feature-store-utils/0.0.5/feature_store_utils-0.0.5.tar.gz

---
+++ packages/feature-store-utils/feature_store_utils-0.0.5.tar.gz/feature_store_utils-0.0.5/README.md
@@ -0,0 +1,60 @@
+# feature store utils
+
+A light-weight package that allows you express ML features in simple yaml, build a training data set and then write them to a feature store. 
+
+
+### some general thoughts on building a training dataset
+https://docs.google.com/presentation/d/1tVkrwCLVwFp8cZC7CmAHSNFhsJrcTdC20MlZfptkSBE/edit?usp=sharing
+
+
+### options for use
+
+1. clone this repo.  create features.yaml.  follow demo notebook. do not check back in.
+2. install as a python package. See https://github.com/BenMacKenzie/churn_model_demo as an example.  Note that you must create a .env file in folder which contains the features.yaml file 
+
+
+
+### Notes
+
+1. Current version is experimental.  Not clear that Jinja is the right way to write parameterized SQL. Might be better to do in Python.
+2. Current version is not optimized. Each feature is calculated individually, whereas if table, filters and time windows are identical, multiple aggregation features can be calculated simultaneously. 
+3. I believe there are around a dozen standard feature types.  The most common have been implemented.  Note that views can fill in a lot of gaps if encountered.  missing:
+  - type 1 lookup.
+  - 1st order aggregations over time series (e.g., just treat it like a fact table)
+  - 2nd order aggregations over time series e.g., max monthly job dbu over 6 month window.
+  - time in state,  e.g., how long was a ticket open.  based on a type 2 table.
+  - time to event in fact table, e.g., time since last call to customer support
+  - scalar functions of two or more features, e.g, time in days between two date
+  - num state changes over interval (rare)
+  - functions of features (e.g., ratio of growth in job dbu to interactive dbu).  Arguably this is not needed for boosted trees. Might be useful for neural nets...but why use a nueral net on heterogeneous data? (actually this kind of thing can be good for model explainability)
+4. Need to illustrate adding features from a related dimension table (using a foreign key...machinery is in place to do so.)
+5. Current version illustrates creating a pipeline which uses the api.  But it would be nice just to generate the code and write it to a notebook so that the package is invisible in production (like bamboolib) 
+9. The demo repo (https://github.com/BenMacKenzie/churn_model_demo) illustrates 'hyper-features' which are features with variable parameters. 
+10. Connecting 'hyper-features' to feature store needs to be worked out.  Currently the option is to add all of them or specify individual version by their (generated) name
+11. Fix feature store feature gen observation dates.  Align with grain of feature, e.g., if grain is monthly make sure feature store contains an observation on first of month.
+
+
+
+### Building
+
+
+
+```
+python3 -m build  
+python3 -m twine upload --repository testpypi dist/*
+
+python3 -m twine upload dist/*
+
+```
+
+
+### Running unit tests on databricks
+
+1. install the databricks extension for vscode
+2. use this repo as a template.  Note the following:
+3. remote_test_harness/pytest_databricks.py
+4. .vscode/launch.json 
+5. write tests as usual (see tests/time_series/time_series_test.py as an example)
+
+
+ 
---
+++ packages/feature-store-utils/feature_store_utils-0.0.5.tar.gz/feature_store_utils-0.0.5/features/feature_generation.py
@@ -0,0 +1,168 @@
+from pyspark.sql import SparkSession
+from databricks.feature_store.client import FeatureStoreClient
+import time
+import datetime
+from features.sql_gen import get_sql_for_feature
+from pyspark.sql import SparkSession
+from databricks.feature_store.client import FeatureStoreClient
+from features.feature_spec import load_data_spec, get_data_spec, get_features, get_feature_tables
+
+
+def create_eo_table(entity_table, pk, start_date, end_date, grain, eo_table_name):
+    data_spec=get_data_spec()
+    calendar_table = data_spec['calendar']
+    schema = data_spec['schema']
+
+    spark.sql(f"use {schema}")
+
+    if grain == 'day':
+        date_col = 'calendarDay'
+    elif grain == 'week':
+        date_col = 'lastCompleteWeek'
+    else:
+        date_col = 'lastCompleteMonth'
+
+    sql = f"with entities as (select distinct {pk} from {entity_table}), observation_dates as (select distinct {date_col} as observation_date from {calendar_table}) select {pk}, observation_date from observation_dates cross join entities \
+    where observation_date >= dateadd({grain}, -1, to_date('{start_date}')) and observation_date <= to_date('{end_date}')"
+    
+
+    spark.sql(sql).createOrReplaceTempView(eo_table_name)
+
+
+
+def register_dimension_table(table):
+    schema = data_spec['schema']
+    spark.sql(f"use database {schema}")
+    source_table_name = table['source_table_name']
+    pk = table['pk']
+    if table['type'] == 'dimension_type2':
+        timestamp_key = table['row_effective_from']
+        fs.register_table(delta_table=f"{schema}.{source_table_name}", primary_keys=pk, timestamp_keys=timestamp_key)
+    else:
+        fs.register_table(delta_table=f"{schema}.{source_table_name}", primary_keys=pk)
+
+
+def update_feature_table(feature_table):
+    build_feature_table(feature_table, drop_existing=False, update=True)
+
+
+
+def build_feature_table(feature_table_name, drop_existing=False, update=False):
+    data_spec = load_data_spec()
+    feature_tables = get_feature_tables()
+    feature_table = feature_tables[feature_table_name]
+    features = get_features()
+    schema = data_spec['schema']
+    spark.sql(f"use database {schema}")
+
+    table_name = feature_table['name']
+    f_table_name = f"{schema}.{table_name}"
+
+    entity_table = feature_table['entity_table']
+    entity_name = feature_table['pk']
+    start_date = feature_table['start_date']
+    end_date = feature_table['end_date']
+    grain = feature_table['grain']
+
+    eo_table_name = f"eo_table_{int(time.time())}"
+
+    eo_table = {
+        "name": eo_table_name,
+        "entity": {
+            "name": entity_name
+        },
+        "observation_date": {
+            "name": "observation_date"
+        }
+    }
+
+    if update:
+        start_date = datetime.today().strftime('%Y-%m-%d')
+        end_date = datetime.today().strftime('%Y-%m-%d')
+
+    create_eo_table(entity_table, entity_name, start_date, end_date, grain, eo_table_name)
+
+    df = spark.sql(f"select * from {eo_table_name}")
+
+    if drop_existing:
+        fs.drop_table(f_table_name)
+
+    feature_list = []
+    for feature_name in feature_table['features']:
+        if feature_name.endswith('*'):
+            prefix = feature_name[:-1]
+            for key in features:
+                if key.startswith(prefix):
+                    feature_list.append(key)
+        else:
+            feature_list.append(feature_name)
+
+    for feature_name in feature_list:
+        select_clause = get_sql_for_feature(feature_name, eo_table)
+        feature_df = spark.sql(select_clause)
+
+        cond = [df[entity_name] == feature_df[entity_name], df['observation_date'] == feature_df['observation_date']]
+
+        df = df.alias('a').join(feature_df.alias('b'), cond, 'left').select('a.*', f"b.{feature_name}")
+
+        feature_spec = features[feature_name]
+        if 'default_value' in feature_spec:
+            df = df.na.fill(feature_spec['default_value'], [feature_name])
+
+    if update:
+        fs.write_table(f_table_name, df=df, mode='merge')
+    else:
+        fs.create_table(f_table_name, primary_keys=feature_table['pk'], timestamp_keys='observation_date', df=df)
+
+
+def update_feature_table(feature_table):
+    build_feature_table(feature_table, drop_existing=False, update=True)
+
+
+def build_training_data_set(d=None):
+    data_spec = load_data_spec(d)
+    features = get_features()
+    
+    eol_table = data_spec['eol_table']
+    eol_table_name = eol_table['name']
+    entity_name = eol_table['entity']['name']
+    observation_date_name = eol_table['observation_date']['name']
+
+    schema = data_spec['schema']
+    spark.sql(f"use database {schema}")
+
+    df = spark.sql(f"select * from {eol_table_name}")
+
+    for feature_name, features_spec in features.items():
+
+        select_clause = get_sql_for_feature(feature_name)
+        feature_df = spark.sql(select_clause)
+
+        cond = [df[entity_name] == feature_df[entity_name],
+                df[observation_date_name] == feature_df[observation_date_name]]
+
+        df = df.alias('a').join(feature_df.alias('b'), cond, 'left').select('a.*', f"b.{feature_name}")
+
+        feature_spec = features[feature_name]
+        if 'default_value' in feature_spec:
+            df = df.na.fill(feature_spec['default_value'], [feature_name])
+
+    return df
+
+
+def register_dimension_table(table):
+    data_spec=get_data_spec()
+    schema = data_spec['schema']
+    spark.sql(f"use database {schema}")
+    source_table_name = table['source_table_name']
+    pk = table['pk']
+    if table['type'] == 'dimension_type2':
+        timestamp_key = table['row_effective_from']
+        fs.register_table(delta_table=f"{schema}.{source_table_name}", primary_keys=pk, timestamp_keys=timestamp_key)
+    else:
+        fs.register_table(delta_table=f"{schema}.{source_table_name}", primary_keys=pk)
+
+
+
+spark = SparkSession.builder.getOrCreate()
+fs = FeatureStoreClient()
