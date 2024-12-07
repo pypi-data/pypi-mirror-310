9ce0cf89a3f87ca994649fde43a4c7c900a09be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:17 +0000

    Add package zaowr-polsl-kisiel/0.0.15/zaowr_polsl_kisiel-0.0.15-py3-none-any.whl

---
+++ packages/zaowr-polsl-kisiel/zaowr_polsl_kisiel-0.0.15-py3-none-any.whl/zaowr_polsl_kisiel/save_calibration.py
@@ -0,0 +1,20 @@
+from json import dump as jdump
+from .exceptions import CalibrationParamsPathNotProvided
+
+
+def save_calibration(
+    calibrationParams: dict[str, list], calibrationParamsPath: str
+) -> None:
+    """
+    Save the provided calibration parameters to specified JSON file
+
+    :return: None
+
+    :raises CalibrationParamsPathNotProvided: Raises an error if the path was not provided or it isn't an instance of a string.
+    """
+
+    if (calibrationParamsPath == "") or (not isinstance(calibrationParamsPath, str)):
+        raise CalibrationParamsPathNotProvided
+
+    with open(calibrationParamsPath, "w", encoding="utf-8") as file:
+        jdump(calibrationParams, file, ensure_ascii=False, indent=4)
