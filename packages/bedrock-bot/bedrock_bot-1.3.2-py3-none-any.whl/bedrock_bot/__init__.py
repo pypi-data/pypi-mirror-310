0f83aac32c0e9c6d720bfbc0f3fd954945db8f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:46 +0000

    Add package bedrock-bot/1.3.2/bedrock_bot-1.3.2-py3-none-any.whl

---
+++ packages/bedrock-bot/bedrock_bot-1.3.2-py3-none-any.whl/bedrock_bot/__init__.py
@@ -0,0 +1,11 @@
+from .models import (
+    Claude3Haiku,
+    Claude3Opus,
+    Claude3Sonnet,
+    Claude35Haiku,
+    Claude35Sonnet,
+    Claude35SonnetV2,
+    MistralLarge,
+)
+
+model_list = [Claude3Haiku, Claude3Sonnet, Claude35Haiku, Claude35Sonnet, Claude35SonnetV2, Claude3Opus, MistralLarge]
---
+++ packages/bedrock-bot/bedrock_bot-1.3.2-py3-none-any.whl/bedrock_bot/models/__init__.py
@@ -0,0 +1,14 @@
+from .base_model import ConversationRole
+from .claude import Claude3Haiku, Claude3Opus, Claude3Sonnet, Claude35Haiku, Claude35Sonnet, Claude35SonnetV2
+from .mistral import MistralLarge
+
+__all__ = [
+    "Claude3Haiku",
+    "Claude3Sonnet",
+    "Claude3Opus",
+    "Claude35Haiku",
+    "Claude35Sonnet",
+    "Claude35SonnetV2",
+    "MistralLarge",
+    "ConversationRole",
+]
