c57fa3395820d028c48d735b84983520e3a53011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:24 +0000

    Add package tee-subprocess/1.2.0/tee_subprocess-1.2.0.tar.gz

---
+++ packages/tee-subprocess/tee_subprocess-1.2.0.tar.gz/tee_subprocess-1.2.0/README.md
@@ -0,0 +1,52 @@
+# tee-subprocess
+
+![GitHub Workflow Status](https://raster.shields.io/github/actions/workflow/status/thearchitector/tee-subprocess/ci.yaml?label=tests&style=flat-square)
+![PyPI - Downloads](https://raster.shields.io/pypi/dm/tee-subprocess?style=flat-square)
+
+A subprocess replacement with tee support for both synchronous and asynchronous contexts.
+
+Supports Python 3.8+.
+
+## Example
+
+Just import the `run` function and use it as you would use `subprocess.run`.
+
+```python
+from tee_subprocess import run
+
+process = run(["python", "--version"], tee=True, text=True, capture_output=True)
+# ==> Python 3.11.2
+print(process.stdout)
+# ==> Python 3.11.2
+```
+
+Changing `stdout` and `stderr` changes the location to which the `tee` occurs. You can supply any of the defined options in `subprocess` or `asyncio.subprocess` (`STDOUT`, `DEVNULL`, etc), as well as a writable text or binary file object; if providing a text file object, you must specify `text = True`.
+
+### Async
+
+Internally, `tee_subprocess` utilizes `asyncio` to concurrently output and capture the subprocess logs. If an event loop is already running, `run` will return an awaitable coroutine. Otherwise, it will call `asyncio.run` for you. Practically, this means you can just treat `run` as a coroutine if you're in an async context.
+
+```python
+async def main():
+    process = await run(["python", "--version"], tee=True, text=True, capture_output=True)
+    # ==> Python 3.11.2
+    print(process.stdout)
+    # ==> Python 3.11.2
+
+asyncio.run(main())
+```
+
+### Static Typing
+
+I do my best to provide a logical static function typing for any permitted invocation style. Mypy _should_ complain about missing or invalid overloads if you attempt to use a combination of arguments with undefined behavior (like supplying `text=True` while also providing a `BytesIO` as `stdout`, or supplying a `PathLike` argument while `shell=True`).
+
+The one fairly large exception to this is `await run(...)` vs `run(...)`. For now, `run` returns a union between a complete process and a coroutine due to the runtime-check for an asyncio context. As a result, you'll have to `cast` the `run(...)` call to either an awaitable or a `CompletedProcess` depending on your specific use. The API may change in the future to avoid this problem.
+
+## Alternatives
+
+[subprocess-tee](https://github.com/pycontribs/subprocess-tee), the motivation for this library, has the same objective but fails to accommodate asynchronous applications and non-shell invocations. This library supports asynchronous contexts as well as direct, non-shell, program execution ("list-style" vs. "shell-style").
+
+## License
+
+MIT License
+Copyright (c) 2023-2024 Elias Gabriel
---
+++ packages/tee-subprocess/tee_subprocess-1.2.0.tar.gz/tee_subprocess-1.2.0/tests/test_tee.py
@@ -0,0 +1,112 @@
+import asyncio
+import platform
+import sys
+from io import BytesIO, StringIO
+from pathlib import Path
+from subprocess import TimeoutExpired, run
+
+import pytest
+
+from tee_subprocess import run as run_tee
+
+OUTPUT = f"Python {platform.python_version()}"
+OUTPUTB = OUTPUT.encode()
+
+
+@pytest.mark.parametrize("aio", (False, True), ids=["sync", "async"])
+@pytest.mark.parametrize(
+    "cmd,shell",
+    (
+        (["python", "--version"], False),
+        ([b"python", b"--version"], False),
+        ([b"python", "--version"], False),
+        ([Path(sys.executable).resolve(), "--version"], False),
+        ("python --version", True),
+        pytest.param(
+            b"python --version",
+            True,
+            marks=pytest.mark.xfail(
+                condition=platform.system() == "Windows",
+                reason="Plain byte arguments are not supported on Windows",
+            ),
+        ),
+    ),
+    ids=["str-list", "bytes-list", "mixed-list", "list-with-pathlike", "str", "bytes"],
+)
+@pytest.mark.parametrize(
+    "kwargs,captured,teed",
+    (
+        # default behaviors are same as non-tee behaviors
+        (dict(tee=False), None, ""),
+        (dict(tee=False, capture_output=True), OUTPUTB, ""),
+        (dict(tee=False, text=True, capture_output=True), OUTPUT, ""),
+        # no capture, but tee (same as stdout as IO)
+        (dict(), None, OUTPUTB),
+        (dict(text=True), None, OUTPUT),
+        # capture and tee
+        (dict(capture_output=True), OUTPUTB, OUTPUTB),
+        (dict(text=True, capture_output=True), OUTPUT, OUTPUT),
+    ),
+    ids=(
+        "no-tee",
+        "no-tee-capture",
+        "no-tee-capture-text",
+        "tee-no-capture",
+        "tee-text-no-capture",
+        "tee-capture",
+        "tee-capture-text",
+    ),
+)
+def test_run(tmp_path, aio, shell, cmd, kwargs, captured, teed):
+    kwargs = dict(**kwargs, shell=shell)
+    tee = kwargs.pop("tee", True)
+    process = run(cmd, **kwargs)
+
+    if tee:
+        kwargs["stdout"] = StringIO() if kwargs.get("text") else BytesIO()
+
+    if aio:
+
+        async def _():
+            return await run_tee(cmd, tee=tee, **kwargs)
+
+        tprocess = asyncio.run(_())
+    else:
+        tprocess = run_tee(cmd, tee=tee, **kwargs)
+
+    pout = process.stdout.strip() if process.stdout else None
+    tout = tprocess.stdout.strip() if tprocess.stdout else None
+    assert tout == captured
+    assert pout == tout
+
+    if tee:
+        assert kwargs["stdout"].getvalue().strip() == teed
+        kwargs["stdout"].close()
+
+
+@pytest.mark.parametrize("aio", (False, True), ids=["sync", "async"])
+def test_run_timeout(aio):
+    with pytest.raises(TimeoutExpired):
+        if aio:
+
+            async def _():
+                return await run_tee(["python", "--version"], timeout=0)
+
+            asyncio.run(_())
+        else:
+            run_tee(["python", "--version"], timeout=0)
+
+
+@pytest.mark.parametrize(
+    "encoding,success",
+    (("latin1", False), ("utf-8", True)),
+    ids=["utf8-to-latin-fail", "utf8-to-utf8-pass"],
+)
+def test_run_encoding(encoding, success):
+    cmd = [
+        "python",
+        "-uc",
+        'import sys; sys.stdout.buffer.write("😊".encode())',
+    ]
+    out = run_tee(cmd, text=True, capture_output=True, encoding=encoding)
+    assert (out.stdout == "😊") is success
