77e1f95f71c678727e623f5652055552f250f18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:05:58 +0000

    Add package himpunan-tim5/0.1.0/himpunan_tim5-0.1.0.tar.gz

---
+++ packages/himpunan-tim5/himpunan_tim5-0.1.0.tar.gz/himpunan_tim5-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="himpunan_tim5",  
+    version="0.1.0",  
+    author="Deny Wahyudi Asaloei",
+    description="Library Python untuk implementasi teori himpunan",
+    packages=find_packages(),
+    install_requires=[],
+)
