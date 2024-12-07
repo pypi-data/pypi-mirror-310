4ab1868508c77882b20cd829d96425f24ee05d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:29 +0000

    Add package numerous/0.33.1/numerous-0.33.1-py3-none-macosx_10_0_arm64.whl

---
+++ packages/numerous/numerous-0.33.1-py3-none-macosx_10_0_arm64.whl/numerous/collections/__init__.py
@@ -0,0 +1,7 @@
+"""The Python SDK for numerous collections."""
+
+__all__ = ["collection", "CollectionReference", "DocumentReference"]
+
+from .collection import collection
+from .collection_reference import CollectionReference
+from .document_reference import DocumentReference
