2e2893c4faf1c79a207c062c8f212a283ae19afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:58 +0000

    Add package elasticipy/2.0.0/elasticipy-2.0.0.tar.gz

---
+++ packages/elasticipy/elasticipy-2.0.0.tar.gz/elasticipy-2.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='Elasticipy',
+    version='2.0.0',
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    url='https://elasticipy.readthedocs.io/',
+    license='MIT Licence',
+    author='Dorian Depriester',
+    author_email='dorian.depriester@ensam.eu',
+    description='Collection of tools to work on strain, stress and stiffness tensors, with plotting features',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+            'scipy',
+            'numpy',
+        ],
+)
