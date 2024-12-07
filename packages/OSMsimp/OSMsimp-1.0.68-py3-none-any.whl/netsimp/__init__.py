1fad884b9d854ec4f1f426bcd61ad2e987645780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:33:23 +0000

    Add package OSMsimp/1.0.68/OSMsimp-1.0.68-py3-none-any.whl

---
+++ packages/OSMsimp/OSMsimp-1.0.68-py3-none-any.whl/netsimp/__init__.py
@@ -0,0 +1,4 @@
+from .extract_OSM import extract_from_PBF
+from .simplification_A import simplification_A
+from .simplification_B import simplification_B
+from .merging_networks import merge_networks
