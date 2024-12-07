6b7ee1e2ee537f494f6c02e631e9605470c8969e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:33 +0000

    Add package CleverPIP/0.1.3/cleverpip-0.1.3.tar.gz

---
+++ packages/CleverPIP/cleverpip-0.1.3.tar.gz/cleverpip-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CleverPIP',  # Your package name
+    version='0.1.3',  # Initial version
+    packages=find_packages(),  # Automatically find packages
+    install_requires=['packaging'],  # Add dependencies here
+    entry_points={
+        'console_scripts': [
+            'cleverpip=src.main:cli',  # 'my_tool' is the CLI command
+        ],
+    },
+    download_url='https://github.com/L1Lbg/CleverPIP/archive/refs/tags/0.1.0.tar.gz',
+    author='Luca',
+    author_email='luca.baeyens@icloud.com',
+    description='A simple PIP tool to automatically upgrade packages to your liking.',
+    url='https://github.com/L1Lbg/CleverPIP',  # Your project URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
