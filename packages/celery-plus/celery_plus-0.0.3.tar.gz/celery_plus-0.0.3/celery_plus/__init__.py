ac876053fe359d4b2d430cceb947dc35e9ccbeb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:33 +0000

    Add package celery-plus/0.0.3/celery_plus-0.0.3.tar.gz

---
+++ packages/celery-plus/celery_plus-0.0.3.tar.gz/celery_plus-0.0.3/celery_plus/__init__.py
@@ -0,0 +1,20 @@
+from inspect import getmembers
+from inspect import isclass
+from types import ModuleType
+from typing import TYPE_CHECKING
+
+from .ischeduler import IScheduler
+from .utils.time_util import wait_time
+
+
+if TYPE_CHECKING:
+    from celery import Celery
+
+
+def setup_periodic_task(sender: "Celery", scheduler_module: ModuleType):
+    classes = getmembers(scheduler_module, isclass)
+    for class_name, class_ in classes:
+        class_().scheduler_register(sender)
+
+
+__all__ = ("IScheduler", "setup_periodic_task", "wait_time")
