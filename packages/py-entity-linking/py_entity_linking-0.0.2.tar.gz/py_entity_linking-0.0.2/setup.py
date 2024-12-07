ea311385060658fb28cbbee22afd03441a2310a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:34:04 +0000

    Add package py-entity-linking/0.0.2/py_entity_linking-0.0.2.tar.gz

---
+++ packages/py-entity-linking/py_entity_linking-0.0.2.tar.gz/py_entity_linking-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='py-entity-linking',
+    version='0.0.2',
+    packages=find_packages(),
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "numpy==1.26.4",
+        "SPARQLWrapper==2.0.0",
+        "sentence_transformers==3.1.1",
+        "aiohttp==3.9.5",
+        "openai==1.54.2",
+        "beautifulsoup4==4.12.2",
+        "fake-useragent==1.5.1"
+    ],
+    author='Nikolas',  
+    description='A library for lightweight entity linking',
+    license_files = "LICENSE.txt",
+    python_requires='>=3.10',
+
+)
