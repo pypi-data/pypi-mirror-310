9e1792a1c77152c278dedeff4d70009049efd6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 03:36:26 +0000

    Add package synapticonn/0.0.1rc1/synapticonn-0.0.1rc1-py2.py3-none-any.whl

---
+++ packages/synapticonn/synapticonn-0.0.1rc1-py2.py3-none-any.whl/synapticonn/plots/style.py
@@ -0,0 +1,28 @@
+""" Style and aesthetics definitions for plots. """
+
+import pathlib
+
+from itertools import cycle
+from functools import wraps
+
+from matplotlib.pyplot import style
+import matplotlib.pyplot as plt
+
+
+###################################################################
+###################################################################
+
+
+def apply_plot_style(style_path=None):
+    """Decorator to apply matplotlib style before a plotting function."""
+
+    if style_path is None:
+        style_path = pathlib.Path('synapticonn', 'plots', 'plots.mplstyle')
+
+    def decorator_plot_style(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            plt.style.use(style_path)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator_plot_style
