33eb04e00dae2b7f9839013bb90ee70545f40732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:33 +0000

    Add package flow360/24.11.0b5/flow360-24.11.0b5.tar.gz

---
+++ packages/flow360/flow360-24.11.0b5.tar.gz/flow360-24.11.0b5/flow360/examples/__init__.py
@@ -0,0 +1,31 @@
+from .actuator_disk import ActuatorDisk
+from .airplane import Airplane
+from .bet_disk import BETDisk
+from .bet_line import BETLine
+from .convergence import Convergence
+from .cylinder2D import Cylinder2D
+from .cylinder3D import Cylinder3D
+from .monitors import MonitorsAndSlices
+from .om6wing import OM6wing
+from .om6wing_user_defined_dynamics import OM6wingUserDefinedDynamics
+from .rotating_spheres import RotatingSpheres
+from .tutorial_2dcrm import Tutorial_2dcrm
+from .tutorial_cht_solver import TutorialCHTSolver
+from .tutorial_periodic_BC import TutorialPeriodicBC
+
+__all__ = [
+    "ActuatorDisk",
+    "Airplane",
+    "BETDisk",
+    "BETLine",
+    "Convergence",
+    "Cylinder2D",
+    "Cylinder3D",
+    "MonitorsAndSlices",
+    "OM6wing",
+    "OM6wingUserDefinedDynamics",
+    "RotatingSpheres",
+    "Tutorial_2dcrm",
+    "TutorialCHTSolver",
+    "TutorialPeriodicBC",
+]
