64de2e606b9e8fbea84853cacf6d5c931c3c1329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:47 +0000

    Add package neos-common/0.14.11/neos_common-0.14.11.tar.gz

---
+++ packages/neos-common/neos_common-0.14.11.tar.gz/neos_common-0.14.11/neos_common/authorization/validator.py
@@ -0,0 +1,68 @@
+import logging
+import uuid
+
+import aws4
+import fastapi
+
+from neos_common.authorization import base
+from neos_common.base import Action, ResourceLike
+from neos_common.client.hub_client import HubClient
+
+logger = logging.getLogger(__name__)
+
+
+class AccessValidator(base.AccessValidator):
+    def __init__(self, hub_client: HubClient) -> None:
+        self._hub_client = hub_client
+
+    async def validate(
+        self,
+        user_id: uuid.UUID,
+        actions: list[Action],
+        resources: list[ResourceLike],
+        logic_operator: str,
+        *,
+        return_allowed_resources: bool = False,
+    ) -> tuple[uuid.UUID, list[str]]:
+        return await self._hub_client.validate_token(
+            principal=user_id,
+            actions=actions,
+            resources=[resource.urn for resource in resources],
+            logic_operator=logic_operator,
+            return_allowed_resources=return_allowed_resources,
+        )
+
+
+class SignatureValidator(base.SignatureValidator):
+    def __init__(self, hub_client: HubClient) -> None:
+        self._hub_client = hub_client
+
+    async def validate(
+        self,
+        request: fastapi.Request,
+        actions: list[Action],
+        resources: list[ResourceLike],
+        logic_operator: str,
+        *,
+        return_allowed_resources: bool = False,
+    ) -> tuple[uuid.UUID, list[str]]:
+        payload = await request.body()
+
+        challenge = aws4.generate_challenge(
+            request.method,
+            request.url,
+            request.headers,
+            payload.decode("utf8"),
+        )
+
+        return await self._hub_client.validate_signature(
+            challenge.access_key_id,
+            "AWS4",
+            challenge.scope,
+            challenge.string_to_sign,
+            challenge.signature,
+            actions,
+            [resource.urn for resource in resources],
+            logic_operator=logic_operator,
+            return_allowed_resources=return_allowed_resources,
+        )
