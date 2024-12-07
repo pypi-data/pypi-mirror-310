16b858eeec8dc2fab64747c3ee228acee3ff461f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 05:06:02 +0000

    Add package polars/1.15.0/polars-1.15.0.tar.gz

---
+++ packages/polars/polars-1.15.0.tar.gz/polars-1.15.0/crates/polars-row/src/decode.rs
@@ -0,0 +1,289 @@
+use arrow::bitmap::{Bitmap, MutableBitmap};
+use arrow::buffer::Buffer;
+use arrow::datatypes::ArrowDataType;
+use arrow::offset::OffsetsBuffer;
+
+use self::encode::fixed_size;
+use self::fixed::get_null_sentinel;
+use self::variable::decode_strview;
+use super::*;
+use crate::fixed::{decode_bool, decode_primitive};
+use crate::variable::decode_binview;
+
+/// Decode `rows` into a arrow format
+/// # Safety
+/// This will not do any bound checks. Caller must ensure the `rows` are valid
+/// encodings.
+pub unsafe fn decode_rows_from_binary<'a>(
+    arr: &'a BinaryArray<i64>,
+    fields: &[EncodingField],
+    dtypes: &[ArrowDataType],
+    rows: &mut Vec<&'a [u8]>,
+) -> Vec<ArrayRef> {
+    assert_eq!(arr.null_count(), 0);
+    rows.clear();
+    rows.extend(arr.values_iter());
+    decode_rows(rows, fields, dtypes)
+}
+
+/// Decode `rows` into a arrow format
+/// # Safety
+/// This will not do any bound checks. Caller must ensure the `rows` are valid
+/// encodings.
+pub unsafe fn decode_rows(
+    // the rows will be updated while the data is decoded
+    rows: &mut [&[u8]],
+    fields: &[EncodingField],
+    dtypes: &[ArrowDataType],
+) -> Vec<ArrayRef> {
+    assert_eq!(fields.len(), dtypes.len());
+    dtypes
+        .iter()
+        .zip(fields)
+        .map(|(dtype, field)| decode(rows, field, dtype))
+        .collect()
+}
+
+unsafe fn decode_validity(rows: &mut [&[u8]], field: &EncodingField) -> Option<Bitmap> {
+    // 2 loop system to avoid the overhead of allocating the bitmap if all the elements are valid.
+
+    let null_sentinel = get_null_sentinel(field);
+    let first_null = (0..rows.len()).find(|&i| {
+        let v;
+        (v, rows[i]) = rows[i].split_at_unchecked(1);
+        v[0] == null_sentinel
+    });
+
+    // No nulls just return None
+    let first_null = first_null?;
+
+    let mut bm = MutableBitmap::new();
+    bm.reserve(rows.len());
+    bm.extend_constant(first_null, true);
+    bm.push(false);
+    bm.extend_from_trusted_len_iter(rows[first_null + 1..].iter_mut().map(|row| {
+        let v;
+        (v, *row) = row.split_at_unchecked(1);
+        v[0] != null_sentinel
+    }));
+    Some(bm.freeze())
+}
+
+// We inline this in an attempt to avoid the dispatch cost.
+#[inline(always)]
+fn dtype_and_data_to_encoded_item_len(
+    dtype: &ArrowDataType,
+    data: &[u8],
+    field: &EncodingField,
+) -> usize {
+    // Fast path: if the size is fixed, we can just divide.
+    if let Some(size) = fixed_size(dtype) {
+        return size;
+    }
+
+    let (non_empty_sentinel, continuation_token) = if field.descending {
+        (
+            !variable::NON_EMPTY_SENTINEL,
+            !variable::BLOCK_CONTINUATION_TOKEN,
+        )
+    } else {
+        (
+            variable::NON_EMPTY_SENTINEL,
+            variable::BLOCK_CONTINUATION_TOKEN,
+        )
+    };
+
+    use ArrowDataType as D;
+    match dtype {
+        D::Binary | D::LargeBinary | D::BinaryView => unsafe {
+            crate::variable::encoded_item_len(data, non_empty_sentinel, continuation_token)
+        },
+        D::Utf8 | D::LargeUtf8 | D::Utf8View => {
+            if field.no_order {
+                unsafe {
+                    crate::variable::encoded_item_len(data, non_empty_sentinel, continuation_token)
+                }
+            } else {
+                let null_sentinel = get_null_sentinel(field);
+                unsafe { crate::variable::encoded_str_len(data, null_sentinel, field.descending) }
+            }
+        },
+
+        D::List(list_field) | D::LargeList(list_field) => {
+            let mut data = data;
+            let mut item_len = 0;
+
+            let list_continuation_token = field.list_continuation_token();
+
+            while data[0] == list_continuation_token {
+                data = &data[1..];
+                let len = dtype_and_data_to_encoded_item_len(list_field.dtype(), data, field);
+                data = &data[len..];
+                item_len += 1 + len;
+            }
+            1 + item_len
+        },
+
+        D::FixedSizeBinary(_) => todo!(),
+        D::FixedSizeList(fsl_field, width) => {
+            let mut data = &data[1..];
+            let mut item_len = 1; // validity byte
+
+            for _ in 0..*width {
+                let len = dtype_and_data_to_encoded_item_len(fsl_field.dtype(), data, field);
+                data = &data[len..];
+                item_len += len;
+            }
+            item_len
+        },
+        D::Struct(struct_fields) => {
+            let mut data = &data[1..];
+            let mut item_len = 1; // validity byte
+
+            for struct_field in struct_fields {
+                let len = dtype_and_data_to_encoded_item_len(struct_field.dtype(), data, field);
+                data = &data[len..];
+                item_len += len;
+            }
+            item_len
+        },
+
+        D::Union(_, _, _) => todo!(),
+        D::Map(_, _) => todo!(),
+        D::Dictionary(_, _, _) => todo!(),
+        D::Decimal(_, _) => todo!(),
+        D::Decimal256(_, _) => todo!(),
+        D::Extension(_, _, _) => todo!(),
+        D::Unknown => todo!(),
+
+        _ => unreachable!(),
+    }
+}
+
+fn rows_for_fixed_size_list<'a>(
+    dtype: &ArrowDataType,
+    field: &EncodingField,
+    width: usize,
+    rows: &mut [&'a [u8]],
+    nested_rows: &mut Vec<&'a [u8]>,
+) {
+    nested_rows.clear();
+    nested_rows.reserve(rows.len() * width);
+
+    // Fast path: if the size is fixed, we can just divide.
+    if let Some(size) = fixed_size(dtype) {
+        for row in rows.iter_mut() {
+            for i in 0..width {
+                nested_rows.push(&row[(i * size)..][..size]);
+            }
+            *row = &row[size * width..];
+        }
+        return;
+    }
+
+    // @TODO: This is quite slow since we need to dispatch for possibly every nested type
+    for row in rows.iter_mut() {
+        for _ in 0..width {
+            let length = dtype_and_data_to_encoded_item_len(dtype, row, field);
+            let v;
+            (v, *row) = row.split_at(length);
+            nested_rows.push(v);
+        }
+    }
+}
+
+unsafe fn decode(rows: &mut [&[u8]], field: &EncodingField, dtype: &ArrowDataType) -> ArrayRef {
+    match dtype {
+        ArrowDataType::Null => NullArray::new(ArrowDataType::Null, rows.len()).to_boxed(),
+        ArrowDataType::Boolean => decode_bool(rows, field).to_boxed(),
+        ArrowDataType::Binary | ArrowDataType::LargeBinary | ArrowDataType::BinaryView => {
+            decode_binview(rows, field).to_boxed()
+        },
+        ArrowDataType::Utf8 | ArrowDataType::LargeUtf8 | ArrowDataType::Utf8View => {
+            let arr = if field.no_order {
+                unsafe { decode_binview(rows, field).to_utf8view_unchecked() }
+            } else {
+                decode_strview(rows, field)
+            };
+            arr.boxed()
+        },
+        ArrowDataType::Struct(fields) => {
+            let validity = decode_validity(rows, field);
+            let values = fields
+                .iter()
+                .map(|struct_fld| decode(rows, field, struct_fld.dtype()))
+                .collect();
+            StructArray::new(dtype.clone(), rows.len(), values, validity).to_boxed()
+        },
+        ArrowDataType::FixedSizeList(fsl_field, width) => {
+            let validity = decode_validity(rows, field);
+
+            // @TODO: we could consider making this into a scratchpad
+            let mut nested_rows = Vec::new();
+            rows_for_fixed_size_list(fsl_field.dtype(), field, *width, rows, &mut nested_rows);
+            let values = decode(&mut nested_rows, field, fsl_field.dtype());
+
+            FixedSizeListArray::new(dtype.clone(), rows.len(), values, validity).to_boxed()
+        },
+        ArrowDataType::List(list_field) | ArrowDataType::LargeList(list_field) => {
+            let mut validity = MutableBitmap::new();
+
+            // @TODO: we could consider making this into a scratchpad
+            let num_rows = rows.len();
+            let mut nested_rows = Vec::new();
+            let mut offsets = Vec::with_capacity(rows.len() + 1);
+            offsets.push(0);
+
+            let list_null_sentinel = field.list_null_sentinel();
+            let list_continuation_token = field.list_continuation_token();
+            let list_termination_token = field.list_termination_token();
+
+            // @TODO: make a specialized loop for fixed size list_field.dtype()
+            for (i, row) in rows.iter_mut().enumerate() {
+                while row[0] == list_continuation_token {
+                    *row = &row[1..];
+                    let len = dtype_and_data_to_encoded_item_len(list_field.dtype(), row, field);
+                    nested_rows.push(&row[..len]);
+                    *row = &row[len..];
+                }
+
+                offsets.push(nested_rows.len() as i64);
+
+                // @TODO: Might be better to make this a 2-loop system.
+                if row[0] == list_null_sentinel {
+                    *row = &row[1..];
+                    validity.reserve(num_rows);
+                    validity.extend_constant(i - validity.len(), true);
+                    validity.push(false);
+                    continue;
+                }
+
+                assert_eq!(row[0], list_termination_token);
+                *row = &row[1..];
+            }
+
+            let validity = if validity.is_empty() {
+                None
+            } else {
+                validity.extend_constant(num_rows - validity.len(), true);
+                Some(validity.freeze())
+            };
+            assert_eq!(offsets.len(), rows.len() + 1);
+
+            let values = decode(&mut nested_rows, field, list_field.dtype());
+
+            ListArray::<i64>::new(
+                dtype.clone(),
+                unsafe { OffsetsBuffer::new_unchecked(Buffer::from(offsets)) },
+                values,
+                validity,
+            )
+            .to_boxed()
+        },
+        dt => {
+            with_match_arrow_primitive_type!(dt, |$T| {
+                decode_primitive::<$T>(rows, field).to_boxed()
+            })
+        },
+    }
+}
---
+++ packages/polars/polars-1.15.0.tar.gz/polars-1.15.0/crates/polars-row/src/row.rs
@@ -0,0 +1,189 @@
+use arrow::array::{BinaryArray, BinaryViewArray};
+use arrow::datatypes::ArrowDataType;
+use arrow::ffi::mmap;
+use arrow::offset::{Offsets, OffsetsBuffer};
+use polars_compute::cast::binary_to_binview;
+
+const BOOLEAN_TRUE_SENTINEL: u8 = 0x03;
+const BOOLEAN_FALSE_SENTINEL: u8 = 0x02;
+
+#[derive(Clone, Default, Copy)]
+pub struct EncodingField {
+    /// Whether to sort in descending order
+    pub descending: bool,
+    /// Whether to sort nulls first
+    pub nulls_last: bool,
+    /// Ignore all order-related flags and don't encode order-preserving.
+    /// This is faster for variable encoding as we can just memcopy all the bytes.
+    pub no_order: bool,
+}
+
+const LIST_CONTINUATION_TOKEN: u8 = 0xFE;
+const EMPTY_STR_TOKEN: u8 = 0x01;
+const NON_EMPTY_STR_TOKEN: u8 = 0x02;
+
+impl EncodingField {
+    pub fn new_sorted(descending: bool, nulls_last: bool) -> Self {
+        EncodingField {
+            descending,
+            nulls_last,
+            no_order: false,
+        }
+    }
+
+    pub fn new_unsorted() -> Self {
+        EncodingField {
+            no_order: true,
+            ..Default::default()
+        }
+    }
+
+    pub(crate) fn bool_true_sentinel(self) -> u8 {
+        if self.descending {
+            !BOOLEAN_TRUE_SENTINEL
+        } else {
+            BOOLEAN_TRUE_SENTINEL
+        }
+    }
+
+    pub(crate) fn bool_false_sentinel(self) -> u8 {
+        if self.descending {
+            !BOOLEAN_FALSE_SENTINEL
+        } else {
+            BOOLEAN_FALSE_SENTINEL
+        }
+    }
+
+    pub fn list_null_sentinel(self) -> u8 {
+        crate::fixed::get_null_sentinel(&self)
+    }
+
+    pub fn list_continuation_token(self) -> u8 {
+        if self.descending {
+            !LIST_CONTINUATION_TOKEN
+        } else {
+            LIST_CONTINUATION_TOKEN
+        }
+    }
+
+    pub fn list_termination_token(self) -> u8 {
+        !self.list_continuation_token()
+    }
+
+    pub fn empty_str_token(self) -> u8 {
+        if self.descending {
+            !EMPTY_STR_TOKEN
+        } else {
+            EMPTY_STR_TOKEN
+        }
+    }
+    pub fn non_empty_str_token(self) -> u8 {
+        if self.descending {
+            !NON_EMPTY_STR_TOKEN
+        } else {
+            NON_EMPTY_STR_TOKEN
+        }
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct RowsEncoded {
+    pub(crate) values: Vec<u8>,
+    pub(crate) offsets: Vec<usize>,
+}
+
+fn checks(offsets: &[usize]) {
+    assert_eq!(
+        size_of::<usize>(),
+        size_of::<i64>(),
+        "only supported on 64bit arch"
+    );
+    assert!(
+        (*offsets.last().unwrap() as u64) < i64::MAX as u64,
+        "overflow"
+    );
+}
+
+unsafe fn rows_to_array(buf: Vec<u8>, offsets: Vec<usize>) -> BinaryArray<i64> {
+    checks(&offsets);
+
+    // SAFETY: we checked overflow
+    let offsets = bytemuck::cast_vec::<usize, i64>(offsets);
+
+    // SAFETY: monotonically increasing
+    let offsets = Offsets::new_unchecked(offsets);
+
+    BinaryArray::new(ArrowDataType::LargeBinary, offsets.into(), buf.into(), None)
+}
+
+impl RowsEncoded {
+    pub(crate) fn new(values: Vec<u8>, offsets: Vec<usize>) -> Self {
+        RowsEncoded { values, offsets }
+    }
+
+    pub fn iter(&self) -> RowsEncodedIter {
+        let iter = self.offsets[1..].iter();
+        let offset = self.offsets[0];
+        RowsEncodedIter {
+            offset,
+            end: iter,
+            values: &self.values,
+        }
+    }
+
+    /// Borrows the buffers and returns a [`BinaryArray`].
+    ///
+    /// # Safety
+    /// The lifetime of that `BinaryArray` is tied to the lifetime of
+    /// `Self`. The caller must ensure that both stay alive for the same time.
+    pub unsafe fn borrow_array(&self) -> BinaryArray<i64> {
+        checks(&self.offsets);
+
+        unsafe {
+            let (_, values, _) = mmap::slice(&self.values).into_inner();
+            let offsets = bytemuck::cast_slice::<usize, i64>(self.offsets.as_slice());
+            let (_, offsets, _) = mmap::slice(offsets).into_inner();
+            let offsets = OffsetsBuffer::new_unchecked(offsets);
+
+            BinaryArray::new(ArrowDataType::LargeBinary, offsets, values, None)
+        }
+    }
+
+    /// This conversion is free.
+    pub fn into_array(self) -> BinaryArray<i64> {
+        unsafe { rows_to_array(self.values, self.offsets) }
+    }
+
+    /// This does allocate views.
+    pub fn into_binview(self) -> BinaryViewArray {
+        binary_to_binview(&self.into_array())
+    }
+
+    #[cfg(test)]
+    pub fn get(&self, i: usize) -> &[u8] {
+        let start = self.offsets[i];
+        let end = self.offsets[i + 1];
+        &self.values[start..end]
+    }
+}
+
+pub struct RowsEncodedIter<'a> {
+    offset: usize,
+    end: std::slice::Iter<'a, usize>,
+    values: &'a [u8],
+}
+
+impl<'a> Iterator for RowsEncodedIter<'a> {
+    type Item = &'a [u8];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let new_offset = *self.end.next()?;
+        let payload = unsafe { self.values.get_unchecked(self.offset..new_offset) };
+        self.offset = new_offset;
+        Some(payload)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.end.size_hint()
+    }
+}
