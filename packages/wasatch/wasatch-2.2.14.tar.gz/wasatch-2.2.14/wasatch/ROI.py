124779e6d8577a3e47d5f8b881317345047ab7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 06:05:12 +0000

    Add package wasatch/2.2.14/wasatch-2.2.14.tar.gz

---
+++ packages/wasatch/wasatch-2.2.14.tar.gz/wasatch-2.2.14/wasatch/ROI.py
@@ -0,0 +1,25 @@
+class ROI:
+    """
+    This class encapsulates a Region Of Interest, which may be either horizontal 
+    (pixels) or vertical (rows/lines).
+
+    Note that self.end is the LAST valid index, not LAST+1; (start, end) is an 
+    open interval, not half-open.
+    """
+
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.len = end - start + 1
+
+    def valid(self):
+        return self.start >= 0 and self.start < self.end
+
+    def crop(self, spectrum):
+        return spectrum[self.start:self.end+1]
+
+    def contains(self, value):
+        return self.start <= value <= self.end
+
+    def __str__(self):
+        return f"({self.start}, {self.end}) inclusive"
