5f62b6d783afc1b44a7e809556a5fe33aae05840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:45 +0000

    Add package UTSGLDM1/0.0.0/utsgldm1-0.0.0.tar.gz

---
+++ packages/UTSGLDM1/utsgldm1-0.0.0.tar.gz/utsgldm1-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="UTSGLDM1",
+    Version="0.1",
+    author="Mostafa Abotaleb",
+    description="Library in Python for Modelling univariate time series using GLDM (Generalized Least Deviation Method). UTSGLDM1 is (Univariate Time Series Using Generalized Least Deviation Method First order) ",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+    
+)
