23c726d76fc9f4fa3dc1a017aa0adacef1bf1057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:11 +0000

    Add package invenio-override/0.0.4/invenio-override-0.0.4.tar.gz

---
+++ packages/invenio-override/invenio-override-0.0.4.tar.gz/invenio-override-0.0.4/invenio_override/assets/semantic-ui/js/invenio_override/theme.js
@@ -0,0 +1,59 @@
+import "semantic-ui-css";
+
+import $ from "jquery";
+import { MultipleOptionsSearchBar } from "@js/invenio_search_ui/components";
+import React from "react";
+import ReactDOM from "react-dom";
+import { i18next } from "@translations/invenio_app_rdm/i18next";
+
+/* sticky notification setup for test instance */
+$(".ui.sticky.test-instance").sticky({
+  context: "body",
+});
+
+/* load Zammad script on document ready
+$(function () {
+  importZammadScript();
+});
+*/
+
+/* function to import Zammad script for feedback form
+function importZammadScript() {
+  let scriptNode = document.createElement("hidden");
+  scriptNode.id = "zammad_form_script";
+  scriptNode.src = "URL";
+  document.head.appendChild(scriptNode);
+
+  $.getScript("URL", () => {
+    $("#feedback-form").ZammadForm({
+      messageTitle: "Contact us",
+      showTitle: true,
+      messageSubmit: "Submit",
+      messageThankYou: "Thank you for your message, (#%s). We will get back to you as quickly as possible!",
+      modal: true,
+    });
+  });
+}
+*/
+
+/* function to toggle visibility of an element by ID
+export function toggleVisibility(id) {
+  const element = document.getElementById(id);
+  const isHidden = element.style.display === "none";
+  element.style.display = isHidden ? "block" : "none";
+}
+window.toggleVisibility = toggleVisibility;
+*/
+
+/* render search bar with multiple options
+const headerSearchbar = document.getElementById("header-search-bar");
+const searchBarOptions = JSON.parse(headerSearchbar.dataset.options);
+
+ReactDOM.render(
+  <MultipleOptionsSearchBar
+    options={searchBarOptions}
+    placeholder={i18next.t("Search records...")}
+  />,
+  headerSearchbar
+);
+*/
