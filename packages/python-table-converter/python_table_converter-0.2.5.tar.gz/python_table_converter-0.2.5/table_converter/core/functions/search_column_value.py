a5d18ba4a47fc8bc913e722a03bf7bfb45b2659d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 04:09:40 +0000

    Add package python-table-converter/0.2.5/python_table_converter-0.2.5.tar.gz

---
+++ packages/python-table-converter/python_table_converter-0.2.5.tar.gz/python_table_converter-0.2.5/table_converter/core/functions/search_column_value.py
@@ -0,0 +1,28 @@
+'''
+This function is used to search for a column value in a row. It will first search in the '__debug__' field, then in the '__debug__.__original__' field, and finally in the row itself. If the value is found, it will be set in the row and returned.
+'''
+
+from collections import OrderedDict
+
+from . get_field_value import get_field_value
+from . set_field_value import set_field_value
+
+def search_column_value(
+    row: OrderedDict,
+    column: str,
+):
+    if '__debug__' in row:
+        value, found = get_field_value(row['__debug__'], column)
+        if found:
+            return value, True
+    value, found = get_field_value(row['__debug__'], column)
+    original, found = get_field_value(row, '__debug__.__original__')
+    if found:
+        value, found = get_field_value(original, column)
+        if found:
+            return value, True
+    value, found = get_field_value(row, column)
+    if found:
+        set_field_value(row, column, value)
+        return value, True
+    return None, False
