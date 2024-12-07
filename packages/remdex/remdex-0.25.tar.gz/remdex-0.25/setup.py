773436b523b342e94399078c1d7d06de0c2c51f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:30 +0000

    Add package remdex/0.25/remdex-0.25.tar.gz

---
+++ packages/remdex/remdex-0.25.tar.gz/remdex-0.25/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='remdex',
+    version='0.25',
+    description='Fast and lightweight federated learning framework',
+    packages=find_packages(), 
+    install_requires=[
+        'cloudpickle',
+        'numpy'
+    ],
+    license='MIT'
+)
+
