6dc1eff7e62ead6bf9247b1251b4f39d84520d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:46 +0000

    Add package zvamz/0.0.18/zvamz-0.0.18-py2.py3-none-any.whl

---
+++ packages/zvamz/zvamz-0.0.18-py2.py3-none-any.whl/zvamz/fcmap.py
@@ -0,0 +1,16 @@
+fc_to_country = {
+    'YEG2': 'Canada', 'YYC4': 'Canada', 'AVP1': 'USA', 'LGB8': 'USA', 'RDU4': 'USA', 'HGR6': 'USA',
+    'WBW2': 'USA', 'YXU1': 'Canada', 'ONT8': 'USA', 'YHM1': 'Canada', 'PHL7': 'USA', 'SMF3': 'USA',
+    'TEB9': 'USA', 'YYZ4': 'Canada', 'MQJ1': 'USA', 'YYC1': 'Canada', 'YYZ7': 'Canada', 'STL3': 'USA',
+    'ALB1': 'USA', 'YXX2': 'Canada', 'YVR2': 'Canada', 'YOW3': 'Canada', 'KRB1': 'USA', 'SWF2': 'USA',
+    'ORF2': 'USA', 'SCK4': 'USA', 'ABE8': 'USA', 'IAH3': 'USA', 'FWA4': 'USA', 'FTW1': 'USA', 'TMB8': 'USA',
+    'PBI3': 'USA', 'ONT2': 'USA', 'EWR4': 'USA', 'CHA1': 'USA', 'PHX6': 'USA', 'CLT2': 'USA', 'MDW2': 'USA',
+    'MKC4': 'USA', 'CMH1': 'USA', 'YYZ9': 'Canada', 'RIC2': 'USA', 'MKE1': 'USA', 'BFI4': 'USA', 'CAE1': 'USA',
+    'IND1': 'USA', 'YYZ1': 'Canada', 'SAT1': 'USA', 'ACY1': 'USA', 'BDL2': 'USA', 'STL8': 'USA', 'YVR4': 'Canada',
+    'MEM1': 'USA', 'YOW1': 'Canada', 'YEG1': 'Canada', 'YOO1': 'Canada', 'LAX9': 'USA', 'LAS1': 'USA',
+    'VGT2': 'USA', 'GYR2': 'USA', 'GYR3': 'USA', 'SBD1': 'USA', 'RDU2': 'USA', 'IND9': 'USA', 'DUS2': 'Germany',
+    'HAJ1': 'Germany', 'DTM2': 'Germany', 'BHX4': 'UK', 'LBA4': 'UK', 'BHX2': 'UK', 'LCY2': 'UK', 'EDI4': 'UK',
+    'LTN1': 'UK', 'HAM2': 'Germany', 'WRO5': 'Poland', 'XDS1': 'Germany', 'EUK5': 'Germany', 'MAN2': 'UK',
+    'MAN3': 'UK', 'BHX1': 'UK', 'LTN2': 'UK', 'BRS1': 'UK', 'EMA1': 'UK', 'LCJ4': 'Poland', 'LCJ3': 'Poland',
+    'RFD2':'USA', 'RMN3':'USA'
+}
---
+++ packages/zvamz/zvamz-0.0.18-py2.py3-none-any.whl/zvamz/marketplaces.py
@@ -0,0 +1,96 @@
+class marketplaces:
+    @staticmethod
+    def US():
+        url = "https://sellingpartnerapi-na.amazon.com"
+        marketplace_id = "ATVPDKIKX0DER"
+        return url, marketplace_id
+
+    @staticmethod
+    def CA():
+        url = "https://sellingpartnerapi-na.amazon.com"
+        marketplace_id = "A2EUQ1WTGCTBG2"
+        return url, marketplace_id
+
+    @staticmethod
+    def MX():
+        url = "https://sellingpartnerapi-na.amazon.com"
+        marketplace_id = "A1AM78C64UM0Y8"
+        return url, marketplace_id
+
+    @staticmethod
+    def BR():
+        url = "https://sellingpartnerapi-na.amazon.com"
+        marketplace_id = "A2Q3Y263D00KWC"
+        return url, marketplace_id
+
+    @staticmethod
+    def UK():
+        url = "https://sellingpartnerapi-eu.amazon.com"
+        marketplace_id = "A1F83G8C2ARO7P"
+        return url, marketplace_id
+
+    @staticmethod
+    def DE():
+        url = "https://sellingpartnerapi-eu.amazon.com"
+        marketplace_id = "A1PA6795UKMFR9"
+        return url, marketplace_id
+
+    @staticmethod
+    def FR():
+        url = "https://sellingpartnerapi-eu.amazon.com"
+        marketplace_id = "A13V1IB3VIYZZH"
+        return url, marketplace_id
+
+    @staticmethod
+    def ES():
+        url = "https://sellingpartnerapi-eu.amazon.com"
+        marketplace_id = "A1RKKUPIHCS9HS"
+        return url, marketplace_id
+
+    @staticmethod
+    def IT():
+        url = "https://sellingpartnerapi-eu.amazon.com"
+        marketplace_id = "APJ6JRA9NG5V4"
+        return url, marketplace_id
+
+    @staticmethod
+    def IN():
+        url = "https://sellingpartnerapi-eu.amazon.com"
+        marketplace_id = "A21TJRUUN4KGV"
+        return url, marketplace_id
+
+    @staticmethod
+    def JP():
+        url = "https://sellingpartnerapi-fe.amazon.com"
+        marketplace_id = "A1VC38T7YXB528"
+        return url, marketplace_id
+
+    @staticmethod
+    def AU():
+        url = "https://sellingpartnerapi-fe.amazon.com"
+        marketplace_id = "A39IBJ37TRP1C6"
+        return url, marketplace_id
+
+    @staticmethod
+    def AE():
+        url = "https://sellingpartnerapi-eu.amazon.com"
+        marketplace_id = "A2VIGQ35RCS4UG"
+        return url, marketplace_id
+
+    @staticmethod
+    def SG():
+        url = "https://sellingpartnerapi-fe.amazon.com"
+        marketplace_id = "A19VAU5U5O7RUS"
+        return url, marketplace_id
+
+    @staticmethod
+    def TR():
+        url = "https://sellingpartnerapi-eu.amazon.com"
+        marketplace_id = "A33AVAJ2PDY3EV"
+        return url, marketplace_id
+
+    @staticmethod
+    def SA():
+        url = "https://sellingpartnerapi-eu.amazon.com"
+        marketplace_id = "A17E79C6D8DWNP"
+        return url, marketplace_id
