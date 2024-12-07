95b79fee144e54a7802aaac8ec303a4a797eb5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:06:35 +0000

    Add package webknossos/0.15.9/webknossos-0.15.9-py3-none-any.whl

---
+++ packages/webknossos/webknossos-0.15.9-py3-none-any.whl/webknossos/dataset/defaults.py
@@ -0,0 +1,28 @@
+import os
+import ssl
+
+import certifi
+
+from ..geometry import Vec3Int
+from .data_format import DataFormat
+
+DEFAULT_WKW_FILE_LEN = 32
+DEFAULT_CHUNK_SHAPE = Vec3Int.full(32)
+DEFAULT_DATA_FORMAT = (
+    DataFormat(os.environ["WK_DEFAULT_DATA_FORMAT"])
+    if "WK_DEFAULT_DATA_FORMAT" in os.environ
+    else DataFormat.WKW
+)
+DEFAULT_CHUNKS_PER_SHARD = (
+    Vec3Int.from_str(os.environ["WK_DEFAULT_CHUNKS_PER_SHARD"])
+    if "WK_DEFAULT_CHUNKS_PER_SHARD" in os.environ
+    else Vec3Int.full(32)
+)
+DEFAULT_CHUNKS_PER_SHARD_ZARR = Vec3Int.full(1)
+DEFAULT_CHUNKS_PER_SHARD_FROM_IMAGES = Vec3Int(128, 128, 1)
+DEFAULT_BIT_DEPTH = 8
+PROPERTIES_FILE_NAME = "datasource-properties.json"
+ZGROUP_FILE_NAME = ".zgroup"
+ZATTRS_FILE_NAME = ".zattrs"
+ZARR_JSON_FILE_NAME = "zarr.json"
+SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())
