cd00f5f76c0b50d1dce38fdaa7027ffc30a1668d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:18 +0000

    Add package entity-linking-test/0.1/entity_linking_test-0.1-py3-none-any.whl

---
+++ packages/entity-linking-test/entity_linking_test-0.1-py3-none-any.whl/linking/1.py
@@ -0,0 +1,4 @@
+from main import navia
+
+api_token = "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"
+navia(api_token=api_token, sentence="We used PCA and FA for our experiments.", mention="PCA", single="No", combination="No", disambiguation="No", embedding_model="Lajavaness/bilingual-embedding-large")
