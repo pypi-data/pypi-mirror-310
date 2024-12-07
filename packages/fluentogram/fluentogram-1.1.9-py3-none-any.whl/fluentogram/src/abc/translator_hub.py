d97a576244aee5081c5892bc1fc3d5abb0e49162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 02:32:51 +0000

    Add package fluentogram/1.1.9/fluentogram-1.1.9-py3-none-any.whl

---
+++ packages/fluentogram/fluentogram-1.1.9-py3-none-any.whl/fluentogram/src/abc/translator_hub.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+"""
+An abstract base for the Translator Hub and Key/Value Translator Hub objects
+"""
+from abc import ABC, abstractmethod
+from typing import Self, Any
+
+from fluentogram.src.abc.runner import AbstractTranslatorRunner
+from fluentogram.src.abc.storage import AbstractStorage
+
+
+class AbstractTranslatorsHub(ABC):
+    """This class should contain a couple of translator objects, usually one object per one locale."""
+
+    @abstractmethod
+    def __init__(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_translator_by_locale(self, locale: str) -> AbstractTranslatorRunner:
+        """
+        Returns a Translator object by selected locale
+        """
+        raise NotImplementedError
+
+class AbstractKvTranslatorHub(AbstractTranslatorsHub, ABC):
+    @abstractmethod
+    def from_storage(self, kv_storage: AbstractStorage) -> Self:
+        """
+        Initializes the Translator Hub with the provided storage
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def put(self, locale: str, key: str, value: Any, mapping_values: dict[str, Any]):
+        raise NotImplementedError
+
+    @abstractmethod
+    def create(self, locale: str, key: str, value: Any, mapping_values: dict[str, Any]):
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete(self, locale: str, *keys):
+        raise NotImplementedError
+
