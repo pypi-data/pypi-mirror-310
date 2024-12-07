da85e5bf63c69653b40dc4f768ad69ea35c4bd57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 01:32:18 +0000

    Add package omero-vitessce/1.2.2/omero_vitessce-1.2.2-py3-none-any.whl

---
+++ packages/omero-vitessce/omero_vitessce-1.2.2-py3-none-any.whl/omero_vitessce/urls.py
@@ -0,0 +1,33 @@
+from django.urls import re_path
+
+from . import views
+
+urlpatterns = [
+    # Generate config Button
+    re_path(r"^generate_config/(?P<obj_type>[a-z]+)/(?P<obj_id>[0-9]+)",
+            views.generate_config, name='generate_config'),
+
+    # Button placeholder
+    re_path(r"^generate_config$", views.vitessce_index,
+            name="generate_config"),
+
+    # json response with ROIs
+    re_path(r"^vitessce_json_rois/(?P<img_ids>[0-9,]+)",
+            views.vitessce_json_rois, name="vitessce_rois"),
+
+    # json response with Vitessce Config
+    re_path(r"^config/(?P<url_config>.+)",
+            views.vitessce_json_config, name="vitessce_config"),
+
+    # Right panel plugin
+    re_path(r"(?P<obj_type>[a-z]+)/(?P<obj_id>[0-9]+)",
+            views.vitessce_panel, name='vitessce_tab'),
+
+    # Index placeholder for right panel plugin
+    re_path(r"^$", views.vitessce_index, name="vitessce_tab"),
+
+    # Open-with
+    re_path(r"^open/",
+            views.vitessce_open, name='open_vitessce')
+
+]
