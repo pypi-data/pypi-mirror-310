2cdcb3de0ba29a5fe487f604dbb3d670698f0153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-07 00:07:09 +0000

    Add package digitalhub-runtime-modelserve/0.8.1/digitalhub_runtime_modelserve-0.8.1-py3-none-any.whl

---
+++ packages/digitalhub-runtime-modelserve/digitalhub_runtime_modelserve-0.8.1-py3-none-any.whl/digitalhub_runtime_modelserve/entities/task/huggingfaceserve_serve/builder.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from digitalhub_runtime_modelserve.entities._base.runtime_entity.builder import RuntimeEntityBuilderHuggingfaceserve
+from digitalhub_runtime_modelserve.entities.task.huggingfaceserve_serve.entity import TaskHuggingfaceserveServe
+from digitalhub_runtime_modelserve.entities.task.huggingfaceserve_serve.spec import (
+    TaskSpecHuggingfaceserveServe,
+    TaskValidatorHuggingfaceserveServe,
+)
+from digitalhub_runtime_modelserve.entities.task.huggingfaceserve_serve.status import TaskStatusHuggingfaceserveServe
+
+from digitalhub.entities.task._base.builder import TaskBuilder
+
+
+class TaskHuggingfaceserveServeBuilder(TaskBuilder, RuntimeEntityBuilderHuggingfaceserve):
+    """
+    TaskHuggingfaceserveServe builder.
+    """
+
+    ENTITY_CLASS = TaskHuggingfaceserveServe
+    ENTITY_SPEC_CLASS = TaskSpecHuggingfaceserveServe
+    ENTITY_SPEC_VALIDATOR = TaskValidatorHuggingfaceserveServe
+    ENTITY_STATUS_CLASS = TaskStatusHuggingfaceserveServe
+    ENTITY_KIND = "huggingfaceserve+serve"
